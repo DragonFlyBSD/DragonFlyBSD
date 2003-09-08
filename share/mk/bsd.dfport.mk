96e28b0c1c0a6909aeb6fc36952ac02f4f27d3ad
Matthew Dillon <dillon@dragonflybsd.org>
2003-09-08 23:39:23 +0000

    Add bsd.dport.mk, which Makefile's in /usr/dports will use instead of bsd.port.mk.  Modify bsd.port.mk to check for the existance of the same port in /usr/dports and, if found, to cd into the dport and run the target there.
    NOTE: /usr/bin/relpath is required (see /usr/src/usr.bin/relpath).

---
+++ share/mk/bsd.dfport.mk
@@ -0,0 +1,7 @@
+# $DragonFly: src/share/mk/Attic/bsd.dfport.mk,v 1.1 2003/09/08 23:39:23 dillon Exp $
+
+PORTSDIR?=	/usr/dports
+
+.include <bsd.own.mk>
+.include "${PORTSDIR}/Mk/bsd.port.mk"
+
--- share/mk/bsd.port.mk
+++ share/mk/bsd.port.mk
@@ -1,7 +1,55 @@
 # $FreeBSD: src/share/mk/bsd.port.mk,v 1.303.2.2 2002/07/17 19:08:23 ru Exp $
-# $DragonFly: src/share/mk/Attic/bsd.port.mk,v 1.2 2003/06/17 04:37:02 dillon Exp $
+# $DragonFly: src/share/mk/Attic/bsd.port.mk,v 1.3 2003/09/08 23:39:23 dillon Exp $
 
 PORTSDIR?=	/usr/ports
+DPORTSDIR?=	/usr/dports
+PORTPATH!=	/usr/bin/relpath ${PORTSDIR} ${.CURDIR}
 
+.if !exists(${DPORTSDIR}/${PORTPATH}/Makefile)
+# If the port does not exist in /usr/dports/<portpath> use the original
+# FreeBSD port
+#
 .include <bsd.own.mk>
 .include "${PORTSDIR}/Mk/bsd.port.mk"
+
+.else
+
+# Otherwise retarget to the DragonFly override port.
+#
+
+TARGETS+=	all
+TARGETS+=	build
+TARGETS+=	checksum
+TARGETS+=	clean
+TARGETS+=	clean-for-cdrom
+TARGETS+=	clean-for-cdrom-list
+TARGETS+=	clean-restricted
+TARGETS+=	clean-restricted-list
+TARGETS+=	configure
+TARGETS+=	deinstall
+TARGETS+=	depend
+TARGETS+=	depends
+TARGETS+=	describe
+TARGETS+=	distclean
+TARGETS+=	extract
+TARGETS+=	fetch
+TARGETS+=	fetch-list
+TARGETS+=	ignorelist
+TARGETS+=	makesum
+TARGETS+=	maintainer
+TARGETS+=	package
+TARGETS+=	realinstall
+TARGETS+=	reinstall
+TARGETS+=	install
+TARGETS+=	tags
+
+.for __target in ${TARGETS}
+.if !target(${__target})
+${__target}:
+	@echo "WARNING, USING DRAGONFLY OVERRIDE ${DPORTSDIR}/${PORTPATH}"
+	cd ${DPORTSDIR}/${PORTPATH} && ${MAKE} -B ${.TARGET}
+.endif
+.endfor
+
+.endif
+
