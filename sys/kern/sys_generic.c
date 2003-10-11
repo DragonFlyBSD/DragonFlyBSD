8b5e4c8fd52a98db7148208bfc5e5d2cb7d33b2d
David Rhodus <drhodus@dragonflybsd.org>
2003-10-11 19:04:58 +0000

    *	Move variable 'p' into a more proper place.

--- sys/kern/sys_generic.c
+++ sys/kern/sys_generic.c
@@ -37,7 +37,7 @@
  *
  *	@(#)sys_generic.c	8.5 (Berkeley) 1/21/94
  * $FreeBSD: src/sys/kern/sys_generic.c,v 1.55.2.10 2001/03/17 10:39:32 peter Exp $
- * $DragonFly: src/sys/kern/sys_generic.c,v 1.12 2003/10/02 16:57:14 drhodus Exp $
+ * $DragonFly: src/sys/kern/sys_generic.c,v 1.13 2003/10/11 19:04:58 drhodus Exp $
  */
 
 #include "opt_ktrace.h"
@@ -154,12 +154,12 @@ dofileread(fp, fd, buf, nbyte, offset, flags, res)
 	int *res;
 {
 	struct thread *td = curthread;
-	struct proc *p = td->td_proc;
 	struct uio auio;
 	struct iovec aiov;
 	long cnt, error = 0;
 #ifdef KTRACE
 	struct iovec ktriov;
+	struct proc *p = td->td_proc; 
 	struct uio ktruio;
 	int didktr = 0;
 #endif
