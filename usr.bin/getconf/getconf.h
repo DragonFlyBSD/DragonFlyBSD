bb850a69d8e3621918ea16676513636834d413cc
Matthew Dillon <dillon@dragonflybsd.org>
2003-11-03 17:15:31 +0000

    Do a minor update of getconf to deal with the addition of intmax_t and friends in the 64 bit address cleanup commit.

--- usr.bin/getconf/getconf.h
+++ usr.bin/getconf/getconf.h
@@ -27,14 +27,12 @@
  * SUCH DAMAGE.
  *
  * $FreeBSD: src/usr.bin/getconf/getconf.h,v 1.4.2.1 2002/10/27 04:18:40 wollman Exp $
- * $DragonFly: src/usr.bin/getconf/getconf.h,v 1.2 2003/06/17 04:29:27 dillon Exp $
+ * $DragonFly: src/usr.bin/getconf/getconf.h,v 1.3 2003/11/03 17:15:31 dillon Exp $
  */
 
-#ifdef STABLE
-typedef long long intmax_t;
-#define	PRIdMAX	"lld"
-#else
 #include <inttypes.h>
+#ifndef PRIdMAX
+#define	PRIdMAX	"lld"	/* XXX remove after intttypes.h abstraction from 5.x is ported */
 #endif
 
 int	find_confstr(const char *name, int *key);
