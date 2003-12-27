c720ddacf86d0669e3177c75e64a5ff1a6ad1424
Matthew Dillon <dillon@dragonflybsd.org>
2003-12-27 23:58:40 +0000

    The attribution for the last commit was incorrect, it should have been:
    Requested-by: Peter Kadau <peter.kadau@tuebingen.mpg.de>

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -37,7 +37,7 @@
  *
  *	@(#)proc.h	8.15 (Berkeley) 5/19/95
  * $FreeBSD: src/sys/sys/proc.h,v 1.99.2.9 2003/06/06 20:21:32 tegge Exp $
- * $DragonFly: src/sys/sys/proc.h,v 1.39 2003/12/27 19:21:05 dillon Exp $
+ * $DragonFly: src/sys/sys/proc.h,v 1.40 2003/12/27 23:58:40 dillon Exp $
  */
 
 #ifndef _SYS_PROC_H_
