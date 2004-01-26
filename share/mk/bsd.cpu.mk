ff9357042955fa71f0ad44bc26b8fc20c6990a1a
Joerg Sonnenberger <joerg@dragonflybsd.org>
2004-01-26 15:42:36 +0000

    Fix make complaining about CCVER being not defined

--- share/mk/bsd.cpu.mk
+++ share/mk/bsd.cpu.mk
@@ -1,11 +1,11 @@
 # $FreeBSD: src/share/mk/bsd.cpu.mk,v 1.2.2.5 2002/07/19 08:09:32 ru Exp $
-# $DragonFly: src/share/mk/bsd.cpu.mk,v 1.3 2004/01/26 15:22:08 joerg Exp $
+# $DragonFly: src/share/mk/bsd.cpu.mk,v 1.4 2004/01/26 15:42:36 joerg Exp $
 
 # include compiler-specific bsd.cpu.mk
 
 .if !defined(CCVER) || ${CCVER} == "gcc2"
 .  include <bsd.cpu.gcc2.mk>
-.elif ${CCVER} == "gcc3"
+.elif defined(CCVER) && ${CCVER} == "gcc3"
 .  include <bsd.cpu.gcc3.mk>
 .elif defined(CCVER_BSD_CPU_MK)
 .  if ${CCVER_BSD_CPU_MK} != ""
