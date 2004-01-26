1ea208a7e4d41a8f29f7bb212bdaa1d86288f04f
Joerg Sonnenberger <joerg@dragonflybsd.org>
2004-01-26 19:09:49 +0000

    To let the crazy Perl Makefiles work, add the support for ../Makefile.sub which is included at the time ../Makefile.inc originally was.

--- share/mk/bsd.init.mk
+++ share/mk/bsd.init.mk
@@ -1,9 +1,17 @@
 # $FreeBSD: src/share/mk/bsd.init.mk,v 1.1.2.1 2002/07/17 19:08:23 ru Exp $
-# $DragonFly: src/share/mk/bsd.init.mk,v 1.2 2003/06/17 04:37:02 dillon Exp $
+# $DragonFly: src/share/mk/bsd.init.mk,v 1.3 2004/01/26 19:09:49 joerg Exp $
 
 # The include file <bsd.init.mk> includes ../Makefile.inc and
 # <bsd.own.mk>; this is used at the top of all <bsd.*.mk> files
 # that actually "build something".
+
+.if target(__<bsd.init.mk>__) && !target(__<bsd.init.mk>__2)
+__<bsd.init.mk>__2:
+.if exists(${.CURDIR}/../Makefile.sub)
+.include "${.CURDIR}/../Makefile.sub"
+.endif
+.endif
+
 
 .if !target(__<bsd.init.mk>__)
 __<bsd.init.mk>__:
