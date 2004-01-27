283b4f069e9e757970a78ffdc512cd191f20c47d
Robert Garrett <rob@dragonflybsd.org>
2004-01-27 00:42:46 +0000

    update rcng to remove FreeBSD and NetBSD

--- etc/rc.d/rcconf.sh
+++ etc/rc.d/rcconf.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
 # $FreeBSD: src/etc/rc.d/rcconf.sh,v 1.2 2003/01/25 20:02:35 mtm Exp $
-# $DragonFly: src/etc/rc.d/rcconf.sh,v 1.2 2003/12/11 23:28:41 dillon Exp $
+# $DragonFly: src/etc/rc.d/rcconf.sh,v 1.3 2004/01/27 00:42:45 rob Exp $
 #
 
 # PROVIDE: rcconf
 # REQUIRE: initdiskless
 # BEFORE:  disks initrandom
-# KEYWORD: DragonFly FreeBSD
+# KEYWORD: DragonFly 
 
 . /etc/rc.subr
 dummy_rc_command "$1"
