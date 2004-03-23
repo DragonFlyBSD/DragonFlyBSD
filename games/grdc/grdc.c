9aeb24d3649695ac382bcd37225eb380bf64f8bd
Chris Pressey <cpressey@dragonflybsd.org>
2004-03-23 18:34:41 +0000

    Style(9) cleanup.
    - Split deeply nested code into its own function, draw_row().
    - Remove pleonastic wrapper function movto().
    - Convert K&R-style function declarations to ANSI style.
    - Remove (void) cast when ignoring return value.
    - Fix indentation and whitespace around operators.
    - Remove once-humourous comments that have now fallen out of context.

--- games/grdc/grdc.c
+++ games/grdc/grdc.c
@@ -7,7 +7,7 @@
  * 10-18-89 added signal handling
  *
  * $FreeBSD: src/games/grdc/grdc.c,v 1.8.2.1 2001/10/02 11:51:49 ru Exp $
- * $DragonFly: src/games/grdc/grdc.c,v 1.2 2003/06/17 04:25:24 dillon Exp $
+ * $DragonFly: src/games/grdc/grdc.c,v 1.3 2004/03/23 18:34:41 cpressey Exp $
  */
 
 #include <err.h>
@@ -24,8 +24,7 @@
 #define XLENGTH 58
 #define YDEPTH  7
 
-/* it won't be */
-time_t now; /* yeah! */
+time_t now;
 struct tm *tm;
 
 short disp[11] = {
@@ -38,41 +37,37 @@ volatile sig_atomic_t sigtermed;
 
 int hascolor = 0;
 
-void set(int, int);
-void standt(int);
-void movto(int, int);
-void sighndl(int);
-void usage(void);
+static void set(int, int);
+static void standt(int);
+static void sighndl(int);
+static void usage(void);
+static void draw_row(int, int);
 
-void sighndl(signo)
-int signo;
+void sighndl(int signo)
 {
-	sigtermed=signo;
+	sigtermed = signo;
 }
 
 int
-main(argc, argv)
-int argc;
-char **argv;
+main(int argc, char **argv)
 {
-long t, a;
-int i, j, s, k;
-int n;
-int ch;
-int scrol;
+	int i, s, k;
+	int n;
+	int ch;
+	int scrol;
 
 	scrol = 0;
 
 	while ((ch = getopt(argc, argv, "s")) != -1)
-	switch (ch) {
-	case 's':
-		scrol = 1;
-		break;
-	case '?':
-	default:
-		usage();
-		/* NOTREACHED */
-	}
+		switch (ch) {
+		case 's':
+			scrol = 1;
+			break;
+		case '?':
+		default:
+			usage();
+			/* NOTREACHED */
+		}
 	argc -= optind;
 	argv += optind;
 
@@ -88,9 +83,9 @@ int scrol;
 
 	initscr();
 
-	signal(SIGINT,sighndl);
-	signal(SIGTERM,sighndl);
-	signal(SIGHUP,sighndl);
+	signal(SIGINT, sighndl);
+	signal(SIGTERM, sighndl);
+	signal(SIGHUP, sighndl);
 
 	cbreak();
 	noecho();
@@ -98,7 +93,7 @@ int scrol;
 
 	hascolor = has_colors();
 
-	if(hascolor) {
+	if (hascolor) {
 		start_color();
 		init_pair(1, COLOR_BLACK, COLOR_RED);
 		init_pair(2, COLOR_RED, COLOR_BLACK);
@@ -109,21 +104,21 @@ int scrol;
 	clear();
 	refresh();
 
-	if(hascolor) {
+	if (hascolor) {
 		attrset(COLOR_PAIR(3));
 
-		mvaddch(YBASE - 2,  XBASE - 3, ACS_ULCORNER);
+		mvaddch(YBASE - 2, XBASE - 3, ACS_ULCORNER);
 		hline(ACS_HLINE, XLENGTH);
-		mvaddch(YBASE - 2,  XBASE - 2 + XLENGTH, ACS_URCORNER);
+		mvaddch(YBASE - 2, XBASE - 2 + XLENGTH, ACS_URCORNER);
 
-		mvaddch(YBASE + YDEPTH - 1,  XBASE - 3, ACS_LLCORNER);
+		mvaddch(YBASE + YDEPTH - 1, XBASE - 3, ACS_LLCORNER);
 		hline(ACS_HLINE, XLENGTH);
-		mvaddch(YBASE + YDEPTH - 1,  XBASE - 2 + XLENGTH, ACS_LRCORNER);
+		mvaddch(YBASE + YDEPTH - 1, XBASE - 2 + XLENGTH, ACS_LRCORNER);
 
-		move(YBASE - 1,  XBASE - 3);
+		move(YBASE - 1, XBASE - 3);
 		vline(ACS_VLINE, YDEPTH);
 
-		move(YBASE - 1,  XBASE - 2 + XLENGTH);
+		move(YBASE - 1, XBASE - 2 + XLENGTH);
 		vline(ACS_VLINE, YDEPTH);
 
 		attrset(COLOR_PAIR(2));
@@ -132,45 +127,34 @@ int scrol;
 		mask = 0;
 		time(&now);
 		tm = localtime(&now);
-		set(tm->tm_sec%10, 0);
-		set(tm->tm_sec/10, 4);
-		set(tm->tm_min%10, 10);
-		set(tm->tm_min/10, 14);
-		set(tm->tm_hour%10, 20);
-		set(tm->tm_hour/10, 24);
+		set(tm->tm_sec % 10, 0);
+		set(tm->tm_sec / 10, 4);
+		set(tm->tm_min % 10, 10);
+		set(tm->tm_min / 10, 14);
+		set(tm->tm_hour % 10, 20);
+		set(tm->tm_hour / 10, 24);
 		set(10, 7);
 		set(10, 17);
-		for(k=0; k<6; k++) {
-			if(scrol) {
-				for(i=0; i<5; i++)
-					new[i] = (new[i]&~mask) | (new[i+1]&mask);
-				new[5] = (new[5]&~mask) | (next[k]&mask);
+		for(k = 0; k < 6; k++) {
+			if (scrol) {
+				for(i = 0; i < 5; i++)
+					new[i] = (new[i] & ~mask) |
+						 (new[i+1] & mask);
+				new[5] = (new[5] & ~mask) | (next[k] & mask);
 			} else
-				new[k] = (new[k]&~mask) | (next[k]&mask);
+				new[k] = (new[k] & ~mask) | (next[k] & mask);
 			next[k] = 0;
-			for(s=1; s>=0; s--) {
+			for (s = 1; s >= 0; s--) {
 				standt(s);
-				for(i=0; i<6; i++) {
-					if((a = (new[i]^old[i])&(s ? new : old)[i]) != 0) {
-						for(j=0,t=1<<26; t; t>>=1,j++) {
-							if(a&t) {
-								if(!(a&(t<<1))) {
-									movto(YBASE + i, XBASE + 2*j);
-								}
-								addstr("  ");
-							}
-						}
-					}
-					if(!s) {
-						old[i] = new[i];
-					}
+				for (i = 0; i < 6; i++) {
+					draw_row(i, s);
 				}
-				if(!s) {
+				if (!s) {
 					refresh();
 				}
 			}
 		}
-		movto(6, 0);
+		move(6, 0);
 		refresh();
 		sleep(1);
 		if (sigtermed) {
@@ -189,16 +173,37 @@ int scrol;
 }
 
 void
+draw_row(int i, int s)
+{
+	long a, t;
+	int j;
+
+	if ((a = (new[i] ^ old[i]) & (s ? new : old)[i]) != 0) {
+		for (j = 0, t = 1 << 26; t; t >>= 1, j++) {
+			if (a & t) {
+				if (!(a & (t << 1))) {
+					move(YBASE + i, XBASE + 2 * j);
+				}
+				addstr("  ");
+			}
+		}
+	}
+	if (!s) {
+		old[i] = new[i];
+	}
+}
+
+void
 set(int t, int n)
 {
-int i, m;
+	int i, m;
 
-	m = 7<<n;
-	for(i=0; i<5; i++) {
-		next[i] |= ((disp[t]>>(4-i)*3)&07)<<n;
-		mask |= (next[i]^old[i])&m;
+	m = 7 << n;
+	for (i = 0; i < 5; i++) {
+		next[i] |= ((disp[t] >> (4 - i) * 3) & 07) << n;
+		mask |= (next[i] ^ old[i]) & m;
 	}
-	if(mask&m)
+	if (mask & m)
 		mask |= m;
 }
 
@@ -206,13 +211,13 @@ void
 standt(int on)
 {
 	if (on) {
-		if(hascolor) {
+		if (hascolor) {
 			attron(COLOR_PAIR(1));
 		} else {
 			attron(A_STANDOUT);
 		}
 	} else {
-		if(hascolor) {
+		if (hascolor) {
 			attron(COLOR_PAIR(2));
 		} else {
 			attroff(A_STANDOUT);
@@ -221,15 +226,8 @@ standt(int on)
 }
 
 void
-movto(int line, int col)
-{
-	move(line, col);
-}
-
-void
 usage(void)
 {
-
-	(void)fprintf(stderr, "usage: grdc [-s] [n]\n");
+	fprintf(stderr, "usage: grdc [-s] [n]\n");
 	exit(1);
 }
