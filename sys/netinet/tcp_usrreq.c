e61cbecc9c8e3f46f6607da5d44af8f58092a080
Jeffrey Hsu <hsu@dragonflybsd.org>
2004-03-31 10:23:10 +0000

    Only enter into wildcard hash table if bind succeeds.

--- sys/netinet/tcp_usrreq.c
+++ sys/netinet/tcp_usrreq.c
@@ -32,7 +32,7 @@
  *
  *	From: @(#)tcp_usrreq.c	8.2 (Berkeley) 1/3/94
  * $FreeBSD: src/sys/netinet/tcp_usrreq.c,v 1.51.2.17 2002/10/11 11:46:44 ume Exp $
- * $DragonFly: src/sys/netinet/tcp_usrreq.c,v 1.9 2004/03/31 00:43:09 hsu Exp $
+ * $DragonFly: src/sys/netinet/tcp_usrreq.c,v 1.10 2004/03/31 10:23:10 hsu Exp $
  */
 
 #include "opt_ipsec.h"
@@ -276,9 +276,10 @@ tcp_usr_listen(struct socket *so, struct thread *td)
 	COMMON_START();
 	if (inp->inp_lport == 0)
 		error = in_pcbbind(inp, (struct sockaddr *)0, td);
-	if (error == 0)
+	if (error == 0) {
 		tp->t_state = TCPS_LISTEN;
-	in_pcbinswildcardhash(inp);
+		in_pcbinswildcardhash(inp);
+	}
 	COMMON_END(PRU_LISTEN);
 }
 
