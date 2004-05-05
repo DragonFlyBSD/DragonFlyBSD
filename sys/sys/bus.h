07d2fd358b3ca7d39ca36e0a8c7ed67b090075f7
Hiten Pandya <hmp@dragonflybsd.org>
2004-05-05 16:57:11 +0000

    Add bus_alloc_resource_any(9).
    Obtained-from:	FreeBSD-4

--- sys/sys/bus.h
+++ sys/sys/bus.h
@@ -23,8 +23,8 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $FreeBSD: src/sys/sys/bus.h,v 1.30.2.4 2002/10/10 15:13:33 jhb Exp $
- * $DragonFly: src/sys/sys/bus.h,v 1.11 2004/04/15 13:31:41 joerg Exp $
+ * $FreeBSD: src/sys/sys/bus.h,v 1.30.2.5 2004/03/17 17:54:25 njl Exp $
+ * $DragonFly: src/sys/sys/bus.h,v 1.12 2004/05/05 16:57:11 hmp Exp $
  */
 
 #ifndef _SYS_BUS_H_
@@ -255,6 +255,12 @@ void	bus_delete_resource(device_t dev, int type, int rid);
 int	bus_child_present(device_t child);
 int	bus_child_pnpinfo_str(device_t child, char *buf, size_t buflen);
 int	bus_child_location_str(device_t child, char *buf, size_t buflen);
+
+static __inline struct resource *
+bus_alloc_resource_any(device_t dev, int type, int *rid, u_int flags)
+{
+	return (bus_alloc_resource(dev, type, rid, 0ul, ~0ul, 1, flags));
+}
 
 /*
  * Access functions for device.
