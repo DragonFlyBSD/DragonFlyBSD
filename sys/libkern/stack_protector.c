6f2a5fe6dc5ae40d30b49d1b8cba1fe9f273a998
Joerg Sonnenberger <joerg@dragonflybsd.org>
2004-06-01 17:31:17 +0000

    Initialize the magic cookie using real numbers instead of a multi-character literal.

--- sys/libkern/stack_protector.c
+++ sys/libkern/stack_protector.c
@@ -23,14 +23,19 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * $DragonFly: src/sys/libkern/stack_protector.c,v 1.3 2003/12/28 05:05:59 drhodus Exp $
+ * $DragonFly: src/sys/libkern/stack_protector.c,v 1.4 2004/06/01 17:31:17 joerg Exp $
  */
 
 #include <sys/param.h>
+#include <sys/endian.h>
 
 void __stack_smash_handler(int damaged, char func[]);
 
-int  __guard = '\0\0\n\377';
+#if BYTE_ORDER == LITTLE_ENDIAN
+int __guard = 0x00000aff;
+#else
+int __guard = 0xff0a0000;
+#endif
 
 void 
 __stack_smash_handler(int damaged, char func[])
