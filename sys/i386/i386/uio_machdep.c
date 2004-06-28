a01cbbbe6e581d3fe0da39360a2023deb58b3b1e
David Rhodus <drhodus@dragonflybsd.org>
2004-06-28 02:49:06 +0000

    Sync in Copyright notice change from FreeBSD.

--- sys/i386/i386/uio_machdep.c
+++ sys/i386/i386/uio_machdep.c
@@ -16,10 +16,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
  * 4. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
@@ -38,7 +34,7 @@
  *
  * @(#)kern_subr.c	8.3 (Berkeley) 1/21/94
  * $FreeBSD: src/sys/i386/i386/uio_machdep.c,v 1.1 2004/03/21 20:28:36 alc Exp $
- * $DragonFly: src/sys/i386/i386/Attic/uio_machdep.c,v 1.4 2004/05/04 12:05:29 hmp Exp $
+ * $DragonFly: src/sys/i386/i386/Attic/uio_machdep.c,v 1.5 2004/06/28 02:49:06 drhodus Exp $
  */
 
 #include <sys/param.h>
