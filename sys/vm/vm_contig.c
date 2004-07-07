5959bdc9f16f769315f2da84cf007ad2bacd3af1
Hiten Pandya <hmp@dragonflybsd.org>
2004-07-07 18:09:06 +0000

    Remove the advertising clause from this file.
    I will settle with 3-clause license.
    
    See the following URL for more information:
    ftp://ftp.cs.berkeley.edu/pub/4bsd/README.Impt.License.Change
    
    Discussed-with: 	Matthew Dillon <dillon@backplane.com>

--- sys/vm/vm_contig.c
+++ sys/vm/vm_contig.c
@@ -16,11 +16,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -37,7 +33,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)vm_page.c	7.4 (Berkeley) 5/7/91
- * $DragonFly: src/sys/vm/vm_contig.c,v 1.6 2004/03/01 06:33:24 dillon Exp $
+ * $DragonFly: src/sys/vm/vm_contig.c,v 1.7 2004/07/07 18:09:06 hmp Exp $
  */
 
 /*
