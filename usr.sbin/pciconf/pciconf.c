c8841b2ed661cff260fe9736ef27dae44ed1b742
Jeroen Ruigrok/asmodai <asmodai@dragonflybsd.org>
2004-07-19 09:28:17 +0000

    Update comment to point to the right file (src/sys/bus/pci/pcireg.h).

--- usr.sbin/pciconf/pciconf.c
+++ usr.sbin/pciconf/pciconf.c
@@ -27,7 +27,7 @@
  * SUCH DAMAGE.
  *
  * $FreeBSD: src/usr.sbin/pciconf/pciconf.c,v 1.11.2.3 2002/09/17 22:09:15 jdp Exp $
- * $DragonFly: src/usr.sbin/pciconf/pciconf.c,v 1.3 2003/08/08 04:18:47 dillon Exp $
+ * $DragonFly: src/usr.sbin/pciconf/pciconf.c,v 1.4 2004/07/19 09:28:17 asmodai Exp $
  */
 
 #include <sys/types.h>
@@ -246,7 +246,7 @@ list_verbose(struct pci_conf *p)
 }
 
 /*
- * This is a direct cut-and-paste from the table in sys/dev/pci/pci.c.
+ * This is a direct cut-and-paste from the table in sys/bus/pci/pcireg.h.
  */
 static struct
 {
