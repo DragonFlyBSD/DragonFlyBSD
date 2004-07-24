acb76797eabd7232eae62bdfae41785a0a49e494
Jeroen Ruigrok/asmodai <asmodai@dragonflybsd.org>
2004-07-24 09:51:18 +0000

    Bump version number for the 1.1-CURRENT tree.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -37,7 +37,7 @@
  *
  *	@(#)param.h	8.3 (Berkeley) 4/4/95
  * $FreeBSD: src/sys/sys/param.h,v 1.61.2.38 2003/05/22 17:12:01 fjoe Exp $
- * $DragonFly: src/sys/sys/param.h,v 1.13 2004/03/30 19:14:13 dillon Exp $
+ * $DragonFly: src/sys/sys/param.h,v 1.14 2004/07/24 09:51:18 asmodai Exp $
  */
 
 #ifndef _SYS_PARAM_H_
@@ -55,7 +55,7 @@
  *	doc/en_US.ISO8859-1/books/porters-handbook/book.sgml
  */
 #undef __DragonFly_version
-#define __DragonFly_version 100000	/* Master, propagated to newvers */
+#define __DragonFly_version 110000	/* Master, propagated to newvers */
 
 /* 
  * __FreeBSD_version numbers for source compatibility.  This is temporary
