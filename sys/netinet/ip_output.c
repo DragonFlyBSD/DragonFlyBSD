e90fc8a2b8af800383a692f48046e31a2a99d103
Matthew Dillon <dillon@dragonflybsd.org>
2004-08-26 20:59:07 +0000

    Bring in FreeBSD/1.214 - UC Regent's advertising clause removal per per letter dated July 22, 1999 (see /usr/src/COPYRIGHT).

--- sys/netinet/ip_output.c
+++ sys/netinet/ip_output.c
@@ -10,11 +10,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -32,7 +28,7 @@
  *
  *	@(#)ip_output.c	8.3 (Berkeley) 1/21/94
  * $FreeBSD: src/sys/netinet/ip_output.c,v 1.99.2.37 2003/04/15 06:44:45 silby Exp $
- * $DragonFly: src/sys/netinet/ip_output.c,v 1.18 2004/08/26 20:57:02 dillon Exp $
+ * $DragonFly: src/sys/netinet/ip_output.c,v 1.19 2004/08/26 20:59:07 dillon Exp $
  */
 
 #define _IP_VHL
