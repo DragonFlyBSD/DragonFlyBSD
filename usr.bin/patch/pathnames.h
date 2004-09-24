48d201a5a8c1dab4aa7166b0812594c101fc43c3
Joerg Sonnenberger <joerg@dragonflybsd.org>
2004-09-24 18:44:28 +0000

    Add GPL-free patch. This is the extended version of Larry Wall's original patch.
    Obtained-from: OpenBSD
    Submitted-by: Sascha Wildner <saw@online.de>

---
+++ usr.bin/patch/pathnames.h
@@ -0,0 +1,14 @@
+/*
+ * $OpenBSD: pathnames.h,v 1.1 2003/07/29 20:10:17 millert Exp $
+ * $DragonFly: src/usr.bin/patch/pathnames.h,v 1.1 2004/09/24 18:44:28 joerg Exp $
+ */
+
+/*
+ * Placed in the public domain by Todd C. Miller <Todd.Miller@courtesan.com>
+ * on July 29, 2003.
+ */
+
+#include <paths.h>
+
+#define	_PATH_ED		"/bin/ed"
+#define	_PATH_MKDIR		"/bin/mkdir"
