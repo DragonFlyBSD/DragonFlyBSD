b6fca1488be927b7e8ebd4aebeb3451c8730be8c
Joerg Sonnenberger <joerg@dragonflybsd.org>
2004-10-23 17:02:38 +0000

    Build framework for GDB 6.2.1.

---
+++ gnu/usr.bin/gdb/gdb/version.c
@@ -0,0 +1,5 @@
+/* $DragonFly: src/gnu/usr.bin/gdb/gdb/Attic/version.c,v 1.1 2004/10/23 17:02:38 joerg Exp $ */
+#include "version.h"
+const char version[] = "6.2.1";
+const char host_name[] = "i386-dragonfly";
+const char target_name[] = "i386-dragonfly";
---
+++ gnu/usr.bin/gdb/libbfd/bfdver.h
@@ -0,0 +1,4 @@
+/* $DragonFly: src/gnu/usr.bin/gdb/libbfd/Attic/bfdver.h,v 1.1 2004/10/23 17:02:38 joerg Exp $ */
+#define BFD_VERSION_DATE 20040709
+#define BFD_VERSION 215910000
+#define BFD_VERSION_STRING "2.15.91 20040709"
