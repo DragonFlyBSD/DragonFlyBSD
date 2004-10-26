62529567e67ed1ad8e1a05a0a44433f64f0c1dbf
Joerg Sonnenberger <joerg@dragonflybsd.org>
2004-10-26 11:19:31 +0000

    Add gzip based on libz. This is faster for decompression and yields better compression than GNU gzip (300 KB for the plan9 ISO image).
    The optional support for decompression of compress and bzip2 is disabled,
    the normal frontends should be used instead. They can be activated by
    setting GZIP_UNCOMPRESS and GZIP_UNBZIP2 respectively.
    
    The only missing features from GNU gzip are the omission of zip support
    (it could only handle the first entry anyway) and unpack support.
    
    This differs from the NetBSD version in that all compile time warnings
    has been fixed.
    
    Obtained-from: NetBSD

---
+++ usr.bin/gzip/unbzip2.c
@@ -0,0 +1,87 @@
+/*	$NetBSD: unbzip2.c,v 1.5 2004/05/25 04:34:40 mrg Exp $	*/
+/*	$DragonFly: src/usr.bin/gzip/unbzip2.c,v 1.1 2004/10/26 11:19:31 joerg Exp $ */
+
+/* This file is #included by gzip.c */
+
+static off_t
+unbzip2(int in, int out, char *pre, size_t prelen, off_t *bytes_in)
+{
+	int		ret, end_of_file;
+	ssize_t		n;
+	off_t		bytes_out = 0;
+	bz_stream	bzs;
+	static char	*inbuf, *outbuf;
+
+	if (inbuf == NULL)
+		inbuf = malloc(BUFLEN);
+	if (outbuf == NULL)
+		outbuf = malloc(BUFLEN);
+	if (inbuf == NULL || outbuf == NULL)
+	        maybe_err("malloc");
+
+	bzs.bzalloc = NULL;
+	bzs.bzfree = NULL;
+	bzs.opaque = NULL;
+
+	end_of_file = 0;
+	ret = BZ2_bzDecompressInit(&bzs, 0, 0);
+	if (ret != BZ_OK)
+	        maybe_errx("bzip2 init");
+
+	/* Prepend. */
+	bzs.avail_in = prelen;
+	bzs.next_in = pre;
+
+	if (bytes_in)
+		*bytes_in = prelen;
+
+	while (ret != BZ_STREAM_END) {
+	        if (bzs.avail_in == 0 && !end_of_file) {
+	                n = read(in, inbuf, BUFLEN);
+	                if (n < 0)
+	                        maybe_err("read");
+	                if (n == 0)
+	                        end_of_file = 1;
+	                bzs.next_in = inbuf;
+	                bzs.avail_in = n;
+			if (bytes_in)
+				*bytes_in += n;
+	        }
+
+	        bzs.next_out = outbuf;
+	        bzs.avail_out = BUFLEN;
+	        ret = BZ2_bzDecompress(&bzs);
+
+	        switch (ret) {
+	        case BZ_STREAM_END:
+	        case BZ_OK:
+	                if (ret == BZ_OK && end_of_file)
+	                        maybe_err("read");
+	                if (!tflag) {
+	                        n = write(out, outbuf, BUFLEN - bzs.avail_out);
+	                        if (n < 0)
+	                                maybe_err("write");
+	                }
+	                bytes_out += n;
+	                break;
+
+	        case BZ_DATA_ERROR:
+	                maybe_warnx("bzip2 data integrity error");
+			break;
+
+	        case BZ_DATA_ERROR_MAGIC:
+	                maybe_warnx("bzip2 magic number error");
+			break;
+
+	        case BZ_MEM_ERROR:
+	                maybe_warnx("bzip2 out of memory");
+			break;
+
+	        }
+	}
+
+	if (ret != BZ_STREAM_END || BZ2_bzDecompressEnd(&bzs) != BZ_OK)
+	        return (-1);
+
+	return (bytes_out);
+}
