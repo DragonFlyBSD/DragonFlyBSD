83246afcab1f5f92a02bed65899843744ca0d794
Joerg Sonnenberger <joerg@dragonflybsd.org>
2004-11-14 19:38:34 +0000

    Fix a lhs-cast by casting first to unsigned char and then to the type-correct char.

--- bin/ed/cbc.c
+++ bin/ed/cbc.c
@@ -38,7 +38,7 @@
  *
  * @(#)cbc.c,v 1.2 1994/02/01 00:34:36 alm Exp
  * $FreeBSD: src/bin/ed/cbc.c,v 1.12.2.1 2001/07/04 22:32:18 kris Exp $
- * $DragonFly: src/bin/ed/cbc.c,v 1.6 2004/11/07 20:54:51 eirikn Exp $
+ * $DragonFly: src/bin/ed/cbc.c,v 1.7 2004/11/14 19:38:34 joerg Exp $
  */
 
 #include <sys/types.h>
@@ -145,7 +145,7 @@ put_des_char(int c, FILE *fp)
 		des_ct = cbc_encode(des_buf, des_n, fp);
 		des_n = 0;
 	}
-	return (des_ct >= 0) ? ((unsigned char)des_buf[des_n++] = c) : EOF;
+	return (des_ct >= 0) ? (des_buf[des_n++] = (char)(unsigned char)c) : EOF;
 #else
 	return (fputc(c, fp));
 #endif
