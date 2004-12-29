18d935e4bfdf73ab06d9faaff30e529a266ad76c
Jeroen Ruigrok/asmodai <asmodai@dragonflybsd.org>
2004-12-29 17:29:35 +0000

    Get rid of an Alpha specific part.

--- lib/msun/src/math_private.h
+++ lib/msun/src/math_private.h
@@ -12,7 +12,7 @@
 /*
  * from: @(#)fdlibm.h 5.1 93/09/24
  * $FreeBSD: src/lib/msun/src/math_private.h,v 1.6 1999/08/28 00:06:43 peter Exp $
- * $DragonFly: src/lib/msun/src/Attic/math_private.h,v 1.3 2003/11/12 20:21:31 eirikn Exp $
+ * $DragonFly: src/lib/msun/src/Attic/math_private.h,v 1.4 2004/12/29 17:29:35 asmodai Exp $
  */
 
 #ifndef _MATH_PRIVATE_H_
@@ -220,32 +220,5 @@ extern float __kernel_sinf (float,float,int);
 extern float __kernel_cosf (float,float);
 extern float __kernel_tanf (float,float,int);
 extern int   __kernel_rem_pio2f (float*,float*,int,int,int,const int*);
-
-#ifdef	__alpha__
-#define __generic___ieee754_acos	__ieee754_acos
-#define __generic___ieee754_asin	__ieee754_asin
-#define __generic___ieee754_atan2	__ieee754_atan2
-#define __generic___ieee754_exp		__ieee754_exp
-#define __generic___ieee754_fmod	__ieee754_fmod
-#define __generic___ieee754_log		__ieee754_log
-#define __generic___ieee754_log10	__ieee754_log10
-#define __generic___ieee754_remainder	__ieee754_remainder
-#define __generic___ieee754_scalb	__ieee754_scalb
-#define __generic___ieee754_sqrt	__ieee754_sqrt
-#define	__generic_atan			atan
-#define	__generic_ceil			ceil
-#define	__generic_copysign		copysign
-#define	__generic_cos			cos
-#define	__generic_finite		finite
-#define	__generic_floor			floor
-#define	__generic_ilogb			ilogb
-#define	__generic_log1p			log1p
-#define	__generic_logb			logb
-#define	__generic_rint			rint
-#define	__generic_scalbn		scalbn
-#define	__generic_significand		significand
-#define	__generic_sin			sin
-#define	__generic_tan			tan
-#endif
 
 #endif /* _MATH_PRIVATE_H_ */
