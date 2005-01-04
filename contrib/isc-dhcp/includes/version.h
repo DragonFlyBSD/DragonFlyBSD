3abfdfdfaa0f71292cef7967aca5a84d19f11624
Joerg Sonnenberger <joerg@dragonflybsd.org>
2005-01-04 20:00:34 +0000

    RIP

--- contrib/isc-dhcp/includes/version.h
+++
@@ -1,4 +0,0 @@
-/* Current version of ISC DHCP Distribution. */
-/* $DragonFly: src/contrib/isc-dhcp/includes/Attic/version.h,v 1.2 2003/10/11 21:14:19 dillon Exp $ */
-
-#define DHCP_VERSION	"V3.0.1rc12"
