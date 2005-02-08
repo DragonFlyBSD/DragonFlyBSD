acbbccb285171fd6d1a37ffec5d2eb3c05f977da
Joerg Sonnenberger <joerg@dragonflybsd.org>
2005-02-08 15:54:01 +0000

    Set so->so_pcb directly to NULL to avoid lvalue cast.

--- sys/netinet6/in6_pcb.c
+++ sys/netinet6/in6_pcb.c
@@ -1,5 +1,5 @@
 /*	$FreeBSD: src/sys/netinet6/in6_pcb.c,v 1.10.2.9 2003/01/24 05:11:35 sam Exp $	*/
-/*	$DragonFly: src/sys/netinet6/in6_pcb.c,v 1.22 2005/02/01 16:09:37 hrs Exp $	*/
+/*	$DragonFly: src/sys/netinet6/in6_pcb.c,v 1.23 2005/02/08 15:54:01 joerg Exp $	*/
 /*	$KAME: in6_pcb.c,v 1.31 2001/05/21 05:45:10 jinmei Exp $	*/
   
 /*
@@ -599,7 +599,7 @@ in6_pcbdetach(struct inpcb *inp)
 #endif /* IPSEC */
 	inp->inp_gencnt = ++ipi->ipi_gencnt;
 	in_pcbremlists(inp);
-	sotoinpcb(so) = 0;
+	so->so_pcb = NULL;
 	sofree(so);
 
 	if (inp->in6p_options)
