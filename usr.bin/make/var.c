39631e417175e5d28403618033fd87fb9512866d
Max Okumoto <okumoto@dragonflybsd.org>
2005-02-15 11:05:22 +0000

    - Copy destroy code at end of Var_Parse() up into else-if blocks.   This temporarily increases the code size.  Much of that goes   away, because the certain branches become unreachable, and thus   can be deleted.

--- usr.bin/make/var.c
+++ usr.bin/make/var.c
@@ -37,7 +37,7 @@
  *
  * @(#)var.c	8.3 (Berkeley) 3/19/94
  * $FreeBSD: src/usr.bin/make/var.c,v 1.83 2005/02/11 10:49:01 harti Exp $
- * $DragonFly: src/usr.bin/make/var.c,v 1.95 2005/02/15 11:00:39 okumoto Exp $
+ * $DragonFly: src/usr.bin/make/var.c,v 1.96 2005/02/15 11:05:22 okumoto Exp $
  */
 
 /*-
@@ -1695,6 +1695,44 @@ Var_Parse(char *foo, GNode *ctxt, Boolean err, size_t *lengthPtr,
 	    *tstr = endc;
 	}
 
+
+	if (v->flags & VAR_FROM_ENV) {
+	    if (rw_str == (char *)Buf_GetAll(v->val, (size_t *)NULL)) {
+		/*
+		 * Returning the value unmodified, so tell the caller to free
+		 * the thing.
+		 */
+		*freePtr = TRUE;
+		VarDestroy(v, FALSE);
+		return (rw_str);
+	    } else {
+		VarDestroy(v, TRUE);
+		return (rw_str);
+	    }
+	} else if (v->flags & VAR_JUNK) {
+	    /*
+	     * Perform any free'ing needed and set *freePtr to FALSE so the caller
+	     * doesn't try to free a static pointer.
+	     */
+	    if (*freePtr) {
+		free(rw_str);
+	    }
+	    if (dynamic) {
+		*freePtr = FALSE;
+		VarDestroy(v, TRUE);
+		rw_str = emalloc(*lengthPtr + 1);
+		strncpy(rw_str, input, *lengthPtr);
+		rw_str[*lengthPtr] = '\0';
+		*freePtr = TRUE;
+		return (rw_str);
+	    } else {
+		*freePtr = FALSE;
+		VarDestroy(v, TRUE);
+		return (err ? var_Error : varNoError);
+	    }
+	} else {
+	    return (rw_str);
+	}
     } else {
 	/*
 	 * If it's not bounded by braces of some sort, life is much simpler.
@@ -1744,44 +1782,44 @@ Var_Parse(char *foo, GNode *ctxt, Boolean err, size_t *lengthPtr,
 	    *freePtr = TRUE;
 	    *lengthPtr = 2;
 	}
-    }
 
-    if (v->flags & VAR_FROM_ENV) {
-	if (rw_str == (char *)Buf_GetAll(v->val, (size_t *)NULL)) {
+	if (v->flags & VAR_FROM_ENV) {
+	    if (rw_str == (char *)Buf_GetAll(v->val, (size_t *)NULL)) {
+		/*
+		 * Returning the value unmodified, so tell the caller to free
+		 * the thing.
+		 */
+		*freePtr = TRUE;
+		VarDestroy(v, FALSE);
+		return (rw_str);
+	    } else {
+		VarDestroy(v, TRUE);
+		return (rw_str);
+	    }
+	} else if (v->flags & VAR_JUNK) {
 	    /*
-	     * Returning the value unmodified, so tell the caller to free
-	     * the thing.
+	     * Perform any free'ing needed and set *freePtr to FALSE so the caller
+	     * doesn't try to free a static pointer.
 	     */
-	    *freePtr = TRUE;
-	    VarDestroy(v, FALSE);
-	    return (rw_str);
+	    if (*freePtr) {
+		free(rw_str);
+	    }
+	    if (dynamic) {
+		*freePtr = FALSE;
+		VarDestroy(v, TRUE);
+		rw_str = emalloc(*lengthPtr + 1);
+		strncpy(rw_str, input, *lengthPtr);
+		rw_str[*lengthPtr] = '\0';
+		*freePtr = TRUE;
+		return (rw_str);
+	    } else {
+		*freePtr = FALSE;
+		VarDestroy(v, TRUE);
+		return (err ? var_Error : varNoError);
+	    }
 	} else {
-	    VarDestroy(v, TRUE);
 	    return (rw_str);
 	}
-    } else if (v->flags & VAR_JUNK) {
-	/*
-	 * Perform any free'ing needed and set *freePtr to FALSE so the caller
-	 * doesn't try to free a static pointer.
-	 */
-	if (*freePtr) {
-	    free(rw_str);
-	}
-	if (dynamic) {
-	    *freePtr = FALSE;
-	    VarDestroy(v, TRUE);
-	    rw_str = emalloc(*lengthPtr + 1);
-	    strncpy(rw_str, input, *lengthPtr);
-	    rw_str[*lengthPtr] = '\0';
-	    *freePtr = TRUE;
-	    return (rw_str);
-	} else {
-	    *freePtr = FALSE;
-	    VarDestroy(v, TRUE);
-	    return (err ? var_Error : varNoError);
-	}
-    } else {
-	return (rw_str);
     }
 }
 
