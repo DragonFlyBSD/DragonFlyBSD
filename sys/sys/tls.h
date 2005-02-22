0fc1128c458c7f4a59e3c9a49b9838b86056ac63
Matthew Dillon <dillon@dragonflybsd.org>
2005-02-22 02:17:56 +0000

    Add system call prototypes for userland.
    Submitted-by: David Xu <davidxu@t2t2.com>

--- sys/sys/tls.h
+++ sys/sys/tls.h
@@ -3,7 +3,7 @@
  *
  *	Implements the architecture independant TLS info structure.
  *
- * $DragonFly: src/sys/sys/tls.h,v 1.1 2005/02/21 21:40:58 dillon Exp $
+ * $DragonFly: src/sys/sys/tls.h,v 1.2 2005/02/22 02:17:56 dillon Exp $
  */
 
 #ifndef _SYS_TLS_H_
@@ -13,5 +13,10 @@ struct tls_info {
 	void *base;
 	int size;
 };
+
+#ifndef _KERNEL
+int sys_set_tls_area(int which, struct tls_info *info, int infosize);
+int sys_get_tls_area(int which, struct tls_info *info, int infosize);
+#endif
 
 #endif
