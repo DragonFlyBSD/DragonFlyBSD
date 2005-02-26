37ae5e01190aace4c2ee096ae8961d6aaca900f1
Max Okumoto <okumoto@dragonflybsd.org>
2005-02-26 11:23:54 +0000

    Change test to check M modifier.

--- usr.bin/make/tests/variables/t1/test.sh
+++ usr.bin/make/tests/variables/t1/test.sh
@@ -1,31 +1,26 @@
 #!/bin/sh
 
-# $DragonFly: src/usr.bin/make/tests/variables/t1/test.sh,v 1.4 2005/02/26 10:48:24 okumoto Exp $
+# $DragonFly: src/usr.bin/make/tests/variables/t1/test.sh,v 1.5 2005/02/26 11:23:54 okumoto Exp $
 
 . ../../common.sh
 
 run_test()
 {
 	cat > Makefile << "_EOF_"
-A	= 0
-AV	= 1
+FILES	= \
+		main.c globals.h \
+		util.c util.h \
+		map.c map.h \
+		parser.y lexer.l \
+		cmdman.1 format.5
 all:
-	echo $A
-	echo ${AV}
-	echo ${A}
-	# The following are soo broken why no syntax error?
-	echo $(
-	echo $)
-	echo ${
-	echo ${A
-	echo ${A)
-	echo ${A){
-	echo ${AV
-	echo ${AV)
-	echo ${AV){
-	echo ${AV{
-	echo ${A{
-	echo $}
+	@echo "all files: ${FILES}"
+	@echo "cfiles: ${FILES:M*.c}"
+	@echo "hfiles: ${FILES:M*.h}"
+	@echo "grammer and lexer: ${FILES:M*.[ly]}"
+	@echo "man page: ${FILES:M*.[1-9]}"
+	@echo "utility files: ${FILES:Mutil.?}"
+	@echo "m files: ${FILES:Mm*}"
 _EOF_
 	$MAKE 1> stdout 2> stderr
 	echo $? > status
