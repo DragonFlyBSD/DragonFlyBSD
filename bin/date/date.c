16963e56247fb59b133c005b29724c029aaa1540
Joerg Sonnenberger <joerg@dragonflybsd.org>
2005-03-14 23:55:41 +0000

    Instead of using the non-standard conforming %+ format string, use nl_langinfo to query the default format string.

--- bin/date/date.c
+++ bin/date/date.c
@@ -33,7 +33,7 @@
  * @(#) Copyright (c) 1985, 1987, 1988, 1993 The Regents of the University of California.  All rights reserved.
  * @(#)date.c	8.2 (Berkeley) 4/28/95
  * $FreeBSD: src/bin/date/date.c,v 1.32.2.6 2001/10/31 17:31:51 dillon Exp $
- * $DragonFly: src/bin/date/date.c,v 1.8 2004/11/07 20:54:51 eirikn Exp $
+ * $DragonFly: src/bin/date/date.c,v 1.9 2005/03/14 23:55:41 joerg Exp $
  */
 
 #include <sys/param.h>
@@ -46,6 +46,7 @@
 #include <string.h>
 #include <syslog.h>
 #include <unistd.h>
+#include <langinfo.h>
 #include <locale.h>
 #include <libutil.h>
 
@@ -137,7 +138,7 @@ main(int argc, char **argv)
 	if (!rflag && time(&tval) == -1)
 		err(1, "time");
 
-	format = "%+";
+	format = nl_langinfo(D_T_FMT);
 
 	/* allow the operands in any order */
 	if (*argv && **argv == '+') {
