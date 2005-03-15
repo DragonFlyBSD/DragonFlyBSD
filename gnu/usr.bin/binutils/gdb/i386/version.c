a5d2870a8ae86b3c60a45ba842301b2dd01a0046
Joerg Sonnenberger <joerg@dragonflybsd.org>
2005-03-15 01:54:28 +0000

    Stop building old GDB.

--- gnu/usr.bin/binutils/gdb/i386/version.c
+++
@@ -1,5 +0,0 @@
-/* $FreeBSD: src/gnu/usr.bin/binutils/gdb/i386/version.c,v 1.4.4.1 2002/05/14 21:20:51 mp Exp $ */
-/* $DragonFly: src/gnu/usr.bin/binutils/gdb/i386/Attic/version.c,v 1.3 2003/11/19 00:51:37 dillon Exp $ */
-char *version = "4.18 (FreeBSD)";
-char *host_name = "i386-unknown-dragonfly";
-char *target_name = "i386-unknown-dragonfly";
