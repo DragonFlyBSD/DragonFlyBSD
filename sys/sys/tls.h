9bf43b68b3054027190600d09a8937dadcc736ec
Joerg Sonnenberger <joerg@dragonflybsd.org>
2005-03-21 23:08:55 +0000

    Include sys/types.h to make it self-contained.

--- sys/sys/tls.h
+++ sys/sys/tls.h
@@ -3,11 +3,13 @@
  *
  *	Implements the architecture independant TLS info structure.
  *
- * $DragonFly: src/sys/sys/tls.h,v 1.3 2005/03/21 17:09:52 joerg Exp $
+ * $DragonFly: src/sys/sys/tls.h,v 1.4 2005/03/21 23:08:55 joerg Exp $
  */
 
 #ifndef _SYS_TLS_H_
 #define _SYS_TLS_H_
+
+#include <sys/types.h>
 
 struct tls_info {
 	void *base;
