47d395cb8e35031d69e43a5d7e8eeec092b9086c
Max Okumoto <okumoto@dragonflybsd.org>
2005-04-22 18:05:56 +0000

    Really removed the hack.  The last patch just added another copy of the hack. sigh

--- usr.bin/make/make.h
+++ usr.bin/make/make.h
@@ -37,7 +37,7 @@
  *
  *	from: @(#)make.h	8.3 (Berkeley) 6/13/95
  * $FreeBSD: src/usr.bin/make/make.h,v 1.29 2005/02/01 10:50:36 harti Exp $
- * $DragonFly: src/usr.bin/make/make.h,v 1.27 2005/04/22 18:01:32 okumoto Exp $
+ * $DragonFly: src/usr.bin/make/make.h,v 1.28 2005/04/22 18:05:56 okumoto Exp $
  */
 
 #ifndef make_h_a91074b9
@@ -53,46 +53,6 @@
 /* buildworld needs this on FreeBSD */
 #ifndef __arysize
 #define __arysize(ary)		(sizeof(ary)/sizeof((ary)[0]))
-#endif
-
-/*
- * This code should be removed if when cross compiling from FreeBSD
- * 4.X is no longer required
- * March 17.	 Max Okumoto.
- */
-#ifndef INT64_MIN
-#define	INT64_MIN	(-0x7fffffffffffffffLL-1)
-#define NEED_STRTOUMAX
-#include <stdlib.h>
-
-typedef	__int64_t	intmax_t;
-typedef	__uint64_t	uintmax_t;
-
-static inline uintmax_t
-strtoumax(const char *nptr, char **endptr, int base)
-{
-	return strtoull(nptr, endptr, base);
-}
-#endif
-
-/*
- * This code should be removed if when cross compiling from FreeBSD
- * 4.X is no longer required
- * March 17.	 Max Okumoto.
- */
-#ifndef INT64_MIN
-#define	INT64_MIN	(-0x7fffffffffffffffLL-1)
-#define NEED_STRTOUMAX
-#include <stdlib.h>
-
-typedef	__int64_t	intmax_t;
-typedef	__uint64_t	uintmax_t;
-
-static inline uintmax_t
-strtoumax(const char *nptr, char **endptr, int base)
-{
-	return strtoull(nptr, endptr, base);
-}
 #endif
 
 struct GNode;
