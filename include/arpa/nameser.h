d5f6c41405bd37310af8c81d38bbf89639a1117b
Joerg Sonnenberger <joerg@dragonflybsd.org>
2005-04-27 16:38:40 +0000

    Use const for internal casts to not conflict with const pointers.

--- include/arpa/nameser.h
+++ include/arpa/nameser.h
@@ -51,7 +51,7 @@
 /*
  *	From: Id: nameser.h,v 8.16 1998/02/06 00:35:58 halley Exp
  * $FreeBSD: src/include/arpa/nameser.h,v 1.14.2.1 2001/06/15 22:08:27 ume Exp $
- * $DragonFly: src/include/arpa/nameser.h,v 1.3 2003/11/14 01:01:47 dillon Exp $
+ * $DragonFly: src/include/arpa/nameser.h,v 1.4 2005/04/27 16:38:40 joerg Exp $
  */
 
 #ifndef _ARPA_NAMESER_H_
@@ -360,7 +360,7 @@ typedef enum __ns_class {
  * Inline versions of get/put short/long.  Pointer is advanced.
  */
 #define NS_GET16(s, cp) { \
-	register u_char *t_cp = (u_char *)(cp); \
+	register const u_char *t_cp = (const u_char *)(cp); \
 	(s) = ((u_int16_t)t_cp[0] << 8) \
 	    | ((u_int16_t)t_cp[1]) \
 	    ; \
@@ -368,7 +368,7 @@ typedef enum __ns_class {
 }
 
 #define NS_GET32(l, cp) { \
-	register u_char *t_cp = (u_char *)(cp); \
+	register const u_char *t_cp = (const u_char *)(cp); \
 	(l) = ((u_int32_t)t_cp[0] << 24) \
 	    | ((u_int32_t)t_cp[1] << 16) \
 	    | ((u_int32_t)t_cp[2] << 8) \
