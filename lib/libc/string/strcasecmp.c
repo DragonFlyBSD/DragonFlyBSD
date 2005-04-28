db49460bb097e7e06a95da9b135a8eafcf632fc4
Joerg Sonnenberger <joerg@dragonflybsd.org>
2005-04-28 19:40:51 +0000

    Readd explicit u_char casts for tolower().
    Requested-by: dillon
    Confusion-by: joerg

--- lib/libc/string/strcasecmp.c
+++ lib/libc/string/strcasecmp.c
@@ -32,7 +32,7 @@
  *
  * @(#)strcasecmp.c	8.1 (Berkeley) 6/4/93
  * $FreeBSD: src/lib/libc/string/strcasecmp.c,v 1.2.8.1 2001/07/09 23:30:03 obrien Exp $
- * $DragonFly: src/lib/libc/string/strcasecmp.c,v 1.6 2005/04/28 18:26:03 joerg Exp $
+ * $DragonFly: src/lib/libc/string/strcasecmp.c,v 1.7 2005/04/28 19:40:51 joerg Exp $
  */
 
 #include <sys/types.h>
@@ -42,10 +42,10 @@
 int
 strcasecmp(const char *s1, const char *s2)
 {
-	while (tolower(*s1) == tolower(*s2++))
+	while (tolower((u_char)*s1) == tolower((u_char)*s2++))
 		if (*s1++ == '\0')
 			return (0);
-	return (tolower(*s1) - tolower(*--s2));
+	return (tolower((u_char)*s1) - tolower((u_char)*--s2));
 }
 
 int
@@ -53,8 +53,8 @@ strncasecmp(const char *s1, const char *s2, size_t n)
 {
 	if (n != 0) {
 		do {
-			if (tolower(*s1) != tolower(*s2++))
-				return (tolower(*s1) - tolower(*--s2));
+			if (tolower((u_char)*s1) != tolower((u_char)*s2++))
+				return (tolower((u_char)*s1) - tolower((u_char)*--s2));
 			if (*s1++ == '\0')
 				break;
 		} while (--n != 0);
