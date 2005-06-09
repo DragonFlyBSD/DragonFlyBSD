35d0dcfc0976180897724d7a1e831ab184b0e8b1
Joerg Sonnenberger <joerg@dragonflybsd.org>
2005-06-09 19:29:00 +0000

    Use M_WAITOK for contigmalloc now that the attach path is interrupt-save.

--- sys/dev/netif/ste/if_ste.c
+++ sys/dev/netif/ste/if_ste.c
@@ -30,7 +30,7 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  *
  * $FreeBSD: src/sys/pci/if_ste.c,v 1.14.2.9 2003/02/05 22:03:57 mbr Exp $
- * $DragonFly: src/sys/dev/netif/ste/if_ste.c,v 1.24 2005/06/09 19:26:55 joerg Exp $
+ * $DragonFly: src/sys/dev/netif/ste/if_ste.c,v 1.25 2005/06/09 19:29:00 joerg Exp $
  */
 
 #include <sys/param.h>
@@ -972,7 +972,7 @@ static int ste_attach(dev)
 
 	/* Allocate the descriptor queues. */
 	sc->ste_ldata = contigmalloc(sizeof(struct ste_list_data), M_DEVBUF,
-	    M_NOWAIT, 0, 0xffffffff, PAGE_SIZE, 0);
+	    M_WAITOK, 0, 0xffffffff, PAGE_SIZE, 0);
 
 	if (sc->ste_ldata == NULL) {
 		device_printf(dev, "no memory for list buffers!\n");
