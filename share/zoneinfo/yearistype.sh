e5af4fe3eb967e737b509ced370d00f4d0e03302
Sascha Wildner <swildner@dragonflybsd.org>
2005-07-13 21:08:07 +0000

    Remove extra whitespace at the end of some lines.

--- share/zoneinfo/yearistype.sh
+++ share/zoneinfo/yearistype.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
 # $FreeBSD: src/share/zoneinfo/yearistype.sh,v 1.1.2.3.8.1 2000/10/25 19:44:09 wollman Exp $
-# $DragonFly: src/share/zoneinfo/yearistype.sh,v 1.2 2003/06/17 04:37:04 dillon Exp $
+# $DragonFly: src/share/zoneinfo/yearistype.sh,v 1.3 2005/07/13 21:08:07 swildner Exp $
 
-: '@(#)yearistype.sh	7.7'
+: '@(#)yearistype.sh	7.8'
 
 case $#-$1 in
 	2-|2-0*|2-*[!0-9]*)
@@ -12,7 +12,7 @@ case $#-$1 in
 esac
 
 case $#-$2 in
-	2-even)	
+	2-even)
 		case $1 in
 			*[24680])			exit 0 ;;
 			*)				exit 1 ;;
@@ -22,7 +22,7 @@ case $#-$2 in
 			*[02468][048]|*[13579][26])	exit 1 ;;
 			*)				exit 0 ;;
 		esac ;;
-	2-odd)	
+	2-odd)
 		case $1 in
 			*[13579])			exit 0 ;;
 			*)				exit 1 ;;
@@ -32,7 +32,7 @@ case $#-$2 in
 			*[02468][048]|*[13579][26])	exit 0 ;;
 			*)				exit 1 ;;
 		esac ;;
-	2-*)	
+	2-*)
 		echo "$0: wild type - $2" >&2 ;;
 esac
 
