1d2547583c2540b4f637c3255bd8abd6ad7060c5
Jeroen Ruigrok/asmodai <asmodai@dragonflybsd.org>
2005-07-22 18:11:05 +0000

    Get rid off extraneous spaces in function prototypes.

--- lib/msun/src/math.h
+++ lib/msun/src/math.h
@@ -12,7 +12,7 @@
 /*
  * from: @(#)fdlibm.h 5.1 93/09/24
  * $FreeBSD: src/lib/msun/src/math.h,v 1.8.2.2 2001/11/23 16:16:18 dd Exp $
- * $DragonFly: src/lib/msun/src/Attic/math.h,v 1.3 2003/11/12 20:21:31 eirikn Exp $
+ * $DragonFly: src/lib/msun/src/Attic/math.h,v 1.4 2005/07/22 18:11:05 asmodai Exp $
  */
 
 #ifndef _MATH_H_
@@ -102,164 +102,164 @@ __BEGIN_DECLS
 /*
  * ANSI/POSIX
  */
-extern double acos (double);
-extern double asin (double);
-extern double atan (double);
-extern double atan2 (double, double);
-extern double cos (double);
-extern double sin (double);
-extern double tan (double);
+extern double acos(double);
+extern double asin(double);
+extern double atan(double);
+extern double atan2(double, double);
+extern double cos(double);
+extern double sin(double);
+extern double tan(double);
 
-extern double cosh (double);
-extern double sinh (double);
-extern double tanh (double);
+extern double cosh(double);
+extern double sinh(double);
+extern double tanh(double);
 
-extern double exp (double);
-extern double frexp (double, int *);
-extern double ldexp (double, int);
-extern double log (double);
-extern double log10 (double);
-extern double modf (double, double *);
+extern double exp(double);
+extern double frexp(double, int *);
+extern double ldexp(double, int);
+extern double log(double);
+extern double log10(double);
+extern double modf(double, double *);
 
-extern double pow (double, double);
-extern double sqrt (double);
+extern double pow(double, double);
+extern double sqrt(double);
 
-extern double ceil (double);
-extern double fabs (double);
-extern double floor (double);
-extern double fmod (double, double);
+extern double ceil(double);
+extern double fabs(double);
+extern double floor(double);
+extern double fmod(double, double);
 
 #if !defined(_ANSI_SOURCE) && !defined(_POSIX_SOURCE)
-extern double erf (double);
-extern double erfc (double);
-extern double gamma (double);
-extern double hypot (double, double);
-extern int isinf (double);
-extern int isnan (double);
-extern int finite (double);
-extern double j0 (double);
-extern double j1 (double);
-extern double jn (int, double);
-extern double lgamma (double);
-extern double y0 (double);
-extern double y1 (double);
-extern double yn (int, double);
+extern double erf(double);
+extern double erfc(double);
+extern double gamma(double);
+extern double hypot(double, double);
+extern int isinf(double);
+extern int isnan(double);
+extern int finite(double);
+extern double j0(double);
+extern double j1(double);
+extern double jn(int, double);
+extern double lgamma(double);
+extern double y0(double);
+extern double y1(double);
+extern double yn(int, double);
 
 #if !defined(_XOPEN_SOURCE)
-extern double acosh (double);
-extern double asinh (double);
-extern double atanh (double);
-extern double cbrt (double);
-extern double logb (double);
-extern double nextafter (double, double);
-extern double remainder (double, double);
-extern double scalb (double, double);
+extern double acosh(double);
+extern double asinh(double);
+extern double atanh(double);
+extern double cbrt(double);
+extern double logb(double);
+extern double nextafter(double, double);
+extern double remainder(double, double);
+extern double scalb(double, double);
 
 #ifndef __cplusplus
-extern int matherr (struct exception *);
+extern int matherr(struct exception *);
 #endif
 
 /*
  * IEEE Test Vector
  */
-extern double significand (double);
+extern double significand(double);
 
 /*
  * Functions callable from C, intended to support IEEE arithmetic.
  */
-extern double copysign (double, double);
-extern int ilogb (double);
-extern double rint (double);
-extern double scalbn (double, int);
+extern double copysign(double, double);
+extern int ilogb(double);
+extern double rint(double);
+extern double scalbn(double, int);
 
 /*
  * BSD math library entry points
  */
-extern double drem (double, double);
-extern double expm1 (double);
-extern double log1p (double);
+extern double drem(double, double);
+extern double expm1(double);
+extern double log1p(double);
 
 /*
  * Reentrant version of gamma & lgamma; passes signgam back by reference
  * as the second argument; user must allocate space for signgam.
  */
 #ifdef _REENTRANT
-extern double gamma_r (double, int *);
-extern double lgamma_r (double, int *);
+extern double gamma_r(double, int *);
+extern double lgamma_r(double, int *);
 #endif /* _REENTRANT */
 
 
 /* float versions of ANSI/POSIX functions */
-extern float acosf (float);
-extern float asinf (float);
-extern float atanf (float);
-extern float atan2f (float, float);
-extern float cosf (float);
-extern float sinf (float);
-extern float tanf (float);
+extern float acosf(float);
+extern float asinf(float);
+extern float atanf(float);
+extern float atan2f(float, float);
+extern float cosf(float);
+extern float sinf(float);
+extern float tanf(float);
 
-extern float coshf (float);
-extern float sinhf (float);
-extern float tanhf (float);
+extern float coshf(float);
+extern float sinhf(float);
+extern float tanhf(float);
 
-extern float expf (float);
-extern float frexpf (float, int *);
-extern float ldexpf (float, int);
-extern float logf (float);
-extern float log10f (float);
-extern float modff (float, float *);
+extern float expf(float);
+extern float frexpf(float, int *);
+extern float ldexpf(float, int);
+extern float logf(float);
+extern float log10f(float);
+extern float modff(float, float *);
 
-extern float powf (float, float);
-extern float sqrtf (float);
+extern float powf(float, float);
+extern float sqrtf(float);
 
-extern float ceilf (float);
-extern float fabsf (float);
-extern float floorf (float);
-extern float fmodf (float, float);
+extern float ceilf(float);
+extern float fabsf(float);
+extern float floorf(float);
+extern float fmodf(float, float);
 
-extern float erff (float);
-extern float erfcf (float);
-extern float gammaf (float);
-extern float hypotf (float, float);
-extern int isnanf (float);
-extern int finitef (float);
-extern float j0f (float);
-extern float j1f (float);
-extern float jnf (int, float);
-extern float lgammaf (float);
-extern float y0f (float);
-extern float y1f (float);
-extern float ynf (int, float);
+extern float erff(float);
+extern float erfcf(float);
+extern float gammaf(float);
+extern float hypotf(float, float);
+extern int isnanf(float);
+extern int finitef(float);
+extern float j0f(float);
+extern float j1f(float);
+extern float jnf(int, float);
+extern float lgammaf(float);
+extern float y0f(float);
+extern float y1f(float);
+extern float ynf(int, float);
 
-extern float acoshf (float);
-extern float asinhf (float);
-extern float atanhf (float);
-extern float cbrtf (float);
-extern float logbf (float);
-extern float nextafterf (float, float);
-extern float remainderf (float, float);
-extern float scalbf (float, float);
+extern float acoshf(float);
+extern float asinhf(float);
+extern float atanhf(float);
+extern float cbrtf(float);
+extern float logbf(float);
+extern float nextafterf(float, float);
+extern float remainderf(float, float);
+extern float scalbf(float, float);
 
 /*
  * float version of IEEE Test Vector
  */
-extern float significandf (float);
+extern float significandf(float);
 
 /*
  * Float versions of functions callable from C, intended to support
  * IEEE arithmetic.
  */
-extern float copysignf (float, float);
-extern int ilogbf (float);
-extern float rintf (float);
-extern float scalbnf (float, int);
+extern float copysignf(float, float);
+extern int ilogbf(float);
+extern float rintf(float);
+extern float scalbnf(float, int);
 
 /*
  * float versions of BSD math library entry points
  */
-extern float dremf (float, float);
-extern float expm1f (float);
-extern float log1pf (float);
+extern float dremf(float, float);
+extern float expm1f(float);
+extern float log1pf(float);
 
 /*
  * Float versions of reentrant version of gamma & lgamma; passes
@@ -267,8 +267,8 @@ extern float log1pf (float);
  * allocate space for signgam.
  */
 #ifdef _REENTRANT
-extern float gammaf_r (float, int *);
-extern float lgammaf_r (float, int *);
+extern float gammaf_r(float, int *);
+extern float lgammaf_r(float, int *);
 #endif	/* _REENTRANT */
 
 #endif /* !_XOPEN_SOURCE */
