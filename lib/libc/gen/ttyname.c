fa602fcf56a0ba7a6918dad38b0035d0e5cb5748
Joerg Sonnenberger <joerg@dragonflybsd.org>
2005-08-23 12:06:48 +0000

    Don't assume that ttys are always located directly in /dev. This assumptions breaks as soon as a proper pts filesystem comes into place. Also ensure the static buffer is not overflown when reading from the database.

--- lib/libc/gen/ttyname.c
+++ lib/libc/gen/ttyname.c
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  * $FreeBSD: src/lib/libc/gen/ttyname.c,v 1.10.6.2 2002/10/15 19:46:46 fjoe Exp $
- * $DragonFly: src/lib/libc/gen/ttyname.c,v 1.6 2005/02/02 05:42:01 dillon Exp $
+ * $DragonFly: src/lib/libc/gen/ttyname.c,v 1.7 2005/08/23 12:06:48 joerg Exp $
  *
  * @(#)ttyname.c	8.2 (Berkeley) 1/27/94
  */
@@ -41,6 +41,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <dirent.h>
+#include <limits.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -53,7 +54,7 @@
 #include <db.h>
 #include "libc_private.h"
 
-static char static_buf[sizeof(_PATH_DEV) + MAXNAMLEN] = _PATH_DEV;
+static char static_buf[PATH_MAX] = _PATH_DEV;
 static char *oldttyname __P((int, struct stat *));
 static char *ttyname_threaded(int fd);
 static char *ttyname_unthreaded(int fd);
@@ -171,7 +172,8 @@ ttyname_unthreaded(int fd)
 		bkey.dev = sb.st_rdev;
 		key.data = &bkey;
 		key.size = sizeof(bkey);
-		if (!(db->get)(db, &key, &data, 0)) {
+		if (!(db->get)(db, &key, &data, 0) &&
+		    (sizeof(_PATH_DEV) + data.size - 1< PATH_MAX)) {
 			bcopy(data.data,
 			    static_buf + sizeof(_PATH_DEV) - 1, data.size);
 			(void)(db->close)(db);
@@ -196,6 +198,8 @@ oldttyname(fd, sb)
 
 	while ( (dirp = readdir(dp)) ) {
 		if (dirp->d_fileno != sb->st_ino)
+			continue;
+		if (sizeof(_PATH_DEV) + dirp->d_namlen >= PATH_MAX)
 			continue;
 		bcopy(dirp->d_name, static_buf + sizeof(_PATH_DEV) - 1,
 		    dirp->d_namlen + 1);
