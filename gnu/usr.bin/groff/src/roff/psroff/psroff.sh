dbf63eef06ff0b1fcba23b97bae3524335fda0a3
Jeroen Ruigrok/asmodai <asmodai@dragonflybsd.org>
2005-09-01 20:47:14 +0000

    Update to groff 1.19.1. This synchronises us with FreeBSD up till 1.19 (Ids adjusted accordingly).
    Big help fixing PATCHES behaviour: joerg

--- gnu/usr.bin/groff/src/roff/psroff/psroff.sh
+++ gnu/usr.bin/groff/src/roff/psroff/psroff.sh
@@ -1,6 +1,6 @@
 #! /bin/sh -
 #
-# $FreeBSD: src/gnu/usr.bin/groff/src/roff/psroff/psroff.sh,v 1.1.10.1 2001/04/26 17:08:36 ru Exp $
-# $DragonFly: src/gnu/usr.bin/groff/src/roff/psroff/psroff.sh,v 1.2 2003/06/17 04:25:46 dillon Exp $
+# $FreeBSD: src/gnu/usr.bin/groff/src/roff/psroff/psroff.sh,v 1.2 2001/07/10 17:23:07 ru Exp $
+# $DragonFly: src/gnu/usr.bin/groff/src/roff/psroff/psroff.sh,v 1.3 2005/09/01 20:47:14 asmodai Exp $
 
 exec groff -Tps -l -C ${1+"$@"}
