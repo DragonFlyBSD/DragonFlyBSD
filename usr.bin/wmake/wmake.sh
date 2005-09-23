c5f6a5591c6200a41ad6cbee1dfb977124c3e245
Simon Schubert <corecode@dragonflybsd.org>
2005-09-23 00:31:37 +0000

    Correctly pass arguments.  This enables doing e.g.
    	wmake -n DESTDIR=/ install
    
    which failed before.

--- usr.bin/wmake/wmake.sh
+++ usr.bin/wmake/wmake.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-# $DragonFly: src/usr.bin/wmake/wmake.sh,v 1.1 2003/08/11 00:25:17 dillon Exp $
+# $DragonFly: src/usr.bin/wmake/wmake.sh,v 1.2 2005/09/23 00:31:37 corecode Exp $
 #
-# This script was written by Matt dillon and has been placed in the
+# This script was written by Matt Dillon and has been placed in the
 # public domain.
 
 path=$PWD
@@ -16,5 +16,6 @@ if [ "$path" = "" ]; then
     echo "Unable to locate Makefile.inc through parent dirs"
 fi
 
-eval `cd $path; make WMAKE_ARGS="$@" -f Makefile.inc1 wmake`
+escaped_args=`echo "$@" | sed -e "s/'/\\'/"`
 
+eval `cd $path; make WMAKE_ARGS="'$escaped_args'" -f Makefile.inc1 wmake`
