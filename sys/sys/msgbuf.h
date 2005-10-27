48ecae3157bfe8ed0540178c8982a8245c3d2a75
Matthew Dillon <dillon@dragonflybsd.org>
2005-10-27 00:07:17 +0000

    Increase the MSGBUF_SIZE from 32K to 64K, boot verbose messages don't fit in 32K anymore.

--- sys/sys/msgbuf.h
+++ sys/sys/msgbuf.h
@@ -32,7 +32,7 @@
  *
  *	@(#)msgbuf.h	8.1 (Berkeley) 6/2/93
  * $FreeBSD: src/sys/sys/msgbuf.h,v 1.14.2.1 2001/01/16 12:26:21 phk Exp $
- * $DragonFly: src/sys/sys/msgbuf.h,v 1.3 2003/08/20 07:31:21 rob Exp $
+ * $DragonFly: src/sys/sys/msgbuf.h,v 1.4 2005/10/27 00:07:17 dillon Exp $
  */
 
 #ifndef _SYS_MSGBUF_H_
@@ -54,7 +54,7 @@ extern struct	msgbuf *msgbufp;
 void	msgbufinit	(void *ptr, size_t size);
 
 #if !defined(MSGBUF_SIZE)
-#define	MSGBUF_SIZE	32768
+#define	MSGBUF_SIZE	65536
 #endif
 
 #endif
