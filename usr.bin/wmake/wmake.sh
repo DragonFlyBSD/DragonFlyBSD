21bd8f0751792af138e945cec46d35ed3a9808c4
Simon Schubert <corecode@dragonflybsd.org>
2006-02-11 10:42:12 +0000

    Allways pass -n to echo so that wmake -n will do the right thing. Note that this only works because of the special behaviour of sh's echo builtin.

--- usr.bin/wmake/wmake.sh
+++ usr.bin/wmake/wmake.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $DragonFly: src/usr.bin/wmake/wmake.sh,v 1.2 2005/09/23 00:31:37 corecode Exp $
+# $DragonFly: src/usr.bin/wmake/wmake.sh,v 1.3 2006/02/11 10:42:12 corecode Exp $
 #
 # This script was written by Matt Dillon and has been placed in the
 # public domain.
@@ -16,6 +16,6 @@ if [ "$path" = "" ]; then
     echo "Unable to locate Makefile.inc through parent dirs"
 fi
 
-escaped_args=`echo "$@" | sed -e "s/'/\\'/"`
+escaped_args=`echo -n "$@" | sed -e "s/'/\\'/"`
 
 eval `cd $path; make WMAKE_ARGS="'$escaped_args'" -f Makefile.inc1 wmake`
