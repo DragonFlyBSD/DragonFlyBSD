5768fd94f8994123a95cb24132ee98283544d4c3
Simon Schubert <corecode@dragonflybsd.org>
2006-02-13 14:47:58 +0000

    Adjust build infrastructure for OpenSSH-4.3p2

--- secure/lib/libssh/version.h
+++ secure/lib/libssh/version.h
@@ -1,12 +1,12 @@
-/* $OpenBSD: version.h,v 1.44 2005/03/16 21:17:39 markus Exp $ */
-/* $DragonFly: src/secure/lib/libssh/version.h,v 1.3 2005/07/11 22:49:45 corecode Exp $ */
+/* $OpenBSD: version.h,v 1.46 2006/02/01 11:27:22 markus Exp $ */
+/* $DragonFly: src/secure/lib/libssh/version.h,v 1.4 2006/02/13 14:47:58 corecode Exp $ */
 
 #ifndef SSH_VERSION
 
 #define SSH_VERSION             (ssh_version_get())
 #define SSH_RELEASE		(ssh_version_get())
-#define SSH_VERSION_BASE        "OpenSSH_4.1p1"
-#define SSH_VERSION_ADDENDUM    "DragonFly-20050712"
+#define SSH_VERSION_BASE        "OpenSSH_4.3p2"
+#define SSH_VERSION_ADDENDUM    "DragonFly-20060212"
 
 const char *ssh_version_get(void);
 void ssh_version_set_addendum(const char *add);
