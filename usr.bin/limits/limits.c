823ff154a25d2789cded049db7ba1e0329fd5ae7
Sascha Wildner <swildner@dragonflybsd.org>
2006-03-06 03:10:51 +0000

    Don't modify p twice betweeen sequence points.

--- usr.bin/limits/limits.c
+++ usr.bin/limits/limits.c
@@ -21,7 +21,7 @@
  * Display/change(+runprogram)/eval resource limits.
  *
  * $FreeBSD: src/usr.bin/limits/limits.c,v 1.7.2.3 2003/05/22 09:26:57 sheldonh Exp $
- * $DragonFly: src/usr.bin/limits/limits.c,v 1.5 2006/01/12 13:43:11 corecode Exp $
+ * $DragonFly: src/usr.bin/limits/limits.c,v 1.6 2006/03/06 03:10:51 swildner Exp $
  */
 
 #include <err.h>
@@ -630,7 +630,7 @@ getshellbyname(const char * shell)
     const char * q;
     const char * p = strrchr(shell, '/');
 
-    p = p ? ++p : shell;
+    p = p ? p + 1 : shell;
     for (i = 0; (q = shellparm[i].name) != NULL; i++) {
 	while (*q) {
 	    int j = strcspn(q, "|");
