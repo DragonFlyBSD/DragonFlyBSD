5fdff524d882ea3e13a0833c8d6600d61ba05d89
Sepherosa Ziehau <sephe@dragonflybsd.org>
2006-05-20 09:13:09 +0000

    - Add ral(4) for Ralink RT2500/RT2501/RT2600 chip based wireless NIC - Add ral(4) to GENERIC and LINT - Add man page for ral(4)   Reviewed-by: swildner
    Thank Damien Bergamini for his work on this driver
    
    For RT2500:
    - Fix a ieee80211_node leakage
    - Due to the inter-dependency nature of DONE/(ENCRYPT|DECRYPT) intr, reap desc
      rings twice if one of them comes.  This change gives me ~17.6% TX performance
      boost on my ASUS WL-107G (WPA is used here):
      Original way of TX/RX intr processing
      ------------------------------------------------------------
      Client connecting to sephe-test, TCP port 5001
      TCP window size: 32.5 KByte (default)
      ------------------------------------------------------------
      [  3] local 192.168.2.14 port 1063 connected with 192.168.2.254 port 5001
      [  3]  0.0- 5.0 sec  10.2 MBytes  17.1 Mbits/sec
      [  3]  5.0-10.0 sec  9.95 MBytes  16.7 Mbits/sec
      [  3] 10.0-15.0 sec  9.67 MBytes  16.2 Mbits/sec
      [  3] 15.0-20.0 sec  10.1 MBytes  17.0 Mbits/sec
      [  3] 20.0-25.0 sec  10.2 MBytes  17.1 Mbits/sec
      [  3] 25.0-30.0 sec  10.0 MBytes  16.8 Mbits/sec
      [  3] 30.0-35.0 sec  9.91 MBytes  16.6 Mbits/sec
      [  3] 35.0-40.0 sec  10.3 MBytes  17.2 Mbits/sec
      [  3] 40.0-45.0 sec  9.87 MBytes  16.6 Mbits/sec
      [  3] 45.0-50.0 sec  9.94 MBytes  16.7 Mbits/sec
      [  3] 50.0-55.0 sec  10.2 MBytes  17.2 Mbits/sec
      [  3] 55.0-60.0 sec  9.73 MBytes  16.3 Mbits/sec
      [  3]  0.0-60.0 sec    120 MBytes  16.8 Mbits/sec
    
      Adapted way of TX/RX intr processing
      ------------------------------------------------------------
      Client connecting to sephe-test, TCP port 5001
      TCP window size: 32.5 KByte (default)
      ------------------------------------------------------------
      [  3] local 192.168.2.14 port 1062 connected with 192.168.2.254 port 5001
      [  3]  0.0- 5.0 sec  11.8 MBytes  19.8 Mbits/sec
      [  3]  5.0-10.0 sec  11.5 MBytes  19.4 Mbits/sec
      [  3] 10.0-15.0 sec  11.1 MBytes  18.7 Mbits/sec
      [  3] 15.0-20.0 sec  12.0 MBytes  20.1 Mbits/sec
      [  3] 20.0-25.0 sec  12.6 MBytes  21.2 Mbits/sec
      [  3] 25.0-30.0 sec  11.7 MBytes  19.6 Mbits/sec
      [  3] 30.0-35.0 sec  12.3 MBytes  20.7 Mbits/sec
      [  3] 35.0-40.0 sec  11.9 MBytes  19.9 Mbits/sec
      [  3] 40.0-45.0 sec  11.9 MBytes  19.9 Mbits/sec
      [  3] 45.0-50.0 sec  12.2 MBytes  20.4 Mbits/sec
      [  3] 50.0-55.0 sec  12.1 MBytes  20.2 Mbits/sec
      [  3] 55.0-60.0 sec  12.3 MBytes  20.7 Mbits/sec
      [  3]  0.0-60.0 sec    143 MBytes  20.0 Mbits/sec
    
    Obtained-from: FreeBSD

---
+++ sys/dev/netif/ral/if_ral_pci.c
@@ -0,0 +1,243 @@
+/*
+ * Copyright (c) 2005, 2006
+ *	Damien Bergamini <damien.bergamini@free.fr>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * $FreeBSD: src/sys/dev/ral/if_ral_pci.c,v 1.4 2006/03/05 23:27:51 silby Exp $
+ * $DragonFly: src/sys/dev/netif/ral/if_ral_pci.c,v 1.1 2006/05/20 09:13:09 sephe Exp $
+ */
+
+/*
+ * PCI/Cardbus front-end for the Ralink RT2560/RT2561/RT2561S/RT2661 driver.
+ */
+
+#include <sys/param.h>
+#include <sys/sysctl.h>
+#include <sys/sockio.h>
+#include <sys/mbuf.h>
+#include <sys/kernel.h>
+#include <sys/socket.h>
+#include <sys/systm.h>
+#include <sys/malloc.h>
+#include <sys/module.h>
+#include <sys/bus.h>
+#include <sys/endian.h>
+
+#include <machine/bus.h>
+#include <machine/resource.h>
+#include <sys/rman.h>
+
+#include <net/bpf.h>
+#include <net/if.h>
+#include <net/if_arp.h>
+#include <net/ethernet.h>
+#include <net/if_dl.h>
+#include <net/if_media.h>
+#include <net/if_types.h>
+
+#include <netproto/802_11/ieee80211_var.h>
+#include <netproto/802_11/ieee80211_radiotap.h>
+
+#include <bus/pci/pcireg.h>
+#include <bus/pci/pcivar.h>
+
+#include <dev/netif/ral/if_ralrate.h>
+#include <dev/netif/ral/rt2560var.h>
+#include <dev/netif/ral/rt2661var.h>
+
+struct ral_opns {
+	int	(*attach)(device_t, int);
+	int	(*detach)(void *);
+	void	(*shutdown)(void *);
+	void	(*suspend)(void *);
+	void	(*resume)(void *);
+};
+
+static const struct ral_opns ral_rt2560_opns = {
+	.attach		= rt2560_attach,
+	.detach		= rt2560_detach,
+	.shutdown	= rt2560_shutdown,
+	.suspend	= rt2560_suspend,
+	.resume		= rt2560_resume,
+};
+
+static const struct ral_opns ral_rt2661_opns = {
+	.attach		= rt2661_attach,
+	.detach		= rt2661_detach,
+	.shutdown	= rt2661_shutdown,
+	.suspend	= rt2661_suspend,
+	.resume		= rt2661_resume,
+};
+
+static const struct ral_pci_ident {
+	uint16_t		vendor;
+	uint16_t		device;
+	const char		*name;
+	const struct ral_opns	*opns;
+} ral_pci_ids[] = {
+	{ 0x1814, 0x0201, "Ralink Technology RT2560", &ral_rt2560_opns },
+	{ 0x1814, 0x0301, "Ralink Technology RT2561S", &ral_rt2661_opns },
+	{ 0x1814, 0x0302, "Ralink Technology RT2561", &ral_rt2661_opns },
+	{ 0x1814, 0x0401, "Ralink Technology RT2661", &ral_rt2661_opns },
+	{ 0, 0, NULL, NULL }
+};
+
+struct ral_pci_softc {
+	/* XXX MUST be the first field */
+	union {
+		struct rt2560_softc sc_rt2560;
+		struct rt2661_softc sc_rt2661;
+	} u;
+
+	const struct ral_opns	*sc_opns;
+	int			mem_rid;
+	struct resource		*mem;
+};
+
+static int ral_pci_probe(device_t);
+static int ral_pci_attach(device_t);
+static int ral_pci_detach(device_t);
+static int ral_pci_shutdown(device_t);
+static int ral_pci_suspend(device_t);
+static int ral_pci_resume(device_t);
+
+static device_method_t ral_pci_methods[] = {
+	/* Device interface */
+	DEVMETHOD(device_probe,		ral_pci_probe),
+	DEVMETHOD(device_attach,	ral_pci_attach),
+	DEVMETHOD(device_detach,	ral_pci_detach),
+	DEVMETHOD(device_shutdown,	ral_pci_shutdown),
+	DEVMETHOD(device_suspend,	ral_pci_suspend),
+	DEVMETHOD(device_resume,	ral_pci_resume),
+
+	{ 0, 0 }
+};
+
+static driver_t ral_pci_driver = {
+	"ral",
+	ral_pci_methods,
+	sizeof (struct ral_pci_softc)
+};
+
+static devclass_t ral_devclass;
+
+DRIVER_MODULE(ral, pci, ral_pci_driver, ral_devclass, 0, 0);
+DRIVER_MODULE(ral, cardbus, ral_pci_driver, ral_devclass, 0, 0);
+
+MODULE_DEPEND(ral, wlan, 1, 1, 1);
+MODULE_DEPEND(ral, pci, 1, 1, 1);
+MODULE_DEPEND(ral, cardbus, 1, 1, 1);
+
+static int
+ral_pci_probe(device_t dev)
+{
+	const struct ral_pci_ident *ident;
+	uint16_t vid, did;
+
+	vid = pci_get_vendor(dev);
+	did = pci_get_device(dev);
+	for (ident = ral_pci_ids; ident->name != NULL; ident++) {
+		if (vid == ident->vendor && did == ident->device) {
+			struct ral_pci_softc *psc = device_get_softc(dev);
+
+			psc->sc_opns = ident->opns;
+			device_set_desc(dev, ident->name);
+			return 0;
+		}
+	}
+	return ENXIO;
+}
+
+/* Base Address Register */
+#define RAL_PCI_BAR0	0x10
+
+static int
+ral_pci_attach(device_t dev)
+{
+	struct ral_pci_softc *psc = device_get_softc(dev);
+	struct rt2560_softc *sc = &psc->u.sc_rt2560;
+	int error;
+
+	/* Assign `dev' earlier, so that we can do possible error clean up */
+	sc->sc_dev = dev;
+
+	if (pci_get_powerstate(dev) != PCI_POWERSTATE_D0) {
+		device_printf(dev, "chip is in D%d power mode "
+		    "-- setting to D0\n", pci_get_powerstate(dev));
+		pci_set_powerstate(dev, PCI_POWERSTATE_D0);
+	}
+
+	/* enable bus-mastering */
+	pci_enable_busmaster(dev);
+
+	psc->mem_rid = RAL_PCI_BAR0;
+	psc->mem = bus_alloc_resource_any(dev, SYS_RES_MEMORY, &psc->mem_rid,
+					  RF_ACTIVE);
+	if (psc->mem == NULL) {
+		device_printf(dev, "could not allocate memory resource\n");
+		return ENXIO;
+	}
+	sc->sc_st = rman_get_bustag(psc->mem);
+	sc->sc_sh = rman_get_bushandle(psc->mem);
+
+	error = psc->sc_opns->attach(dev, pci_get_device(dev));
+	if (error != 0)
+		ral_pci_detach(dev);
+
+	return error;
+}
+
+static int
+ral_pci_detach(device_t dev)
+{
+	struct ral_pci_softc *psc = device_get_softc(dev);
+
+	if (device_is_attached(dev))
+		psc->sc_opns->detach(psc);
+
+	bus_generic_detach(dev);
+
+	if (psc->mem != NULL) {
+		bus_release_resource(dev, SYS_RES_MEMORY, psc->mem_rid,
+				     psc->mem);
+	}
+	return 0;
+}
+
+static int
+ral_pci_shutdown(device_t dev)
+{
+	struct ral_pci_softc *psc = device_get_softc(dev);
+
+	psc->sc_opns->shutdown(psc);
+	return 0;
+}
+
+static int
+ral_pci_suspend(device_t dev)
+{
+	struct ral_pci_softc *psc = device_get_softc(dev);
+
+	psc->sc_opns->suspend(psc);
+	return 0;
+}
+
+static int
+ral_pci_resume(device_t dev)
+{
+	struct ral_pci_softc *psc = device_get_softc(dev);
+
+	psc->sc_opns->resume(psc);
+	return 0;
+}
---
+++ sys/dev/netif/ral/rt2560var.h
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) 2005, 2006
+ *	Damien Bergamini <damien.bergamini@free.fr>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * $FreeBSD: src/sys/dev/ral/rt2560var.h,v 1.1 2006/03/05 20:36:56 damien Exp $
+ * $DragonFly: src/sys/dev/netif/ral/rt2560var.h,v 1.1 2006/05/20 09:13:09 sephe Exp $
+ */
+
+struct rt2560_rx_radiotap_header {
+	struct ieee80211_radiotap_header wr_ihdr;
+	uint64_t	wr_tsf;
+	uint8_t		wr_flags;
+	uint8_t		wr_rate;
+	uint16_t	wr_chan_freq;
+	uint16_t	wr_chan_flags;
+	uint8_t		wr_antenna;
+	uint8_t		wr_antsignal;
+};
+
+#define RT2560_RX_RADIOTAP_PRESENT					\
+	((1 << IEEE80211_RADIOTAP_TSFT) |				\
+	 (1 << IEEE80211_RADIOTAP_FLAGS) |				\
+	 (1 << IEEE80211_RADIOTAP_RATE) |				\
+	 (1 << IEEE80211_RADIOTAP_CHANNEL) |				\
+	 (1 << IEEE80211_RADIOTAP_ANTENNA) |				\
+	 (1 << IEEE80211_RADIOTAP_DB_ANTSIGNAL))
+
+struct rt2560_tx_radiotap_header {
+	struct ieee80211_radiotap_header wt_ihdr;
+	uint8_t		wt_flags;
+	uint8_t		wt_rate;
+	uint16_t	wt_chan_freq;
+	uint16_t	wt_chan_flags;
+	uint8_t		wt_antenna;
+};
+
+#define RT2560_TX_RADIOTAP_PRESENT					\
+	((1 << IEEE80211_RADIOTAP_FLAGS) |				\
+	 (1 << IEEE80211_RADIOTAP_RATE) |				\
+	 (1 << IEEE80211_RADIOTAP_CHANNEL) |				\
+	 (1 << IEEE80211_RADIOTAP_ANTENNA))
+
+struct rt2560_tx_data {
+	bus_dmamap_t			map;
+	struct mbuf			*m;
+	struct ieee80211_node		*ni;
+	struct ral_rssdesc		id;
+};
+
+struct rt2560_tx_ring {
+	bus_dma_tag_t		desc_dmat;
+	bus_dma_tag_t		data_dmat;
+	bus_dmamap_t		desc_map;
+	bus_addr_t		physaddr;
+	struct rt2560_tx_desc	*desc;
+	struct rt2560_tx_data	*data;
+	int			count;
+	int			queued;
+	int			cur;
+	int			next;
+	int			cur_encrypt;
+	int			next_encrypt;
+};
+
+struct rt2560_rx_data {
+	bus_dmamap_t	map;
+	struct mbuf	*m;
+	int		drop;
+};
+
+struct rt2560_rx_ring {
+	bus_dma_tag_t		desc_dmat;
+	bus_dma_tag_t		data_dmat;
+	bus_dmamap_t		desc_map;
+	bus_addr_t		physaddr;
+	struct rt2560_rx_desc	*desc;
+	struct rt2560_rx_data	*data;
+	int			count;
+	int			cur;
+	int			next;
+	int			cur_decrypt;
+};
+
+struct rt2560_node {
+	struct ieee80211_node	ni;
+	struct ral_rssadapt	rssadapt;
+};
+
+struct rt2560_softc {
+	/*
+	 * NOTE: following four fields MUST be in the
+	 * same order as in rt2661_softc
+	 */
+	struct ieee80211com	sc_ic;
+	bus_space_tag_t		sc_st;
+	bus_space_handle_t	sc_sh;
+	device_t		sc_dev;
+
+	int			sc_irq_rid;
+	struct resource		*sc_irq;
+	void			*sc_ih;
+
+	int			(*sc_newstate)(struct ieee80211com *,
+				    enum ieee80211_state, int);
+
+	struct callout		scan_ch;
+	struct callout		rssadapt_ch;
+
+	int			sc_tx_timer;
+
+	uint32_t		asic_rev;
+	uint32_t		eeprom_rev;
+	uint8_t			rf_rev;
+
+	struct rt2560_tx_ring	txq;
+	struct rt2560_tx_ring	prioq;
+	struct rt2560_tx_ring	atimq;
+	struct rt2560_tx_ring	bcnq;
+	struct rt2560_rx_ring	rxq;
+
+	struct ieee80211_beacon_offsets	sc_bo;
+
+	uint32_t		rf_regs[4];
+	uint8_t			txpow[14];
+
+	struct {
+		uint8_t	reg;
+		uint8_t	val;
+	}			bbp_prom[16];
+
+	int			led_mode;
+	int			hw_radio;
+	int			rx_ant;
+	int			tx_ant;
+	int			nb_ant;
+
+	int			dwelltime;
+
+	struct bpf_if		*sc_drvbpf;
+
+	union {
+		struct rt2560_rx_radiotap_header th;
+		uint8_t	pad[64];
+	}			sc_rxtapu;
+#define sc_rxtap	sc_rxtapu.th
+	int			sc_rxtap_len;
+
+	union {
+		struct rt2560_tx_radiotap_header th;
+		uint8_t	pad[64];
+	}			sc_txtapu;
+#define sc_txtap	sc_txtapu.th
+	int			sc_txtap_len;
+
+	struct sysctl_ctx_list	sysctl_ctx;
+	struct sysctl_oid	*sysctl_tree;
+};
+
+int	rt2560_attach(device_t, int);
+int	rt2560_detach(void *);
+void	rt2560_shutdown(void *);
+void	rt2560_suspend(void *);
+void	rt2560_resume(void *);
