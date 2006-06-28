d88d9b3d7840da2ea7bc7d5a4395d4aabd7389a7
Sascha Wildner <swildner@dragonflybsd.org>
2006-06-28 21:37:37 +0000

    Remove ports and dfports specific include files.

--- share/mk/bsd.port.post.mk
+++
@@ -1,8 +0,0 @@
-# $FreeBSD: src/share/mk/bsd.port.post.mk,v 1.3.2.1 2002/07/17 19:08:23 ru Exp $
-# $DragonFly: src/share/mk/Attic/bsd.port.post.mk,v 1.2 2003/06/17 04:37:02 dillon Exp $
-
-AFTERPORTMK=	yes
-
-.include <bsd.port.mk>
-
-.undef AFTERPORTMK
