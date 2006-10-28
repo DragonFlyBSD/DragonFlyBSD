45703ffa8a891497fcc8661bc6d3f6afecb1da69
Peter Avalos <pavalos@dragonflybsd.org>
2006-10-28 20:42:54 +0000

    Add a __DECONST to bring this up to WARNS6.

--- sbin/mount/mount_ufs.c
+++ sbin/mount/mount_ufs.c
@@ -33,7 +33,7 @@
  * @(#) Copyright (c) 1993, 1994 The Regents of the University of California.  All rights reserved.
  * @(#)mount_ufs.c	8.4 (Berkeley) 4/26/95
  * $FreeBSD: src/sbin/mount/mount_ufs.c,v 1.16.2.3 2001/08/01 08:27:29 obrien Exp $
- * $DragonFly: src/sbin/mount/mount_ufs.c,v 1.8 2005/04/03 17:13:08 joerg Exp $
+ * $DragonFly: src/sbin/mount/mount_ufs.c,v 1.9 2006/10/28 20:42:54 pavalos Exp $
  */
 
 #include <sys/param.h>
@@ -87,7 +87,7 @@ mount_ufs(int argc, const char **argv)
 	if (argc != 2)
 		ufs_usage();
 
-        args.fspec = argv[0];		/* The name of the device file. */
+        args.fspec = __DECONST(char *, argv[0]);	/* The name of the device file. */
 	fs_name = argv[1];		/* The mount point. */
 
 #define DEFAULT_ROOTUID	-2
