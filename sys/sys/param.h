aefcf6d56ba2c5e54cfcde5852fc35c677efb518
Matthew Dillon <dillon@dragonflybsd.org>
2007-01-25 18:42:41 +0000

    Setup for 1.8 release - bump HEAD to 1.7.1 and synchronize preview tag.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -37,7 +37,7 @@
  *
  *	@(#)param.h	8.3 (Berkeley) 4/4/95
  * $FreeBSD: src/sys/sys/param.h,v 1.61.2.38 2003/05/22 17:12:01 fjoe Exp $
- * $DragonFly: src/sys/sys/param.h,v 1.37 2006/09/10 01:26:40 dillon Exp $
+ * $DragonFly: src/sys/sys/param.h,v 1.38 2007/01/25 18:42:41 dillon Exp $
  */
 
 #ifndef _SYS_PARAM_H_
@@ -55,9 +55,10 @@
  *	doc/en_US.ISO8859-1/books/porters-handbook/book.sgml
  *
  * 170000 - base development version after 1.6 branch
+ * 170001 - base development version before 1.8 branch
  */
 #undef __DragonFly_version
-#define __DragonFly_version 170000	/* Master, propagated to newvers */
+#define __DragonFly_version 170001	/* Master, propagated to newvers */
 
 #ifdef __FreeBSD__
 /* 
