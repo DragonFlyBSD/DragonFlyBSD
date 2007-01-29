8a8fd91695a7eb65cf37fa4295d2d9780c41f40d
Thomas E. Spanjaard <tgen@dragonflybsd.org>
2007-01-29 20:55:15 +0000

    MFC rev. 1.27:
    Fix iovec checks to match properly. iovec == MAXIOV and iovec == SMALLIOV
    are both legal in those respective statements.
    
    On-behalf-of:		Joerg Sonnenberger <joerg@britannica.bec.de>

--- sys/kern/kern_subr.c
+++ sys/kern/kern_subr.c
@@ -37,7 +37,7 @@
  *
  *	@(#)kern_subr.c	8.3 (Berkeley) 1/21/94
  * $FreeBSD: src/sys/kern/kern_subr.c,v 1.31.2.2 2002/04/21 08:09:37 bde Exp $
- * $DragonFly: src/sys/kern/kern_subr.c,v 1.26 2007/01/06 03:16:21 dillon Exp $
+ * $DragonFly: src/sys/kern/kern_subr.c,v 1.26.2.1 2007/01/29 20:55:15 tgen Exp $
  */
 
 #include "opt_ddb.h"
@@ -322,9 +322,9 @@ iovec_copyin(struct iovec *uiov, struct iovec **kiov, struct iovec *siov,
 	struct iovec *iovp;
 	int error, i;
 
-	if (iov_cnt >= UIO_MAXIOV)
+	if (iov_cnt > UIO_MAXIOV)
 		return EMSGSIZE;
-	if (iov_cnt >= UIO_SMALLIOV) {
+	if (iov_cnt > UIO_SMALLIOV) {
 		MALLOC(*kiov, struct iovec *, sizeof(struct iovec) * iov_cnt,
 		    M_IOV, M_WAITOK);
 	} else {
