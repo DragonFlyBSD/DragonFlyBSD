bf5db2e8d66cc9270de7cb7836c3ca3470a3e02a
Peter Avalos <pavalos@dragonflybsd.org>
2007-03-29 00:49:01 +0000

    Upgrade to OpenSSH 4.6p1.

--- secure/lib/libssh/version.h
+++ secure/lib/libssh/version.h
@@ -1,12 +1,12 @@
 /* $OpenBSD: version.h,v 1.46 2006/02/01 11:27:22 markus Exp $ */
-/* $DragonFly: src/secure/lib/libssh/version.h,v 1.6 2006/11/20 17:15:46 pavalos Exp $ */
+/* $DragonFly: src/secure/lib/libssh/version.h,v 1.7 2007/03/29 00:49:01 pavalos Exp $ */
 
 #ifndef SSH_VERSION
 
 #define SSH_VERSION             (ssh_version_get())
 #define SSH_RELEASE		(ssh_version_get())
-#define SSH_VERSION_BASE        "OpenSSH_4.5p1"
-#define SSH_VERSION_ADDENDUM    "DragonFly-20061120"
+#define SSH_VERSION_BASE        "OpenSSH_4.6p1"
+#define SSH_VERSION_ADDENDUM    "DragonFly-20070328"
 
 const char *ssh_version_get(void);
 void ssh_version_set_addendum(const char *add);
