4856fc30c13165452f678485ffba4a615787045b
Sepherosa Ziehau <sephe@dragonflybsd.org>
2007-04-01 14:09:00 +0000

    MFC if_ti.c:
    rev 1.43
      Don't try holding serializer in ti_rxeof().  This function is only called by
      intr routine which alrealy holds serializer.
    
    rev 1.45
      It is TIGON1 that uses NIC local memory.

--- sys/dev/netif/ti/if_ti.c
+++ sys/dev/netif/ti/if_ti.c
@@ -30,7 +30,7 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  *
  * $FreeBSD: src/sys/pci/if_ti.c,v 1.25.2.14 2002/02/15 04:20:20 silby Exp $
- * $DragonFly: src/sys/dev/netif/ti/if_ti.c,v 1.42 2006/10/25 20:55:59 dillon Exp $
+ * $DragonFly: src/sys/dev/netif/ti/if_ti.c,v 1.42.2.1 2007/04/01 14:09:00 sephe Exp $
  */
 
 /*
@@ -1696,12 +1696,10 @@ ti_rxeof(struct ti_softc *sc)
 		 * If we received a packet with a vlan tag, pass it
 		 * to vlan_input() instead of ether_input().
 		 */
-		lwkt_serialize_enter(ifp->if_serializer);
 		if (have_tag)
 			VLAN_INPUT_TAG(m, vlan_tag);
 		else
 			ifp->if_input(ifp, m);
-		lwkt_serialize_exit(ifp->if_serializer);
 	}
 
 	/* Only necessary on the Tigon 1. */
@@ -1728,7 +1726,7 @@ ti_txeof(struct ti_softc *sc)
 		uint32_t idx = 0;
 
 		idx = sc->ti_tx_saved_considx;
-		if (sc->ti_hwrev != TI_HWREV_TIGON) {
+		if (sc->ti_hwrev == TI_HWREV_TIGON) {
 			if (idx > 383)
 				CSR_WRITE_4(sc, TI_WINBASE,
 				    TI_TX_RING_BASE + 6144);
