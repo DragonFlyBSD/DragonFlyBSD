089844dff4a9db5b9b6c24677997264c7ff93ccb
Peter Avalos <pavalos@dragonflybsd.org>
2007-08-05 16:09:50 +0000

    Use the "builtin" shell function to make sure that the requested command is handled as a shell function.
    Obtained-from: FreeBSD

--- usr.bin/alias/generic.sh
+++ usr.bin/alias/generic.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $FreeBSD: src/usr.bin/alias/generic.sh,v 1.1.2.1 2002/07/19 18:20:57 wollman Exp $
-# $DragonFly: src/usr.bin/alias/generic.sh,v 1.2 2003/06/17 04:29:25 dillon Exp $
+# $FreeBSD: src/usr.bin/alias/generic.sh,v 1.2 2005/10/24 22:32:19 cperciva Exp $
+# $DragonFly: src/usr.bin/alias/generic.sh,v 1.3 2007/08/05 16:09:50 pavalos Exp $
 # This file is in the public domain.
-${0##*/} ${1+"$@"}
+builtin ${0##*/} ${1+"$@"}
