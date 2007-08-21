fc3f9779a7eaae77ebf45424093fed8e689a9c7d
Simon Schubert <corecode@dragonflybsd.org>
2007-08-21 19:40:24 +0000

    The first commit of a series of 6 commits for the amd64 port.
    This commit includes header files for amd64 cpu that are in
    sys/cpu/amd64/include.
    
    The #1 task to port for amd64 is to get the world/kernel cross-compiled
    for the 64-bit target. This involves creating a platform and cpu target
    for the build.  The skeleton infrastructure for the cpu and platform is
    the sys/cpu/amd64 directory and the sys/platform/pc64 directory.
    
    The first stage of this #1 task is to have the world cross-build.  It
    includes the building of cross toolchains, the libraries and the world.
    This series of commits makes the world cross-build (buildworld) for the
    amd64 port with these environment variables:
    TARGET_CPU=amd64; TARGET_PLATFORM=pc64; NO_GCC41=1
    
    This is the first commit: resurrect headers for sys/cpu/amd64/include from
    CVS Attic; patch and mark them as CPU specific.
    
    On-behalf-of:  Noah Yan <noah.yan@gmail.com>
    Submitted-by:  Noah Yan <noah.yan@gmail.com>

---
+++ sys/cpu/amd64/include/coredump.h
@@ -0,0 +1,14 @@
+/*
+ * $DragonFly: src/sys/cpu/amd64/include/coredump.h,v 1.1 2007/08/21 19:40:24 corecode Exp $
+ */
+
+#ifndef _CPU_COREDUMP_H_
+#define _CPU_COREDUMP_H_
+/*
+ * The pcb is augmented with machine-dependent additional data for
+ * core dumps. For the i386: ???
+ */
+struct md_coredump {
+};
+
+#endif
---
+++ sys/cpu/amd64/include/math.h
@@ -0,0 +1,12 @@
+/* 
+ * $NetBSD: math.h,v 1.2 2003/10/28 00:55:28 kleink Exp $
+ * $DragonFly: src/sys/cpu/amd64/include/math.h,v 1.1 2007/08/21 19:40:24 corecode Exp $
+ */
+
+#ifndef _CPU_MATH_H_
+#define _CPU_MATH_H_
+
+#define	__HAVE_LONG_DOUBLE
+#define	__HAVE_NANF
+
+#endif
