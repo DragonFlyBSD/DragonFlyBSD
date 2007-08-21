a888a18d54da6949cef7c7ea8e34667fa1b82be1
Simon Schubert <corecode@dragonflybsd.org>
2007-08-21 20:04:09 +0000

    Define macros also for amd64.
    On-behalf-of:  Noah Yan <noah.yan@gmail.com>
    Submitted-by:  Noah Yan <noah.yan@gmail.com>

--- sys/sys/disklabel32.h
+++ sys/sys/disklabel32.h
@@ -32,7 +32,7 @@
  *
  *	@(#)disklabel.h	8.2 (Berkeley) 7/10/94
  * $FreeBSD: src/sys/sys/disklabel.h,v 1.49.2.7 2001/05/27 05:58:26 jkh Exp $
- * $DragonFly: src/sys/sys/disklabel32.h,v 1.27 2007/06/18 05:13:42 dillon Exp $
+ * $DragonFly: src/sys/sys/disklabel32.h,v 1.28 2007/08/21 20:04:09 corecode Exp $
  */
 
 #ifndef _SYS_DISKLABEL32_H_
@@ -56,7 +56,7 @@
  */
 
 /* XXX these should be defined per controller (or drive) elsewhere, not here! */
-#ifdef __i386__
+#if defined(__i386__) || defined(__amd64__) 
 #define LABELSECTOR32	1			/* sector containing label */
 #define LABELOFFSET32	0			/* offset of label in sector */
 #endif
