b94e35031c7ff5a0806afe7eda7d2a1c1aeae45f
Matthew Dillon <dillon@dragonflybsd.org>
2007-11-01 22:05:23 +0000

    Add a HAMMER kernel build option, add a VFS type for HAMMER, add a file type for key-access (database) files.

--- sys/sys/vfscache.h
+++ sys/sys/vfscache.h
@@ -63,7 +63,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $DragonFly: src/sys/sys/vfscache.h,v 1.10 2007/08/13 17:49:16 dillon Exp $
+ * $DragonFly: src/sys/sys/vfscache.h,v 1.11 2007/11/01 22:05:22 dillon Exp $
  */
 /*
  * This module serves as a focal point for virtually all filesystem and
@@ -97,7 +97,8 @@
 /*
  * Vnode types.  VNON means no type.
  */
-enum vtype	{ VNON, VREG, VDIR, VBLK, VCHR, VLNK, VSOCK, VFIFO, VBAD };
+enum vtype	{ VNON, VREG, VDIR, VBLK, VCHR, VLNK, VSOCK, VFIFO, VBAD,
+		  VDATABASE };
 
 /*
  * Vnode tag types.
@@ -109,7 +110,7 @@ enum vtagtype	{
 	VT_PORTAL, VT_NULL, VT_UNUSED10, VT_KERNFS, VT_PROCFS, VT_AFS,
 	VT_ISOFS, VT_UNION, VT_MSDOSFS, VT_TFS, VT_VFS, VT_CODA, VT_NTFS,
 	VT_HPFS, VT_NWFS, VT_SMBFS, VT_UDF, VT_EXT2FS, VT_SYNTH,
-	VT_USERFS
+	VT_USERFS, VT_HAMMER
 };
 
 /*
