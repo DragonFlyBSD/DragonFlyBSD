a0d4dc35219bdaab957cb476d1c42565cf26086d
Peter Avalos <pavalos@dragonflybsd.org>
2007-11-22 15:53:16 +0000

    Mark some functions as __printflike().
    Obtained-from: FreeBSD

--- sys/sys/camlib.h
+++ sys/sys/camlib.h
@@ -23,7 +23,7 @@
  * SUCH DAMAGE.
  *
  * $FreeBSD: src/lib/libcam/camlib.h,v 1.2 1999/08/28 00:04:06 peter Exp $
- * $DragonFly: src/sys/sys/camlib.h,v 1.3 2006/05/21 03:43:47 dillon Exp $
+ * $DragonFly: src/sys/sys/camlib.h,v 1.4 2007/11/22 15:53:16 pavalos Exp $
  */
 /*
  * Buffer encoding/decoding routines taken from the original FreeBSD SCSI
@@ -158,11 +158,11 @@ int			cam_get_device(const char *path, char *dev_name,
 /*
  * Buffer encoding/decoding routines, from the old SCSI library.
  */
-int csio_decode(struct ccb_scsiio *csio, char *fmt, ...);
+int csio_decode(struct ccb_scsiio *csio, char *fmt, ...) __printflike(2, 3);
 int csio_decode_visit(struct ccb_scsiio *csio, char *fmt,
 		      void (*arg_put)(void *, int, void *, int, char *),
 		      void *puthook);
-int buff_decode(u_int8_t *buff, size_t len, char *fmt, ...);
+int buff_decode(u_int8_t *buff, size_t len, char *fmt, ...) __printflike(3, 4);
 int buff_decode_visit(u_int8_t *buff, size_t len, char *fmt,
 		      void (*arg_put)(void *, int, void *, int, char *),
 		      void *puthook);
@@ -174,7 +174,7 @@ int csio_build_visit(struct ccb_scsiio *csio, u_int8_t *data_ptr,
 		     int timeout, char *cmd_spec,
 		     int (*arg_get)(void *hook, char *field_name),
 		     void *gethook);
-int csio_encode(struct ccb_scsiio *csio, char *fmt, ...);
+int csio_encode(struct ccb_scsiio *csio, char *fmt, ...) __printflike(2, 3);
 int buff_encode_visit(u_int8_t *buff, size_t len, char *fmt,
 		      int (*arg_get)(void *hook, char *field_name),
 		      void *gethook);
