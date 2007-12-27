9765f86eac5eb21eec90ac77cece8900107c49ba
Matthias Schmidt <matthias@dragonflybsd.org>
2007-12-27 11:52:10 +0000

    o Change my personal copyright to the DragonFly copyright o Add a HISTORY entry to the man page

--- usr.bin/pkg_search/pkg_search.sh
+++ usr.bin/pkg_search/pkg_search.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 #
-# Copyright (c) 2006-07 Matthias Schmidt <schmidtm@mathematik.uni-marburg.de>
+# Copyright (c) 2007 The DragonFly Project.  All rights reserved.
+#
+# This code is derived from software contributed to The DragonFly Project
+# by Matthias Schmidt <schmidtm@mathematik.uni-marburg.de>, University of
+# Marburg.
 #
 # All rights reserved.
 #
@@ -12,6 +16,9 @@
 # - Redistributions in binary form must reproduce the above copyright notice, 
 #   this list of conditions and the following disclaimer in the documentation 
 #   and/or other materials provided with the distribution.
+# - Neither the name of The DragonFly Project nor the names of its
+#   contributors may be used to endorse or promote products derived
+#   from this software without specific, prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -25,7 +32,7 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# $DragonFly: src/usr.bin/pkg_search/pkg_search.sh,v 1.3 2007/12/13 17:14:57 swildner Exp $
+# $DragonFly: src/usr.bin/pkg_search/pkg_search.sh,v 1.4 2007/12/27 11:52:10 matthias Exp $
 
 UNAME=`uname -s`
 VERSION=`uname -r | cut -d '.' -f 1,2`
