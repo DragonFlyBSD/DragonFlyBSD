650da96a8bb8cbaafa16dde39a996b5b8faf0de5
Simon Schubert <corecode@dragonflybsd.org>
2008-01-05 13:37:15 +0000

    Add support for newer CPU models.
    Obtained-from:  FreeBSD

--- share/mk/bsd.cpu.gcc41.mk
+++ share/mk/bsd.cpu.gcc41.mk
@@ -1,14 +1,14 @@
-# $DragonFly: src/share/mk/bsd.cpu.gcc41.mk,v 1.3 2007/01/19 10:38:59 corecode Exp $
+# $DragonFly: src/share/mk/bsd.cpu.gcc41.mk,v 1.4 2008/01/05 13:37:15 corecode Exp $
 
 # Set default CPU compile flags and baseline CPUTYPE for each arch.  The
 # compile flags must support the minimum CPU type for each architecture but
 # may tune support for more advanced processors.
 
 .if !defined(CPUTYPE) || empty(CPUTYPE)
+_CPUCFLAGS =
 . if ${MACHINE_ARCH} == "i386"
-_CPUCFLAGS = -mtune=pentiumpro
 MACHINE_CPU = i486
-.elif ${MACHINE_ARCH} == "amd64"
+. elif ${MACHINE_ARCH} == "amd64"
 MACHINE_CPU = amd64 sse2 sse
 . endif
 .else
@@ -17,87 +17,115 @@ MACHINE_CPU = amd64 sse2 sse
 # between e.g. i586 and pentium)
 
 . if ${MACHINE_ARCH} == "i386"
-.  if ${CPUTYPE} == "pentiumpro"
-CPUTYPE = i686
-.  elif ${CPUTYPE} == "pentium"
-CPUTYPE = i586
+.  if ${CPUTYPE} == "nocona"
+CPUTYPE = prescott
+.  elif ${CPUTYPE} == "core" || ${CPUTYPE} == "core2"
+CPUTYPE = prescott
+.  elif ${CPUTYPE} == "p4"
+CPUTYPE = pentium4
+.  elif ${CPUTYPE} == "p4m"
+CPUTYPE = pentium4m
+.  elif ${CPUTYPE} == "p3"
+CPUTYPE = pentium3
+.  elif ${CPUTYPE} == "p3m"
+CPUTYPE = pentium3m
+.  elif ${CPUTYPE} == "p-m"
+CPUTYPE = pentium-m
+.  elif ${CPUTYPE} == "p2"
+CPUTYPE = pentium2
+.  elif ${CPUTYPE} == "i686"
+CPUTYPE = pentiumpro
+.  elif ${CPUTYPE} == "i586/mmx"
+CPUTYPE = pentium-mmx
+.  elif ${CPUTYPE} == "i586"
+CPUTYPE = pentium
+.  elif ${CPUTYPE} == "opteron" || ${CPUTYPE} == "athlon64" || \
+     ${CPUTYPE} == "k8"
+CPUTYPE = athlon-mp
 .  elif ${CPUTYPE} == "k7"
 CPUTYPE = athlon
 .  endif
+. elif ${MACHINE_ARCH} == "amd64"
+.  if ${CPUTYPE} == "prescott" || ${CPUTYPE} == "core2"
+CPUTYPE = nocona
+.  endif
 . endif
 
+###############################################################################
 # Logic to set up correct gcc optimization flag.  This must be included
 # after /etc/make.conf so it can react to the local value of CPUTYPE
 # defined therein.  Consult:
-#	http://gcc.gnu.org/onlinedocs/gcc/i386-and-x86-64-Options.html
-#	http://gcc.gnu.org/onlinedocs/gcc/SPARC-Options.html
+#	http://gcc.gnu.org/onlinedocs/gcc/ARM-Options.html
+#	http://gcc.gnu.org/onlinedocs/gcc/IA-64-Options.html
 #	http://gcc.gnu.org/onlinedocs/gcc/RS-6000-and-PowerPC-Options.html
+#	http://gcc.gnu.org/onlinedocs/gcc/SPARC-Options.html
+#	http://gcc.gnu.org/onlinedocs/gcc/i386-and-x86-64-Options.html
 
 . if ${MACHINE_ARCH} == "i386"
-.  if ${CPUTYPE} == "k8" || ${CPUTYPE} == "opteron" || ${CPUTYPE} == "athlon64" || ${CPUTYPE} == "athlon-fx"
-_CPUCFLAGS = -march=${CPUTYPE}
-.  elif ${CPUTYPE} == "athlon-mp" || ${CPUTYPE} == "athlon-xp" || ${CPUTYPE} == "athlon-4" || ${CPUTYPE} == "athlon-tbird" || ${CPUTYPE} == "athlon"
-_CPUCFLAGS = -march=${CPUTYPE}
-.  elif ${CPUTYPE} == "k6-3" || ${CPUTYPE} == "k6-2" || ${CPUTYPE} == "k6"
-_CPUCFLAGS = -march=${CPUTYPE}
+.  if ${CPUTYPE} == "crusoe"
+_CPUCFLAGS = -march=i686 -falign-functions=0 -falign-jumps=0 -falign-loops=0
 .  elif ${CPUTYPE} == "k5"
 _CPUCFLAGS = -march=pentium
-.  elif ${CPUTYPE} == "p4"
-_CPUCFLAGS = -march=pentium4
-.  elif ${CPUTYPE} == "pentium-m"
-_CPUCFLAGS = -march=pentium-m
-.  elif ${CPUTYPE} == "p3"
-_CPUCFLAGS = -march=pentium3
-.  elif ${CPUTYPE} == "p2"
-_CPUCFLAGS = -march=pentium2
-.  elif ${CPUTYPE} == "i686"
-_CPUCFLAGS = -march=pentiumpro
-.  elif ${CPUTYPE} == "i586/mmx"
-_CPUCFLAGS = -march=pentium-mmx
-.  elif ${CPUTYPE} == "i586"
-_CPUCFLAGS = -march=pentium
-.  elif ${CPUTYPE} == "i486"
-_CPUCFLAGS = -march=i486
-.  endif
+.  else
+_CPUCFLAGS = -march=${CPUTYPE}
+.  endif # GCC on 'i386'
+. elif ${MACHINE_ARCH} == "amd64"
+_CPUCFLAGS = -march=${CPUTYPE}
 . endif
 
 # Set up the list of CPU features based on the CPU type.  This is an
 # unordered list to make it easy for client makefiles to test for the
 # presence of a CPU feature.
 
-.if ${MACHINE_ARCH} == "i386"
-. if ${CPUTYPE} == "k8" || ${CPUTYPE} == "opteron" || ${CPUTYPE} == "athlon64" || ${CPUTYPE} == "athlon-fx"
-MACHINE_CPU = athlon-xp k7 3dnow sse mmx k6 k5 i586 i486 i386
-. elif ${CPUTYPE} == "athlon-mp" || ${CPUTYPE} == "athlon-xp" || ${CPUTYPE} == "athlon-4"
-MACHINE_CPU = athlon-xp k7 3dnow sse mmx k6 k5 i586 i486 i386
-. elif ${CPUTYPE} == "athlon" || ${CPUTYPE} == "athlon-tbird"
+. if ${MACHINE_ARCH} == "i386"
+.  if ${CPUTYPE} == "opteron" || ${CPUTYPE} == "athlon64"
+MACHINE_CPU = athlon-xp athlon k7 3dnow sse2 sse mmx k6 k5 i586 i486 i386
+.  elif ${CPUTYPE} == "athlon-mp" || ${CPUTYPE} == "athlon-xp" || \
+    ${CPUTYPE} == "athlon-4"
+MACHINE_CPU = athlon-xp athlon k7 3dnow sse mmx k6 k5 i586 i486 i386
+.  elif ${CPUTYPE} == "athlon" || ${CPUTYPE} == "athlon-tbird"
 MACHINE_CPU = athlon k7 3dnow mmx k6 k5 i586 i486 i386
-. elif ${CPUTYPE} == "k6-3" || ${CPUTYPE} == "k6-2"
+.  elif ${CPUTYPE} == "k6-3" || ${CPUTYPE} == "k6-2"
 MACHINE_CPU = 3dnow mmx k6 k5 i586 i486 i386
 .  elif ${CPUTYPE} == "k6"
 MACHINE_CPU = mmx k6 k5 i586 i486 i386
 .  elif ${CPUTYPE} == "k5"
 MACHINE_CPU = k5 i586 i486 i386
-.  elif ${CPUTYPE} == "p4" || ${CPUTYPE} == "pentium-m"
+.  elif ${CPUTYPE} == "c3"
+MACHINE_CPU = 3dnow mmx i586 i486 i386
+.  elif ${CPUTYPE} == "c3-2"
+MACHINE_CPU = sse mmx i586 i486 i386
+.  elif ${CPUTYPE} == "prescott"
+MACHINE_CPU = sse3 sse2 sse i686 mmx i586 i486 i386
+.  elif ${CPUTYPE} == "pentium4" || ${CPUTYPE} == "pentium4m" || ${CPUTYPE} == "pentium-m"
 MACHINE_CPU = sse2 sse i686 mmx i586 i486 i386
-.  elif ${CPUTYPE} == "p3"
+.  elif ${CPUTYPE} == "pentium3" || ${CPUTYPE} == "pentium3m"
 MACHINE_CPU = sse i686 mmx i586 i486 i386
-.  elif ${CPUTYPE} == "p2"
+.  elif ${CPUTYPE} == "pentium2"
 MACHINE_CPU = i686 mmx i586 i486 i386
-.  elif ${CPUTYPE} == "i686"
+.  elif ${CPUTYPE} == "pentiumpro"
 MACHINE_CPU = i686 i586 i486 i386
-.  elif ${CPUTYPE} == "i586/mmx"
+.  elif ${CPUTYPE} == "pentium-mmx"
 MACHINE_CPU = mmx i586 i486 i386
-.  elif ${CPUTYPE} == "i586"
+.  elif ${CPUTYPE} == "pentium"
 MACHINE_CPU = i586 i486 i386
 .  elif ${CPUTYPE} == "i486"
 MACHINE_CPU = i486 i386
 .  elif ${CPUTYPE} == "i386"
 MACHINE_CPU = i386
-.  else
-.warning Unknown CPUTYPE=${CPUTYPE}
 .  endif
 . elif ${MACHINE_ARCH} == "amd64"
-MACHINE_CPU = amd64 sse2 sse
+.  if ${CPUTYPE} == "opteron" || ${CPUTYPE} == "athlon64" || ${CPUTYPE} == "k8"
+MACHINE_CPU = k8 3dnow
+.  elif ${CPUTYPE} == "nocona"
+MACHINE_CPU = sse3
+.  endif
+MACHINE_CPU += amd64 sse2 sse mmx
 . endif
+.endif
+
+# NB: COPTFLAGS is handled in /usr/src/sys/conf/kern.pre.mk
+
+.if !defined(NO_CPU_CFLAGS)
+CFLAGS += ${_CPUCFLAGS}
 .endif
