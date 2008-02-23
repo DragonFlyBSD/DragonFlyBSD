3df22e805d7616a3a431c8227d444f4181bb561c
Sascha Wildner <swildner@dragonflybsd.org>
2008-02-23 00:59:23 +0000

    Finally, re-add an #endif I removed. :)

--- sys/sys/varsym.h
+++ sys/sys/varsym.h
@@ -3,7 +3,7 @@
  *
  *	Implements structures used for variant symlink support.
  * 
- * $DragonFly: src/sys/sys/varsym.h,v 1.6 2008/02/23 00:42:44 swildner Exp $
+ * $DragonFly: src/sys/sys/varsym.h,v 1.7 2008/02/23 00:59:23 swildner Exp $
  */
 
 #ifndef _SYS_VARSYM_H_
@@ -59,5 +59,7 @@ void	varsymdrop(varsym_t var);
 void	varsymset_init(struct varsymset *varsymset, struct varsymset *copy);
 void	varsymset_clean(struct varsymset *varsymset);
 int	varsymreplace(char *cp, int linklen, int maxlen);
+
+#endif	/* _KERNEL */
 
 #endif
