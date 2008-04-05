e0abe7925fd1a135c0fb17956e1212b515630f7d
Sascha Wildner <swildner@dragonflybsd.org>
2008-04-05 22:10:14 +0000

    Ansify a bit.
    I've left out raising WARNS since there are still warnings left.
    
    Submitted-by:  Kevin L. Kane <kevin.kane@gmail.com>
    Dragonfly-bug: <https://bugs.dragonflybsd.org/issue77>

--- usr.bin/lex/lib/libmain.c
+++ usr.bin/lex/lib/libmain.c
@@ -2,17 +2,15 @@
  *
  * $Header: /home/daffy/u0/vern/flex/RCS/libmain.c,v 1.4 95/09/27 12:47:55 vern Exp $
  * $FreeBSD: src/usr.bin/lex/lib/libmain.c,v 1.3 1999/10/27 07:56:49 obrien Exp $
- * $DragonFly: src/usr.bin/lex/lib/libmain.c,v 1.2 2003/06/17 04:29:28 dillon Exp $
+ * $DragonFly: src/usr.bin/lex/lib/libmain.c,v 1.3 2008/04/05 22:10:14 swildner Exp $
  */
 
 extern int yylex();
 
-int main( argc, argv )
-int argc;
-char *argv[];
-	{
+int main(int argc, char *argv[])
+{
 	while ( yylex() != 0 )
 		;
 
 	return 0;
-	}
+}
