e73982fa092f73456cdd69c9af55aa7d2a2365e8
Matthew Dillon <dillon@dragonflybsd.org>
2008-07-18 00:13:23 +0000

    MFC 1.45 for the 2.0 release: Properly set B_INVAL after failed read.

--- sys/vfs/nfs/nfs_bio.c
+++ sys/vfs/nfs/nfs_bio.c
@@ -35,7 +35,7 @@
  *
  *	@(#)nfs_bio.c	8.9 (Berkeley) 3/30/95
  * $FreeBSD: /repoman/r/ncvs/src/sys/nfsclient/nfs_bio.c,v 1.130 2004/04/14 23:23:55 peadar Exp $
- * $DragonFly: src/sys/vfs/nfs/nfs_bio.c,v 1.43.2.1 2008/07/14 17:46:41 dillon Exp $
+ * $DragonFly: src/sys/vfs/nfs/nfs_bio.c,v 1.43.2.2 2008/07/18 00:13:23 dillon Exp $
  */
 
 
@@ -528,7 +528,7 @@ again:
 		    vfs_busy_pages(vp, bp);
 		    error = nfs_doio(vp, &bp->b_bio2, td);
 		    if (error) {
-			bp->b_flags |= B_ERROR;
+			bp->b_flags |= B_ERROR | B_INVAL;
 			brelse(bp);
 			return (error);
 		    }
