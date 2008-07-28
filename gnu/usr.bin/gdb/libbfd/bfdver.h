643b986cd4a8b9ad89d89700042c6c78dfb9968a
Simon Schubert <corecode@dragonflybsd.org>
2008-07-28 22:33:21 +0000

    Allow gdb to be cross-compiled.
    If you compile gdb with TARGET_ARCH set, it will yield a on the
    host runnable gdb which can debug ${TARGET_ARCH}.
    
    Pay attention not to actually install this binary, or you won't have
    a host debugger anymore.  Consider this a developer-only feature for now.
    
    Requested-by:  Jordan Gordeev

--- gnu/usr.bin/gdb/libbfd/bfdver.h
+++
@@ -1,5 +0,0 @@
-/* $DragonFly: src/gnu/usr.bin/gdb/libbfd/Attic/bfdver.h,v 1.2 2008/01/14 21:36:38 corecode Exp $ */
-#define BFD_VERSION_DATE 20070907
-#define BFD_VERSION 218500000
-#define BFD_VERSION_STRING  "(GNU Binutils)" "2.18.50.20070907"
-#define REPORT_BUGS_TO "<http://bugs.dragonflybsd.org/>"
--- gnu/usr.bin/gdb/libgdb/version.c
+++ gnu/usr.bin/gdb/libgdb/version.c
@@ -1,5 +1,5 @@
-/* $DragonFly: src/gnu/usr.bin/gdb/libgdb/version.c,v 1.2 2008/01/14 21:36:38 corecode Exp $ */
+/* $DragonFly: src/gnu/usr.bin/gdb/libgdb/version.c,v 1.3 2008/07/28 22:33:21 corecode Exp $ */
 #include "version.h"
 const char version[] = "6.7.1";
 const char host_name[] = MACHINE_ARCH"-dragonfly";
-const char target_name[] = MACHINE_ARCH"-dragonfly";
+const char target_name[] = TARGET_ARCH"-dragonfly";
