77d51bac35d8bf797a1398dc03df262422a643f3
Joerg Sonnenberger <joerg@dragonflybsd.org>
2008-08-11 00:04:12 +0000

    _PATH_MKDIR is unused, nuke it.

--- usr.bin/patch/pathnames.h
+++ usr.bin/patch/pathnames.h
@@ -1,6 +1,6 @@
 /*
  * $OpenBSD: pathnames.h,v 1.1 2003/07/29 20:10:17 millert Exp $
- * $DragonFly: src/usr.bin/patch/pathnames.h,v 1.1 2004/09/24 18:44:28 joerg Exp $
+ * $DragonFly: src/usr.bin/patch/pathnames.h,v 1.2 2008/08/11 00:04:12 joerg Exp $
  */
 
 /*
@@ -11,4 +11,3 @@
 #include <paths.h>
 
 #define	_PATH_ED		"/bin/ed"
-#define	_PATH_MKDIR		"/bin/mkdir"
