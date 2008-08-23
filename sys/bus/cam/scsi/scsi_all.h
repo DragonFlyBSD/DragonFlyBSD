3a33d431f8c59fba5a6e57f8aac858ed366e8219
Peter Avalos <pavalos@dragonflybsd.org>
2008-08-23 22:27:27 +0000

    Update SCSI opcodes and ASCs.
    http://www.t10.org/lists/1spc-lst.htm
    
    Note opcodes for scanner and communication devices are taken from the
    previous revision because they are not listed in the files any more.  Also,
    note newly added ASCs are all marked with 'XXX TBD' and take SS_RDEF action
    for now.  Some ASCs need SS_TUR for error recovery or SS_FATAL to prevent
    further retrials.  We should deal with them later.
    
    Obtained-from: FreeBSD

--- sys/bus/cam/scsi/scsi_all.h
+++ sys/bus/cam/scsi/scsi_all.h
@@ -14,8 +14,8 @@
  *
  * Ported to run under 386BSD by Julian Elischer (julian@tfs.com) Sept 1992
  *
- * $FreeBSD: src/sys/cam/scsi/scsi_all.h,v 1.14.2.5 2003/08/24 03:26:37 ken Exp $
- * $DragonFly: src/sys/bus/cam/scsi/scsi_all.h,v 1.14 2008/05/18 20:30:19 pavalos Exp $
+ * $FreeBSD: src/sys/cam/scsi/scsi_all.h,v 1.31 2008/08/07 17:25:05 jkim Exp $
+ * $DragonFly: src/sys/bus/cam/scsi/scsi_all.h,v 1.15 2008/08/23 22:27:27 pavalos Exp $
  */
 
 /*
@@ -34,7 +34,7 @@
 
 #ifdef _KERNEL
 /*
- * This is the number of seconds we wait for devices to settle after a SCSI
+ * This is the number of milliseconds we wait for devices to settle after a SCSI
  * bus reset.
  */
 extern int scsi_delay;
@@ -618,24 +618,26 @@ struct ata_pass_16 {
 /*
  * Device Types
  */
-#define T_DIRECT	0x00
-#define T_SEQUENTIAL	0x01
-#define T_PRINTER	0x02
-#define T_PROCESSOR	0x03
-#define T_WORM		0x04
-#define T_CDROM		0x05
-#define T_SCANNER 	0x06
-#define T_OPTICAL 	0x07
-#define T_CHANGER	0x08
-#define T_COMM		0x09
-#define T_ASC0		0x0a
-#define T_ASC1		0x0b
+#define	T_DIRECT	0x00
+#define	T_SEQUENTIAL	0x01
+#define	T_PRINTER	0x02
+#define	T_PROCESSOR	0x03
+#define	T_WORM		0x04
+#define	T_CDROM		0x05
+#define	T_SCANNER	0x06
+#define	T_OPTICAL 	0x07
+#define	T_CHANGER	0x08
+#define	T_COMM		0x09
+#define	T_ASC0		0x0a
+#define	T_ASC1		0x0b
 #define	T_STORARRAY	0x0c
 #define	T_ENCLOSURE	0x0d
 #define	T_RBC		0x0e
 #define	T_OCRW		0x0f
-#define T_NODEVICE	0x1F
-#define	T_ANY		0xFF	/* Used in Quirk table matches */
+#define	T_OSD		0x11
+#define	T_ADC		0x12
+#define	T_NODEVICE	0x1f
+#define	T_ANY		0xff	/* Used in Quirk table matches */
 
 #define T_REMOV		1
 #define	T_FIXED		0
@@ -1000,7 +1002,7 @@ struct asc_table_entry {
 
 struct op_table_entry {
 	u_int8_t    opcode;
-	u_int16_t   opmask;
+	u_int32_t   opmask;
 	const char  *desc;
 };
 
