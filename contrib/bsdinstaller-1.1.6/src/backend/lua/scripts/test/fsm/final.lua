9832e62f1476f8b897c00914cfdc933aad57c84b
Sascha Wildner <swildner@dragonflybsd.org>
2008-09-28 17:33:35 +0000

    Remove Lua backend and add README.DELETED.

--- contrib/bsdinstaller-1.1.6/src/backend/lua/scripts/test/fsm/final.lua
+++
@@ -1,9 +0,0 @@
--- $Id: final.lua,v 1.4 2004/11/26 00:31:03 cpressey Exp $
-
-return {
-    name = "final",
-    action = function(fsm)
-	print("Finally, this is the end!")
-	return nil
-    end
-}
--- contrib/bsdinstaller-1.1.6/src/backend/lua/scripts/test/fsm/start.lua
+++
@@ -1,9 +0,0 @@
--- $Id: start.lua,v 1.4 2004/11/26 00:31:03 cpressey Exp $
-
-return {
-    name = "start",
-    action = function(fsm)
-	print("This is the start.")
-	return "middle"
-    end
-}
