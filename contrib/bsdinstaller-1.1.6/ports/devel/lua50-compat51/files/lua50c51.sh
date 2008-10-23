4d14f86786eb0f06ee69ee59c1bc8842ff1f5cdc
Sascha Wildner <swildner@dragonflybsd.org>
2008-10-23 16:56:23 +0000

    Remove more stuff: ports/, root_skels/ (which we maintain in nrelease/), scripts/ and src/lib/lua.

--- contrib/bsdinstaller-1.1.6/ports/devel/lua50-compat51/files/lua50c51.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec /usr/local/bin/lua50 -l/usr/local/share/lua/5.0/compat-5.1.lua $*
--- contrib/bsdinstaller-1.1.6/src/lib/lua/pty/pty.lua
+++
@@ -1,8 +0,0 @@
--- $Id: pty.lua,v 1.1 2005/04/03 19:52:01 cpressey Exp $
--- Lua wrapper functions for Lua 5.0.x Pty (pseudo-terminal) binding.
-
-module("pty")
-
-Pty = require("lpty")
-
-return Pty
