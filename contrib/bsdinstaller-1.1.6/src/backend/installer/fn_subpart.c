8e219cc85f0e12aa11bbf2b4e0b97f7f4ad4d3f9
Matthias Schmidt <matthias@dragonflybsd.org>
2009-01-03 14:16:58 +0100

    Fix a typo.  Actually is 16, not 12

--- contrib/bsdinstaller-1.1.6/src/backend/installer/fn_subpart.c
+++ contrib/bsdinstaller-1.1.6/src/backend/installer/fn_subpart.c
@@ -784,7 +784,7 @@ fn_create_subpartitions(struct i_fn_args *a)
 	 * it must be in the form "adXsY".
 	 */
 	if (use_hammer == 1) {
-		command_add(cmds, "%s%s if=/dev/zero of=/dev/%s bs=32k count=12",
+		command_add(cmds, "%s%s if=/dev/zero of=/dev/%s bs=32k count=16",
 		    a->os_root, cmd_name(a, "DD"),
 		    slice_get_raw_device_name(storage_get_selected_slice(a->s)));
 		command_add(cmds, "%s%s -B -r -w %s auto",
@@ -794,7 +794,7 @@ fn_create_subpartitions(struct i_fn_args *a)
 		commands_free(cmds);
 		fn_create_subpartitions_hammer(a);
 	} else {
-		command_add(cmds, "%s%s if=/dev/zero of=/dev/%s bs=32k count=12",
+		command_add(cmds, "%s%s if=/dev/zero of=/dev/%s bs=32k count=16",
 		    a->os_root, cmd_name(a, "DD"),
 		    slice_get_raw_device_name(storage_get_selected_slice(a->s)));
 		command_add(cmds, "%s%s -B -r -w %s auto",
