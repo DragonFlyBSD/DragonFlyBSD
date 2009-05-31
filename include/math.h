00d61692ffc57ec276cfda6bc737e9bbdfb97d3a
Stathis Kamperis <beket@dragonflybsd.org>
2009-05-31 23:35:54 +0300

    libm: Import fmax{,f,l} and fmin{,f,l} functions from FreeBSD.

--- include/math.h
+++ include/math.h
@@ -320,6 +320,15 @@ float	nanf(const char *);
 long double	nanl(const char *);
 float	nextafterf(float, float);
 
+/* 7.12.12 maximum, minimum, positive difference */
+double	fmax(double, double);
+float	fmaxf(float, float);
+long double	fmaxl(long double, long double);
+
+double  fmin(double, double);
+float   fminf(float, float);
+long double     fminl(long double, long double);
+
 #endif /* __ISO_C_VISIBLE >= 1999 */
 
 #if __ISO_C_VISIBLE >= 1999
