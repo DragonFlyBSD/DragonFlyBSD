c23609ad82e291baab33f00278178779488e60ce
Matthew Dillon <dillon@apollo.backplane.com>
2009-07-13 14:11:11 -0700

    pipe - add required critical section around tsleep_interlock() call.

--- sys/kern/sys_pipe.c
+++ sys/kern/sys_pipe.c
@@ -605,9 +605,11 @@ pipe_read(struct file *fp, struct uio *uio, struct ucred *cred, int fflags)
 		 * are held.
 		 */
 		rpipe->pipe_state |= PIPE_WANTR;
+		crit_enter();
 		tsleep_interlock(rpipe);
 		lwkt_reltoken(&wlock);
 		error = tsleep(rpipe, PCATCH, "piperd", 0);
+		crit_exit();
 		++pipe_rblocked_count;
 		if (error)
 			break;
