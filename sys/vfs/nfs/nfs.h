f88ac120b52b5198f44339ecc84e18fb29786d69
Matthew Dillon <dillon@apollo.backplane.com>
2009-07-14 23:00:52 -0700

    NFS - Make the NFS read-ahead defaults & limits more generous.

--- sys/vfs/nfs/nfs.h
+++ sys/vfs/nfs/nfs.h
@@ -82,10 +82,10 @@
 #define	NFS_WSIZE	8192		/* Def. write data size <= 8192 */
 #define	NFS_RSIZE	8192		/* Def. read data size <= 8192 */
 #define NFS_READDIRSIZE	8192		/* Def. readdir size */
-#define	NFS_DEFRAHEAD	1		/* Def. read ahead # blocks */
-#define	NFS_MAXRAHEAD	4		/* Max. read ahead # blocks */
+#define	NFS_DEFRAHEAD	4		/* Def. read ahead # blocks */
+#define	NFS_MAXRAHEAD	32		/* Max. read ahead # blocks */
 #define	NFS_MAXUIDHASH	64		/* Max. # of hashed uid entries/mp */
-#define	NFS_MAXASYNCDAEMON 	20	/* Max. number async_daemons runnable */
+#define	NFS_MAXASYNCDAEMON 	64	/* Max. number async_daemons runnable */
 #define NFS_MAXGATHERDELAY	100	/* Max. write gather delay (msec) */
 #ifndef NFS_GATHERDELAY
 #define NFS_GATHERDELAY		20	/* Default write gather delay (msec) */
