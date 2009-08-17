DragonFlyBSD/DragonFlyBSD

commit 7ba1363dc2de5d5270486f696e0444e69cd2bf99

disk/fd - fix panics
* Call disk_setdiskinfo() on startup.

* Adjust kernel to not panic if a disk device is opened before
  disk_setdiskinfo() has been called.

sys/dev/disk/fd/fd.c
@@ -1008,7 +1008,9 @@ fd_probe(device_t dev)
 static int
 fd_attach(device_t dev)
 {
-	struct	fd_data *fd;
+	struct disk_info info;
+	struct fd_data *fd;
+	struct fd_type *ft;
 
 	fd = device_get_softc(dev);
 
@@ -1047,6 +1049,19 @@ fd_attach(device_t dev)
 			  device_get_unit(dev), 512, DEVSTAT_NO_ORDERED_TAGS,
 			  DEVSTAT_TYPE_FLOPPY | DEVSTAT_TYPE_IF_OTHER,
 			  DEVSTAT_PRIORITY_FD);
+
+	if (fd->type != NO_TYPE) {
+		bzero(&info, sizeof(info));
+		ft = &fd_types[fd->type - 1];
+		info.d_media_blksize = 128 << ft->secsize;
+		info.d_media_blocks = ft->size;
+		info.d_dsflags = DSO_COMPATPARTA | DSO_COMPATMBR;
+		info.d_nheads = ft->heads;
+		info.d_secpertrack = ft->sectrac;
+		info.d_secpercyl = ft->sectrac * ft->heads;
+		info.d_ncylinders = ft->size / info.d_secpercyl;
+		disk_setdiskinfo(&fd->disk, &info);
+	}
 	return (0);
 }
 

sys/kern/subr_disk.c
@@ -657,9 +657,14 @@ diskopen(struct dev_open_args *ap)
 
 	/*
 	 * dp can't be NULL here XXX.
+	 *
+	 * d_slice will be NULL if setdiskinfo() has not been called yet.
+	 * setdiskinfo() is typically called whether the disk is present
+	 * or not (e.g. CD), but the base disk device is created first
+	 * and there may be a race.
 	 */
 	dp = dev->si_disk;
-	if (dp == NULL)
+	if (dp == NULL || dp->d_slice == NULL)
 		return (ENXIO);
 	error = 0;
 

commit cfa47be2c68f263beefed8602f21a4502ad4e426

fd - Fix panic due to device_busy
* Fix panic introduced by 7ba1363dc2de5d52704, caused by a too
  early call to device_busy, before the device is marked as
  attached.

NOTE: This might have yet unknown repercussions on the use of
      floppies.

Suggested-By: Matthew Dillon

sys/dev/disk/fd/fd.c
@@ -1523,8 +1523,9 @@ fdstrategy(struct dev_strategy_args *ap)
 
 	/* Tell devstat we are starting on the transaction */
 	devstat_start_transaction(&fd->device_stats);
+#if 0
 	device_busy(fd->dev);
-
+#endif
 	fdstart(fdc);
 	crit_exit();
 	return(0);
@@ -2042,7 +2043,9 @@ fdstate(fdc_p fdc)
 			/* ALL DONE */
 			fd->skip = 0;
 			fdc->bio = NULL;
+#if 0
 			device_unbusy(fd->dev);
+#endif
 			devstat_end_transaction_buf(&fd->device_stats, bp);
 			biodone(bio);
 			fdc->fd = (fd_p) 0;
@@ -2214,7 +2217,9 @@ retrier(struct fdc_data *fdc)
 		bp->b_resid += bp->b_bcount - fdc->fd->skip;
 		fdc->bio = NULL;
 		fdc->fd->skip = 0;
+#if 0
 		device_unbusy(fd->dev);
+#endif
 		devstat_end_transaction_buf(&fdc->fd->device_stats, bp);
 		biodone(bio);
 		fdc->state = FINDWORK;
@@ -2266,7 +2271,9 @@ fdformat(cdev_t dev, struct fd_formb *finfo, struct ucred *cred)
 	if (rv == EWOULDBLOCK) {
 		/* timed out */
 		rv = EIO;
+#if 0
 		device_unbusy(fd->dev);
+#endif
 		biodone(&bp->b_bio1);
 	}
 	if (bp->b_flags & B_ERROR)