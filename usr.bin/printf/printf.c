8a65ba3d8f8a805773a99458f6f18f034ad84d57
Simon Schubert <corecode@dragonflybsd.org>
2009-08-20 02:24:41 +0200

    printf(1): don't use getopt
    printf must not use getopt, since any passed string is valid, also
    strings starting with -, i.e.
    	printf -f%s oo
    has to print "-foo" and not error out with "illegal option".

--- usr.bin/printf/printf.c
+++ usr.bin/printf/printf.c
@@ -98,26 +98,19 @@ main(int argc, char **argv)
 #endif
 {
 	static const char *skip1, *skip2;
-	int ch, chopped, end, fieldwidth, precision, rval;
+	int chopped, end, fieldwidth, precision, rval;
 	char convch, nextch, *format, *fmt, *start;
 
 #ifndef BUILTIN
 	setlocale(LC_NUMERIC, "");
 #endif
-	while ((ch = getopt(argc, argv, "")) != -1)
-		switch (ch) {
-		case '?':
-		default:
-			usage();
-			return (1);
-		}
-	argc -= optind;
-	argv += optind;
 
-	if (argc < 1) {
+	if (argc < 2) {
 		usage();
 		return (1);
 	}
+
+	argv++;
 
 	/*
 	 * Basic algorithm is to scan the format string for conversion
