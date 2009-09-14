fd43fc00b09e2bbfc1641b01f542e0d07634cb16
Matthew Dillon <dillon@apollo.backplane.com>
2009-09-14 11:12:33 -0700

    Kernel - change sync_on_panic default to 0
    No longer attempt to sync the buffer cache when a panic occurs.  This
    has always been problematic, HAMMER will ignore the attempt anyway,
    and now we are formalizing that we no longer try.
    
    This also improves the chances of the system dumping and rebooting
    after a panic vs freezing up.

--- sys/kern/kern_shutdown.c
+++ sys/kern/kern_shutdown.c
@@ -114,7 +114,7 @@ SYSCTL_INT(_debug, OID_AUTO, trace_on_panic, CTLFLAG_RW,
 	&trace_on_panic, 0, "Print stack trace on kernel panic");
 #endif
 
-static int sync_on_panic = 1;
+static int sync_on_panic = 0;
 SYSCTL_INT(_kern, OID_AUTO, sync_on_panic, CTLFLAG_RW,
 	&sync_on_panic, 0, "Do a sync before rebooting from a panic");
 
