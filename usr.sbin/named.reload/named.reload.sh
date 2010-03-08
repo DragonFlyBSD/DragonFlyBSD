ee65b806ac08b188bcab21ef0f1efda2cd5bdef7
Jan Lentfer <lentferj@df64devel.lan.net>
2010-03-08 11:17:32 +0100

    BIND: entirely purge BIND and related stuff from base

--- usr.sbin/named.reload/named.reload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -
-#
-#	from named.reload	5.2 (Berkeley) 6/27/89
-# $FreeBSD: src/usr.sbin/named.reload/named.reload.sh,v 1.2 1999/08/28 01:17:23 peter Exp $
-# $DragonFly: src/usr.sbin/named.reload/named.reload.sh,v 1.3 2004/05/27 18:15:43 dillon Exp $
-#
-
-exec %DESTSBIN%/%INDOT%rndc reload
--- usr.sbin/named.restart/named.restart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -
-#
-# $DragonFly: src/usr.sbin/named.restart/named.restart.sh,v 1.4 2004/05/31 17:52:30 dillon Exp $
-#
-# RNDC does not support 'restart' yet, so do it using RCNG.
-
-/sbin/rcrestart named
-
