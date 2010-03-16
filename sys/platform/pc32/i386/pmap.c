45139d69b22f48973cf872f55acfc22eb682cc63
Aggelos Economopoulos <aoiko@cc.ece.ntua.gr>
2010-03-16 15:13:25 +0200

    pmap: Warning -> Debug
    This message does not concern the system administrator,
    try not to get them worried for no reason.

--- sys/platform/pc32/i386/pmap.c
+++ sys/platform/pc32/i386/pmap.c
@@ -3453,7 +3453,7 @@ pmap_interlock_wait(struct vmspace *vm)
 	struct pmap *pmap = &vm->vm_pmap;
 
 	if (pmap->pm_active & CPUMASK_LOCK) {
-		kprintf("Warning: pmap_interlock %08x\n", pmap->pm_active);
+		kprintf("Debug: pmap_interlock %08x\n", pmap->pm_active);
 		while (pmap->pm_active & CPUMASK_LOCK) {
 			cpu_pause();
 			cpu_ccfence();
