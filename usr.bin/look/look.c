245af092bcdeb9d7adbf787d790072472232407d
Sascha Wildner <saw@online.de>
2010-04-08 15:09:21 +0200

    Fix look(1) on x86_64.
    SIZE_T_MAX, being ULONG_MAX, was -1 when cast to off_t (int64_t).
    
    Reported-by: Urmas Reinsalu <urki@klaasikoda.ee>

--- usr.bin/look/look.c
+++ usr.bin/look/look.c
@@ -134,7 +134,7 @@ main(int argc, char **argv)
 	do {
 		if ((fd = open(file, O_RDONLY, 0)) < 0 || fstat(fd, &sb))
 			err(2, "%s", file);
-		if (sb.st_size > (off_t)SIZE_T_MAX)
+		if ((size_t)sb.st_size > SIZE_T_MAX)
 			errx(2, "%s: %s", file, strerror(EFBIG));
 		if ((front = mmap(NULL, (size_t)sb.st_size, PROT_READ, MAP_SHARED, fd, (off_t)0)) == MAP_FAILED)
 			err(2, "%s", file);
