0de295ce560cc85cb4f845d4cb2021eb3ba0e8c4
Samuel J. Greear <sjg@thesjg.com>
2010-07-02 21:20:21 +0000

    dev - Add kq support to psm

--- sys/dev/misc/psm/psm.c
+++ sys/dev/misc/psm/psm.c
@@ -72,6 +72,7 @@
 #include <sys/conf.h>
 #include <sys/device.h>
 #include <sys/poll.h>
+#include <sys/event.h>
 #include <sys/syslog.h>
 #include <sys/malloc.h>
 #include <sys/rman.h>
@@ -242,6 +243,7 @@ static d_close_t psmclose;
 static d_read_t psmread;
 static d_ioctl_t psmioctl;
 static d_poll_t psmpoll;
+static d_kqfilter_t psmkqfilter;
 
 static int enable_aux_dev (KBDC);
 static int disable_aux_dev (KBDC);
@@ -261,6 +263,8 @@ static int reinitialize (struct psm_softc *, int);
 static char *model_name (int);
 static void psmintr (void *);
 static void psmtimeout (void *);
+static void psmfilter_detach(struct knote *);
+static int psmfilter(struct knote *, long);
 
 /* vendor specific features */
 typedef int probefunc_t (struct psm_softc *);
@@ -342,12 +346,13 @@ static struct isa_pnp_id psm_ids[] = {
 #define CDEV_MAJOR        21
 
 static struct dev_ops psm_ops = {
-	{ PSM_DRIVER_NAME, CDEV_MAJOR, 0 },
+	{ PSM_DRIVER_NAME, CDEV_MAJOR, D_KQFILTER },
 	.d_open =	psmopen,
 	.d_close =	psmclose,
 	.d_read =	psmread,
 	.d_ioctl =	psmioctl,
 	.d_poll =	psmpoll,
+	.d_kqfilter =	psmkqfilter
 };
 
 /* debug message level */
@@ -2397,6 +2402,62 @@ psmpoll(struct dev_poll_args *ap)
     crit_exit();
     ap->a_events = revents;
     return (0);
+}
+
+static struct filterops psmfiltops =
+	{ 1, NULL, psmfilter_detach, psmfilter };
+
+static int
+psmkqfilter(struct dev_kqfilter_args *ap)
+{
+	cdev_t dev = ap->a_head.a_dev;
+	struct psm_softc *sc = PSM_SOFTC(PSM_UNIT(dev));
+	struct knote *kn = ap->a_kn;
+	struct klist *klist;
+
+	ap->a_result = 0;
+
+	switch (kn->kn_filter) {
+	case EVFILT_READ:
+		kn->kn_fop = &psmfiltops;
+		kn->kn_hook = (caddr_t)sc;
+	default:
+		ap->a_result = 1;
+		return (0);
+	}
+
+	crit_enter();
+	klist = &sc->rsel.si_note;
+	SLIST_INSERT_HEAD(klist, kn, kn_selnext);
+	crit_exit();
+
+	return (0);
+}
+
+static void
+psmfilter_detach(struct knote *kn)
+{
+	struct psm_softc *sc = (struct psm_softc *)kn->kn_hook;
+	struct klist *klist;
+
+	crit_enter();
+	klist = &sc->rsel.si_note;
+	SLIST_REMOVE(klist, kn, knote, kn_selnext);
+	crit_exit();
+}
+
+static int
+psmfilter(struct knote *kn, long hint)
+{
+	struct psm_softc *sc = (struct psm_softc *)kn->kn_hook;
+	int ready = 0;
+
+	crit_enter();
+	if (sc->queue.count > 0)
+		ready = 1;
+	crit_exit();
+
+	return (ready);
 }
 
 /* vendor/model specific routines */
