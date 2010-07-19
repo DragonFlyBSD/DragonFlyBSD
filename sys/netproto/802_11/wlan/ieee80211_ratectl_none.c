b3cbafa94fa05a70832a1337f7debce2179f292a
Joe Talbott <josepht@dragonflybsd.org>
2010-07-19 13:41:11 -0400

    ratectl_none - first pass at updating to the new wlan infrastructure
    This will ultimately be the default ratectl algorithm for wireless
    NIC drivers.

--- sys/netproto/802_11/wlan/ieee80211_ratectl_none.c
+++ sys/netproto/802_11/wlan/ieee80211_ratectl_none.c
@@ -45,31 +45,40 @@
 
 static void	*none_ratectl_attach(struct ieee80211vap *);
 static void	none_ratectl_detach(void *);
-static void	none_ratectl_data_alloc(struct ieee80211_node *);
-static void	none_ratectl_data_free(struct ieee80211_node *);
-static void	none_ratectl_data_dup(const struct ieee80211_node *,
-				      struct ieee80211_node *);
-static void	none_ratectl_newstate(void *, enum ieee80211_state);
-static void	none_ratectl_tx_complete(void *, struct ieee80211_node *,
-			int, const struct ieee80211_ratectl_res[],
-			int, int, int, int);
-static void	none_ratectl_newassoc(void *, struct ieee80211_node *, int);
-static int	none_ratectl_findrate(void *, struct ieee80211_node *, int,
-				      int[], int);
+static void none_ratectl_init(struct ieee80211vap *);
+static void none_ratectl_deinit(struct ieee80211vap *);
+static void none_ratectl_node_init(struct ieee80211_node *);
+static void none_ratectl_node_deinit(struct ieee80211_node *);
+static void none_ratectl_rate(struct ieee80211vap *, void *, uint32_t);
+static void	none_ratectl_tx_complete(const struct ieee80211vap *,
+			struct ieee80211_node *, int, void *, void *);
+static void none_ratectl_tx_update(const struct ieee80211vap *,
+			const struct ieee80211_node *, void *, void *, void *);
+static void none_ratectl_setinterval(const struct ieee80211vap *, int);
 
 const struct ieee80211_ratectl ieee80211_ratectl_none = {
 	.rc_name	= "none",
-	.rc_ratectl	= IEEE80211_RATECTL_NONE,
 	.rc_attach	= none_ratectl_attach,
 	.rc_detach	= none_ratectl_detach,
-	.rc_data_alloc	= none_ratectl_data_alloc,
-	.rc_data_free	= none_ratectl_data_free,
-	.rc_data_dup	= none_ratectl_data_dup,
-	.rc_newstate	= none_ratectl_newstate,
+	.rc_init	= none_ratectl_init,
+	.rc_deinit	= none_ratectl_deinit,
+	.rc_node_init	= none_ratectl_node_init,
+	.rc_node_deinit	= none_ratectl_node_deinit,
+	.rc_rate = none_ratectl_rate,
 	.rc_tx_complete	= none_ratectl_tx_complete,
-	.rc_newassoc	= none_ratectl_newassoc,
-	.rc_findrate	= none_ratectl_findrate
+	.rc_tx_update = none_ratectl_tx_update,
+	.rc_setinterval = none_ratectl_setinterval,
 };
+
+IEEE80211_RATECTL_MODULE(ieee80211_ratectl_none, 1);
+IEEE80211_RATECTL_ALG(ieee80211_ratectl_none, IEEE80211_RATECTL_NONE,
+	ieee80211_ratectl_none);
+
+static void
+none_ratectl_setinterval(const struct ieee80211vap *vap __unused,
+	int msecs __unused)
+{
+}
 
 static void *
 none_ratectl_attach(struct ieee80211vap *vap)
@@ -86,49 +95,35 @@ none_ratectl_detach(void *arg __unused)
 }
 
 static void
-none_ratectl_data_alloc(struct ieee80211_node *ni __unused)
+none_ratectl_init(struct ieee80211vap *vap __unused)
 {
 }
 
 static void
-none_ratectl_data_free(struct ieee80211_node *ni __unused)
+none_ratectl_deinit(struct ieee80211vap *vap __unused)
 {
 }
 
 static void
-none_ratectl_data_dup(const struct ieee80211_node *oni __unused,
-		      struct ieee80211_node *nni __unused)
+none_ratectl_node_init(struct ieee80211_node *ni __unused)
 {
 }
 
 static void
-none_ratectl_newstate(void *arg __unused, enum ieee80211_state state __unused)
+none_ratectl_node_deinit(struct ieee80211_node *ni __unused)
 {
 }
 
 static void
-none_ratectl_tx_complete(void *arg __unused, struct ieee80211_node *ni __unused,
-			 int frame_len __unused,
-			 const struct ieee80211_ratectl_res res[] __unused,
-			 int res_len __unused,
-			 int data_retries __unused, int rts_retries __unused,
-			 int is_fail __unused)
+none_ratectl_tx_complete(const struct ieee80211vap *vap __unused,
+			 struct ieee80211_node *ni __unused, int ok __unused,
+			 void *arg1 __unused, void *arg2 __unused)
 {
 }
 
 static void
-none_ratectl_newassoc(void *arg __unused, struct ieee80211_node *ni __unused,
-		      int is_new __unused)
+none_ratectl_tx_update(const struct ieee80211vap *vap __unused,
+			 const struct ieee80211_node *ni __unused,
+			 void *arg1 __unused, void *arg2 __unused, void *arg3 __unused)
 {
-}
-
-static int
-none_ratectl_findrate(void * arg __unused, struct ieee80211_node *ni,
-		      int frame_len __unused, int rateidx[], int rateidx_len)
-{
-	int i;
-
-	for (i = 0; i < rateidx_len; ++i)
-		rateidx[i] = ni->ni_txrate;
-	return rateidx_len;
 }
