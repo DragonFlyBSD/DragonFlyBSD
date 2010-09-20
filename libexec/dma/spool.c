a0a50d0b13c215918457bf5baf92cf0be6a841dc
Matthias Schmidt <matthias@dragonflybsd.org>
2010-09-20 10:59:36 +0200

    dma - Cast correctly to uintmax_t when using %PRIxMAX
    Author: Peter Pentchev <roam@ringlet.net>

--- libexec/dma/spool.c
+++ libexec/dma/spool.c
@@ -92,7 +92,7 @@ newspoolf(struct queue *queue)
 	 */
 	if (fstat(fd, &st) != 0)
 		goto fail;
-	if (asprintf(&queue->id, "%"PRIxMAX, st.st_ino) < 0)
+	if (asprintf(&queue->id, "%"PRIxMAX, (uintmax_t)st.st_ino) < 0)
 		goto fail;
 
 	queue->mailf = fdopen(fd, "r+");
