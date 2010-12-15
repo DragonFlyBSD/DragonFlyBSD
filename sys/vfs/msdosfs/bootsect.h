e3f642ab7b903a18dd32f83ac931e6e23d710eea
Sascha Wildner <saw@online.de>
2010-12-15 08:01:03 +0100

    msdosfs: Add a define of 0x28 as extended boot record signature.
    As per http://support.microsoft.com/kb/140418 (Detailed Explanation
    of FAT Boot Sector), the "...extended boot record signature must be
    either 0x28 or 0x29...".

--- sys/vfs/msdosfs/bootsect.h
+++ sys/vfs/msdosfs/bootsect.h
@@ -40,6 +40,7 @@ struct extboot {
 	int8_t		exReserved1;		/* reserved */
 	int8_t		exBootSignature;	/* ext. boot signature (0x29) */
 #define	EXBOOTSIG	0x29
+#define	EXBOOTSIG2	0x28
 	int8_t		exVolumeID[4];		/* volume ID number */
 	int8_t		exVolumeLabel[11];	/* volume label */
 	int8_t		exFileSysType[8];	/* fs type (FAT12 or FAT16) */
