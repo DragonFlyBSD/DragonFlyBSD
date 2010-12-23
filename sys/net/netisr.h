a91f9815c9eb424a4960bbd52fbbd85b7d27a510
Sepherosa Ziehau <sephe@dragonflybsd.org>
2010-12-23 13:12:39 +0800

    netisr: Add netisr barrier which stalls all netisrs
    netisr_barrier_set()
      Set a netisr barrier, which stalls all netisr.  Currently it must be
      called from netisr0.
    
    netisr_barrier_rem()
      Remove the netisr barrier, which unstalls all netisr.  Currently it
      must be called from netisr0.
    
    These interfaces could be used to work out a lockless pcb lookup or
    iteration (on network hotpath e.g. input/output) at the cost of
    relatively expensive pcb adding and removing (e.g. connect(2)).

--- sys/net/netisr.h
+++ sys/net/netisr.h
@@ -178,6 +178,10 @@ void		netisr_register_rollup(netisr_ru_t ru_func);
 void		netisr_characterize(int num, struct mbuf **mp, int hoff);
 int		netisr_queue(int, struct mbuf *);
 
+struct netisr_barrier *netisr_barrier_create(void);
+void		netisr_barrier_set(struct netisr_barrier *);
+void		netisr_barrier_rem(struct netisr_barrier *);
+
 void		netmsg_service_port_init(lwkt_port_t);
 void		netmsg_service_sync(void);
 void		schednetisr(int);
