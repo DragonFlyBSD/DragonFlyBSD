b04035f5f123720a5b9d6f1aa64f3db8e011adbe
Sepherosa Ziehau <sephe@dragonflybsd.org>
2011-03-01 23:39:08 +0800

    kern.mk: Prevent gcc4.4 from generating SSSE3, SSE4 and AVX instructions

--- sys/platform/pc64/conf/kern.mk
+++ sys/platform/pc64/conf/kern.mk
@@ -9,8 +9,17 @@
 # reserved for user applications.
 #
 CFLAGS+=	-mpreferred-stack-boundary=4
-CFLAGS+=	-mcmodel=kernel -mno-red-zone \
-		-mfpmath=387 -mno-sse -mno-sse2 -mno-sse3 -mno-mmx -mno-3dnow \
-		-msoft-float -fno-asynchronous-unwind-tables \
-		-fno-omit-frame-pointer
+CFLAGS+=	-mcmodel=kernel -mno-red-zone -mfpmath=387
+
+CFLAGS+=	-mno-mmx -mno-3dnow -mno-sse -mno-sse2 -mno-sse3
+
+.if ${CCVER} == "gcc44"
+CFLAGS+=	-mno-ssse3 -mno-sse4.1 -mno-sse4.2 -mno-sse4 -mno-sse4a \
+		-mno-sse5 
+CFLAGS+=	-mno-abm -mno-aes -mno-avx -mno-pclmul -mno-popcnt
+.endif
+
+CFLAGS+=	-msoft-float
+CFLAGS+=	-fno-asynchronous-unwind-tables -fno-omit-frame-pointer
+
 INLINE_LIMIT?=	8000
