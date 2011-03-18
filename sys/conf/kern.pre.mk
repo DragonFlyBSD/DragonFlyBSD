0acedea88440426fcfbe1688bef28c756f4adb84
Sascha Wildner <saw@online.de>
2011-03-18 22:05:22 +0100

    Fix loading of a kernel that was linked with ld 2.20 on x86_64.
    The MAXPAGESIZE changed between binutils 2.17 and 2.20 from 0x100000
    to 0x200000 for x86_64. But we have 0x100000 coded into our ldscript
    for the kernel.
    
    The real solution would be to specify "CONSTANT (MAXPAGESIZE)" in
    the ldscript but unfortunately binutils 2.17 (at least our version;
    later snapshots like 2.17.50 or 2.17.90 do) doesn't have support for
    this keyword.
    
    We will do that too once we get rid of binutils 2.17. Until then,
    set max-page-size to 0x100000 as a workaround when linking on
    x86_64.

--- sys/conf/kern.pre.mk
+++ sys/conf/kern.pre.mk
@@ -95,6 +95,11 @@ SYSTEM_OBJS= locore.o ${OBJS} ioconf.o config.o hack.So
 SYSTEM_LD= @${LD} -Bdynamic -T $S/platform/$P/conf/ldscript.$M \
 	-export-dynamic -dynamic-linker /red/herring \
 	-o ${.TARGET} -X ${SYSTEM_OBJS} vers.o
+.if ${P} == "pc64"
+# XXX swildner: Workaround to make kernels linked with binutils220 boot.
+#
+SYSTEM_LD+= -z max-page-size=1048576
+.endif
 SYSTEM_LD_TAIL= @${OBJCOPY} --strip-symbol gcc2_compiled. ${.TARGET} ; \
 	${SIZE} ${.TARGET} ; chmod 755 ${.TARGET}
 SYSTEM_DEP+= $S/platform/$P/conf/ldscript.$M
