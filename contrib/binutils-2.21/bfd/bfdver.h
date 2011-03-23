e0c39ae810d358f200787e473e194f1746ed0746
Sascha Wildner <saw@online.de>
2011-03-23 21:46:44 +0100

    binutils221: Bring in some needed files instead of generating them.
    Submitted-by: John Marino <john@marino.st>

---
+++ contrib/binutils-2.21/bfd/bfdver.h
@@ -0,0 +1,4 @@
+#define BFD_VERSION_DATE 20110306
+#define BFD_VERSION 221000000
+#define BFD_VERSION_STRING  "(GNU Binutils) " "2.21.0.20110306"
+#define REPORT_BUGS_TO "<http://bugs.dragonflybsd.org>"
