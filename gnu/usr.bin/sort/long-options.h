6bc9ad8df4391a10a63ea26063244f4de9897472
John Marino <draco@marino.st>
2011-04-26 23:57:16 +0200

    sort: Remove GNU sort from base

--- gnu/usr.bin/sort/long-options.h
+++
@@ -1,7 +0,0 @@
-/*
- * $DragonFly: src/gnu/usr.bin/sort/long-options.h,v 1.2 2003/11/09 11:41:16 eirikn Exp $
- */
-
-void
-  parse_long_options(int _argc, char **_argv, const char *_command_name,
-			   const char *_version_string, void (*_usage) (int));
