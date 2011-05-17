3385e5b7b79897e14d0c17d477a34bb9257b257c
Sascha Wildner <saw@online.de>
2011-05-17 16:06:12 +0200

    Revert "gcc 44: Change shared library search order (embedded rpath)"
    This reverts commit e42b0192f1e7ce6df0aa461075bde4978fa72adc.
    
    This commit broke the pkgsrc bootstrap (which is using its own
    libfetch that is not compatible with ours).
    
    Reported-by:   Max Herrgard <herrgard@gmail.com>
    Dragonfly-bug: <http://bugs.dragonflybsd.org/issue2076>

--- gnu/usr.bin/cc44/cc_prep/config/dragonfly-native.h
+++ gnu/usr.bin/cc44/cc_prep/config/dragonfly-native.h
@@ -5,20 +5,19 @@
 #define LINK_LIBGCC_SPEC ""
 #define LIBGCC_SPEC "%{shared: -lgcc_pic} %{!shared: -lgcc}"
 
-#define LG44 	"lib/gcc"GCCSHORTVER
-#define PLG44	PREFIX2"/"LG44
-#define ULG44	"/usr/"LG44
-
 #undef	LINK_SPEC
-#define LINK_SPEC DFBSD_LINK_SPEC " \
--L"PREFIX2"/lib \
--L"PLG44" \
-%{!static: -rpath /usr/lib -rpath-link "PREFIX2"/lib} \
-%{!static: -rpath "ULG44" -rpath-link "PLG44"} \
-%{pg: -L"PLG44"/profile \
-  %{!static: -rpath "ULG44"/profile -rpath-link "PLG44"/profile}} \
-%{pg: -L"PREFIX2"/lib/profile \
-  %{!static: -rpath /usr/lib/profile -rpath-link "PREFIX2"/lib/profile}}"
+#define LINK_SPEC							\
+    DFBSD_LINK_SPEC "							\
+    %{pg: -L"PREFIX2"/lib/gcc"GCCSHORTVER"/profile			\
+      %{!static: -rpath /usr/lib/gcc"GCCSHORTVER"/profile		\
+        -rpath-link "PREFIX2"/lib/gcc"GCCSHORTVER"/profile}}		\
+    -L"PREFIX2"/lib/gcc"GCCSHORTVER"					\
+    %{!static: -rpath /usr/lib/gcc"GCCSHORTVER" -rpath-link "PREFIX2"/lib/gcc"GCCSHORTVER"}	\
+    %{pg: -L"PREFIX2"/lib/profile					\
+      %{!static: -rpath /usr/lib/profile				\
+        -rpath-link "PREFIX2"/lib/profile}}	 			\
+    %{!static: -rpath /usr/lib -rpath-link "PREFIX2"/lib} 		\
+  "
 
 /*#define TARGET_SYSTEM_ROOT_RELOCATABLE */
 /*#define TARGET_SYSTEM_ROOT		"/"*/
@@ -29,7 +28,7 @@
 #define STANDARD_STARTFILE_PREFIX	0
 #define TOOLDIR_BASE_PREFIX		"./"
 
-#define STARTFILE_PREFIX_SPEC		PREFIX2"/lib/ "PLG44"/"
+#define STARTFILE_PREFIX_SPEC		PREFIX2"/lib/ "PREFIX2"/lib/gcc"GCCSHORTVER"/"
 
 #define GPLUSPLUS_INCLUDE_DIR		PREFIX2"/include/c++/"GCCPOINTVER
 #undef	GPLUSPLUS_TOOL_INCLUDE_DIR
@@ -40,4 +39,4 @@
 #undef	CORSS_INCLUDE_DIR
 #undef	TOOL_INCLUDE_DIR
 #undef	SYSTEM_INCLUDE_DIR
-#define	STANDARD_INCLUDE_DIR		PREFIX2"/include"
+#define	STANDARD_INCLUDE_DIR			PREFIX2"/include"
