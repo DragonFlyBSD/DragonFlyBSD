d603dc214d28dadd84d9e09af214f5c4bb361686
Sepherosa Ziehau <sephe@dragonflybsd.org>
2011-05-22 19:22:33 +0800

    x86_64/lapic: Allow vmware with single CPU to boot SMP kernel

--- sys/platform/pc64/apic/lapic.c
+++ sys/platform/pc64/apic/lapic.c
@@ -37,6 +37,7 @@
 #include <machine_base/apic/lapic.h>
 #include <machine_base/apic/ioapic_abi.h>
 #include <machine/segments.h>
+#include <machine/specialreg.h>
 #include <sys/thread2.h>
 
 #include <machine/intr_machdep.h>
@@ -741,6 +742,18 @@ lapic_config(void)
 		mp_naps = ap_max;
 	}
 
+	if ((cpu_feature2 & CPUID2_VMM) && mp_naps == 0) {
+		/*
+		 * XXX
+		 * Special hack for vmware.  It looks like that
+		 * if only one CPU is configured (mp_naps == 0)
+		 * in vmware (cpu_feature2 & CPUID2_VMM),
+		 * then LAPIC will not work at all.
+		 */
+		kprintf("LAPIC: single CPU virtual machine detected, "
+			"disable LAPIC\n");
+		return ENXIO;
+	}
 	return 0;
 }
 
