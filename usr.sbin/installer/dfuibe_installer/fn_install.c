91856cf5ffc1c463b9e1c638274c612e7fa8b4e5
Sascha Wildner <saw@online.de>
2011-05-24 14:05:04 +0200

    nrelease/installer: No longer build and offer separate UP and SMP kernels.
    Due to sephe's recent work, SMP kernels should boot on UP machines as
    well. This is to give his work more exposure.
    
    Also remove the (now redundant) *_SMP configs.
    
    In-discussion-with: sephe

--- usr.sbin/installer/dfuibe_installer/fn_install.c
+++ usr.sbin/installer/dfuibe_installer/fn_install.c
@@ -660,24 +660,6 @@ fn_install_os(struct i_fn_args *a)
 	command_add(cmds, "%s%s 600 %smnt/var/log/install.log",
 	    a->os_root, cmd_name(a, "CHMOD"), a->os_root);
 
-	/*
-	 * Move the kernel used for booting (kernel.UP or kernel.SMP) to
-	 * /boot/kernel and remove the other kernel
-	 */
-	command_add(cmds,
-	    "%s%s %smnt`%s%s $(%s%s -n kern.bootfile)` %smnt/boot/kernel",
-	    a->os_root, cmd_name(a, "MV"),
-	    a->os_root,
-	    a->os_root, cmd_name(a, "DIRNAME"),
-	    a->os_root, cmd_name(a, "SYSCTL"),
-	    a->os_root);
-	command_add(cmds,
-	    "%s%s -R noschg %smnt/boot/kernel.[SU]M*P; %s%s -rf %smnt/boot/kernel.[SU]M*P",
-	    a->os_root, cmd_name(a, "CHFLAGS"),
-	    a->os_root,
-	    a->os_root, cmd_name(a, "RM"),
-	    a->os_root);
-
 	/* Do some preparation if encrypted partitions were configured */
 	if (needcrypt) {
 		command_add(cmds,
