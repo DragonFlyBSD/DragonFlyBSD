9458c398c0b97d30032a6541f8ab452cd93f5a86
YONETANI Tomokazu <y0netan1@dragonflybsd.org>
2011-05-27 22:27:39 +0900

    kernel: Don't disturb about-to-be reaped zombies.
    Processes in zombproc with p_nthreads == 0 are about to be reaped,
    so just leave them in peace.
    
    Dragonfly-bug: <http://bugs.dragonflybsd.org/issue1996>

--- sys/kern/kern_proc.c
+++ sys/kern/kern_proc.c
@@ -949,6 +949,11 @@ sysctl_kern_proc(SYSCTL_HANDLER_ARGS)
 
 			if (!PRISON_CHECK(cr1, p->p_ucred))
 				continue;
+
+			/* let dying zombies reaped quietly */
+			if (doingzomb && p->p_nthreads == 0)
+				continue;
+
 			PHOLD(p);
 			error = sysctl_out_proc(p, req, flags);
 			PRELE(p);
