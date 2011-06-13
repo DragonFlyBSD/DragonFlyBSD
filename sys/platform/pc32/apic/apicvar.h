64c4a24d9cd128eb1ed21395b2ef31b2173db267
Sepherosa Ziehau <sephe@dragonflybsd.org>
2011-06-13 19:03:32 +0800

    i386: Remove unused declaration in apicvar.h

--- sys/platform/pc32/apic/apicvar.h
+++ sys/platform/pc32/apic/apicvar.h
@@ -51,52 +51,5 @@
 #define	APIC_IPI_DEST_ALL	-2
 #define	APIC_IPI_DEST_OTHERS	-3
 
-/*
- * An APIC enumerator is a psuedo bus driver that enumerates APIC's including
- * CPU's and I/O APIC's.
- */
-struct apic_enumerator {
-	const char *apic_name;
-	int (*apic_probe)(void);
-	int (*apic_probe_cpus)(void);
-	int (*apic_setup_local)(void);
-	int (*apic_setup_io)(void);
-	SLIST_ENTRY(apic_enumerator) apic_next;
-};
-
-u_int	apic_irq_to_idt(u_int irq);
-u_int	apic_idt_to_irq(u_int vector);
-void	apic_register_enumerator(struct apic_enumerator *enumerator);
-void	*ioapic_create(uintptr_t addr, int32_t id, int intbase);
-int	ioapic_disable_pin(void *cookie, u_int pin);
-void	ioapic_enable_mixed_mode(void);
-int	ioapic_get_vector(void *cookie, u_int pin);
-int	ioapic_next_logical_cluster(void);
-void	ioapic_register(void *cookie);
-int	ioapic_remap_vector(void *cookie, u_int pin, int vector);
-int	ioapic_set_extint(void *cookie, u_int pin);
-int	ioapic_set_nmi(void *cookie, u_int pin);
-int	ioapic_set_polarity(void *cookie, u_int pin, char activehi);
-int	ioapic_set_triggermode(void *cookie, u_int pin, char edgetrigger);
-int	ioapic_set_smi(void *cookie, u_int pin);
-void	lapic_create(u_int apic_id, int boot_cpu);
-void	lapic_disable(void);
-void	lapic_dump(const char *str);
-void	lapic_enable_intr(u_int vector);
-void	lapic_eoi(void);
-int	lapic_id(void);
-void	lapic_init(uintptr_t addr);
-int	lapic_intr_pending(u_int vector);
-void	lapic_ipi_raw(register_t icrlo, u_int dest);
-void	lapic_ipi_vectored(u_int vector, int dest);
-int	lapic_ipi_wait(int delay);
-void	lapic_handle_intr(struct intrframe *frame);
-void	lapic_set_logical_id(u_int apic_id, u_int cluster, u_int cluster_id);
-int	lapic_set_lvt_mask(u_int apic_id, u_int lvt, u_char masked);
-int	lapic_set_lvt_mode(u_int apic_id, u_int lvt, u_int32_t mode);
-int	lapic_set_lvt_polarity(u_int apic_id, u_int lvt, u_char activehi);
-int	lapic_set_lvt_triggermode(u_int apic_id, u_int lvt, u_char edgetrigger);
-void	lapic_setup(void);
-
 #endif /* !LOCORE */
 #endif /* _MACHINE_APICVAR_H_ */
