ea2195fb9df1e8fbf00ea0676037ba0cd380cd59
Sascha Wildner <saw@online.de>
2011-12-04 20:15:43 +0100

    kernel: Move some dereferences below the NULL checks.

--- sys/gnu/vfs/ext2fs/ext2_linux_balloc.c
+++ sys/gnu/vfs/ext2fs/ext2_linux_balloc.c
@@ -182,12 +182,13 @@ ext2_free_blocks(struct mount * mp, unsigned long block,
 	unsigned long i;
 	int bitmap_nr;
 	struct ext2_group_desc * gdp;
-	struct ext2_super_block * es = sb->s_es;
+	struct ext2_super_block * es;
 
 	if (!sb) {
 		kprintf ("ext2_free_blocks: nonexistent device");
 		return;
 	}
+	es = sb->s_es;
 	lock_super (VFSTOEXT2(mp)->um_devvp);
 	if (block < es->s_first_data_block ||
 	    (block + count) > es->s_blocks_count) {
@@ -267,7 +268,7 @@ ext2_new_block(struct mount * mp, unsigned long goal,
 	int i, j, k, tmp;
 	int bitmap_nr;
 	struct ext2_group_desc * gdp;
-	struct ext2_super_block * es = sb->s_es;
+	struct ext2_super_block * es;
 
 #ifdef EXT2FS_DEBUG
 	static int goal_hits = 0, goal_attempts = 0;
@@ -276,6 +277,7 @@ ext2_new_block(struct mount * mp, unsigned long goal,
 		kprintf ("ext2_new_block: nonexistent device");
 		return 0;
 	}
+	es = sb->s_es;
 	lock_super (VFSTOEXT2(mp)->um_devvp);
 
         ext2_debug ("goal=%lu.\n", goal);
