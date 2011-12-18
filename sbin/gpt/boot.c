d342e161fec9649aba79a4d28e01da9cfa029632
François Tigeot <ftigeot@wolfpond.org>
2011-12-18 20:48:19 +0100

    gpt(8): increase size of boot slice to 768MB
    * aligning it to the size of the boot slice created by the
      live cd installer

--- sbin/gpt/boot.c
+++ sbin/gpt/boot.c
@@ -78,7 +78,7 @@ bootset(int fd)
 		err(1, "unable to find uuid for 'DragonFly Label32'");
 	entry = 0;
 	block = 0;
-	size = 256 * 1024 * 1024 / 512;
+	size = 768 * 1024 * 1024 / 512;
 
 	gpt = map_find(MAP_TYPE_PRI_GPT_HDR);
 	if (gpt == NULL)
