cacf1050443d1dab513c7d9e60299057f1f28877
Venkatesh Srinivas <me@endeavour.zapto.org>
2011-12-31 00:21:51 -0500

    kernel -- KTR: Fix vkernel32 build.
    KTR_INFO needed to be updated for the typesafe KTR patchset.

--- sys/platform/vkernel/i386/trap.c
+++ sys/platform/vkernel/i386/trap.c
@@ -343,16 +343,17 @@ userexit(struct lwp *lp)
 #define	KTR_KERNENTRY	KTR_ALL
 #endif
 KTR_INFO_MASTER(kernentry);
-KTR_INFO(KTR_KERNENTRY, kernentry, trap, 0, "pid=%d, tid=%d, trapno=%d, eva=%p",
-	 sizeof(int) + sizeof(int) + sizeof(int) + sizeof(vm_offset_t));
-KTR_INFO(KTR_KERNENTRY, kernentry, trap_ret, 0, "pid=%d, tid=%d",
-	 sizeof(int) + sizeof(int));
-KTR_INFO(KTR_KERNENTRY, kernentry, syscall, 0, "pid=%d, tid=%d, call=%d",
-	 sizeof(int) + sizeof(int) + sizeof(int));
-KTR_INFO(KTR_KERNENTRY, kernentry, syscall_ret, 0, "pid=%d, tid=%d, err=%d",
-	 sizeof(int) + sizeof(int) + sizeof(int));
-KTR_INFO(KTR_KERNENTRY, kernentry, fork_ret, 0, "pid=%d, tid=%d",
-	 sizeof(int) + sizeof(int));
+KTR_INFO(KTR_KERNENTRY, kernentry, trap, 0,
+        "TRAP(pid %d, tid %d, trapno %d, eva %lu)",
+        pid_t pid, lwpid_t tid,  register_t trapno, vm_offset_t eva);
+KTR_INFO(KTR_KERNENTRY, kernentry, trap_ret, 0, "TRAP_RET(pid %d, tid %d)",
+        pid_t pid, lwpid_t tid);
+KTR_INFO(KTR_KERNENTRY, kernentry, syscall, 0, "SYSC(pid %d, tid %d, nr %d)",
+        pid_t pid, lwpid_t tid,  register_t trapno);
+KTR_INFO(KTR_KERNENTRY, kernentry, syscall_ret, 0, "SYSRET(pid %d, tid %d, err %d)",
+        pid_t pid, lwpid_t tid,  int err);
+KTR_INFO(KTR_KERNENTRY, kernentry, fork_ret, 0, "FORKRET(pid %d, tid %d)",
+        pid_t pid, lwpid_t tid);
 
 /*
  * Exception, fault, and trap interface to the kernel.
