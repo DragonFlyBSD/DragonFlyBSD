fb1511701b1cb3fcf5638518190a09b1dedde807
Sascha Wildner <saw@online.de>
2012-01-22 20:35:23 +0100

    fwcontrol(8): Remove an unused variable and raise WARNS to 2.

--- sys/bus/firewire/fwcrom.c
+++ sys/bus/firewire/fwcrom.c
@@ -304,7 +304,7 @@ crom_desc(struct crom_context *cc, char *buf, int len)
 {
 	struct csrreg *reg;
 	struct csrdirectory *dir;
-	char *desc, st;
+	char *desc;
 	u_int16_t crc;
 
 	reg = crom_get(cc);
