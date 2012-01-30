99c914f0f61fec278e1d1d8022fd999d519717e7
John Marino <draco@marino.st>
2012-01-30 08:48:20 +0100

    custom cc script: Fix "Unterminated quoted string" error
    When updating gcc46 to handle c++ by default, the old quotation mark
    was accidently left in place, causing a world build breakage at
    include/rpc.

--- libexec/customcc/cc.sh
+++ libexec/customcc/cc.sh
@@ -37,7 +37,7 @@ elif [ "${CCVER}" = "clangsvn" ]; then
 elif [ "${CCVER}" = "gcc46" ]; then
 	INCOPT="-nostdinc \
 	    -isysroot @@INCPREFIX@@ \
-	    -isystem /usr/include" \
+	    -isystem /usr/include \
 	    -isystem /usr/include/c++/4.4"
 fi
 
