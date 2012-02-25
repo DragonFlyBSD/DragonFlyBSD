5b83fbbeda22b16e793ab5e57e822db693b5eddb
Sascha Wildner <saw@online.de>
2012-02-25 15:14:46 +0100

    kernel/i386: Remove the empty CPU_HAS_FXSR kernel option.

--- sys/platform/pc32/i386/identcpu.c
+++ sys/platform/pc32/i386/identcpu.c
@@ -950,9 +950,6 @@ printcpuinfo(void)
 #ifdef CPU_HAS_SSE2
 	kprintf("Use SSE2 (lfence, mfence)\n");
 #endif
-#ifdef CPU_HAS_FXSR
-	kprintf("Use FXSR (sfence)\n");
-#endif
 }
 
 void
@@ -1282,10 +1279,6 @@ finish:
 #ifdef CPU_HAS_SSE2
 	if ((cpu_feature & CPUID_SSE2) == 0)
 		panic("CPU does not has SSE2, remove options CPU_HAS_SSE2\n");
-#endif
-#ifdef CPU_HAS_FXSR
-	if ((cpu_feature & CPUID_FXSR) == 0)
-		panic("CPU does not has FXSR, remove options CPU_HAS_FXSR\n");
 #endif
 }
 
