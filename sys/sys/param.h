19af187033dafb57f0739a8b0b21f74d4e5373e8
Matthias Rampke <matthias@rampke.de>
2012-04-17 23:33:16 +0200

    kill zombies if the parent set SIG_IGN on SIGCHLD
    fix for http://bugs.dragonflybsd.org/issues/2349

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -90,9 +90,10 @@
  * 300100 - 3.1 master
  * 300101 - i4b (ISDN) removal
  * 300102 - <sys/ata.h> is now just a link of <sys/nata.h>
+ * 300103 - if SIG_IGN is set on SIGCHLD, do not keep zombie children
  */
 #undef __DragonFly_version
-#define __DragonFly_version 300102	/* propagated to newvers */
+#define __DragonFly_version 300103	/* propagated to newvers */
 
 #include <sys/_null.h>
 
