1d9af22786bf013ba84d4628f02044a9f562e3ac
Sascha Wildner <saw@online.de>
2012-08-07 09:16:38 +0200

    mpt(4): Fix a porting mistake I did in 6d259fc1.
    I left in both the code of the "#if __FreeBSD_version" as well as that of
    its "#else". This caused the geometry to be calculated twice.

--- sys/dev/disk/mpt/mpt_cam.c
+++ sys/dev/disk/mpt/mpt_cam.c
@@ -3931,24 +3931,6 @@ static void
 mpt_calc_geometry(struct ccb_calc_geometry *ccg, int extended)
 {
 	cam_calc_geometry(ccg, extended);
-	uint32_t size_mb;
-	uint32_t secs_per_cylinder;
-
-	if (ccg->block_size == 0) {
-		ccg->ccb_h.status = CAM_REQ_INVALID;
-		return;
-	}
-	size_mb = ccg->volume_size / ((1024L * 1024L) / ccg->block_size);
-	if (size_mb > 1024 && extended) {
-		ccg->heads = 255;
-		ccg->secs_per_track = 63;
-	} else {
-		ccg->heads = 64;
-		ccg->secs_per_track = 32;
-	}
-	secs_per_cylinder = ccg->heads * ccg->secs_per_track;
-	ccg->cylinders = ccg->volume_size / secs_per_cylinder;
-	ccg->ccb_h.status = CAM_REQ_CMP;
 }
 
 /****************************** Timeout Recovery ******************************/
