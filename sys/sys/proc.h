b0e7c1af39723c81eeb7812724d121ee0a5b9c54
Sascha Wildner <saw@online.de>
2012-10-24 21:20:36 +0200

    <sys/proc.h>: Adjust the names of two unused bits to use bit numbering.

--- sys/sys/proc.h
+++ sys/sys/proc.h
@@ -349,10 +349,10 @@ struct	proc {
 #define	P_PPWAIT	0x00010	/* Parent is waiting for child to exec/exit */
 #define	P_PROFIL	0x00020	/* Has started profiling */
 #define P_POSTEXIT	0x00040 /* Prevent procfs from stepping after this pt */
-#define	P_UNUSED4	0x00080	/* was: Sleep is interruptible */
+#define	P_UNUSED7	0x00080	/* was: Sleep is interruptible */
 #define	P_SUGID		0x00100	/* Had set id privileges since last exec */
 #define	P_SYSTEM	0x00200	/* System proc: no sigs, stats or swapping */
-#define	P_UNUSED2	0x00400	/* was: SIGSTOP status */
+#define	P_UNUSED10	0x00400	/* was: SIGSTOP status */
 #define	P_TRACED	0x00800	/* Debugged process being traced */
 #define	P_WAITED	0x01000	/* SIGSTOP status was returned by wait3/4 */
 #define	P_WEXIT		0x02000	/* Working on exiting (master exit) */
