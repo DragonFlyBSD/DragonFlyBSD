ffa8f94ea4d22a7ec8d1ab03eda0ce60d219e574
John Marino <draco@marino.st>
2012-10-26 21:49:50 +0200

    gcc41 removal: Part 1 of 2: makefiles
    GCC 4.1 had been disconnected from the base build after GCC 4.7 was
    brought in.  The transition as gone relatively smoothly, so it's safe
    to remove the rest of GCC 4.1 now, starting with the makefiles.

--- gnu/usr.bin/cc41/cc_prep/gthr-default.h
+++
@@ -1,7 +0,0 @@
-/* XXX marino: disable GTHREAD_USE_WEAK to deal with gcc 4.6 error
-               weakref '__gthrw_<something>' must have static linkage.
-               GTHREAD is for use with GNAT, the Ada compiler, which isn't
-               even switched on. */
-#define GTHREAD_USE_WEAK 0
-
-#include "gthr-posix.h"
