c9e065d383e21ab9bb636ce06152982597b83587
John Marino <draco@marino.st>
2012-10-28 14:27:26 +0100

    usr.bin/make: Remove legacy make files
    usr.bin/make has been replaced with usr.bin/bmake, retaining the program
    name "make".  usr.bin/make was previously disconnected from the build,
    so these files were not longer used.

--- usr.bin/make/tests/basic/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# $DragonFly: src/usr.bin/make/tests/basic/test.sh,v 1.4 2005/02/25 12:28:13 okumoto Exp $
-
-. ../common.sh
-
-DIR="t?"
-
-eval_cmd $1
--- usr.bin/make/tests/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# $DragonFly: src/usr.bin/make/tests/test.sh,v 1.4 2005/02/25 12:28:13 okumoto Exp $
-
-. ./common.sh
-
-DIR="basic variables"
-
-eval_cmd $1
