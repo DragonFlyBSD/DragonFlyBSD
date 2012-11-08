8edad6a26ff69b719a9a896754cedcbbde2974eb
John Marino <netbsd@marino.st>
2012-11-08 11:46:55 +0100

    bsd.init.mk: Establish compatibility with FreeBSD makefiles
    FreeBSD changed NOMAN to NO_MAN and NOSHARED to NO_SHARED.  Rather than
    follow suit, establish NO_MAN and NO_SHARED as aliases for the DragonFly
    versions.  If both are defined, the DragonFly version takes precedence.
    
    These were put in bsd.init.mk because they have to be loaded after the
    makefile being processed but before bsd.prog.mk, bsd.lib.mk, etc.

--- share/mk/bsd.init.mk
+++ share/mk/bsd.init.mk
@@ -1,5 +1,4 @@
 # $FreeBSD: src/share/mk/bsd.init.mk,v 1.1.2.1 2002/07/17 19:08:23 ru Exp $
-# $DragonFly: src/share/mk/bsd.init.mk,v 1.7 2005/04/12 23:35:37 okumoto Exp $
 
 # The include file <bsd.init.mk> includes ../Makefile.inc and
 # <bsd.own.mk>; this is used at the top of all <bsd.*.mk> files
@@ -17,3 +16,12 @@ __<bsd.init.mk>__:
 .MAIN: all
 
 .endif # !target(__<bsd.init.mk>__)
+
+# Establish compatibility with FreeBSD Makefiles
+.if defined(NO_SHARED) && !defined(NOSHARED)
+NOSHARED= ${NO_SHARED}
+.endif
+
+.if defined(NO_MAN) && !defined(NOMAN)
+NOMAN= ${NO_MAN}
+.endif
