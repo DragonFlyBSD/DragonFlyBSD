9a1272865c3cb6e079e1554031dcc712a881598b
Sepherosa Ziehau <sephe@dragonflybsd.org>
2013-07-22 14:21:08 +0800

    tools: Add bench tools for accept(2) and connect(2)
    The main reasons behind making these tools instead of using netperf TCP_CC
    test are:
    - Netperf TCP_CC creates new server for each testing client
    - Netperf TCP_CC creates new listen socket on different TCP port for each
      testing client
    - Netperf TCP_CC created listen socket is in blocking mode
    - Netperf TCP_CC closes accepted sockets immediately
    
    All of the above listed items make netperf TCP_CC do not reflect real life
    TCP server usage.
    
    Tools in accept_connect:
    - "connect_client"
      Fork certain amount of processes to perform blocking connect(2)/close(2)
      for the certain amount of time.
    - "accpet_server"
      Fork certain amount of processes to perform blocking accept(2) on the same
      addr/port bound socket.  close(2) on the accepted socket is called
      immediately.  If -r is specified, each processes will create their own
      listen socket on the same port/addr w/ SO_REUSEPORT sockopt.
    - "kq_accept_server"
      Fork certain amount of processes to perform non-blocking accept(2) on the
      same addr/port bound socket w/ help of kqueue(2).  close(2) on the accepted
      socket is called after the other side closed the connection.  If -r is
      specified, each processes will create their own listen socket on the same
      port/addr w/ SO_REUSEPORT sockopt.
    
    kq_accept_server closely mimics the real life TCP server.

---
+++ tools/tools/netrate/accept_connect/connect_client/connect_client.c
@@ -0,0 +1,153 @@
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static void	mainloop(const struct sockaddr_in *, long, u_long *);
+
+static int	global_stopped;
+
+static void
+usage(const char *cmd)
+{
+	fprintf(stderr, "%s -4 inet4 -p port "
+	    "[-i n_instance] [-l duration]\n", cmd);
+	exit(1);
+}
+
+int
+main(int argc, char *argv[])
+{
+	struct sockaddr_in in;
+	int opt, ninst, i;
+	long dur;
+	u_long *result, sum;
+
+	memset(&in, 0, sizeof(in));
+	in.sin_family = AF_INET;
+
+	ninst = 1;
+	dur = 10;
+
+	while ((opt = getopt(argc, argv, "4:p:i:l:")) != -1) {
+		switch (opt) {
+		case '4':
+			if (inet_pton(AF_INET, optarg, &in.sin_addr) <= 0) {
+				fprintf(stderr, "invalid inet address %s\n",
+				    optarg);
+				usage(argv[0]);
+			}
+			break;
+
+		case 'p':
+			in.sin_port = htons(atoi(optarg));
+			break;
+
+		case 'i':
+			ninst = atoi(optarg);
+			break;
+
+		case 'l':
+			dur = strtol(optarg, NULL, 10);
+			break;
+
+		default:
+			usage(argv[0]);
+		}
+	}
+
+	if (ninst < 1 || dur < 1 ||
+	    in.sin_port == 0 || in.sin_addr.s_addr == INADDR_ANY)
+		usage(argv[0]);
+
+	result = mmap(NULL, ninst * sizeof(u_long), PROT_READ | PROT_WRITE,
+	    MAP_ANON | MAP_SHARED, -1, 0);
+	if (result == MAP_FAILED) {
+		fprintf(stderr, "mmap failed: %d\n", errno);
+		exit(1);
+	}
+	memset(result, 0, ninst * sizeof(u_long));
+
+	for (i = 0; i < ninst; ++i) {
+		pid_t pid;
+
+		pid = fork();
+		if (pid == 0) {
+			mainloop(&in, dur, &result[i]);
+			exit(0);
+		} else if (pid < 0) {
+			fprintf(stderr, "fork failed: %d\n", errno);
+		}
+	}
+
+	for (i = 0; i < ninst; ++i) {
+		pid_t pid;
+
+		pid = waitpid(-1, NULL, 0);
+		if (pid < 0) {
+			fprintf(stderr, "waitpid failed: %d\n", errno);
+			exit(1);
+		}
+	}
+
+	sum = 0;
+	for (i = 0; i < ninst; ++i)
+		sum += result[i];
+	printf("%.2f\n", (double)sum / (double)dur);
+
+	exit(0);
+}
+
+static void
+signal_handler(int signum __unused)
+{
+	global_stopped = 1;
+}
+
+
+static void
+mainloop(const struct sockaddr_in *in, long dur, u_long *res)
+{
+	struct itimerval it;
+	u_long count = 0;
+
+	if (signal(SIGALRM, signal_handler) == SIG_ERR) {
+		fprintf(stderr, "signal failed: %d\n", errno);
+		return;
+	}
+
+	it.it_interval.tv_sec = 0;
+	it.it_interval.tv_usec = 0;
+	it.it_value.tv_sec = dur;
+	it.it_value.tv_usec = 0;
+
+	if (setitimer(ITIMER_REAL, &it, NULL) < 0) {
+		fprintf(stderr, "setitimer failed: %d\n", errno);
+		return;
+	}
+
+	while (!global_stopped) {
+		int s;
+
+		s = socket(AF_INET, SOCK_STREAM, 0);
+		if (s < 0)
+			continue;
+
+		if (connect(s,
+		    (const struct sockaddr *)in, sizeof(*in)) == 0)
+			++count;
+		close(s);
+	}
+	*res = count;
+}
---
+++ tools/tools/netrate/accept_connect/kq_accept_server/kq_accept_server.c
@@ -0,0 +1,183 @@
+#include <sys/types.h>
+#include <sys/event.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define EVENT_MAX	128
+
+static void	mainloop(int, const struct sockaddr_in *);
+
+static void
+usage(const char *cmd)
+{
+	fprintf(stderr, "%s -p port [-i n_instance] [-r]\n", cmd);
+	exit(1);
+}
+
+static int
+create_socket(const struct sockaddr_in *in, int reuseport)
+{
+	int serv_s, on, flags;
+
+	serv_s = socket(AF_INET, SOCK_STREAM, 0);
+	if (serv_s < 0) {
+		fprintf(stderr, "socket failed: %d\n", errno);
+		exit(1);
+	}
+
+	on = 1;
+	if (!reuseport) {
+		if (setsockopt(serv_s, SOL_SOCKET, SO_REUSEADDR,
+		    &on, sizeof(on)) < 0) {
+			fprintf(stderr, "setsockopt(REUSEADDR) failed: %d\n",
+			    errno);
+			exit(1);
+		}
+	} else {
+		if (setsockopt(serv_s, SOL_SOCKET, SO_REUSEPORT,
+		    &on, sizeof(on)) < 0) {
+			fprintf(stderr, "setsockopt(REUSEPORT) failed: %d\n",
+			    errno);
+			exit(1);
+		}
+	}
+
+	flags = fcntl(serv_s, F_GETFL, 0);
+	if (flags < 0) {
+		fprintf(stderr, "fcntl(F_GETFL) failed: %d\n", errno);
+		exit(1);
+	}
+	if (fcntl(serv_s, F_SETFL, flags | O_NONBLOCK) < 0) {
+		fprintf(stderr, "fcntl(F_SETFL) failed: %d\n", errno);
+		exit(1);
+	}
+
+	if (bind(serv_s, (const struct sockaddr *)in, sizeof(*in)) < 0) {
+		fprintf(stderr, "bind failed: %d\n", errno);
+		exit(1);
+	}
+
+	if (listen(serv_s, -1) < 0) {
+		fprintf(stderr, "listen failed: %d\n", errno);
+		exit(1);
+	}
+	return serv_s;
+}
+
+int
+main(int argc, char *argv[])
+{
+	struct sockaddr_in in;
+	int opt, ninst, serv_s, i, reuseport;
+
+	memset(&in, 0, sizeof(in));
+	in.sin_family = AF_INET;
+	in.sin_addr.s_addr = INADDR_ANY;
+
+	ninst = 1;
+	reuseport = 0;
+
+	while ((opt = getopt(argc, argv, "p:i:r")) != -1) {
+		switch (opt) {
+		case 'p':
+			in.sin_port = htons(atoi(optarg));
+			break;
+
+		case 'i':
+			ninst = atoi(optarg);
+			break;
+
+		case 'r':
+			reuseport = 1;
+			break;
+
+		default:
+			usage(argv[0]);
+		}
+	}
+
+	if (ninst < 1 || in.sin_port == 0)
+		usage(argv[0]);
+
+	serv_s = -1;
+	if (!reuseport)
+		serv_s = create_socket(&in, 0);
+
+	for (i = 1; i < ninst; ++i) {
+		pid_t pid;
+
+		pid = fork();
+		if (pid == 0) {
+			mainloop(serv_s, &in);
+			exit(0);
+		} else if (pid < 0) {
+			fprintf(stderr, "fork failed: %d\n", errno);
+		}
+	}
+
+	mainloop(serv_s, &in);
+	exit(0);
+}
+
+static void
+mainloop(int serv_s, const struct sockaddr_in *in)
+{
+	struct kevent change_evt0[EVENT_MAX];
+	int kq, nchange;
+
+	if (serv_s < 0)
+		serv_s = create_socket(in, 1);
+
+	kq = kqueue();
+	if (kq < 0) {
+		fprintf(stderr, "kqueue failed: %d\n", errno);
+		exit(1);
+	}
+
+	EV_SET(&change_evt0[0], serv_s, EVFILT_READ, EV_ADD, 0, 0, NULL);
+	nchange = 1;
+
+	for (;;) {
+		const struct kevent *change_evt = NULL;
+		struct kevent evt[EVENT_MAX];
+		int i, nevt;
+
+		if (nchange > 0)
+			change_evt = change_evt0;
+
+		nevt = kevent(kq, change_evt, nchange, evt, EVENT_MAX, NULL);
+		if (nevt < 0) {
+			fprintf(stderr, "kevent failed: %d\n", errno);
+			exit(1);
+		}
+		nchange = 0;
+
+		for (i = 0; i < nevt; ++i) {
+			if (evt[i].ident == (u_int)serv_s) {
+				while (nchange < EVENT_MAX) {
+					int s;
+
+					s = accept(serv_s, NULL, NULL);
+					if (s < 0)
+						break;
+					EV_SET(&change_evt0[nchange], s,
+					    EVFILT_READ, EV_ADD, 0, 0, NULL);
+					++nchange;
+				}
+			} else {
+				close(evt[i].ident);
+			}
+		}
+	}
+	/* NEVER REACHED */
+}
