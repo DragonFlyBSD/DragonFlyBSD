0f8a88d31940fd98bdf965c8dc407f39532a82ed
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2013-11-21 10:50:04 -0800

    Fix indentation from the previous commit.

--- sys/vfs/hammer/hammer_ioctl.c
+++ sys/vfs/hammer/hammer_ioctl.c
@@ -231,8 +231,8 @@ hammer_ioctl(hammer_inode_t ip, u_long com, caddr_t data, int fflag,
 		}
 		break;
 	case HAMMERIOC_PFS_ITERATE:
-			error = hammer_ioc_pfs_iterate(
-				&trans, (struct hammer_ioc_pfs_iterate *)data);
+		error = hammer_ioc_pfs_iterate(
+			&trans, (struct hammer_ioc_pfs_iterate *)data);
 		break;
 	default:
 		error = EOPNOTSUPP;
