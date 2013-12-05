91e2d9989e9ad47ffa9b01b88d136ec1d2b743b6
Sascha Wildner <saw@online.de>
2013-12-06 00:26:04 +0100

    Remove libxpg4 stub which never played a role for DragonFly.

--- lib/libxpg4/fakelib.c
+++
@@ -1,3 +0,0 @@
-/* $FreeBSD: src/lib/libxpg4/fakelib.c,v 1.1.2.1 2000/06/04 21:49:57 ache Exp $ */
-/* $DragonFly: src/lib/libxpg4/fakelib.c,v 1.2 2003/06/17 04:26:52 dillon Exp $ */
-static int ___fake_library___;
