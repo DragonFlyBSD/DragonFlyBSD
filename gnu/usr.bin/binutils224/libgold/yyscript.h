0a0a0d3c3538fbe1d261721622077c070f3c5de3
John Marino <draco@marino.st>
2013-12-06 17:11:17 +0100

    gold 2.24: Sync with binutils 2.22
    I had forgotten that the build for gold changed on DragonFly between
    binutils 2.21 and binutils 2.22.  When I based 2.24 on 2.21, I lost
    these change (namely a common Makefile.gld and the support for the
    new utility incremental dump).
    
    I also forgot the update the generated yyscript.[ch] files.
    This commit brings binutils 2.24 in-line with 2.22.

--- gnu/usr.bin/binutils224/libgold/yyscript.h
+++ gnu/usr.bin/binutils224/libgold/yyscript.h
@@ -1,20 +1,19 @@
-/* A Bison parser, made by GNU Bison 2.4.3.  */
+/* A Bison parser, made by GNU Bison 2.7.12-4996.  */
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
-
-      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
-   2009, 2010 Free Software Foundation, Inc.
-
+/* Bison interface for Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+   
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-
+   
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
+   
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
@@ -27,10 +26,19 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-
+   
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
+#ifndef YY_YY_YYSCRIPT_H_INCLUDED
+# define YY_YY_YYSCRIPT_H_INCLUDED
+/* Enabling traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
 
 /* Tokens.  */
 #ifndef YYTOKENTYPE
@@ -242,12 +250,10 @@
 
 
 
-
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef union YYSTYPE
 {
-
-/* Line 1685 of yacc.c  */
+/* Line 2053 of yacc.c  */
 #line 53 "yyscript.y"
 
   /* A string.  */
@@ -279,11 +285,27 @@ typedef union YYSTYPE
   enum Script_section_type section_type;
 
 
-
-/* Line 1685 of yacc.c  */
-#line 285 "yyscript.h"
+/* Line 2053 of yacc.c  */
+#line 290 "yyscript.h"
 } YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 #endif
+
+
+#ifdef YYPARSE_PARAM
+#if defined __STDC__ || defined __cplusplus
+int yyparse (void *YYPARSE_PARAM);
+#else
+int yyparse ();
+#endif
+#else /* ! YYPARSE_PARAM */
+#if defined __STDC__ || defined __cplusplus
+int yyparse (void* closure);
+#else
+int yyparse ();
+#endif
+#endif /* ! YYPARSE_PARAM */
+
+#endif /* !YY_YY_YYSCRIPT_H_INCLUDED  */
