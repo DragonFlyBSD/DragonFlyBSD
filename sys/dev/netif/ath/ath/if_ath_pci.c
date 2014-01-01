572ff6f6e8b95055988f178b6ba12ce77bb5b3c2
Matthew Dillon <dillon@apollo.backplane.com>
2014-01-01 10:28:42 -0800

    ath - Reimport fresh from FreeBSD 01-Jan-2014 for re-port
    * Bring the FreeBSD files in verbatim for the atheros driver.  Create
      a sub-directory "ath", move if_ath* and ah_* into ath/
    
    * as-of (fbsd) git commitid 843c1c95adbb59b90772ccb36241906ccf1923ed

--- sys/dev/netif/ath/ath/if_ath_pci.c
+++ sys/dev/netif/ath/ath/if_ath_pci.c
@@ -25,21 +25,27 @@
  * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGES.
- *
- * $FreeBSD: head/sys/dev/ath/if_ath_pci.c 192147 2009-05-15 17:02:11Z imp $
  */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
 /*
  * PCI/Cardbus front-end for the Atheros Wireless LAN controller driver.
  */
+#include "opt_ath.h"
 
 #include <sys/param.h>
 #include <sys/systm.h> 
+#include <sys/malloc.h>
 #include <sys/module.h>
 #include <sys/kernel.h>
 #include <sys/lock.h>
 #include <sys/mutex.h>
 #include <sys/errno.h>
+
+#include <machine/bus.h>
+#include <machine/resource.h>
 #include <sys/bus.h>
 #include <sys/rman.h>
 
@@ -48,13 +54,20 @@
 #include <net/if.h>
 #include <net/if_media.h>
 #include <net/if_arp.h>
+#include <net/ethernet.h>
 
-#include <netproto/802_11/ieee80211_var.h>
+#include <net80211/ieee80211_var.h>
 
-#include <dev/netif/ath/ath/if_athvar.h>
+#include <dev/ath/if_athvar.h>
 
-#include <bus/pci/pcivar.h>
-#include <bus/pci/pcireg.h>
+#include <dev/pci/pcivar.h>
+#include <dev/pci/pcireg.h>
+
+/* For EEPROM firmware */
+#ifdef	ATH_EEPROM_FIRMWARE
+#include <sys/linker.h>
+#include <sys/firmware.h>
+#endif	/* ATH_EEPROM_FIRMWARE */
 
 /*
  * PCI glue.
@@ -69,20 +82,60 @@ struct ath_pci_softc {
 
 #define	BS_BAR	0x10
 #define	PCIR_RETRY_TIMEOUT	0x41
+#define	PCIR_CFG_PMCSR		0x48
+
+#define	DEFAULT_CACHESIZE	32
+
+static void
+ath_pci_setup(device_t dev)
+{
+	uint8_t cz;
+
+	/* XXX TODO: need to override the _system_ saved copies of this */
+
+	/*
+	 * If the cache line size is 0, force it to a reasonable
+	 * value.
+	 */
+	cz = pci_read_config(dev, PCIR_CACHELNSZ, 1);
+	if (cz == 0) {
+		pci_write_config(dev, PCIR_CACHELNSZ,
+		    DEFAULT_CACHESIZE / 4, 1);
+	}
+
+	/* Override the system latency timer */
+	pci_write_config(dev, PCIR_LATTIMER, 0xa8, 1);
+
+	/* If a PCI NIC, force wakeup */
+#ifdef	ATH_PCI_WAKEUP_WAR
+	/* XXX TODO: don't do this for non-PCI (ie, PCIe, Cardbus!) */
+	if (1) {
+		uint16_t pmcsr;
+		pmcsr = pci_read_config(dev, PCIR_CFG_PMCSR, 2);
+		pmcsr |= 3;
+		pci_write_config(dev, PCIR_CFG_PMCSR, pmcsr, 2);
+		pmcsr &= ~3;
+		pci_write_config(dev, PCIR_CFG_PMCSR, pmcsr, 2);
+	}
+#endif
+
+	/*
+	 * Disable retry timeout to keep PCI Tx retries from
+	 * interfering with C3 CPU state.
+	 */
+	pci_write_config(dev, PCIR_RETRY_TIMEOUT, 0, 1);
+}
 
 static int
 ath_pci_probe(device_t dev)
 {
 	const char* devname;
 
-	wlan_serialize_enter();
 	devname = ath_hal_probe(pci_get_vendor(dev), pci_get_device(dev));
 	if (devname != NULL) {
 		device_set_desc(dev, devname);
-		wlan_serialize_exit();
 		return BUS_PROBE_DEFAULT;
 	}
-	wlan_serialize_exit();
 	return ENXIO;
 }
 
@@ -93,8 +146,11 @@ ath_pci_attach(device_t dev)
 	struct ath_softc *sc = &psc->sc_sc;
 	int error = ENXIO;
 	int rid;
+#ifdef	ATH_EEPROM_FIRMWARE
+	const struct firmware *fw = NULL;
+	const char *buf;
+#endif
 
-	wlan_serialize_enter();
 	sc->sc_dev = dev;
 
 	/*
@@ -103,10 +159,9 @@ ath_pci_attach(device_t dev)
 	pci_enable_busmaster(dev);
 
 	/*
-	 * Disable retry timeout to keep PCI Tx retries from
-	 * interfering with C3 CPU state.
+	 * Setup other PCI bus configuration parameters.
 	 */
-	pci_write_config(dev, PCIR_RETRY_TIMEOUT, 0, 1);
+	ath_pci_setup(dev);
 
 	/* 
 	 * Setup memory-mapping of PCI registers.
@@ -138,9 +193,8 @@ ath_pci_attach(device_t dev)
 		goto bad1;
 	}
 	if (bus_setup_intr(dev, psc->sc_irq,
-			   INTR_MPSAFE,
-			   ath_intr, sc, &psc->sc_ih,
-			   &wlan_global_serializer)) {
+			   INTR_TYPE_NET | INTR_MPSAFE,
+			   NULL, ath_intr, sc, &psc->sc_ih)) {
 		device_printf(dev, "could not establish interrupt\n");
 		goto bad2;
 	}
@@ -148,8 +202,8 @@ ath_pci_attach(device_t dev)
 	/*
 	 * Setup DMA descriptor area.
 	 */
-	if (bus_dma_tag_create(sc->sc_dmat,	/* parent */
-			       4, 0,			/* alignment, bounds */
+	if (bus_dma_tag_create(bus_get_dma_tag(dev),	/* parent */
+			       1, 0,			/* alignment, bounds */
 			       BUS_SPACE_MAXADDR_32BIT,	/* lowaddr */
 			       BUS_SPACE_MAXADDR,	/* highaddr */
 			       NULL, NULL,		/* filter, filterarg */
@@ -157,17 +211,61 @@ ath_pci_attach(device_t dev)
 			       ATH_MAX_SCATTER,		/* nsegments */
 			       0x3ffff,			/* maxsegsize XXX */
 			       BUS_DMA_ALLOCNOW,	/* flags */
+			       NULL,			/* lockfunc */
+			       NULL,			/* lockarg */
 			       &sc->sc_dmat)) {
 		device_printf(dev, "cannot allocate DMA tag\n");
 		goto bad3;
 	}
 
-	error = ath_attach(pci_get_device(dev), sc);
-	if (error == 0)	{				/* success */
-		wlan_serialize_exit();
-		return 0;
-	}
+#ifdef	ATH_EEPROM_FIRMWARE
+	/*
+	 * If there's an EEPROM firmware image, load that in.
+	 */
+	if (resource_string_value(device_get_name(dev), device_get_unit(dev),
+	    "eeprom_firmware", &buf) == 0) {
+		if (bootverbose)
+			device_printf(dev, "%s: looking up firmware @ '%s'\n",
+			    __func__, buf);
 
+		fw = firmware_get(buf);
+		if (fw == NULL) {
+			device_printf(dev, "%s: couldn't find firmware\n",
+			    __func__);
+			goto bad3;
+		}
+
+		device_printf(dev, "%s: EEPROM firmware @ %p\n",
+		    __func__, fw->data);
+		sc->sc_eepromdata =
+		    malloc(fw->datasize, M_TEMP, M_WAITOK | M_ZERO);
+		if (! sc->sc_eepromdata) {
+			device_printf(dev, "%s: can't malloc eepromdata\n",
+			    __func__);
+			goto bad3;
+		}
+		memcpy(sc->sc_eepromdata, fw->data, fw->datasize);
+		firmware_put(fw, 0);
+	}
+#endif /* ATH_EEPROM_FIRMWARE */
+
+	ATH_LOCK_INIT(sc);
+	ATH_PCU_LOCK_INIT(sc);
+	ATH_RX_LOCK_INIT(sc);
+	ATH_TX_LOCK_INIT(sc);
+	ATH_TX_IC_LOCK_INIT(sc);
+	ATH_TXSTATUS_LOCK_INIT(sc);
+
+	error = ath_attach(pci_get_device(dev), sc);
+	if (error == 0)					/* success */
+		return 0;
+
+	ATH_TXSTATUS_LOCK_DESTROY(sc);
+	ATH_PCU_LOCK_DESTROY(sc);
+	ATH_RX_LOCK_DESTROY(sc);
+	ATH_TX_IC_LOCK_DESTROY(sc);
+	ATH_TX_LOCK_DESTROY(sc);
+	ATH_LOCK_DESTROY(sc);
 	bus_dma_tag_destroy(sc->sc_dmat);
 bad3:
 	bus_teardown_intr(dev, psc->sc_irq, psc->sc_ih);
@@ -176,7 +274,6 @@ bad2:
 bad1:
 	bus_release_resource(dev, SYS_RES_MEMORY, BS_BAR, psc->sc_sr);
 bad:
-	wlan_serialize_exit();
 	return (error);
 }
 
@@ -186,9 +283,13 @@ ath_pci_detach(device_t dev)
 	struct ath_pci_softc *psc = device_get_softc(dev);
 	struct ath_softc *sc = &psc->sc_sc;
 
-	wlan_serialize_enter();
 	/* check if device was removed */
 	sc->sc_invalid = !bus_child_present(dev);
+
+	/*
+	 * Do a config read to clear pre-existing pci error status.
+	 */
+	(void) pci_read_config(dev, PCIR_COMMAND, 4);
 
 	ath_detach(sc);
 
@@ -199,7 +300,16 @@ ath_pci_detach(device_t dev)
 	bus_dma_tag_destroy(sc->sc_dmat);
 	bus_release_resource(dev, SYS_RES_MEMORY, BS_BAR, psc->sc_sr);
 
-	wlan_serialize_exit();
+	if (sc->sc_eepromdata)
+		free(sc->sc_eepromdata, M_TEMP);
+
+	ATH_TXSTATUS_LOCK_DESTROY(sc);
+	ATH_PCU_LOCK_DESTROY(sc);
+	ATH_RX_LOCK_DESTROY(sc);
+	ATH_TX_IC_LOCK_DESTROY(sc);
+	ATH_TX_LOCK_DESTROY(sc);
+	ATH_LOCK_DESTROY(sc);
+
 	return (0);
 }
 
@@ -208,9 +318,7 @@ ath_pci_shutdown(device_t dev)
 {
 	struct ath_pci_softc *psc = device_get_softc(dev);
 
-	wlan_serialize_enter();
 	ath_shutdown(&psc->sc_sc);
-	wlan_serialize_exit();
 	return (0);
 }
 
@@ -219,9 +327,7 @@ ath_pci_suspend(device_t dev)
 {
 	struct ath_pci_softc *psc = device_get_softc(dev);
 
-	wlan_serialize_enter();
 	ath_suspend(&psc->sc_sc);
-	wlan_serialize_exit();
 
 	return (0);
 }
@@ -231,9 +337,12 @@ ath_pci_resume(device_t dev)
 {
 	struct ath_pci_softc *psc = device_get_softc(dev);
 
-	wlan_serialize_enter();
+	/*
+	 * Suspend/resume resets the PCI configuration space.
+	 */
+	ath_pci_setup(dev);
+
 	ath_resume(&psc->sc_sc);
-	wlan_serialize_exit();
 
 	return (0);
 }
@@ -247,7 +356,7 @@ static device_method_t ath_pci_methods[] = {
 	DEVMETHOD(device_suspend,	ath_pci_suspend),
 	DEVMETHOD(device_resume,	ath_pci_resume),
 
-	DEVMETHOD_END
+	{ 0,0 }
 };
 static driver_t ath_pci_driver = {
 	"ath",
@@ -255,9 +364,7 @@ static driver_t ath_pci_driver = {
 	sizeof (struct ath_pci_softc)
 };
 static	devclass_t ath_devclass;
-DRIVER_MODULE(ath, pci, ath_pci_driver, ath_devclass, NULL, NULL);
-MODULE_VERSION(ath, 1);
-
-MODULE_DEPEND(ath, ath_hal, 1, 1, 1);	/* Atheros HAL */
-MODULE_DEPEND(ath, ath_rate, 1, 1, 1);	/* rate control algorithm */
-MODULE_DEPEND(ath, wlan, 1, 1, 1);	/* 802.11 media layer */
+DRIVER_MODULE(ath_pci, pci, ath_pci_driver, ath_devclass, 0, 0);
+MODULE_VERSION(ath_pci, 1);
+MODULE_DEPEND(ath_pci, wlan, 1, 1, 1);		/* 802.11 media layer */
+MODULE_DEPEND(ath_pci, if_ath, 1, 1, 1);	/* if_ath driver */
