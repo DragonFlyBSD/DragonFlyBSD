f501b47d55566ff036908318f5dd1c67dc079e60
Matthew Dillon <dillon@apollo.backplane.com>
2014-01-01 17:40:54 -0800

    ieee80211 - Sync support for atheros
    * Add some stuff from FreeBSD to support the atheros refresh.

--- sys/netproto/802_11/ieee80211_regdomain.h
+++ sys/netproto/802_11/ieee80211_regdomain.h
@@ -258,6 +258,7 @@ enum RegdomainCode {
 	SKU_SR9			= 0x0298, /* Ubiquiti SR9 (900MHz/GSM) */
 	SKU_XR9			= 0x0299, /* Ubiquiti XR9 (900MHz/GSM) */
 	SKU_GZ901		= 0x029a, /* Zcomax GZ-901 (900MHz/GSM) */
+	SKU_XC900M              = 0x029b, /* Xagyl XC900M (900MHz/GSM) */
 };
 
 #if defined(__KERNEL__) || defined(_KERNEL)
