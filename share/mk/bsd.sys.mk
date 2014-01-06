33a8e86f8796637241bc570b2dcc5f78e782a940
Sascha Wildner <saw@online.de>
2014-01-07 00:49:53 +0100

    Add -Wformat-security to WARNS >= 2.

--- share/mk/bsd.sys.mk
+++ share/mk/bsd.sys.mk
@@ -29,7 +29,7 @@ CWARNFLAGS	+=	-Werror
 .   endif
 .  endif
 .  if ${WARNS} >= 2
-CWARNFLAGS	+=	-Wall -Wno-pointer-sign
+CWARNFLAGS	+=	-Wall -Wformat-security -Wno-pointer-sign
 .  endif
 .  if ${WARNS} >= 3
 CWARNFLAGS	+=	-Wextra -Wno-unused-parameter -Wstrict-prototypes\
