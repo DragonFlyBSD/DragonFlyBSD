DragonFlyBSD/DragonFlyBSD

commit cfd79120debdd52626136c5985f3a9e3d4fe42bf

jails/netinet6: Only select jailed ips for outgoing
Outgoing IP address selection for jailed processes using ipv6
was broken and selected the first ip6 address available.
Now the code checks whether an ip6 is available to the jail
in in6_ifawithascope.

sys/netinet6/in6.c
@@ -76,6 +76,7 @@
 #include <sys/time.h>
 #include <sys/kernel.h>
 #include <sys/syslog.h>
+#include <sys/jail.h>
 
 #include <sys/thread2.h>
 #include <sys/msgport2.h>
@@ -2010,7 +2011,7 @@ in6_prefixlen2mask(struct in6_addr *maskp, int len)
  * return the best address out of the same scope
  */
 struct in6_ifaddr *
-in6_ifawithscope(struct ifnet *oifp, struct in6_addr *dst)
+in6_ifawithscope(struct ifnet *oifp, struct in6_addr *dst, struct ucred *cred)
 {
 	int dst_scope =	in6_addrscope(dst), src_scope, best_scope = 0;
 	int blen = -1;
@@ -2066,6 +2067,11 @@ in6_ifawithscope(struct ifnet *oifp, struct in6_addr *dst)
 
 			if (((struct in6_ifaddr *)ifa)->ia6_flags &
 			    IN6_IFF_DETACHED)
+				continue;
+
+			/* Skip adresses not valid for current jail */
+			if (cred != NULL &&
+			    !jailed_ip(cred->cr_prison, (struct sockaddr *)(ifa->ifa_addr)) != 0)
 				continue;
 
 			/*

sys/netinet6/in6.h
@@ -653,7 +653,7 @@ struct sockaddr_in6;
 int	in6_cksum (struct mbuf *, uint8_t, uint32_t, uint32_t);
 int	in6_localaddr (struct in6_addr *);
 int	in6_addrscope (struct in6_addr *);
-struct	in6_ifaddr *in6_ifawithscope (struct ifnet *, struct in6_addr *);
+struct	in6_ifaddr *in6_ifawithscope (struct ifnet *, struct in6_addr *, struct ucred *);
 struct	in6_ifaddr *in6_ifawithifp (struct ifnet *, struct in6_addr *);
 void	in6_if_up (struct ifnet *);
 

sys/netinet6/in6_pcb.c
@@ -463,7 +463,7 @@ in6_selectsrc(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 	if (pi && pi->ipi6_ifindex) {
 		/* XXX boundary check is assumed to be already done. */
 		ia6 = in6_ifawithscope(ifindex2ifnet[pi->ipi6_ifindex],
-				       dst);
+				       dst, cred);
 
 		if (ia6 && jailed) {
 			jsin6.sin6_addr = (&ia6->ia_addr)->sin6_addr;
@@ -500,7 +500,7 @@ in6_selectsrc(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 			return (0);
 		}
 		ia6 = in6_ifawithscope(ifindex2ifnet[dstsock->sin6_scope_id],
-				       dst);
+				       dst, cred);
 
 		if (ia6 && jailed) {
 			jsin6.sin6_addr = (&ia6->ia_addr)->sin6_addr;
@@ -533,7 +533,7 @@ in6_selectsrc(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 		}
 
 		if (ifp) {
-			ia6 = in6_ifawithscope(ifp, dst);
+			ia6 = in6_ifawithscope(ifp, dst, cred);
 			if (ia6 == 0) {
 				*errorp = EADDRNOTAVAIL;
 				return (0);
@@ -555,7 +555,7 @@ in6_selectsrc(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 			sin6_next = satosin6(opts->ip6po_nexthop);
 			rt = nd6_lookup(&sin6_next->sin6_addr, 1, NULL);
 			if (rt) {
-				ia6 = in6_ifawithscope(rt->rt_ifp, dst);
+				ia6 = in6_ifawithscope(rt->rt_ifp, dst, cred);
 				if (ia6 == 0)
 					ia6 = ifatoia6(rt->rt_ifa);
 			}
@@ -609,7 +609,7 @@ in6_selectsrc(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 		 */
 
 		if (ro->ro_rt) {
-			ia6 = in6_ifawithscope(ro->ro_rt->rt_ifa->ifa_ifp, dst);
+			ia6 = in6_ifawithscope(ro->ro_rt->rt_ifa->ifa_ifp, dst, cred);
 			if (ia6 && jailed) {
 				jsin6.sin6_addr = (&ia6->ia_addr)->sin6_addr;
 				if (!jailed_ip(cred->cr_prison,

sys/netinet6/in6_src.c
@@ -173,7 +173,7 @@ in6_selectsrc(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 	if (pi && pi->ipi6_ifindex) {
 		/* XXX boundary check is assumed to be already done. */
 		ia6 = in6_ifawithscope(ifindex2ifnet[pi->ipi6_ifindex],
-				       dst);
+				       dst, cred);
 
 		if (ia6 && jailed) {
 			jsin6.sin6_addr = (&ia6->ia_addr)->sin6_addr;
@@ -210,7 +210,7 @@ in6_selectsrc(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 			return (0);
 		}
 		ia6 = in6_ifawithscope(ifindex2ifnet[dstsock->sin6_scope_id],
-				       dst);
+				       dst, cred);
 
 		if (ia6 && jailed) {
 			jsin6.sin6_addr = (&ia6->ia_addr)->sin6_addr;
@@ -243,7 +243,7 @@ in6_selectsrc(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 		}
 
 		if (ifp) {
-			ia6 = in6_ifawithscope(ifp, dst);
+			ia6 = in6_ifawithscope(ifp, dst, cred);
 			if (ia6 == NULL) {
 				*errorp = EADDRNOTAVAIL;
 				return (0);
@@ -265,7 +265,7 @@ in6_selectsrc(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 			sin6_next = satosin6(opts->ip6po_nexthop);
 			rt = nd6_lookup(&sin6_next->sin6_addr, 1, NULL);
 			if (rt) {
-				ia6 = in6_ifawithscope(rt->rt_ifp, dst);
+				ia6 = in6_ifawithscope(rt->rt_ifp, dst, cred);
 				if (ia6 == NULL)
 					ia6 = ifatoia6(rt->rt_ifa);
 			}
@@ -323,7 +323,7 @@ in6_selectsrc(struct sockaddr_in6 *dstsock, struct ip6_pktopts *opts,
 		 */
 
 		if (ro->ro_rt) {
-			ia6 = in6_ifawithscope(ro->ro_rt->rt_ifa->ifa_ifp, dst);
+			ia6 = in6_ifawithscope(ro->ro_rt->rt_ifa->ifa_ifp, dst, cred);
 			if (ia6 && jailed) {
 				jsin6.sin6_addr = (&ia6->ia_addr)->sin6_addr;
 				if (!jailed_ip(cred->cr_prison,

commit bdd6673534cb3bcd6b21de070f0e556637d049a0

jails/netinet6: unbreak source selection after cfd7912
Commit cfd7912 broke source address selection for the host. It
selected link-local addresses for local global routeable addresses.

Reported-By: ftigeot

sys/netinet6/in6.c
@@ -2017,6 +2017,10 @@ in6_ifawithscope(struct ifnet *oifp, struct in6_addr *dst, struct ucred *cred)
 	int blen = -1;
 	struct ifnet *ifp;
 	struct in6_ifaddr *ifa_best = NULL;
+	int jailed = 0;
+
+	if(cred && cred->cr_prison)
+		jailed = 1;
 
 	if (oifp == NULL) {
 #if 0
@@ -2070,7 +2074,7 @@ in6_ifawithscope(struct ifnet *oifp, struct in6_addr *dst, struct ucred *cred)
 				continue;
 
 			/* Skip adresses not valid for current jail */
-			if (cred != NULL &&
+			if (jailed &&
 			    !jailed_ip(cred->cr_prison, (struct sockaddr *)(ifa->ifa_addr)) != 0)
 				continue;
 