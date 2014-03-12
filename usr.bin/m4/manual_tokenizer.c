917beb172f5c4ee492fa7b47ce8c161066b977e8
Sascha Wildner <saw@online.de>
2014-03-12 13:36:01 +0100

    Now that the base issue is fixed, enable -Wsign-compare again in scanners.
    Disabling warnings is usually not fixing them.

--- usr.bin/m4/manual_tokenizer.c
+++ usr.bin/m4/manual_tokenizer.c
@@ -488,7 +488,7 @@ char *yytext;
  *
  * $FreeBSD: src/usr.bin/m4/tokenizer.l,v 1.3 2012/11/17 01:54:24 svnexp Exp $
  */
-#pragma GCC diagnostic ignored "-Wsign-compare"
+
 #include "parser.h"
 #include <assert.h>
 #include <stdlib.h>
@@ -1570,7 +1570,7 @@ YY_BUFFER_STATE yy_scan_bytes  (yyconst char * yybytes, yy_size_t  _yybytes_len 
 	YY_BUFFER_STATE b;
 	char *buf;
 	yy_size_t n;
-	int i;
+	yy_size_t i;
 
 	/* Get memory for full buffer, including space for trailing EOB's. */
 	n = _yybytes_len + 2;
