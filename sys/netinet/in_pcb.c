44d187af60539a2e03e8fd1e0554a8ec5b6f2ae4
Sepherosa Ziehau <sephe@dragonflybsd.org>
2014-03-16 20:07:51 +0800

    in_pcbconn_bind: Change argument name from 'nam' to 'remote'
    Increase readability.

--- sys/netinet/in_pcb.c
+++ sys/netinet/in_pcb.c
@@ -563,12 +563,12 @@ in_pcblookup_localremote(struct inpcbinfo *pcbinfo, struct in_addr laddr,
 }
 
 int
-in_pcbconn_bind(struct inpcb *inp, const struct sockaddr *nam,
+in_pcbconn_bind(struct inpcb *inp, const struct sockaddr *remote,
     struct thread *td)
 {
 	struct proc *p = td->td_proc;
 	unsigned short *lastport;
-	const struct sockaddr_in *sin = (const struct sockaddr_in *)nam;
+	const struct sockaddr_in *sin = (const struct sockaddr_in *)remote;
 	struct sockaddr_in jsin;
 	struct inpcbinfo *pcbinfo = inp->inp_pcbinfo;
 	struct ucred *cred = NULL;
