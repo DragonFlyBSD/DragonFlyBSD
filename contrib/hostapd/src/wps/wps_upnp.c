b833bff77c7cb20178550cc2289d7941aa2a1ac2
John Marino <draco@marino.st>
2014-05-28 13:22:00 +0200

    WPA Supplicant upgrade: Add local modifications and READMEs
    Both vendor sources only need modification on a single file: wps_upnp.c
    This is step 3 of 4 of the upgrade.

--- contrib/hostapd/src/wps/wps_upnp.c
+++ contrib/hostapd/src/wps/wps_upnp.c
@@ -824,7 +824,7 @@ fail:
 }
 
 
-#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
+#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__DragonFly__)
 #include <sys/sysctl.h>
 #include <net/route.h>
 #include <net/if_dl.h>
@@ -865,7 +865,7 @@ static int eth_get(const char *device, u8 ea[ETH_ALEN])
 	}
 	return 0;
 }
-#endif /* __FreeBSD__ */
+#endif /* __FreeBSD__ || __DragonFly__ */
 
 
 /**
@@ -911,7 +911,7 @@ int get_netif_info(const char *net_if, unsigned *ip_addr, char **ip_addr_text,
 		goto fail;
 	}
 	os_memcpy(mac, req.ifr_addr.sa_data, 6);
-#elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
+#elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__DragonFly__)
 	if (eth_get(net_if, mac) < 0) {
 		wpa_printf(MSG_ERROR, "WPS UPnP: Failed to get MAC address");
 		goto fail;
