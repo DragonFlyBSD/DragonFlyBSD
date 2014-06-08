f27044c047dde214095b9e0a2efedeec9c813e8d
François Tigeot <ftigeot@wolfpond.org>
2014-06-08 23:14:56 +0200

    drm/i915: Fix unresponsive X server issues
    * In some circumstances, the drm/i915 driver slept on irq_lock with no
      timeout and never woke up, causing the X server to become unresponsive
    
    * It turns the absence of timeout came from the original FreeBSD version
      of this driver; Linux uses a 1 second timeout by default
    
    * Add the same 1-second timeout to the culprit function calls, hopefully
      fixing this issue for good

--- sys/dev/drm/i915/i915_gem.c
+++ sys/dev/drm/i915/i915_gem.c
@@ -661,7 +661,7 @@ i915_gem_ring_throttle(struct drm_device *dev, struct drm_file *file)
 			    !(i915_seqno_passed(ring->get_seqno(ring), seqno) ||
 			    atomic_read(&dev_priv->mm.wedged)))
 				ret = -lksleep(ring, &ring->irq_lock, PCATCH,
-				    "915thr", 0);
+				    "915thr", 1*hz);
 			ring->irq_put(ring);
 			if (ret == 0 && atomic_read(&dev_priv->mm.wedged))
 				ret = -EIO;
@@ -2495,7 +2495,7 @@ i915_wait_request(struct intel_ring_buffer *ring, uint32_t seqno, bool do_retire
 			    && !atomic_read(&dev_priv->mm.wedged) &&
 			    ret == 0) {
 				ret = -lksleep(ring, &ring->irq_lock, flags,
-				    "915gwr", 0);
+				    "915gwr", 1*hz);
 			}
 			ring->irq_put(ring);
 			lockmgr(&ring->irq_lock, LK_RELEASE);
