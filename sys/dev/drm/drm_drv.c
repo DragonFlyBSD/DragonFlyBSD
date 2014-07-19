d268c8721108fc80828019f99d78ba98d26310f3
François Tigeot <ftigeot@wolfpond.org>
2014-07-19 09:54:07 +0200

    drm(4): This device is MPSAFE
    * And has always been since the initial import from FreeBSD 11 years ago.
    
    * Tested with Radeon and i915 hardware for good measure.

--- sys/dev/drm/drm_drv.c
+++ sys/dev/drm/drm_drv.c
@@ -181,7 +181,7 @@ static drm_ioctl_desc_t		  drm_ioctls[256] = {
 };
 
 static struct dev_ops drm_cdevsw = {
-	{ "drm", 0, D_TRACKCLOSE },
+	{ "drm", 0, D_TRACKCLOSE | D_MPSAFE },
 	.d_open =	drm_open,
 	.d_close =	drm_close,
 	.d_read =	drm_read,
