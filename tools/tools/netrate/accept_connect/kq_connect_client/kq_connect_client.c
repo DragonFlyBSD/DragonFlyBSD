626b6155e27e4e23e44f7d2b4d06c1480379e122
Sepherosa Ziehau <sephe@dragonflybsd.org>
2014-08-13 20:58:32 +0800

    tools/kq_connect_client: Add option to create UDP socket before connect
    And do connect and send 18B datagram before closing the UDP socket.  This
    simulates DNS resolving, though this UDP socket does not receive a reply.

--- tools/tools/netrate/accept_connect/kq_connect_client/kq_connect_client.c
+++ tools/tools/netrate/accept_connect/kq_connect_client/kq_connect_client.c
@@ -16,13 +16,14 @@
 #include <string.h>
 #include <unistd.h>
 
-static void	mainloop(const struct sockaddr_in *, int, int, long, u_long *);
+static void	mainloop(const struct sockaddr_in *, int, int, long, u_long *,
+		    int);
 
 static void
 usage(const char *cmd)
 {
 	fprintf(stderr, "%s -4 inet4 [-4 inet4_1] -p port "
-	    "[-i n_instance] [-c conn_max] [-l duration]\n", cmd);
+	    "[-i n_instance] [-c conn_max] [-l duration] [-u]\n", cmd);
 	exit(1);
 }
 
@@ -30,7 +31,7 @@ int
 main(int argc, char *argv[])
 {
 	struct sockaddr_in *in, *tmp;
-	int opt, ninst, nconn, i, in_max, in_cnt;
+	int opt, ninst, nconn, i, in_max, in_cnt, do_udp;
 	long dur;
 	u_long *result, sum;
 	u_short port;
@@ -39,6 +40,7 @@ main(int argc, char *argv[])
 	nconn = 8;
 	dur = 10;
 	port = 0;
+	do_udp = 0;
 
 	in_max = 8;
 	in = calloc(in_max, sizeof(struct sockaddr_in));
@@ -48,7 +50,7 @@ main(int argc, char *argv[])
 	}
 	in_cnt = 0;
 
-	while ((opt = getopt(argc, argv, "4:p:i:l:c:")) != -1) {
+	while ((opt = getopt(argc, argv, "4:p:i:l:c:u")) != -1) {
 		switch (opt) {
 		case '4':
 			if (in_cnt >= in_max) {
@@ -92,6 +94,10 @@ main(int argc, char *argv[])
 			dur = strtol(optarg, NULL, 10);
 			break;
 
+		case 'u':
+			do_udp = 1;
+			break;
+
 		default:
 			usage(argv[0]);
 		}
@@ -119,7 +125,7 @@ main(int argc, char *argv[])
 
 		pid = fork();
 		if (pid == 0) {
-			mainloop(in, in_cnt, nconn, dur, &result[i]);
+			mainloop(in, in_cnt, nconn, dur, &result[i], do_udp);
 			exit(0);
 		} else if (pid < 0) {
 			fprintf(stderr, "fork failed: %d\n", errno);
@@ -145,8 +151,30 @@ main(int argc, char *argv[])
 }
 
 static void
+udp_send(const struct sockaddr_in *in)
+{
+	uint8_t d[18];
+	int s;
+
+	s = socket(AF_INET, SOCK_DGRAM, 0);
+	if (s < 0) {
+		fprintf(stderr, "socket DGRAM failed: %d\n", errno);
+		return;
+	}
+
+	if (connect(s, (const struct sockaddr *)in, sizeof(*in)) < 0) {
+		fprintf(stderr, "connect DGRAM failed: %d\n", errno);
+		goto done;
+	}
+
+	write(s, d, sizeof(d));
+done:
+	close(s);
+}
+
+static void
 mainloop(const struct sockaddr_in *in, int in_cnt, int nconn_max,
-    long dur, u_long *res)
+    long dur, u_long *res, int do_udp)
 {
 	struct kevent *evt_change0, *evt;
 	int kq, nchange = 0, nconn = 0, nevt_max;
@@ -186,6 +214,12 @@ mainloop(const struct sockaddr_in *in, int in_cnt, int nconn_max,
 			const struct sockaddr_in *tmp;
 			int s;
 
+			tmp = &in[in_idx % in_cnt];
+			++in_idx;
+
+			if (do_udp)
+				udp_send(tmp);
+
 			s = socket(AF_INET, SOCK_STREAM, 0);
 			if (s < 0) {
 				fprintf(stderr, "socket failed: %d\n", errno);
@@ -196,9 +230,6 @@ mainloop(const struct sockaddr_in *in, int in_cnt, int nconn_max,
 				fprintf(stderr, "ioctl failed: %d\n", errno);
 				return;
 			}
-
-			tmp = &in[in_idx % in_cnt];
-			++in_idx;
 
 			n = connect(s, (const struct sockaddr *)tmp,
 			    sizeof(*tmp));
