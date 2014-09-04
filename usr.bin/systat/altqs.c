aa0a803a57c7119e5b39d166a1bc779fb1917014
Matthew Dillon <dillon@apollo.backplane.com>
2014-09-04 10:53:28 -0700

    systat - Add -altq display, bug fixes
    * Add systat -altq.  This displays altq packet statistics in a nice
      [ifname x altqname] matrix.  Packets, bytes, and drops are displayed.
    
      Previously the only way to do this was to use 'pfctl -s queue -vvv'
      which is formatted so horribly that picking information out of it
      at a glance is impossible.
    
      Example usage:  systat -altq 1
    
    * Correct a bug in -ifstat that caused the statistics to not display
      when switching out and then back into the ifstat display.

--- usr.bin/systat/altqs.c
+++ usr.bin/systat/altqs.c
@@ -24,8 +24,6 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $FreeBSD: src/usr.bin/systat/ifstat.c,v 1.7 2008/01/12 00:11:26 delphij Exp $
  */
 
 #include <sys/types.h>
@@ -40,30 +38,57 @@
 #include <err.h>
 #include <errno.h>
 
+#include <net/altq/altq.h>
+#include <net/altq/altq_cbq.h>
+#include <net/altq/altq_priq.h>
+#include <net/altq/altq_hfsc.h>
+#include <net/altq/altq_fairq.h>
+
+#include <net/pf/pfvar.h>
+
 #include "systat.h"
 #include "extern.h"
 #include "convtbl.h"
 
-                                /* Column numbers */
-
-#define C1	0		/*  0-19 */
-#define C2	20		/* 20-39 */
-#define C3	40		/* 40-59 */
-#define C4	60		/* 60-80 */
-#define C5	80		/* Used for label positioning. */
-
-static const int col0 = 0;
-static const int col1 = C1;
-static const int col2 = C2;
-static const int col3 = C3;
-static const int col4 = C4;
-static const int col5 = C5;
-
-
+static SLIST_HEAD(, qcol) qcols;
 static SLIST_HEAD(, if_stat) curlist;
+static int pf_fd = -1;
+static int qccols;
+static int TopSection1;
+static int TopSection2;
+static int TopSection3;
+
+struct qcol {
+	SLIST_ENTRY(qcol) link;
+	char *qname;
+	int col;
+};
+
+typedef struct qcol qcol_t;
+
+union class_stats {
+	class_stats_t		cbq;
+	struct priq_classstats	priq;
+	struct hfsc_classstats	hfsc;
+	struct fairq_classstats	fairq;
+};
+
+struct queue_stats {
+	SLIST_ENTRY(queue_stats) link;
+	struct pfioc_altq	pa;
+	struct pfioc_qstats	pq;
+	qcol_t			*qc;
+	union class_stats	ostats;
+	union class_stats	nstats;
+	uint64_t  old_bytes;
+	uint64_t  old_packets;
+};
+
+typedef struct queue_stats queue_stats_t;
 
 struct if_stat {
-	SLIST_ENTRY(if_stat)	 link;
+	SLIST_ENTRY(if_stat) link;
+	SLIST_HEAD(, queue_stats) queues;
 	char	if_name[IF_NAMESIZE];
 	struct	ifmibdata if_mib;
 	struct	timeval tv;
@@ -73,91 +98,49 @@ struct if_stat {
 	u_long	if_in_traffic_peak;
 	u_long	if_out_traffic_peak;
 	u_int	if_row;			/* Index into ifmib sysctl */
-	u_int	if_ypos;		/* 0 if not being displayed */
+	u_int	row;			/* display row (relative) */
 	u_int	display;
 };
 
+typedef struct if_stat if_stat_t;
+
 extern	 u_int curscale;
 
-static	 void  right_align_string(struct if_stat *);
+static	 void  load_altqs(void);
+static	 void  print_altq(if_stat_t *p, queue_stats_t *q);
+static	 void  right_align_string(if_stat_t *);
 static	 void  getifmibdata(const int, struct ifmibdata *);
 static	 void  sort_interface_list(void);
 static	 u_int getifnum(void);
 
 #define IFSTAT_ERR(n, s)	do {					\
 	putchar('');							\
-	closeifstat(wnd);						\
+	closealtqs(wnd);						\
 	err((n), (s));							\
 } while (0)
 
-#define STARTING_ROW	(8)
-#define ROW_SPACING	(3)
-
-#define TOPLINE 5
-#define TOPLABEL \
-"      Interface           Traffic               Peak                Total"
-
-#define CLEAR_LINE(y, x)	do {					\
-	wmove(wnd, y, x);						\
-	wclrtoeol(wnd);							\
-} while (0)
-
-#define IN_col2		(ifp->if_in_curtraffic)
-#define OUT_col2	(ifp->if_out_curtraffic)
-#define IN_col3		(ifp->if_in_traffic_peak)
-#define OUT_col3	(ifp->if_out_traffic_peak)
-#define IN_col4		(ifp->if_mib.ifmd_data.ifi_ibytes)
-#define OUT_col4	(ifp->if_mib.ifmd_data.ifi_obytes)
-
-#define EMPTY_COLUMN 	"                    "
-#define CLEAR_COLUMN(y, x)	mvprintw((y), (x), "%20s", EMPTY_COLUMN);
-
-#define DOPUTRATE(c, r, d)	do {					\
-	CLEAR_COLUMN(r, c);						\
-	mvprintw(r, (c), "%10.3f %s%s  ",				\
-		 convert(d##_##c, curscale),				\
-		 get_string(d##_##c, curscale),				\
-		 "/s");							\
-} while (0)
-
-#define DOPUTTOTAL(c, r, d)	do {					\
- 	CLEAR_COLUMN((r), (c));						\
- 	mvprintw((r), (c), "%12.3f %s  ",				\
-		 convert(d##_##c, SC_AUTOBYTE),				\
-		 get_string(d##_##c, SC_AUTOBYTE));			\
-} while (0)
-
-#define PUTRATE(c, r)	do {						\
-	DOPUTRATE(c, (r), IN);						\
-	DOPUTRATE(c, (r)+1, OUT);					\
-} while (0)
-
-#define PUTTOTAL(c, r)	do {						\
-	DOPUTTOTAL(c, (r), IN);						\
-	DOPUTTOTAL(c, (r)+1, OUT);					\
-} while (0)
-
-#define PUTNAME(p) do {							\
-	mvprintw(p->if_ypos, 0, "%s", p->if_name);			\
-	mvprintw(p->if_ypos, col2-3, "%s", (const char *)"in");		\
-	mvprintw(p->if_ypos+1, col2-3, "%s", (const char *)"out");	\
-} while (0)
-
+#define TOPLINE		1
+#define TOPQSTART	20
+#define TOPQWIDTH	10
 
 WINDOW *
-openifstat(void)
+openaltqs(void)
 {
-	struct   if_stat *p = NULL;
+	if_stat_t *p = NULL;
 	u_int	 n = 0, i = 0;
+
+	pf_fd = open("/dev/pf", O_RDONLY);
 
 	n = getifnum();		/* NOTE: can return < 0 */
 
 	SLIST_INIT(&curlist);
+	SLIST_INIT(&qcols);
 	for (i = 0; i < n; i++) {
-		p = (struct if_stat *)calloc(1, sizeof(struct if_stat));
+		p = (if_stat_t *)calloc(1, sizeof(if_stat_t));
 		if (p == NULL)
 			IFSTAT_ERR(1, "out of memory");
 		SLIST_INSERT_HEAD(&curlist, p, link);
+		SLIST_INIT(&p->queues);
 		p->if_row = i+1;
 		getifmibdata(p->if_row, &p->if_mib);
 		right_align_string(p);
@@ -169,6 +152,7 @@ openifstat(void)
 		if (p->if_mib.ifmd_data.ifi_ibytes != 0)
 			p->display = 1;
 	}
+	load_altqs();
 
 	sort_interface_list();
 
@@ -176,15 +160,27 @@ openifstat(void)
 }
 
 void
-closeifstat(WINDOW *w)
+closealtqs(WINDOW *w)
 {
-	struct if_stat	*node = NULL;
+	if_stat_t	*node = NULL;
+	queue_stats_t	*q;
 
 	while (!SLIST_EMPTY(&curlist)) {
 		node = SLIST_FIRST(&curlist);
 		SLIST_REMOVE_HEAD(&curlist, link);
+		while ((q = SLIST_FIRST(&node->queues)) != NULL) {
+			SLIST_REMOVE_HEAD(&node->queues, link);
+			free(q);
+		}
 		free(node);
 	}
+	while (!SLIST_EMPTY(&qcols)) {
+		qcol_t *qc = SLIST_FIRST(&qcols);
+		SLIST_REMOVE_HEAD(&qcols, link);
+		free(qc->qname);
+		free(qc);
+	}
+	qccols = 0;
 
 	if (w != NULL) {
 		wclear(w);
@@ -192,45 +188,62 @@ closeifstat(WINDOW *w)
 		delwin(w);
 	}
 
-	return;
-}
-
-
-void
-labelifstat(void)
-{
-
-	wmove(wnd, TOPLINE, 0);
-	wclrtoeol(wnd);
-	mvprintw(TOPLINE, 0, "%s", TOPLABEL);
-
-	return;
-}
-
-void
-showifstat(void)
-{
-	struct	if_stat *ifp = NULL;
-	SLIST_FOREACH(ifp, &curlist, link) {
-		if (ifp->display == 0)
-			continue;
-		PUTNAME(ifp);
-		PUTRATE(col2, ifp->if_ypos);
-		PUTRATE(col3, ifp->if_ypos);
-		PUTTOTAL(col4, ifp->if_ypos);
+	if (pf_fd >= 0) {
+		close(pf_fd);
+		pf_fd = -1;
 	}
 
 	return;
 }
 
-int
-initifstat(void)
+void
+labelaltqs(void)
 {
+	wmove(wnd, TOPLINE, 0);
+	wclrtoeol(wnd);
+}
+
+void
+showaltqs(void)
+{
+	if_stat_t *p = NULL;
+	queue_stats_t	*q;
+	qcol_t		*qc;
+
+	mvprintw(TopSection1, 0, "        PACKETS");
+	mvprintw(TopSection2, 0, "        BYTES");
+	mvprintw(TopSection3, 0, "        DROPS");
+	SLIST_FOREACH(qc, &qcols, link) {
+		mvprintw(TopSection1, TOPQSTART + TOPQWIDTH * qc->col,
+			 "%9s", qc->qname);
+		mvprintw(TopSection2, TOPQSTART + TOPQWIDTH * qc->col,
+			 "%9s", qc->qname);
+		mvprintw(TopSection3, TOPQSTART + TOPQWIDTH * qc->col,
+			 "%9s", qc->qname);
+	}
+
+	SLIST_FOREACH(p, &curlist, link) {
+		if (p->display == 0)
+			continue;
+		mvprintw(TopSection1 + p->row, 0, "%s", p->if_name);
+		mvprintw(TopSection2 + p->row, 0, "%s", p->if_name);
+		mvprintw(TopSection3 + p->row, 0, "%s", p->if_name);
+		SLIST_FOREACH(q, &p->queues, link) {
+			print_altq(p, q);
+		}
+	}
+}
+
+int
+initaltqs(void)
+{
+	TopSection1 = TOPLINE;
+
 	return 1;
 }
 
 void
-fetchifstat(void)
+fetchaltqs(void)
 {
 	struct	if_stat *ifp = NULL;
 	struct	timeval tv, new_tv, old_tv;
@@ -292,6 +305,8 @@ fetchifstat(void)
 
 	}
 
+	load_altqs();
+
 	if (we_need_to_sort_interface_list)
 		sort_interface_list();
 
@@ -303,7 +318,7 @@ fetchifstat(void)
  * (first sixteen or so characters), so we need to do some alignment.
  */
 static void
-right_align_string(struct if_stat *ifp)
+right_align_string(if_stat_t *ifp)
 {
 	int	 str_len = 0, pad_len = 0;
 	char	*newstr = NULL, *ptr = NULL;
@@ -329,7 +344,7 @@ right_align_string(struct if_stat *ifp)
  * This function iterates through our list of interfaces, identifying
  * those that are to be displayed (ifp->display = 1).  For each interf-
  * rface that we're displaying, we generate an appropriate position for
- * it on the screen (ifp->if_ypos).
+ * it on the screen (ifp->row).
  *
  * This function is called any time a change is made to an interface's
  * ``display'' state.
@@ -337,16 +352,16 @@ right_align_string(struct if_stat *ifp)
 void
 sort_interface_list(void)
 {
-	struct	if_stat	*ifp = NULL;
-	u_int	y = 0;
+	if_stat_t *ifp;
+	u_int y;
 
-	y = STARTING_ROW;
+	y = 1;
 	SLIST_FOREACH(ifp, &curlist, link) {
-		if (ifp->display) {
-			ifp->if_ypos = y;
-			y += ROW_SPACING;
-		}
+		if (ifp->display)
+			ifp->row = ++y;
 	}
+	TopSection2 = TopSection1 + y + 4;
+	TopSection3 = TopSection2 + y + 4;
 }
 
 static
@@ -386,15 +401,163 @@ getifmibdata(int row, struct ifmibdata *data)
 		IFSTAT_ERR(2, "sysctl error getting interface data");
 }
 
+static void
+load_altqs(void)
+{
+	struct pfioc_altq pa;
+	struct pfioc_qstats pq;
+	if_stat_t *p;
+	queue_stats_t *q;
+	qcol_t *qc;
+	int i;
+	int n;
+
+	bzero(&pa, sizeof(pa));
+	bzero(&pq, sizeof(pq));
+
+	if (ioctl(pf_fd, DIOCGETALTQS, &pa))
+		return;
+	n = pa.nr;
+	for (i = 0; i < n; ++i) {
+		pa.nr = i;
+		if (ioctl(pf_fd, DIOCGETALTQ, &pa))
+			return;
+		if (pa.altq.qid <= 0)
+			continue;
+
+		SLIST_FOREACH(p, &curlist, link) {
+			if (strcmp(pa.altq.ifname, p->if_mib.ifmd_name) == 0)
+				break;
+		}
+		if (p == NULL)
+			continue;
+		SLIST_FOREACH(q, &p->queues, link) {
+			if (strcmp(pa.altq.qname, q->pa.altq.qname) == 0)
+				break;
+		}
+		if (q == NULL) {
+			q = calloc(1, sizeof(*q));
+			q->pa = pa;
+			SLIST_INSERT_HEAD(&p->queues, q, link);
+		} else {
+			q->pa.ticket = pa.ticket;
+		}
+		q->ostats = q->nstats;
+		q->pq.nr = i;
+		q->pq.ticket = q->pa.ticket;
+		q->pq.buf = &q->nstats;
+		q->pq.nbytes = sizeof(q->nstats);
+		if (ioctl(pf_fd, DIOCGETQSTATS, &q->pq) < 0) {
+			SLIST_REMOVE(&p->queues, q, queue_stats, link);
+			free(q);
+		}
+		SLIST_FOREACH(qc, &qcols, link) {
+			if (strcmp(q->pa.altq.qname, qc->qname) == 0)
+				break;
+		}
+		if (qc == NULL) {
+			qc = calloc(1, sizeof(*qc));
+			qc->qname = strdup(q->pa.altq.qname);
+			qc->col = qccols++;
+			SLIST_INSERT_HEAD(&qcols, qc, link);
+		}
+		q->qc = qc;
+	}
+}
+
+static
+void
+print_altq(if_stat_t *p, queue_stats_t *q)
+{
+	uint64_t xmit_pkts;
+	uint64_t xmit_bytes;
+	uint64_t drop_pkts;
+	uint64_t drop_bytes __unused;
+
+	switch(q->pa.altq.scheduler) {
+	case ALTQT_CBQ:
+		xmit_pkts = q->nstats.cbq.xmit_cnt.packets;
+		xmit_bytes = q->nstats.cbq.xmit_cnt.bytes;
+		drop_pkts = q->nstats.cbq.drop_cnt.packets;
+		drop_bytes = q->nstats.cbq.drop_cnt.bytes;
+		xmit_pkts -= q->ostats.cbq.xmit_cnt.packets;
+		xmit_bytes -= q->ostats.cbq.xmit_cnt.bytes;
+		drop_pkts -= q->ostats.cbq.drop_cnt.packets;
+		drop_bytes -= q->ostats.cbq.drop_cnt.bytes;
+		break;
+	case ALTQT_PRIQ:
+		xmit_pkts = q->nstats.priq.xmitcnt.packets;
+		xmit_bytes = q->nstats.priq.xmitcnt.bytes;
+		drop_pkts = q->nstats.priq.dropcnt.packets;
+		drop_bytes = q->nstats.priq.dropcnt.bytes;
+		xmit_pkts -= q->ostats.priq.xmitcnt.packets;
+		xmit_bytes -= q->ostats.priq.xmitcnt.bytes;
+		drop_pkts -= q->ostats.priq.dropcnt.packets;
+		drop_bytes -= q->ostats.priq.dropcnt.bytes;
+		break;
+	case ALTQT_HFSC:
+		xmit_pkts = q->nstats.hfsc.xmit_cnt.packets;
+		xmit_bytes = q->nstats.hfsc.xmit_cnt.bytes;
+		drop_pkts = q->nstats.hfsc.drop_cnt.packets;
+		drop_bytes = q->nstats.hfsc.drop_cnt.bytes;
+		xmit_pkts -= q->ostats.hfsc.xmit_cnt.packets;
+		xmit_bytes -= q->ostats.hfsc.xmit_cnt.bytes;
+		drop_pkts -= q->ostats.hfsc.drop_cnt.packets;
+		drop_bytes -= q->ostats.hfsc.drop_cnt.bytes;
+		break;
+	case ALTQT_FAIRQ:
+		xmit_pkts = q->nstats.fairq.xmit_cnt.packets;
+		xmit_bytes = q->nstats.fairq.xmit_cnt.bytes;
+		drop_pkts = q->nstats.fairq.drop_cnt.packets;
+		drop_bytes = q->nstats.fairq.drop_cnt.bytes;
+		xmit_pkts -= q->ostats.fairq.xmit_cnt.packets;
+		xmit_bytes -= q->ostats.fairq.xmit_cnt.bytes;
+		drop_pkts -= q->ostats.fairq.drop_cnt.packets;
+		drop_bytes -= q->ostats.fairq.drop_cnt.bytes;
+		break;
+	default:
+		xmit_pkts = 0;
+		xmit_bytes = 0;
+		drop_pkts = 0;
+		drop_bytes = 0;
+		break;
+	}
+	if (xmit_pkts == 0)
+		mvprintw(TopSection1 + p->row,
+			 TOPQSTART + q->qc->col * TOPQWIDTH,
+			 "%9s", "");
+	else
+		mvprintw(TopSection1 + p->row,
+			 TOPQSTART + q->qc->col * TOPQWIDTH,
+			 "%9jd",  (intmax_t)xmit_pkts);
+
+	if (xmit_bytes == 0)
+		mvprintw(TopSection2 + p->row,
+			 TOPQSTART + q->qc->col * TOPQWIDTH,
+			 "%9s", "");
+	else
+		mvprintw(TopSection2 + p->row,
+			 TOPQSTART + q->qc->col * TOPQWIDTH,
+			 "%9jd",  (intmax_t)xmit_bytes);
+	if (drop_pkts == 0)
+		mvprintw(TopSection3 + p->row,
+			 TOPQSTART + q->qc->col * TOPQWIDTH,
+			 "%9s", "");
+	else
+		mvprintw(TopSection3 + p->row,
+			 TOPQSTART + q->qc->col * TOPQWIDTH,
+			 "%9jd",  (intmax_t)drop_pkts);
+}
+
 int
-cmdifstat(const char *cmd, char *args)
+cmdaltqs(const char *cmd, char *args)
 {
 	int	retval = 0;
 
 	retval = ifcmd(cmd, args);
 	/* ifcmd() returns 1 on success */
 	if (retval == 1) {
-		showifstat();
+		showaltqs();
 		refresh();
 	}
 
