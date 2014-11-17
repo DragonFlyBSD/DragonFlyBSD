DragonFlyBSD/DragonFlyBSD

commit efa391c40c414e7071b2041f1888180deb1806b9

icmp: Utilize in_cksum_skip()
This avoids unnecessary m_data and m_len adjustment before and after
in_cksum().

sys/netinet/ip_icmp.c
@@ -290,15 +290,12 @@ icmp_input(struct mbuf **mp, int *offp, int proto)
 		return(IPPROTO_DONE);
 	}
 	ip = mtod(m, struct ip *);
-	m->m_len -= hlen;
-	m->m_data += hlen;
-	icp = mtod(m, struct icmp *);
-	if (in_cksum(m, icmplen)) {
+
+	if (in_cksum_skip(m, hlen + icmplen, hlen)) {
 		icmpstat.icps_checksum++;
 		goto freeit;
 	}
-	m->m_len += hlen;
-	m->m_data -= hlen;
+	icp = mtod(m, struct icmp *);
 
 	if (m->m_pkthdr.rcvif && m->m_pkthdr.rcvif->if_type == IFT_FAITH) {
 		/*

commit a3f5db13444ab566a676a0f666d3e73ed27ac48b

icmp: Fix wrong icmp header cast
This was introduced by efa391c40c414e7071b2041f1888180deb1806b9

sys/netinet/ip_icmp.c
@@ -298,7 +298,7 @@ icmp_input(struct mbuf **mp, int *offp, int proto)
 		icmpstat.icps_checksum++;
 		goto freeit;
 	}
-	icp = mtod(m, struct icmp *);
+	icp = (struct icmp *)((caddr_t)ip + hlen);
 
 	if (m->m_pkthdr.rcvif && m->m_pkthdr.rcvif->if_type == IFT_FAITH) {
 		/*