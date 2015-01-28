d6f0f4f59d73c9576778a9d74154283a48a26981
Sascha Wildner <saw@online.de>
2015-01-28 21:58:53 +0100

    Remove <sys/_posix.h>. It serves no purpose anymore.

--- sys/sys/cdefs.h
+++ sys/sys/cdefs.h
@@ -496,8 +496,6 @@
 #endif
 
 /*-
- * The following definitions are an extension of the behavior originally
- * implemented in <sys/_posix.h>, but with a different level of granularity.
  * POSIX.1 requires that the macros we test be defined before any standard
  * header file is included.
  *
