1f8a8a66e45285a62691983936d453c541fbb15b
John Marino <draco@marino.st>
2015-02-10 22:15:00 +0100

    rtsol(8): Fix loop bug (aggressive-loop-optimizations)
    The index was being checked for validity after the array was accessed,
    putting the check first makes gcc 5.0 happy.

--- usr.sbin/rtsold/probe.c
+++ usr.sbin/rtsold/probe.c
@@ -121,7 +121,7 @@ defrouter_probe(int ifindex)
 		goto closeandend;
 	}
 
-	for(i = 0; dr.defrouter[i].if_index && i < PRLSTSIZ; i++) {
+	for(i = 0; i < PRLSTSIZ && dr.defrouter[i].if_index; i++) {
 		if (ifindex && dr.defrouter[i].if_index == ifindex) {
 			/* sanity check */
 			if (!IN6_IS_ADDR_LINKLOCAL(&dr.defrouter[i].rtaddr)) {
