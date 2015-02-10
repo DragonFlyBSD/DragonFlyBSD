748a243b13dd295c04c9e06923a801157f6943e5
John Marino <draco@marino.st>
2015-02-10 23:01:25 +0100

    keyserv(8): Fix size argument on debugging output
    This was another size of pointer problem.

--- usr.sbin/keyserv/keyserv.c
+++ usr.sbin/keyserv/keyserv.c
@@ -442,7 +442,7 @@ key_get_conv_2_svc_prog(uid_t uid, keybuf arg)
 
 	if (debugging)
 		fprintf(stderr, "get_conv(%ld, %.*s) = ", uid,
-			(int)sizeof (arg), arg);
+			(int)sizeof (xdrproc_t), arg);
 
 
 	res.status = pk_get_conv_key(uid, arg, &res);
