6776732e08b3c693d7558554dfa1d1ee52ac4c3b
John Marino <draco@marino.st>
2015-02-11 21:11:37 +0100

    Removal of unused gcc 4.4 sources and makefiles
    It's been confirmed that a gcc50-built world and kernel boots and
    functions as expected.  With that good news, it is safe to remove
    the gcc 4.4 sources and makefiles.  It wasn't building and this will
    make the cloning of a shallow repo faster.

--- contrib/gcc-4.4/gcc/config/i386/dragonfly64.h
+++
@@ -1,5 +0,0 @@
-/* Definitions for AMD x86_64 running DragonFly BSD with ELF Format */
-
-#undef  TARGET_VERSION
-#define TARGET_VERSION fprintf (stderr, " (x86-64 DragonFly/ELF)");
-
--- gnu/usr.bin/cc44/cc_prep/gthr-default.h
+++
@@ -1,1 +0,0 @@
-#include "gthr-posix.h"
