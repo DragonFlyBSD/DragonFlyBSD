4e1da0596d56f314dad38e2bde3eaec4b9131881
Sascha Wildner <saw@online.de>
2015-03-12 22:57:46 +0100

    kernel/usb4bsd: Unbreak 'template' module (new file missing in Makefile).

--- sys/bus/u4b/template/usb_template_serialnet.c
+++ sys/bus/u4b/template/usb_template_serialnet.c
@@ -38,7 +38,6 @@ __FBSDID("$FreeBSD$");
 #include USB_GLOBAL_INCLUDE_FILE
 #else
 #include <sys/stdint.h>
-#include <sys/stddef.h>
 #include <sys/param.h>
 #include <sys/queue.h>
 #include <sys/types.h>
@@ -50,18 +49,17 @@ __FBSDID("$FreeBSD$");
 #include <sys/mutex.h>
 #include <sys/condvar.h>
 #include <sys/sysctl.h>
-#include <sys/sx.h>
 #include <sys/unistd.h>
 #include <sys/callout.h>
 #include <sys/malloc.h>
 #include <sys/priv.h>
 
-#include <dev/usb/usb.h>
-#include <dev/usb/usbdi.h>
-#include <dev/usb/usb_core.h>
-#include <dev/usb/usb_cdc.h>
+#include <bus/u4b/usb.h>
+#include <bus/u4b/usbdi.h>
+#include <bus/u4b/usb_core.h>
+#include <bus/u4b/usb_cdc.h>
 
-#include <dev/usb/template/usb_template.h>
+#include <bus/u4b/template/usb_template.h>
 #endif		/* USB_GLOBAL_INCLUDE_FILE */
 
 #define	MODEM_IFACE_0 0
