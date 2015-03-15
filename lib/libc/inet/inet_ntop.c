731fb7dd6fc9ae2ce20ef7b3b26eac9bd2b31a21
Sascha Wildner <saw@online.de>
2015-03-15 22:40:02 +0100

    libc/inet: Add a missing errno on failure.
    Taken-from: FreeBSD (r261602)

--- lib/libc/inet/inet_ntop.c
+++ lib/libc/inet/inet_ntop.c
@@ -170,8 +170,10 @@ inet_ntop6(const u_char *src, char *dst, size_t size)
 		if (i == 6 && best.base == 0 && (best.len == 6 ||
 		    (best.len == 7 && words[7] != 0x0001) ||
 		    (best.len == 5 && words[5] == 0xffff))) {
-			if (!inet_ntop4(src+12, tp, sizeof tmp - (tp - tmp)))
+			if (!inet_ntop4(src+12, tp, sizeof tmp - (tp - tmp))) {
+				errno = ENOSPC;
 				return (NULL);
+			}
 			tp += strlen(tp);
 			break;
 		}
