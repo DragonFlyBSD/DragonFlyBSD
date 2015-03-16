04e8519d4e6317ef5c7589912e1c42d459a9cbba
Michael Neumann <mneumann@ntecs.de>
2015-03-16 17:50:18 +0100

    drm: Add firmware.h Linux compatiblity layer

---
+++ sys/dev/drm/include/linux/firmware.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2015 Michael Neumann
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice unmodified, this list of conditions, and the following
+ *    disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _LINUX_FIRMWARE_H_
+#define _LINUX_FIRMWARE_H_
+
+#include <sys/bus.h>
+#include <sys/firmware.h>
+
+#define MODULE_FIRMWARE(name)
+
+static inline int
+request_firmware(const struct firmware **fw, const char *name, __unused device_t dev) {
+	*fw = firmware_get(name);
+	if (*fw) {
+		return 0;
+	}
+	return -1;
+}
+
+static inline void
+release_firmware(const struct firmware *fw) {
+	firmware_put(fw, FIRMWARE_UNLOAD);
+}
+
+#endif	/* _LINUX_FIRMWARE_H_ */
