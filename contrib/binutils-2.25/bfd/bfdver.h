14d0585ff96dee3aef0960fd7967546ed92467c4
John Marino <draco@marino.st>
2015-03-30 20:44:57 +0200

    binutils 2.25: Add DF READMEs and local modifications

---
+++ contrib/binutils-2.25/bfd/bfdver.h
@@ -0,0 +1,4 @@
+#define BFD_VERSION_DATE 20141223
+#define BFD_VERSION 225000000
+#define BFD_VERSION_STRING  "(GNU Binutils) " "2.25"
+#define REPORT_BUGS_TO "<http://www.sourceware.org/bugzilla/>"
--- contrib/binutils-2.25/gold/options.cc
+++ contrib/binutils-2.25/gold/options.cc
@@ -1142,8 +1142,8 @@ General_options::finalize()
 	       || this->user_set_sysroot()
 	       || *TARGET_SYSTEM_ROOT != '\0')
 	{
-	  this->add_to_library_path_with_sysroot("/lib");
-	  this->add_to_library_path_with_sysroot("/usr/lib");
+	  this->add_to_library_path_with_sysroot(USRDATA_PREFIX "/lib");
+	  this->add_to_library_path_with_sysroot(USRDATA_PREFIX "/usr/lib");
 	}
       else
 	this->add_to_library_path_with_sysroot(TOOLLIBDIR);
