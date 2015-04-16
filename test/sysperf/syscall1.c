adc1b605934bacf302ccccade779ae0d96f51f2a
Matthew Dillon <dillon@apollo.backplane.com>
2015-04-16 09:39:01 -0700

    sysperf - Adjust syscall tests
    * Remove getuid_msg.c, no longer used.
    
    * Adjust syscall1.c to be more verbose.
    
    * Refactor syscall2.c to test on all threads in a somewhat more
      sophisticated manner.  The test expects the scheduler to move
      the threads to their own cpus (and it should).

--- test/sysperf/syscall1.c
+++ test/sysperf/syscall1.c
@@ -1,7 +1,7 @@
 /*
  * syscall1.c
  *
- * $DragonFly: src/test/sysperf/syscall1.c,v 1.3 2007/05/20 23:22:17 dillon Exp $
+ * Single thread getuid timing test.
  */
 
 #include "blib.h"
@@ -9,26 +9,27 @@
 int
 main(int ac, char **av)
 {
-    long long count = 0;
-    long long max;
-    char c;
-    int j;
+	long long count = 0;
+	long long max;
+	char c;
+	int j;
 
-    printf("timing standard getuid() syscall\n");
+	printf("timing standard getuid() syscall, single thread\n");
+	printf("if using powerd, run several times\n");
 
-    start_timing();
-    while (stop_timing(0, NULL) == 0) {
-	for (j = 0; j < 100; ++j)
-	    getuid();
-	count += 100;
-    }
-    max = count;
-    start_timing();
-    for (count = 0; count < max; count += 100) {
-	for (j = 0; j < 100; ++j)
-	    getuid();
-    }
-    stop_timing(count, "getuid()");
-    return(0);
+	start_timing();
+	while (stop_timing(0, NULL) == 0) {
+		for (j = 0; j < 100; ++j)
+			getuid();
+		count += 100;
+	}
+	max = count;
+	start_timing();
+	for (count = 0; count < max; count += 100) {
+		for (j = 0; j < 100; ++j)
+			getuid();
+	}
+	stop_timing(count, "getuid()");
+	return(0);
 }
 
--- test/sysperf/syscall3.c
+++ test/sysperf/syscall3.c
@@ -1,19 +1,15 @@
 /*
  * syscall3.c
- *
- * $DragonFly: src/test/sysperf/syscall3.c,v 1.2 2004/10/31 20:19:24 eirikn Exp $
  */
 
 #include "blib.h"
 
-extern int getuid_msg(void);
-
 int
 main(int ac, char **av)
 {
-    printf("(non timing) one process, endless loop calling getuid_msg()\n");
+    printf("(non timing) one process, endless loop calling getuid()\n");
     for (;;) 
-	getuid_msg();
+	getuid();
     /* not reached */
     return(0);
 }
