f4c88b0b86f1810db0482795bc097597d1197c54
Sascha Wildner <saw@online.de>
2015-07-04 01:16:34 +0200

    i386 removal, part 12/x: Remove sys/platform/pc32.

--- sys/platform/pc32/acpica/acpi_sdt_var.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ACPI_SDT_VAR_H_
-#define _ACPI_SDT_VAR_H_
-
-void		*sdt_sdth_map(vm_paddr_t);
-void		sdt_sdth_unmap(struct acpi_sdth *);
-
-vm_paddr_t	sdt_search(const uint8_t *);
-
-#endif	/* !_ACPI_SDT_VAR_H_ */
--- sys/platform/pc32/include/msi_machdep.h
+++
@@ -1,6 +0,0 @@
-#ifndef _ARCH_MSI_MACHDEP_H_
-#define	_ARCH_MSI_MACHDEP_H_
-
-#define MSI_X86_ADDR_BASE	0xfee00000
-
-#endif	/* !_ARCH_MSI_MACHDEP_H_ */
