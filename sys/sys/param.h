1b0bfe89d75e3553208c14302943d3a42007c5a2
John Marino <draco@marino.st>
2015-09-09 23:46:54 +0200

    bump __DragonFly_version for environ, __progname change
    The "environ" and "__progname" symbols no longer appear in libc.
    Certain linker flags can result in a link failure on these symbols.  This
    has been seen in llvm (likely requiring a patch for all versions to treat
    DragonFly like FreeBD).  Since this is a behavior change, I think it is
    worth a version bump in case makefiles have to check the version to
    adjust.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -125,9 +125,10 @@
  * 400300 - 4.3 development
  * 400301 - posix compliant iconv (no const qualifier)
  * 400302 - Replacement of libm with OpenBSD's libm
+ * 400303 - environ and __program are no longer linkable symbols
  */
 #undef __DragonFly_version
-#define __DragonFly_version 400302	/* propagated to newvers */
+#define __DragonFly_version 400303	/* propagated to newvers */
 
 #include <sys/_null.h>
 
