acf467cbfaf7ed6f2f14b125043829cb82aee35e
François Tigeot <ftigeot@wolfpond.org>
2015-09-13 12:46:59 +0200

    drm/i915: properly reenable gen8 pipe IRQs
    This is commit 18c22448d791a5c329528e495f19b639dd7a3dec from Linux 3.17.8
    Original author: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Original commit message:
    
    commit 1180e20606fd7c5d76dc5b2a1594fa51ba5a0f31 upstream.
    
    We were missing the pipe B/C vblank bits! Take a look at
    gen8_de_irq_postinstall for a comparison.
    
    This should fix a bunch of IGT tests.
    
    There are a few more things we could improve on this code, but this
    should be the minimal fix to unblock us.
    
    v2: s/extra_iir/extra_ier/ because IIR doesn't make sense (Ville)
    
    Bugzilla:https://bugs.freedesktop.org/show_bug.cgi?id=83640
    Testcase: igt/*
    Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- sys/dev/drm/i915/i915_irq.c
+++ sys/dev/drm/i915/i915_irq.c
@@ -3446,12 +3446,13 @@ static void gen8_irq_reset(struct drm_device *dev)
 
 void gen8_irq_power_well_post_enable(struct drm_i915_private *dev_priv)
 {
+	uint32_t extra_ier = GEN8_PIPE_VBLANK | GEN8_PIPE_FIFO_UNDERRUN;
 
 	lockmgr(&dev_priv->irq_lock, LK_EXCLUSIVE);
 	GEN8_IRQ_INIT_NDX(DE_PIPE, PIPE_B, dev_priv->de_irq_mask[PIPE_B],
-			  ~dev_priv->de_irq_mask[PIPE_B]);
+			  ~dev_priv->de_irq_mask[PIPE_B] | extra_ier);
 	GEN8_IRQ_INIT_NDX(DE_PIPE, PIPE_C, dev_priv->de_irq_mask[PIPE_C],
-			  ~dev_priv->de_irq_mask[PIPE_C]);
+			  ~dev_priv->de_irq_mask[PIPE_C] | extra_ier);
 	lockmgr(&dev_priv->irq_lock, LK_RELEASE);
 }
 
