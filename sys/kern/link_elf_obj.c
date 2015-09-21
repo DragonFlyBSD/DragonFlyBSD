96e935d23e2fe088e6edbff415efb2c365940b55
John Marino <draco@marino.st>
2015-09-21 17:48:53 +0200

    kernel: Handle zero-length ELF sections better
    FreeBSD simply skips all zero-length ELF sections when scanning an
    REL type file.  The first time we tried it, DF panicked with a lost
    bigs message.  It turns out this was because the skip should have been
    applied in 3 places and we missed the first one.
    
    The same approach is needed by the boot loader as well.

--- sys/kern/link_elf_obj.c
+++ sys/kern/link_elf_obj.c
@@ -515,6 +515,8 @@ link_elf_obj_load_file(const char *filename, linker_file_t * result)
 	symtabindex = -1;
 	symstrindex = -1;
 	for (i = 0; i < hdr->e_shnum; i++) {
+		if (shdr[i].sh_size == 0)
+			continue;
 		switch (shdr[i].sh_type) {
 		case SHT_PROGBITS:
 		case SHT_NOBITS:
@@ -617,12 +619,12 @@ link_elf_obj_load_file(const char *filename, linker_file_t * result)
 	/* Size up code/data(progbits) and bss(nobits). */
 	alignmask = 0;
 	for (i = 0; i < hdr->e_shnum; i++) {
+		if (shdr[i].sh_size == 0)
+			continue;
 		switch (shdr[i].sh_type) {
 		case SHT_PROGBITS:
 		case SHT_NOBITS:
 			alignmask = shdr[i].sh_addralign - 1;
-			if (alignmask == -1)
-                                alignmask = 0;
 			mapsize += alignmask;
 			mapsize &= ~alignmask;
 			mapsize += shdr[i].sh_size;
@@ -691,12 +693,12 @@ link_elf_obj_load_file(const char *filename, linker_file_t * result)
 	ra = 0;
 	alignmask = 0;
 	for (i = 0; i < hdr->e_shnum; i++) {
+		if (shdr[i].sh_size == 0)
+			continue;
 		switch (shdr[i].sh_type) {
 		case SHT_PROGBITS:
 		case SHT_NOBITS:
 			alignmask = shdr[i].sh_addralign - 1;
-			if (alignmask == -1)
-                                alignmask = 0;
 			mapbase += alignmask;
 			mapbase &= ~alignmask;
 			if (ef->shstrtab && shdr[i].sh_name != 0)
