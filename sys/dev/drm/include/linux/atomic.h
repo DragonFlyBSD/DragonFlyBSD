cee8e1413b6825bc88e8ddbffdeda7b69b828bc4
François Tigeot <ftigeot@wolfpond.org>
2015-10-31 10:02:16 +0100

    drm: Add atomic_cmpxchg()

--- sys/dev/drm/include/linux/atomic.h
+++ sys/dev/drm/include/linux/atomic.h
@@ -119,6 +119,11 @@ atomic_dec(atomic_t *v)
 	return atomic_fetchadd_int(&v->counter, -1) - 1;
 }
 
+static inline int atomic_cmpxchg(atomic_t *v, int old, int new)
+{
+	return atomic_cmpxchg_int(&v->counter, old, new);
+}
+
 static inline int atomic_add_unless(atomic_t *v, int add, int unless)
 {
         int c, old;
@@ -126,7 +131,6 @@ static inline int atomic_add_unless(atomic_t *v, int add, int unless)
         for (;;) {
                 if (unlikely(c == unless))
                         break;
-                // old = atomic_cmpxchg((v), c, c + (a)); /*Linux*/
                 old = atomic_cmpxchg_int(&v->counter, c, c + add);
                 if (likely(old == c))
                         break;
