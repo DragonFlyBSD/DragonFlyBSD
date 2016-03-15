9dac599db3677ed6fe9164e098db15b53f2501d9
Imre Vadász <imre@vdsz.com>
2016-03-15 23:06:24 +0100

    sys/sys/linker.h: Add MODINFOMD_FW_HANDLE constant, for EFI system table.
    * Will be used for passing the EFI System Table from the bootloader.
    
    Taken-From: FreeBSD

--- sys/sys/linker.h
+++ sys/sys/linker.h
@@ -277,6 +277,7 @@ int linker_ddb_symbol_values(c_linker_sym_t _sym, linker_symval_t *_symval);
 #define MODINFOMD_HOWTO         0x0007          /* (from 5.x) boothowto */
 #define MODINFOMD_KERNEND       0x0008          /* (from 5.x) kernend */
 #define MODINFOMD_SHDR		0x0009		/* section header table */
+#define MODINFOMD_FW_HANDLE	0x000a		/* Firmware dependent handle */
 #define MODINFOMD_NOCOPY	0x8000		/* don't copy this metadata to the kernel */
 
 #define MODINFOMD_DEPLIST	(0x4001 | MODINFOMD_NOCOPY)	/* depends on */
