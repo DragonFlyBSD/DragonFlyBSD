5b99e5f81e1bbf22afeafdb3394e73b06e8fa519
Sascha Wildner <saw@online.de>
2016-03-15 23:01:48 +0100

    pppd(8): Fix an apparent bug that caused GCC whining and raise WARNS to 2.
    Pointed-out-by: ivadasz
    Discussed-with: sephe, ivadasz

--- usr.sbin/pppd/auth.c
+++ usr.sbin/pppd/auth.c
@@ -1320,7 +1320,7 @@ ip_addr_check(u_int32_t addr, struct wordlist *addrs)
 	} else {
 	    np = getnetbyname (ptr_word);
 	    if (np != NULL && np->n_addrtype == AF_INET) {
-		a = htonl (*(u_int32_t *)np->n_net);
+		a = htonl (np->n_net);
 		if (ptr_mask == NULL) {
 		    /* calculate appropriate mask for net */
 		    ah = ntohl(a);
