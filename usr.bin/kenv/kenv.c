5b8e8af5d21404d5a642e0768881d6814910c134
Sascha Wildner <saw@online.de>
2016-03-19 21:51:42 +0100

    Sync the kenv(1) utility with FreeBSD.
    Some new options and it uses the kenv() system call now.

--- usr.bin/kenv/kenv.c
+++ usr.bin/kenv/kenv.c
@@ -1,4 +1,4 @@
-/*
+/*-
  * Copyright (c) 2000  Peter Wemm <peter@freebsd.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -22,41 +22,65 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $FreeBSD: src/usr.bin/kenv/kenv.c,v 1.1.2.2 2001/12/19 04:52:15 dd Exp $
- * $DragonFly: src/usr.bin/kenv/kenv.c,v 1.3 2005/02/25 14:55:51 joerg Exp $
+ * $FreeBSD: head/bin/kenv/kenv.c 250666 2013-05-15 18:38:28Z delphij $
  */
+
 #include <sys/types.h>
 #include <sys/sysctl.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
 #include <err.h>
+#include <kenv.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-static char sbuf[1024];
+static void	usage(void);
+static int	kdumpenv(void);
+static int	kgetenv(const char *);
+static int	ksetenv(const char *, char *);
+static int	kunsetenv(const char *);
+
+static int hflag = 0;
+static int Nflag = 0;
+static int qflag = 0;
+static int uflag = 0;
+static int vflag = 0;
 
 static void
 usage(void)
 {
-	(void)fprintf(stderr, "usage: kenv [-h] [variable]\n");
+	(void)fprintf(stderr, "%s\n%s\n%s\n",
+	    "usage: kenv [-hNq]",
+	    "       kenv [-qv] variable[=value]",
+	    "       kenv [-q] -u variable");
 	exit(1);
 }
 
 int
 main(int argc, char **argv)
 {
-	int real_oid[CTL_MAXNAME];
-	size_t oidlen, slen;
-	int ch, error, hflag, i;
-	char *env, *eq, *var, *val;
+	char *env, *eq, *val;
+	int ch, error;
 
-	hflag = 0;
+	error = 0;
+	val = NULL;
 	env = NULL;
-	while ((ch = getopt(argc, argv, "h")) != -1) {
+	while ((ch = getopt(argc, argv, "hNquv")) != -1) {
 		switch (ch) {
 		case 'h':
 			hflag++;
+			break;
+		case 'N':
+			Nflag++;
+			break;
+		case 'q':
+			qflag++;
+			break;
+		case 'u':
+			uflag++;
+			break;
+		case 'v':
+			vflag++;
 			break;
 		default:
 			usage();
@@ -66,63 +90,116 @@ main(int argc, char **argv)
 	argv += optind;
 	if (argc > 0) {
 		env = argv[0];
+		eq = strchr(env, '=');
+		if (eq != NULL) {
+			*eq++ = '\0';
+			val = eq;
+		}
 		argv++;
 		argc--;
 	}
-	if (argc > 0)
+	if ((hflag || Nflag) && env != NULL)
 		usage();
-	oidlen = __arysize(real_oid);
-	error = sysctlnametomib("kern.environment", real_oid, &oidlen);
-	if (error) 
-		err(1, "cannot find kern.environment base sysctl OID");
-	if (oidlen + 1 >= __arysize(real_oid))
-		errx(1, "kern.environment base OID too large");
-	real_oid[oidlen] = 0;
-	for (i = 0; ; i++) {
-		real_oid[oidlen + 1] = i;
-		slen = sizeof(sbuf) - 1;
-		error = sysctl(real_oid, oidlen + 2, sbuf, &slen, NULL, 0);
-		if (error < 0) {
-			if (errno != ENOENT)
-				err(1, "sysctl kern.environment.%d\n", i);
-			break;
+	if (argc > 0 || ((uflag || vflag) && env == NULL))
+		usage();
+	if (env == NULL) {
+		error = kdumpenv();
+		if (error && !qflag)
+			warn("kdumpenv");
+	} else if (val == NULL) {
+		if (uflag) {
+			error = kunsetenv(env);
+			if (error && !qflag)
+				warnx("unable to unset %s", env);
+		} else {
+			error = kgetenv(env);
+			if (error && !qflag)
+				warnx("unable to get %s", env);
 		}
-		sbuf[sizeof(sbuf) - 1] = '\0';
-		eq = strchr(sbuf, '=');
-		if (eq == NULL)
-			err(1, "malformed environment string: %s\n", sbuf);
-		var = sbuf;
-		*eq = '\0';
-		val = eq + 1;
-		if (env) {
-			if (strcmp(var, env) != 0)
-				continue;
-			printf("%s\n", val);
-			break;
-		}
-		if (hflag) {
-			if (strncmp(var, "hint.", 5) != 0)
-				continue;
-			/* FALLTHROUGH */
-		}
-		printf("%s=\"", var);
-		while (*val) {
-			switch (*val) {
-			case '"':
-				putchar('\\');
-				putchar('"');
-				break;
-			case '\\':
-				putchar('\\');
-				putchar('\\');
-				break;
-			default:
-				putchar(*val);
-				break;
-			}
-			val++;
-		}
-		printf("\"\n");
+	} else {
+		error = ksetenv(env, val);
+		if (error && !qflag)
+			warnx("unable to set %s to %s", env, val);
 	}
-	exit(0);
+	return (error);
+}
+
+static int
+kdumpenv(void)
+{
+	char *buf, *cp;
+	int buflen, envlen;
+
+	envlen = kenv(KENV_DUMP, NULL, NULL, 0);
+	if (envlen < 0)
+		return (-1);
+	for (;;) {
+		buflen = envlen * 120 / 100;
+		buf = malloc(buflen + 1);
+		if (buf == NULL)
+			return (-1);
+		memset(buf, 0, buflen + 1);	/* Be defensive */
+		envlen = kenv(KENV_DUMP, NULL, buf, buflen);
+		if (envlen < 0) {
+			free(buf);
+			return (-1);
+		}
+		if (envlen > buflen)
+			free(buf);
+		else
+			break;
+	}
+
+	for (; *buf != '\0'; buf += strlen(buf) + 1) {
+		if (hflag) {
+			if (strncmp(buf, "hint.", 5) != 0)
+				continue;
+		}
+		cp = strchr(buf, '=');
+		if (cp == NULL)
+			continue;
+		*cp++ = '\0';
+		if (Nflag)
+			printf("%s\n", buf);
+		else
+			printf("%s=\"%s\"\n", buf, cp);
+		buf = cp;
+	}
+	return (0);
+}
+
+static int
+kgetenv(const char *env)
+{
+	char buf[1024];
+	int ret;
+
+	ret = kenv(KENV_GET, env, buf, sizeof(buf));
+	if (ret == -1)
+		return (ret);
+	if (vflag)
+		printf("%s=\"%s\"\n", env, buf);
+	else
+		printf("%s\n", buf);
+	return (0);
+}
+
+static int
+ksetenv(const char *env, char *val)
+{
+	int ret;
+
+	ret = kenv(KENV_SET, env, val, strlen(val)+1);
+	if (ret == 0)
+		printf("%s=\"%s\"\n", env, val);
+	return (ret);
+}
+
+static int
+kunsetenv(const char *env)
+{
+	int ret;
+
+	ret = kenv(KENV_UNSET, env, NULL, 0);
+	return (ret);
 }
