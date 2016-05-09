DragonFlyBSD/DragonFlyBSD

commit bd519ccc430654ba519981ec2781920010d5bb19

drm/radeon: Partial sync with Linux 3.18

sys/dev/drm/radeon/cik.c
@@ -8233,7 +8233,7 @@ restart_ih:
 		WREG32(IH_RB_RPTR, rptr);
 	}
 	if (queue_hotplug)
-		taskqueue_enqueue(rdev->tq, &rdev->hotplug_work);
+		schedule_work(&rdev->hotplug_work);
 	if (queue_reset) {
 		rdev->needs_reset = true;
 		wake_up_all(&rdev->fence_queue);

sys/dev/drm/radeon/evergreen.c
@@ -5131,7 +5131,7 @@ restart_ih:
 		WREG32(IH_RB_RPTR, rptr);
 	}
 	if (queue_hotplug)
-		taskqueue_enqueue(rdev->tq, &rdev->hotplug_work);
+		schedule_work(&rdev->hotplug_work);
 	if (queue_hdmi)
 		taskqueue_enqueue(rdev->tq, &rdev->audio_work);
 	if (queue_thermal && rdev->pm.dpm_enabled)

sys/dev/drm/radeon/r100.c
@@ -796,7 +796,7 @@ irqreturn_t r100_irq_process(struct radeon_device *rdev)
 		status = r100_irq_ack(rdev);
 	}
 	if (queue_hotplug)
-		taskqueue_enqueue(rdev->tq, &rdev->hotplug_work);
+		schedule_work(&rdev->hotplug_work);
 	if (rdev->msi_enabled) {
 		switch (rdev->family) {
 		case CHIP_RS400:

sys/dev/drm/radeon/r600.c
@@ -4213,7 +4213,7 @@ restart_ih:
 		WREG32(IH_RB_RPTR, rptr);
 	}
 	if (queue_hotplug)
-		taskqueue_enqueue(rdev->tq, &rdev->hotplug_work);
+		schedule_work(&rdev->hotplug_work);
 	if (queue_hdmi)
 		taskqueue_enqueue(rdev->tq, &rdev->audio_work);
 	if (queue_thermal && rdev->pm.dpm_enabled)

sys/dev/drm/radeon/radeon.h
@@ -24,8 +24,6 @@
  * Authors: Dave Airlie
  *          Alex Deucher
  *          Jerome Glisse
- *
- * $FreeBSD: head/sys/dev/drm2/radeon/radeon.h 254885 2013-08-25 19:37:15Z dumbbell $
  */
 
 #ifndef __RADEON_H__
@@ -72,6 +70,13 @@
 
 #include <contrib/dev/acpica/source/include/acpi.h>
 #include <dev/acpica/acpivar.h>
+
+#include <linux/atomic.h>
+#include <linux/wait.h>
+#include <linux/list.h>
+#include <linux/kref.h>
+#include <linux/hashtable.h>
+#include <linux/fence.h>
 
 #include <drm/ttm/ttm_bo_api.h>
 #include <drm/ttm/ttm_bo_driver.h>
@@ -359,17 +364,18 @@ struct radeon_fence_driver {
 	/* sync_seq is protected by ring emission lock */
 	uint64_t			sync_seq[RADEON_NUM_RINGS];
 	atomic64_t			last_seq;
-	bool				initialized;
+	bool				initialized, delayed_irq;
 	struct delayed_work		lockup_work;
 };
 
 struct radeon_fence {
 	struct radeon_device		*rdev;
 	unsigned int			kref;
-	/* protected by radeon_fence.lock */
 	uint64_t			seq;
 	/* RB, DMA, etc. */
 	unsigned			ring;
+
+	wait_queue_t			fence_wake;
 };
 
 int radeon_fence_driver_start_ring(struct radeon_device *rdev, int ring);
@@ -788,6 +794,7 @@ struct radeon_irq {
 int radeon_irq_kms_init(struct radeon_device *rdev);
 void radeon_irq_kms_fini(struct radeon_device *rdev);
 void radeon_irq_kms_sw_irq_get(struct radeon_device *rdev, int ring);
+bool radeon_irq_kms_sw_irq_get_delayed(struct radeon_device *rdev, int ring);
 void radeon_irq_kms_sw_irq_put(struct radeon_device *rdev, int ring);
 void radeon_irq_kms_pflip_irq_get(struct radeon_device *rdev, int crtc);
 void radeon_irq_kms_pflip_irq_put(struct radeon_device *rdev, int crtc);
@@ -2354,7 +2361,7 @@ struct radeon_device {
 	struct radeon_rlc rlc;
 	struct radeon_mec mec;
 	struct taskqueue *tq;
-	struct task hotplug_work;
+	struct work_struct hotplug_work;
 	struct task audio_work;
 	int num_crtc; /* number of crtcs */
 	struct lock dc_hw_i2c_mutex; /* display controller hw i2c mutex */

sys/dev/drm/radeon/radeon_irq_kms.c
@@ -42,7 +42,7 @@
 /**
  * radeon_driver_irq_handler_kms - irq handler for KMS
  *
- * @void *arg: args
+ * @int irq, void *arg: args
  *
  * This is the irq handler for the radeon KMS driver (all asics).
  * radeon_irq_process is a macro that points to the per-asic
@@ -78,9 +78,10 @@ irqreturn_t radeon_driver_irq_handler_kms(void *arg)
  * and calls the hotplug handler for each one, then sends
  * a drm hotplug event to alert userspace.
  */
-static void radeon_hotplug_work_func(void *arg, int pending)
+static void radeon_hotplug_work_func(struct work_struct *work)
 {
-	struct radeon_device *rdev = arg;
+	struct radeon_device *rdev = container_of(work, struct radeon_device,
+						  hotplug_work);
 	struct drm_device *dev = rdev->ddev;
 	struct drm_mode_config *mode_config = &dev->mode_config;
 	struct drm_connector *connector;
@@ -265,16 +266,14 @@ int radeon_irq_kms_init(struct radeon_device *rdev)
 	/* enable msi */
 	rdev->msi_enabled = (rdev->ddev->irq_type == PCI_INTR_TYPE_MSI);
 
-	TASK_INIT(&rdev->hotplug_work, 0, radeon_hotplug_work_func, rdev);
+	INIT_WORK(&rdev->hotplug_work, radeon_hotplug_work_func);
 	TASK_INIT(&rdev->audio_work, 0, r600_audio_update_hdmi, rdev);
 
 	rdev->irq.installed = true;
-	DRM_UNLOCK(rdev->ddev);
 	r = drm_irq_install(rdev->ddev, rdev->ddev->irq);
-	DRM_LOCK(rdev->ddev);
 	if (r) {
 		rdev->irq.installed = false;
-		taskqueue_drain(rdev->tq, &rdev->hotplug_work);
+		flush_work(&rdev->hotplug_work);
 		return r;
 	}
 
@@ -295,7 +294,7 @@ void radeon_irq_kms_fini(struct radeon_device *rdev)
 	if (rdev->irq.installed) {
 		drm_irq_uninstall(rdev->ddev);
 		rdev->irq.installed = false;
-		taskqueue_drain(rdev->tq, &rdev->hotplug_work);
+		flush_work(&rdev->hotplug_work);
 	}
 }
 
@@ -319,6 +318,21 @@ void radeon_irq_kms_sw_irq_get(struct radeon_device *rdev, int ring)
 		radeon_irq_set(rdev);
 		lockmgr(&rdev->irq.lock, LK_RELEASE);
 	}
+}
+
+/**
+ * radeon_irq_kms_sw_irq_get_delayed - enable software interrupt
+ *
+ * @rdev: radeon device pointer
+ * @ring: ring whose interrupt you want to enable
+ *
+ * Enables the software interrupt for a specific ring (all asics).
+ * The software interrupt is generally used to signal a fence on
+ * a particular ring.
+ */
+bool radeon_irq_kms_sw_irq_get_delayed(struct radeon_device *rdev, int ring)
+{
+	return atomic_inc_return(&rdev->irq.ring_int[ring]) == 1;
 }
 
 /**

sys/dev/drm/radeon/rs600.c
@@ -803,7 +803,7 @@ irqreturn_t rs600_irq_process(struct radeon_device *rdev)
 		status = rs600_irq_ack(rdev);
 	}
 	if (queue_hotplug)
-		taskqueue_enqueue(rdev->tq, &rdev->hotplug_work);
+		schedule_work(&rdev->hotplug_work);
 	if (queue_hdmi)
 		taskqueue_enqueue(rdev->tq, &rdev->audio_work);
 	if (rdev->msi_enabled) {

sys/dev/drm/radeon/si.c
@@ -6689,7 +6689,7 @@ restart_ih:
 		WREG32(IH_RB_RPTR, rptr);
 	}
 	if (queue_hotplug)
-		taskqueue_enqueue(rdev->tq, &rdev->hotplug_work);
+		schedule_work(&rdev->hotplug_work);
 	if (queue_thermal && rdev->pm.dpm_enabled)
 		taskqueue_enqueue(rdev->tq, &rdev->pm.dpm.thermal.work);
 	rdev->ih.rptr = rptr;

commit d027657a0e0cb14764e32c792359ce5d56d943c0

drm/radeon: Revert "drm/radeon: Partial sync with Linux 3.18"
This partially reverts commit bd519ccc430654ba519981ec2781920010d5bb19

Caused driver teardown problems.

sys/dev/drm/radeon/cik.c
@@ -8233,7 +8233,7 @@ restart_ih:
 		WREG32(IH_RB_RPTR, rptr);
 	}
 	if (queue_hotplug)
-		schedule_work(&rdev->hotplug_work);
+		taskqueue_enqueue(rdev->tq, &rdev->hotplug_work);
 	if (queue_reset) {
 		rdev->needs_reset = true;
 		wake_up_all(&rdev->fence_queue);

sys/dev/drm/radeon/evergreen.c
@@ -5131,7 +5131,7 @@ restart_ih:
 		WREG32(IH_RB_RPTR, rptr);
 	}
 	if (queue_hotplug)
-		schedule_work(&rdev->hotplug_work);
+		taskqueue_enqueue(rdev->tq, &rdev->hotplug_work);
 	if (queue_hdmi)
 		taskqueue_enqueue(rdev->tq, &rdev->audio_work);
 	if (queue_thermal && rdev->pm.dpm_enabled)

sys/dev/drm/radeon/r100.c
@@ -796,7 +796,7 @@ irqreturn_t r100_irq_process(struct radeon_device *rdev)
 		status = r100_irq_ack(rdev);
 	}
 	if (queue_hotplug)
-		schedule_work(&rdev->hotplug_work);
+		taskqueue_enqueue(rdev->tq, &rdev->hotplug_work);
 	if (rdev->msi_enabled) {
 		switch (rdev->family) {
 		case CHIP_RS400:

sys/dev/drm/radeon/r600.c
@@ -4213,7 +4213,7 @@ restart_ih:
 		WREG32(IH_RB_RPTR, rptr);
 	}
 	if (queue_hotplug)
-		schedule_work(&rdev->hotplug_work);
+		taskqueue_enqueue(rdev->tq, &rdev->hotplug_work);
 	if (queue_hdmi)
 		taskqueue_enqueue(rdev->tq, &rdev->audio_work);
 	if (queue_thermal && rdev->pm.dpm_enabled)

sys/dev/drm/radeon/radeon.h
@@ -364,18 +364,17 @@ struct radeon_fence_driver {
 	/* sync_seq is protected by ring emission lock */
 	uint64_t			sync_seq[RADEON_NUM_RINGS];
 	atomic64_t			last_seq;
-	bool				initialized, delayed_irq;
+	bool				initialized;
 	struct delayed_work		lockup_work;
 };
 
 struct radeon_fence {
 	struct radeon_device		*rdev;
 	unsigned int			kref;
+	/* protected by radeon_fence.lock */
 	uint64_t			seq;
 	/* RB, DMA, etc. */
 	unsigned			ring;
-
-	wait_queue_t			fence_wake;
 };
 
 int radeon_fence_driver_start_ring(struct radeon_device *rdev, int ring);
@@ -794,7 +793,6 @@ struct radeon_irq {
 int radeon_irq_kms_init(struct radeon_device *rdev);
 void radeon_irq_kms_fini(struct radeon_device *rdev);
 void radeon_irq_kms_sw_irq_get(struct radeon_device *rdev, int ring);
-bool radeon_irq_kms_sw_irq_get_delayed(struct radeon_device *rdev, int ring);
 void radeon_irq_kms_sw_irq_put(struct radeon_device *rdev, int ring);
 void radeon_irq_kms_pflip_irq_get(struct radeon_device *rdev, int crtc);
 void radeon_irq_kms_pflip_irq_put(struct radeon_device *rdev, int crtc);
@@ -2361,7 +2359,7 @@ struct radeon_device {
 	struct radeon_rlc rlc;
 	struct radeon_mec mec;
 	struct taskqueue *tq;
-	struct work_struct hotplug_work;
+	struct task hotplug_work;
 	struct task audio_work;
 	int num_crtc; /* number of crtcs */
 	struct lock dc_hw_i2c_mutex; /* display controller hw i2c mutex */

sys/dev/drm/radeon/radeon_irq_kms.c
@@ -42,7 +42,7 @@
 /**
  * radeon_driver_irq_handler_kms - irq handler for KMS
  *
- * @int irq, void *arg: args
+ * @void *arg: args
  *
  * This is the irq handler for the radeon KMS driver (all asics).
  * radeon_irq_process is a macro that points to the per-asic
@@ -78,10 +78,9 @@ irqreturn_t radeon_driver_irq_handler_kms(void *arg)
  * and calls the hotplug handler for each one, then sends
  * a drm hotplug event to alert userspace.
  */
-static void radeon_hotplug_work_func(struct work_struct *work)
+static void radeon_hotplug_work_func(void *arg, int pending)
 {
-	struct radeon_device *rdev = container_of(work, struct radeon_device,
-						  hotplug_work);
+	struct radeon_device *rdev = arg;
 	struct drm_device *dev = rdev->ddev;
 	struct drm_mode_config *mode_config = &dev->mode_config;
 	struct drm_connector *connector;
@@ -266,14 +265,16 @@ int radeon_irq_kms_init(struct radeon_device *rdev)
 	/* enable msi */
 	rdev->msi_enabled = (rdev->ddev->irq_type == PCI_INTR_TYPE_MSI);
 
-	INIT_WORK(&rdev->hotplug_work, radeon_hotplug_work_func);
+	TASK_INIT(&rdev->hotplug_work, 0, radeon_hotplug_work_func, rdev);
 	TASK_INIT(&rdev->audio_work, 0, r600_audio_update_hdmi, rdev);
 
 	rdev->irq.installed = true;
+	DRM_UNLOCK(rdev->ddev);
 	r = drm_irq_install(rdev->ddev, rdev->ddev->irq);
+	DRM_LOCK(rdev->ddev);
 	if (r) {
 		rdev->irq.installed = false;
-		flush_work(&rdev->hotplug_work);
+		taskqueue_drain(rdev->tq, &rdev->hotplug_work);
 		return r;
 	}
 
@@ -294,7 +295,7 @@ void radeon_irq_kms_fini(struct radeon_device *rdev)
 	if (rdev->irq.installed) {
 		drm_irq_uninstall(rdev->ddev);
 		rdev->irq.installed = false;
-		flush_work(&rdev->hotplug_work);
+		taskqueue_drain(rdev->tq, &rdev->hotplug_work);
 	}
 }
 
@@ -330,10 +331,12 @@ void radeon_irq_kms_sw_irq_get(struct radeon_device *rdev, int ring)
  * The software interrupt is generally used to signal a fence on
  * a particular ring.
  */
+#ifdef TODO_954605c
 bool radeon_irq_kms_sw_irq_get_delayed(struct radeon_device *rdev, int ring)
 {
 	return atomic_inc_return(&rdev->irq.ring_int[ring]) == 1;
 }
+#endif
 
 /**
  * radeon_irq_kms_sw_irq_put - disable software interrupt

sys/dev/drm/radeon/rs600.c
@@ -803,7 +803,7 @@ irqreturn_t rs600_irq_process(struct radeon_device *rdev)
 		status = rs600_irq_ack(rdev);
 	}
 	if (queue_hotplug)
-		schedule_work(&rdev->hotplug_work);
+		taskqueue_enqueue(rdev->tq, &rdev->hotplug_work);
 	if (queue_hdmi)
 		taskqueue_enqueue(rdev->tq, &rdev->audio_work);
 	if (rdev->msi_enabled) {

sys/dev/drm/radeon/si.c
@@ -6689,7 +6689,7 @@ restart_ih:
 		WREG32(IH_RB_RPTR, rptr);
 	}
 	if (queue_hotplug)
-		schedule_work(&rdev->hotplug_work);
+		taskqueue_enqueue(rdev->tq, &rdev->hotplug_work);
 	if (queue_thermal && rdev->pm.dpm_enabled)
 		taskqueue_enqueue(rdev->tq, &rdev->pm.dpm.thermal.work);
 	rdev->ih.rptr = rptr;