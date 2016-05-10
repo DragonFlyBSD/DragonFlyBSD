d920e19426323722b7569dcf0674234dcb322121
zrj <rimvydas.jasinskas@gmail.com>
2016-05-10 09:07:52 +0300

    drm/radeon: Nuke some orphaned externs.
    Spotted-by: swildner

--- sys/dev/drm/radeon/evergreen_blit_shaders.h
+++ sys/dev/drm/radeon/evergreen_blit_shaders.h
@@ -25,11 +25,8 @@
 #ifndef EVERGREEN_BLIT_SHADERS_H
 #define EVERGREEN_BLIT_SHADERS_H
 
-extern const u32 evergreen_ps[];
-extern const u32 evergreen_vs[];
 extern const u32 evergreen_default_state[];
 
-extern const u32 evergreen_ps_size, evergreen_vs_size;
 extern const u32 evergreen_default_size;
 
 #endif
