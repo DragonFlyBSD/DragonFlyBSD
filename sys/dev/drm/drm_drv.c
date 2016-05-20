240f0ad6e4174ffec8892758ad94f744ef8a4970
zrj <rimvydas.jasinskas@gmail.com>
2016-05-20 19:02:09 +0300

    drm/radeon: Print device description after it gets set.
    Noticed while checking debug outputs with drm.debug=0x21f
    
    Changes:
     -[drm:drm_probe] desc : (null)
     +[drm:drm_probe] desc : Oland PRO [Radeon R7 240]
    and
     -[drm:drm_probe] desc : (null)
     +[drm:drm_probe] desc : Tobago PRO [Radeon R7 360 / R9 360 OEM]

--- sys/dev/drm/drm_drv.c
+++ sys/dev/drm/drm_drv.c
@@ -933,8 +933,8 @@ int drm_probe(device_t kdev, drm_pci_id_list_t *idlist)
 	id_entry = drm_find_description(vendor, device, idlist);
 	if (id_entry != NULL) {
 		if (!device_get_desc(kdev)) {
-			DRM_DEBUG("desc : %s\n", device_get_desc(kdev));
 			device_set_desc(kdev, id_entry->name);
+			DRM_DEBUG("desc : %s\n", device_get_desc(kdev));
 		}
 		return 0;
 	}
