b4d214925c8f708d2225d8a54f8bb2fa49238618
Matthew Dillon <dillon@apollo.backplane.com>
2016-07-28 09:39:57 -0700

    kernel - Fix getpid() issue in vfork() when threaded
    * upmap->invfork was a 0 or 1, but in a threaded program it is possible
      for multiple threads to be in vfork() at the same time.  Change invfork
      to a count.
    
    * Fixes improper getpid() return when concurrent vfork()s are occuring in
      a threaded program.

--- sys/kern/kern_exit.c
+++ sys/kern/kern_exit.c
@@ -498,7 +498,7 @@ exit1(int rv)
 	 */
 	if (p->p_flags & P_PPWAIT) {
 		if (p->p_pptr && p->p_pptr->p_upmap)
-			p->p_pptr->p_upmap->invfork = 0;
+			atomic_add_int(&p->p_pptr->p_upmap->invfork, -1);
 		atomic_clear_int(&p->p_flags, P_PPWAIT);
 		wakeup(p->p_pptr);
 	}
