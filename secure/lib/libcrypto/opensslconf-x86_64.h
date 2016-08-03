cb698b8d44ed60e0d61817dd229b55039d876be6
Peter Avalos <pavalos@dragonflybsd.org>
2016-08-03 12:05:00 -0700

    Add IDEA support for libcrypto(3).
    The patent expired years ago.

--- secure/lib/libcrypto/opensslconf-x86_64.h
+++ secure/lib/libcrypto/opensslconf-x86_64.h
@@ -14,9 +14,6 @@ extern "C" {
 #ifndef OPENSSL_NO_GMP
 # define OPENSSL_NO_GMP
 #endif
-#ifndef OPENSSL_NO_IDEA
-# define OPENSSL_NO_IDEA
-#endif
 #ifndef OPENSSL_NO_JPAKE
 # define OPENSSL_NO_JPAKE
 #endif
@@ -73,9 +70,6 @@ extern "C" {
 # endif
 # if defined(OPENSSL_NO_GMP) && !defined(NO_GMP)
 #  define NO_GMP
-# endif
-# if defined(OPENSSL_NO_IDEA) && !defined(NO_IDEA)
-#  define NO_IDEA
 # endif
 # if defined(OPENSSL_NO_JPAKE) && !defined(NO_JPAKE)
 #  define NO_JPAKE
