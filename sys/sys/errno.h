329111df7bb1e6c835b3e1835b384ecf2dd3aaf7
Sascha Wildner <saw@online.de>
2016-08-13 21:24:19 +0200

    <sys/errno.h>: Use __BSD_VISIBLE instead of !_POSIX_SOURCE.
    _POSIX_SOURCE is no longer set since it was superseded by
    _POSIX_C_SOURCE. In other words, #ifndef _POSIX_SOURCE is
    always true, unless IEEE Std 1003.1-1988 is explicitly
    requested.
    
    While here, clean up the visibility of all errno defines.
    Those not mentioned in any standard are __BSD_VISIBLE now.
    We can further restrict the standard ones at some later
    point.

--- sys/sys/errno.h
+++ sys/sys/errno.h
@@ -67,9 +67,9 @@ static __inline int *__error(void)
 #define	ENOMEM		12		/* Cannot allocate memory */
 #define	EACCES		13		/* Permission denied */
 #define	EFAULT		14		/* Bad address */
-#ifndef _POSIX_SOURCE
+#if __BSD_VISIBLE
 #define	ENOTBLK		15		/* Block device required */
-#endif
+#endif /* __BSD_VISIBLE */
 #define	EBUSY		16		/* Device busy */
 #define	EEXIST		17		/* File exists */
 #define	EXDEV		18		/* Cross-device link */
@@ -80,9 +80,7 @@ static __inline int *__error(void)
 #define	ENFILE		23		/* Too many open files in system */
 #define	EMFILE		24		/* Too many open files */
 #define	ENOTTY		25		/* Inappropriate ioctl for device */
-#ifndef _POSIX_SOURCE
 #define	ETXTBSY		26		/* Text file busy */
-#endif
 #define	EFBIG		27		/* File too large */
 #define	ENOSPC		28		/* No space left on device */
 #define	ESPIPE		29		/* Illegal seek */
@@ -96,7 +94,6 @@ static __inline int *__error(void)
 
 /* non-blocking and interrupt i/o */
 #define	EAGAIN		35		/* Resource temporarily unavailable */
-#ifndef _POSIX_SOURCE
 #define	EWOULDBLOCK	EAGAIN		/* Operation would block */
 #define	EINPROGRESS	36		/* Operation now in progress */
 #define	EALREADY	37		/* Operation already in progress */
@@ -108,10 +105,14 @@ static __inline int *__error(void)
 #define	EPROTOTYPE	41		/* Protocol wrong type for socket */
 #define	ENOPROTOOPT	42		/* Protocol not available */
 #define	EPROTONOSUPPORT	43		/* Protocol not supported */
+#if __BSD_VISIBLE
 #define	ESOCKTNOSUPPORT	44		/* Socket type not supported */
+#endif /*__BSD_VISIBLE */
 #define	EOPNOTSUPP	45		/* Operation not supported */
 #define	ENOTSUP		EOPNOTSUPP	/* Operation not supported */
+#if __BSD_VISIBLE
 #define	EPFNOSUPPORT	46		/* Protocol family not supported */
+#endif /* __BSD_VISIBLE */
 #define	EAFNOSUPPORT	47		/* Address family not supported by protocol family */
 #define	EADDRINUSE	48		/* Address already in use */
 #define	EADDRNOTAVAIL	49		/* Can't assign requested address */
@@ -125,60 +126,63 @@ static __inline int *__error(void)
 #define	ENOBUFS		55		/* No buffer space available */
 #define	EISCONN		56		/* Socket is already connected */
 #define	ENOTCONN	57		/* Socket is not connected */
+#if __BSD_VISIBLE
 #define	ESHUTDOWN	58		/* Can't send after socket shutdown */
 #define	ETOOMANYREFS	59		/* Too many references: can't splice */
+#endif /* __BSD_VISIBLE */
 #define	ETIMEDOUT	60		/* Operation timed out */
 #define	ECONNREFUSED	61		/* Connection refused */
 
 #define	ELOOP		62		/* Too many levels of symbolic links */
-#endif /* _POSIX_SOURCE */
 #define	ENAMETOOLONG	63		/* File name too long */
 
 /* should be rearranged */
-#ifndef _POSIX_SOURCE
 #define	EHOSTDOWN	64		/* Host is down */
 #define	EHOSTUNREACH	65		/* No route to host */
-#endif /* _POSIX_SOURCE */
 #define	ENOTEMPTY	66		/* Directory not empty */
 
 /* quotas & mush */
-#ifndef _POSIX_SOURCE
+#if __BSD_VISIBLE
 #define	EPROCLIM	67		/* Too many processes */
 #define	EUSERS		68		/* Too many users */
+#endif /* __BSD_VISIBLE */
 #define	EDQUOT		69		/* Disc quota exceeded */
 
 /* Network File System */
 #define	ESTALE		70		/* Stale NFS file handle */
+#if __BSD_VISIBLE
 #define	EREMOTE		71		/* Too many levels of remote in path */
 #define	EBADRPC		72		/* RPC struct is bad */
 #define	ERPCMISMATCH	73		/* RPC version wrong */
 #define	EPROGUNAVAIL	74		/* RPC prog. not avail */
 #define	EPROGMISMATCH	75		/* Program version wrong */
 #define	EPROCUNAVAIL	76		/* Bad procedure for program */
-#endif /* _POSIX_SOURCE */
+#endif /* __BSD_VISIBLE */
 
 #define	ENOLCK		77		/* No locks available */
 #define	ENOSYS		78		/* Function not implemented */
 
-#ifndef _POSIX_SOURCE
+#if __BSD_VISIBLE
 #define	EFTYPE		79		/* Inappropriate file type or format */
 #define	EAUTH		80		/* Authentication error */
 #define	ENEEDAUTH	81		/* Need authenticator */
+#endif /* __BSD_VISIBLE */
 #define	EIDRM		82		/* Identifier removed */
 #define	ENOMSG		83		/* No message of desired type */
 #define	EOVERFLOW	84		/* Value too large to be stored in data type */
 #define	ECANCELED	85		/* Operation canceled */
 #define	EILSEQ		86		/* Illegal byte sequence */
+#if __BSD_VISIBLE
 #define	ENOATTR		87		/* Attribute not found */
 #define	EDOOFUS		88		/* Programming error */
-#endif /* _POSIX_SOURCE */
+#endif /* __BSD_VISIBLE */
 
 #define	EBADMSG		89		/* Bad message */
 #define	EMULTIHOP	90		/* Multihop attempted */
 #define	ENOLINK		91		/* Link has been severed */
 #define	EPROTO		92		/* Protocol error */
 
-#ifndef _POSIX_SOURCE
+#if __BSD_VISIBLE
 #define	ENOMEDIUM	93		/* linux */
 #define	EUNUSED94	94
 #define	EUNUSED95	95
@@ -189,7 +193,7 @@ static __inline int *__error(void)
 #define	EASYNC		99		/* XXX */
 
 #define	ELAST		99		/* Must be equal largest errno */
-#endif /* _POSIX_SOURCE */
+#endif /* __BSD_VISIBLE */
 
 #if defined(_KERNEL) || defined(_KERNEL_STRUCTURES)
 /* pseudo-errors returned inside kernel to modify return to process */
