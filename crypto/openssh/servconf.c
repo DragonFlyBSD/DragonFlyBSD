4bde49b71e31daca1872f938e6f2c2e132cbc396
Peter Avalos <pavalos@dragonflybsd.org>
2016-08-13 13:49:59 -0700

    sshd(8): Disable tunneled cleartext passwords.
    This was previously our default that got wiped away when I removed our
    local changes to OpenSSH. It's unlikely that anyone running master saw
    any change to their sshd behavior, because they likely had the
    PasswordAuthentication line uncommented in their configuration file.

--- crypto/openssh/servconf.c
+++ crypto/openssh/servconf.c
@@ -346,7 +346,7 @@ fill_default_server_options(ServerOptions *options)
 	if (options->ip_qos_bulk == -1)
 		options->ip_qos_bulk = IPTOS_THROUGHPUT;
 	if (options->version_addendum == NULL)
-		options->version_addendum = xstrdup("");
+		options->version_addendum = xstrdup(SSH_VERSION_DRAGONFLY);
 	if (options->fwd_opts.streamlocal_bind_mask == (mode_t)-1)
 		options->fwd_opts.streamlocal_bind_mask = 0177;
 	if (options->fwd_opts.streamlocal_bind_unlink == -1)
