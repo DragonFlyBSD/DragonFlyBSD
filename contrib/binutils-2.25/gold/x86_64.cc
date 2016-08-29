eee86d5a9b698bc2d10bfce462dd5172ed60b767
John Marino <draco@marino.st>
2016-08-29 14:10:42 -0500

    binutils 2.25 linkers: Set default interpreter to valid path
    I originally thought these interpreter definitions didn't affect anything,
    but they may explain random stderr messages during building.  In any case,
    it doesn't hurt to correct the definitions and this was also done for
    devel/binutils in dports.

--- contrib/binutils-2.25/gold/x86_64.cc
+++ contrib/binutils-2.25/gold/x86_64.cc
@@ -1021,7 +1021,7 @@ const Target::Target_info Target_x86_64<64>::x86_64_info =
   true,			// is_default_stack_executable
   true,			// can_icf_inline_merge_sections
   '\0',			// wrap_char
-  "/lib/ld64.so.1",     // program interpreter
+  "/libexec/ld-elf.so.2",     // program interpreter
   0x400000,		// default_text_segment_address
   0x1000,		// abi_pagesize (overridable by -z max-page-size)
   0x1000,		// common_pagesize (overridable by -z common-page-size)
