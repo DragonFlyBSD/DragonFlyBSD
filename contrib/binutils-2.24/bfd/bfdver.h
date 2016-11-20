c80085678ed8b87f203af020e14e8079a552fdd0
zrj <rimvydas.jasinskas@gmail.com>
2016-11-20 15:47:14 +0200

    Retire the binutils-2.24.
    Remove contrib sources and build Makefiles.

--- contrib/binutils-2.24/bfd/bfdver.h
+++
@@ -1,4 +0,0 @@
-#define BFD_VERSION_DATE 20131202
-#define BFD_VERSION 224000000
-#define BFD_VERSION_STRING  "(GNU Binutils) " "2.24"
-#define REPORT_BUGS_TO "<http://www.sourceware.org/bugzilla/>"
