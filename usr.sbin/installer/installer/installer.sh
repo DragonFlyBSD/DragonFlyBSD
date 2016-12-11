DragonFlyBSD/DragonFlyBSD

commit f7df6c8e7aca21d55ca9c7b676a89f849a923546

installer - Several improvements
- Improved detection of the booting media (LiveCD)
- Sets TTY variable correctly for vkernel. This way its only
  console won't be filled up with messages that mess up menus.
- Detect 'TTY_INST' variable which means the installer was called
  from the LiveDVD's installDragonFly.sh script.

In-discussion with: swildner

usr.sbin/installer/installer/installer.sh
@@ -64,11 +64,13 @@ installer_start()
 	esac
 
 	if [ "X$pfi_frontend" = "Xauto" ]; then
-		if [ "X$DISPLAY" = "X" ]; then
-			if [ "X$LIVECD" = "X" ]; then
-				pfi_frontend="curseslog"
-			else
+		if [ "X$TTY_INST" = "X" ]; then
+		    if $(is_livecd); then
+				TTY=/dev/ttyv1
 				pfi_frontend="cursesvty"
+			else
+				TTY=$(tty)
+				pfi_frontend="curseslog"
 			fi
 		else
 			pfi_frontend="cursesx11"
@@ -170,6 +172,27 @@ installer_start()
 	esac
 }
 
+is_livecd()
+{
+    local _ttyv1=$(grep -w "^ttyv1" /etc/ttys)
+    local cdmnt=$(mount -t cd9660 | grep 'cd[0-9]' | cut -w -f1)
+    local guest=$(sysctl -n kern.vmm_guest)
+
+    #
+    # ttyv1 isn't configured for the LiveCD/DVD so use
+    # that as a clue for now. Vkernels will be forced
+    # to use 'curseslog' to avoid polluting its only
+    # terminal.
+    #
+    [ "${guest}" = "vkernel" ] && return 1;
+
+    if [ ! -z "${cdmnt}" -a -z "${_ttyv1}" ]; then
+	return 0	# Return success, it's a LiveCD
+    else
+	return 1
+    fi
+}
+
 ### MAIN ###
 
 if [ $# -gt 1 ]; then
@@ -177,25 +200,10 @@ if [ $# -gt 1 ]; then
 	exit 1
 fi
 
-# Check if we are booted from a LiveCD, DVD etc. ttyv1 isn't configured in
-# this case, so use that as a clue for now. Also, we have to use /dev/console
-# in vkernels.
 #
-_ttyv1=`grep -w "^ttyv1" /etc/ttys`
-if [ "`sysctl -n kern.vmm_guest`" = "vkernel" ]; then
-	SOURCE_DIR=/
-	TTY=/dev/console
-elif [ -z "$_ttyv1" ]; then
-	LIVECD=YES
-	SOURCE_DIR=/
-	TTY=/dev/ttyv1
-elif [ $# = 1 -a -d $1 ]; then
-	SOURCE_DIR=$1/
-	TTY=/dev/`w | awk '{ print $2 }' | tail -n1`
-else
-	SOURCE_DIR=/
-	TTY=/dev/`w | awk '{ print $2 }' | tail -n1`
-fi
+# Source directory for the installation
+#
+SOURCE_DIR=${sdir}/
 
 ps auwwwxxx > /tmp/ps.txt
 if grep -q dfuibe_installer /tmp/ps.txt; then

commit 4086570b6fc8851d09613acab85061ee3f9d3f08

installer: Fix source directory specification.
It got broken in f7df6c8e7a.

While here, pass -o to all backend invocations.

usr.sbin/installer/installer/installer.sh
@@ -9,7 +9,11 @@ background_backend()
 {
 	RENDEZVOUS=$1
 	TRANSPORT=$2
-	$pfi_backend -r $RENDEZVOUS -t $TRANSPORT >/dev/null 2>&1
+	$pfi_backend \
+	    -o $SOURCE_DIR \
+	    -r $RENDEZVOUS \
+	    -t $TRANSPORT \
+	    >/dev/null 2>&1
 	RESULT=$?
 	case "$RESULT" in
 	0)
@@ -79,11 +83,17 @@ installer_start()
 
 	case "X$pfi_frontend" in
 	Xqt)
-		$pfi_backend -r $RENDEZVOUS -t $pfi_dfui_transport
+		$pfi_backend \
+		    -o $SOURCE_DIR \
+		    -r $RENDEZVOUS \
+		    -t $pfi_dfui_transport
 		RESULT=$?
 		;;
 	Xcgi)
-		$pfi_backend -r $RENDEZVOUS -t $pfi_dfui_transport
+		$pfi_backend \
+		    -o $SOURCE_DIR \
+		    -r $RENDEZVOUS \
+		    -t $pfi_dfui_transport
 		RESULT=$?
 		;;
 	Xcursesvty)
@@ -147,7 +157,10 @@ installer_start()
 		fi
 		rm -f /tmp/ps.txt
 		sleep 1
-		$pfi_backend -r $RENDEZVOUS -t $pfi_dfui_transport
+		$pfi_backend \
+		    -o $SOURCE_DIR \
+		    -r $RENDEZVOUS \
+		    -t $pfi_dfui_transport
 		RESULT=$?
 		sleep 1
 		killall dfuife_curses
@@ -197,12 +210,19 @@ is_installmedia()
 if [ $# -gt 1 ]; then
 	echo "usage: installer [source_directory]"
 	exit 1
+elif [ $# = 1 -a ! -d $1 ]; then
+	echo "source_directory does not exist or is no directory"
+	exit 1
 fi
 
 #
 # Source directory for the installation
 #
-SOURCE_DIR=${sdir}/
+if [ $# = 1 ]; then
+	SOURCE_DIR=$1
+else
+	SOURCE_DIR=/
+fi
 
 ps auwwwxxx > /tmp/ps.txt
 if grep -q dfuibe_installer /tmp/ps.txt; then