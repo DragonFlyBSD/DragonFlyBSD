864231c19af309bbba217b50313058ab63eb1531
Matthew Dillon <dillon@apollo.backplane.com>
2017-01-30 09:39:51 -0800

    kernel - Fix VTEXT/open race
    * For now add O_RDWR to the list of flags that must use an exclusive
      namecache lock when executing open().  This fixes a VTEXT/open race.
    
    * This is not perfect w/regards to performance, so further enhancement may
      be forthcoming.
    
    Reported-by: mjg_ (Mateusz Guzik)

--- sys/kern/vfs_syscalls.c
+++ sys/kern/vfs_syscalls.c
@@ -1935,9 +1935,16 @@ kern_open(struct nlookupdata *nd, int oflags, int mode, int *res)
 	 * and unlocked.
 	 *
 	 * Request a shared lock on the vnode if possible.
+	 *
+	 * Executable binaries can race VTEXT against O_RDWR opens, so
+	 * use an exclusive lock for O_RDWR opens as well.
+	 *
+	 * NOTE: We need a flag to separate terminal vnode locking from
+	 *	 parent locking.  O_CREAT needs parent locking, but O_TRUNC
+	 *	 and O_RDWR only need to lock the terminal vnode exclusively.
 	 */
 	nd->nl_flags |= NLC_LOCKVP;
-	if ((flags & (O_CREAT|O_TRUNC)) == 0)
+	if ((flags & (O_CREAT|O_TRUNC|O_RDWR)) == 0)
 		nd->nl_flags |= NLC_SHAREDLOCK;
 
 	error = vn_open(nd, fp, flags, cmode);
