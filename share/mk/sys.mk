6954d52dca31dc7773729c0bf92e14fb1dab5aae
zrj <rimvydas.jasinskas@gmail.com>
2017-02-08 18:27:24 +0200

    Prevent implicit -Wsystem-headers passing from CFLAGS to CXXFLAGS.
    Upcoming clang will be the first base system part that very heavily depends on
    c++ headers, this prevents setting any level of WARNS in clang's makefiles
    without performing certain unsafe manipulations of CXXFLAGS. To prevent that
    it makes sense to allow only explict use directly in CXXFLAGS where it is
    needed given c++11 headers are part of contrib and quite noisy with deprecated
    attributes for previous features/interfaces to c++11 standard.

--- share/mk/sys.mk
+++ share/mk/sys.mk
@@ -61,7 +61,7 @@ CXX		?=	c++
 CXX_LINK	?=	${CXX}
 NXCXX		?=	${NXENV} ${CXX}
 NXCXX_LINK	?=	${NXENV} ${CXX_LINK}
-CXXFLAGS	?=	${CXXINCLUDES} ${CFLAGS:N-std=*:N-Wnested-externs:N-W*-prototypes:N-Wno-pointer-sign:N-Wold-style-definition}
+CXXFLAGS	?=	${CXXINCLUDES} ${CFLAGS:N-std=*:N-Wnested-externs:N-W*-prototypes:N-Wno-pointer-sign:N-Wold-style-definition:N-Wsystem-headers}
 .if !defined(SYSBUILD) && defined(.MAKE.BUILT.BY) && ${.MAKE.BUILT.BY:Mgcc47}
 CXXFLAGS	+=	-D_GLIBCXX_USE_CXX11_ABI=0
 .endif
