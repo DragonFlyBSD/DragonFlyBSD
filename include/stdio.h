58696e283b323aa14d58b780ac4c1329f1e55ecf
Sascha Wildner <saw@online.de>
2017-04-14 19:25:48 +0200

    <stdio.h>: ftello() and fseeko() were in SUSv2, so extend visibility.
    See:
    
    http://pubs.opengroup.org/onlinepubs/007908799/xsh/fseek.html
    http://pubs.opengroup.org/onlinepubs/007908799/xsh/ftell.html

--- include/stdio.h
+++ include/stdio.h
@@ -284,7 +284,7 @@ int	 ferror_unlocked(FILE *);
 int	 fileno_unlocked(FILE *);
 #endif
 
-#if __POSIX_VISIBLE >= 200112
+#if __XSI_VISIBLE >= 500 || __POSIX_VISIBLE >= 200112
 int	 fseeko(FILE *, __off_t, int);
 __off_t	 ftello(FILE *);
 #endif
