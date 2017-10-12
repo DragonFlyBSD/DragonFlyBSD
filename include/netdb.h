52d3d728667e0146427d1548bb17fcc282de9902
Sascha Wildner <saw@online.de>
2017-10-12 10:16:06 +0200

    <netdb.h>: Adjust comment a bit.

--- include/netdb.h
+++ include/netdb.h
@@ -192,8 +192,7 @@ struct addrinfo {
 #define	NI_NAMEREQD	0x00000004
 #define	NI_NUMERICSERV	0x00000008
 #define	NI_DGRAM	0x00000010
-/* XXX deprecated */
-#define NI_WITHSCOPEID	0x00000020
+#define	NI_WITHSCOPEID	0x00000020	/* XXX deprecated */
 #define	NI_NUMERICSCOPE	0x00000040
 
 
