557f890e444b8fb31c7b78925fbe783daae9ecb5
Aaron LI <aly@aaronly.me>
2017-10-17 22:41:42 +0800

    pf: use IN6_IS_SCOPE_EMBED to check kernel-internal form addresses
    Use IN6_IS_SCOPE_EMBED to check kernel-internal form addresses
    (s6_addr16[1] filled).
    
    Taken-from: OpenBSD sys/net/pf.c v.1.520

--- sys/net/pf/pf.c
+++ sys/net/pf/pf.c
@@ -6301,7 +6301,7 @@ pf_route6(struct mbuf **m, struct pf_rule *r, int dir, struct ifnet *oifp,
 	 * If the packet is too large for the outgoing interface,
 	 * send back an icmp6 error.
 	 */
-	if (IN6_IS_ADDR_LINKLOCAL(&dst->sin6_addr))
+	if (IN6_IS_SCOPE_EMBED(&dst->sin6_addr))
 		dst->sin6_addr.s6_addr16[1] = htons(ifp->if_index);
 	if ((u_long)m0->m_pkthdr.len <= ifp->if_mtu) {
 		nd6_output(ifp, ifp, m0, dst, NULL);
