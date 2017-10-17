ae12d7f3e2f1be15cfcf502c1dcda5a63d55ffe7
Aaron LI <aly@aaronly.me>
2017-10-17 22:55:45 +0800

    pf: Always skip "urpf-failed" test for IPv6 link local addresses
    We could re-embed the scope-id before we do the route lookup,
    but then we would just find the very interface we've received
    the packet on anyway.
    
    Taken-from: OpenBSD sys/net/pf.c v.1.625

--- sys/net/pf/pf.c
+++ sys/net/pf/pf.c
@@ -5910,6 +5910,12 @@ pf_routable(struct pf_addr *addr, sa_family_t af, struct pfi_kif *kif)
 		break;
 #ifdef INET6
 	case AF_INET6:
+		/*
+		 * Skip check for addresses with embedded interface scope,
+		 * as they would always match anyway.
+		 */
+		if (IN6_IS_SCOPE_EMBED(&addr->v6))
+			goto out;
 		dst6 = (struct sockaddr_in6 *)&ro.ro_dst;
 		dst6->sin6_family = AF_INET6;
 		dst6->sin6_len = sizeof(*dst6);
