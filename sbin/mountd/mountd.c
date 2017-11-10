611231b9189c7b0119817d1c40a3bf008b84cabe
Aaron LI <aly@aaronly.me>
2017-11-10 19:02:16 +0800

    mountd(8): Fix conversion of prefix length to a netmask
    Taken-from: FreeBSD usr.sbin/mountd/mountd.c r219125

--- sbin/mountd/mountd.c
+++ sbin/mountd/mountd.c
@@ -2588,7 +2588,7 @@ makemask(struct sockaddr_storage *ssp, int bitlen)
 		return (-1);
 	for (i = 0; i < len; i++) {
 		bits = (bitlen > CHAR_BIT) ? CHAR_BIT : bitlen;
-		*p++ = (1 << bits) - 1;
+		*p++ = (u_char)~0 << (CHAR_BIT - bits);
 		bitlen -= bits;
 	}
 	return 0;
