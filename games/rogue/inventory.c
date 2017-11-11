800de66bb089f86a50156ea6882cc3105c706b41
zrj <rimvydas.jasinskas@gmail.com>
2017-11-11 16:22:08 +0200

    rogue(6): Fix potion mixing.
    Taken-from: freebsd-ports(games/bsdgnames)

--- games/rogue/inventory.c
+++ games/rogue/inventory.c
@@ -417,6 +417,7 @@ mix_colors(void)
 		k = get_rand(0, (POTIONS - 1));
 		memcpy(t, id_potions[j].title, MAX_ID_TITLE_LEN);
 		memcpy(id_potions[j].title, id_potions[k].title, MAX_ID_TITLE_LEN);
+		memcpy(id_potions[k].title, t, MAX_ID_TITLE_LEN);
 	}
 }
 
