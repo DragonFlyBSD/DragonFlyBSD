d2a8bc111e3bd61125d686ddbdfad416772ccdc4
Peeter Must <karu.pruun@gmail.com>
2017-11-27 13:25:09 +0200

    kernel - Bring in definition of _IOWINT from FreeBSD.
    * This is needed for evdev.
    
    Obtained-from: FreeBSD

--- sys/sys/ioccom.h
+++ sys/sys/ioccom.h
@@ -54,6 +54,7 @@
 #define	_IOC(inout,group,num,len)	((unsigned long) \
 	((inout) | (((len) & IOCPARM_MASK) << 16) | ((group) << 8) | (num)))
 #define	_IO(g,n)	_IOC(IOC_VOID,	(g), (n), 0)
+#define	_IOWINT(g,n)	_IOC(IOC_VOID,	(g), (n), sizeof(int))
 #define	_IOR(g,n,t)	_IOC(IOC_OUT,	(g), (n), sizeof(t))
 #define	_IOW(g,n,t)	_IOC(IOC_IN,	(g), (n), sizeof(t))
 /* this should be _IORW, but stdio got there first */
