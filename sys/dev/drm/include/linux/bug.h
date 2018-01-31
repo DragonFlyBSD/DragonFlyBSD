ff464e6b04e10ebfb5f282ec078c673a1dffa971
zrj <rimvydas.jasinskas@gmail.com>
2018-01-31 17:19:23 +0200

    drm/linux: Improve linux/bug.h
    Suppresses 399 -Wempty-body warnings.

--- sys/dev/drm/include/linux/bug.h
+++ sys/dev/drm/include/linux/bug.h
@@ -32,13 +32,13 @@
 #include <asm/bug.h>
 #include <linux/compiler.h>
 
-#define	BUILD_BUG_ON(CONDITION)	CTASSERT(!(CONDITION))
+#define	BUILD_BUG_ON(x)	CTASSERT(!(x))
 
 #define	BUILD_BUG_ON_NOT_POWER_OF_2(n)			      \
 	CTASSERT(((n) != 0) && (((n) & ((n) - 1)) == 0))
 
-#define BUILD_BUG()	BUILD_BUG_ON(1)
+#define	BUILD_BUG()	BUILD_BUG_ON(1)
 
-#define BUILD_BUG_ON_MSG(cond, msg)
+#define	BUILD_BUG_ON_MSG(cond, msg)	do { } while (0)
 
 #endif /* LINUX_BUG_H */
