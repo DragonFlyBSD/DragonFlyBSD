c2962d9da8200947ee394b190eea39d437fb1af7
Imre Vadász <imre@vdsz.com>
2018-02-22 22:54:33 +0100

    syscons - Detangle sysmouse from tty handling, implement a custom FIFO.
    * This avoids unnecessary IPIs and other overhead from the tty handling.
    
    * This also makes reads from the character device always get only complete
      sysmouse packets.

--- sys/dev/misc/syscons/sysmouse.c
+++ sys/dev/misc/syscons/sysmouse.c
@@ -28,18 +28,19 @@
  * $FreeBSD: src/sys/dev/syscons/sysmouse.c,v 1.2.2.2 2001/07/16 05:21:24 yokota Exp $
  */
 
-/* MPSAFE NOTE: This file uses the tty_token mostly for the linesw access and
- *		for the internal mouse structures. The latter ones could be protected
- *		by a local lock.
+/* MPSAFE NOTE: Take care with locking in sysmouse_event which is called
+ *              from syscons.
  */
 #include "opt_syscons.h"
 
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/conf.h>
-#include <sys/proc.h>
+#include <sys/device.h>
+#include <sys/event.h>
+#include <sys/uio.h>
 #include <sys/priv.h>
-#include <sys/tty.h>
+#include <sys/vnode.h>
 #include <sys/kernel.h>
 #include <sys/thread2.h>
 
@@ -50,124 +51,162 @@
 
 #ifndef SC_NO_SYSMOUSE
 
+#define FIFO_SIZE	256
+
+struct event_fifo {
+	mouse_info_t buf[FIFO_SIZE];
+	int start;
+	int fill;
+	unsigned int dropped;
+};
+
+struct sysmouse_state {
+	struct event_fifo *fifo;
+	int level;	/* sysmouse protocol level */
+	mousestatus_t syncstatus;
+	mousestatus_t readstatus;	/* Only needed for button status */
+	int opened;
+	struct lock sm_lock;
+	struct kqinfo rkq;
+};
+
 static d_open_t		smopen;
 static d_close_t	smclose;
+static d_read_t		smread;
 static d_ioctl_t	smioctl;
+static d_kqfilter_t	smkqfilter;
 
 static struct dev_ops sm_ops = {
-	{ "sysmouse", 0, D_TTY | D_MPSAFE },
+	{ "sysmouse", 0, D_MPSAFE },
 	.d_open =	smopen,
 	.d_close =	smclose,
-	.d_read =	ttyread,
+	.d_read =	smread,
 	.d_ioctl =	smioctl,
-	.d_kqfilter =	ttykqfilter,
-	.d_revoke =	ttyrevoke
+	.d_kqfilter =	smkqfilter,
 };
 
 /* local variables */
-static struct tty	*sysmouse_tty;
-static int		mouse_level;	/* sysmouse protocol level */
-static mousestatus_t	mouse_status;
+static struct sysmouse_state mouse_state;
 
-static void		smstart(struct tty *tp);
-static int		smparam(struct tty *tp, struct termios *t);
+static int	sysmouse_evtopkt(struct sysmouse_state *sc, mouse_info_t *info,
+		    u_char *buf);
+static void	smqueue(struct sysmouse_state *sc, mouse_info_t *info);
+static int	pktlen(struct sysmouse_state *sc);
+static void	smfilter_detach(struct knote *);
+static int	smfilter(struct knote *, long);
+static void	smget(struct sysmouse_state *sc, mouse_info_t *info);
+static void	smpop(struct sysmouse_state *sc);
+
+static int
+pktlen(struct sysmouse_state *sc)
+{
+	if (sc->level == 0)
+		return 5;
+	else
+		return 8;
+}
 
 static int
 smopen(struct dev_open_args *ap)
 {
 	cdev_t dev = ap->a_head.a_dev;
-	struct tty *tp;
+	struct sysmouse_state *sc = &mouse_state;
 	int ret;
 
-	DPRINTF(5, ("smopen: dev:%d,%d, vty:%d\n",
-		major(dev), minor(dev), SC_VTY(dev)));
+	DPRINTF(5, ("smopen: dev:%d,%d\n",
+		major(dev), minor(dev)));
 
-	lwkt_gettoken(&tty_token);
-	tp = dev->si_tty = ttymalloc(dev->si_tty);
-	if (!(tp->t_state & TS_ISOPEN)) {
-		sysmouse_tty = tp;
-		tp->t_oproc = smstart;
-		tp->t_param = smparam;
-		tp->t_stop = nottystop;
-		tp->t_dev = dev;
-		ttychars(tp);
-		tp->t_iflag = 0;
-		tp->t_oflag = 0;
-		tp->t_lflag = 0;
-		tp->t_cflag = TTYDEF_CFLAG;
-		tp->t_ispeed = tp->t_ospeed = TTYDEF_SPEED;
-		smparam(tp, &tp->t_termios);
-		(*linesw[tp->t_line].l_modem)(tp, 1);
-	} else if (tp->t_state & TS_XCLUDE && priv_check_cred(ap->a_cred, PRIV_ROOT, 0)) {
-		lwkt_reltoken(&tty_token);
-		return EBUSY;
+	lockmgr(&sc->sm_lock, LK_EXCLUSIVE);
+	if (!sc->opened) {
+		sc->fifo = kmalloc(sizeof(struct event_fifo),
+		    M_SYSCONS, M_WAITOK | M_ZERO);
+		sc->opened = 1;
+		bzero(&sc->readstatus, sizeof(sc->readstatus));
+		bzero(&sc->syncstatus, sizeof(sc->syncstatus));
+		ret = 0;
+	} else {
+		ret = EBUSY;
 	}
+	lockmgr(&sc->sm_lock, LK_RELEASE);
 
-	ret = (*linesw[tp->t_line].l_open)(dev, tp);
-	lwkt_reltoken(&tty_token);
 	return ret;
 }
 
 static int
 smclose(struct dev_close_args *ap)
 {
-	cdev_t dev = ap->a_head.a_dev;
-	struct tty *tp;
+	struct sysmouse_state *sc = &mouse_state;
 
-	tp = dev->si_tty;
-	crit_enter();
-	lwkt_gettoken(&tty_token);
-	mouse_level = 0;
-	(*linesw[tp->t_line].l_close)(tp, ap->a_fflag);
-	ttyclose(tp);
-	lwkt_reltoken(&tty_token);
-	crit_exit();
+	lockmgr(&sc->sm_lock, LK_EXCLUSIVE);
+	sc->opened = 0;
+	sc->level = 0;
+	kfree(sc->fifo, M_SYSCONS);
+	sc->fifo = NULL;
+	lockmgr(&sc->sm_lock, LK_RELEASE);
 
 	return 0;
-}
-
-static void
-smstart(struct tty *tp)
-{
-	struct clist *rbp;
-	u_char buf[PCBURST];
-
-	crit_enter();
-	lwkt_gettoken(&tty_token);
-	if (!(tp->t_state & (TS_TIMEOUT | TS_BUSY | TS_TTSTOP))) {
-		tp->t_state |= TS_BUSY;
-		rbp = &tp->t_outq;
-		while (rbp->c_cc)
-			q_to_b(rbp, buf, PCBURST);
-		tp->t_state &= ~TS_BUSY;
-		ttwwakeup(tp);
-	}
-	lwkt_reltoken(&tty_token);
-	crit_exit();
 }
 
 static int
-smparam(struct tty *tp, struct termios *t)
+smread(struct dev_read_args *ap)
 {
-	lwkt_gettoken(&tty_token);
-	tp->t_ispeed = t->c_ispeed;
-	tp->t_ospeed = t->c_ospeed;
-	tp->t_cflag = t->c_cflag;
-	lwkt_reltoken(&tty_token);
-	return 0;
+	struct sysmouse_state *sc = &mouse_state;
+	mousestatus_t backupstatus;
+	mouse_info_t info;
+	u_char buf[8];
+	struct uio *uio = ap->a_uio;
+	int error = 0, val, cnt = 0;
+
+	lockmgr(&sc->sm_lock, LK_EXCLUSIVE);
+	while (sc->fifo->fill <= 0) {
+		/* Buffer too small to fit a complete mouse packet */
+		if (uio->uio_resid < pktlen(sc)) {
+			error = EIO;
+			goto done;
+		}
+		if (ap->a_ioflag & IO_NDELAY) {
+			error = EAGAIN;
+			goto done;
+		}
+		error = lksleep(sc, &sc->sm_lock, PCATCH, "smread", 0);
+		if (error == EINTR || error == ERESTART) {
+			goto done;
+		}
+	}
+
+	do {
+		/* Buffer too small to fit a complete mouse packet */
+		if (uio->uio_resid < pktlen(sc)) {
+			error = EIO;
+			goto done;
+		}
+		smget(sc, &info);
+		backupstatus = sc->readstatus;
+		val = sysmouse_evtopkt(sc, &info, buf);
+		if (val > 0) {
+			error = uiomove(buf, val, uio);
+			if (error != 0) {
+				sc->readstatus = backupstatus;
+				goto done;
+			}
+			cnt++;
+		}
+		smpop(sc);
+	} while (sc->fifo->fill > 0);
+
+done:
+	lockmgr(&sc->sm_lock, LK_RELEASE);
+	if (cnt > 0 && error != EFAULT)
+		return 0;
+	return error;
 }
 
 static int
 smioctl(struct dev_ioctl_args *ap)
 {
-	cdev_t dev = ap->a_head.a_dev;
-	struct tty *tp;
+	struct sysmouse_state *sc = &mouse_state;
 	mousehw_t *hw;
 	mousemode_t *mode;
-	int error;
-
-	lwkt_gettoken(&tty_token);
-	tp = dev->si_tty;
 
 	switch (ap->a_cmd) {
 	case MOUSE_GETHWINFO:	/* get device information */
@@ -177,12 +216,13 @@ smioctl(struct dev_ioctl_args *ap)
 		hw->type = MOUSE_MOUSE;
 		hw->model = MOUSE_MODEL_GENERIC;
 		hw->hwid = 0;
-		lwkt_reltoken(&tty_token);
 		return 0;
 
 	case MOUSE_GETMODE:	/* get protocol/mode */
 		mode = (mousemode_t *)ap->a_data;
-		mode->level = mouse_level;
+		lockmgr(&sc->sm_lock, LK_EXCLUSIVE);
+		mode->level = sc->level;
+		lockmgr(&sc->sm_lock, LK_RELEASE);
 		switch (mode->level) {
 		case 0: /* emulate MouseSystems protocol */
 			mode->protocol = MOUSE_PROTO_MSC;
@@ -204,7 +244,6 @@ smioctl(struct dev_ioctl_args *ap)
 			mode->syncmask[1] = MOUSE_SYS_SYNC;
 			break;
 		}
-		lwkt_reltoken(&tty_token);
 		return 0;
 
 	case MOUSE_SETMODE:	/* set protocol/mode */
@@ -212,80 +251,198 @@ smioctl(struct dev_ioctl_args *ap)
 		if (mode->level == -1)
 			; 	/* don't change the current setting */
 		else if ((mode->level < 0) || (mode->level > 1)) {
-			lwkt_reltoken(&tty_token);
 			return EINVAL;
 		} else {
-			mouse_level = mode->level;
+			lockmgr(&sc->sm_lock, LK_EXCLUSIVE);
+			sc->level = mode->level;
+			lockmgr(&sc->sm_lock, LK_RELEASE);
 		}
-		lwkt_reltoken(&tty_token);
 		return 0;
 
 	case MOUSE_GETLEVEL:	/* get operation level */
-		*(int *)ap->a_data = mouse_level;
-		lwkt_reltoken(&tty_token);
+		lockmgr(&sc->sm_lock, LK_EXCLUSIVE);
+		*(int *)ap->a_data = sc->level;
+		lockmgr(&sc->sm_lock, LK_RELEASE);
 		return 0;
 
 	case MOUSE_SETLEVEL:	/* set operation level */
 		if ((*(int *)ap->a_data  < 0) || (*(int *)ap->a_data > 1)) {
-			lwkt_reltoken(&tty_token);
 			return EINVAL;
 		}
-		mouse_level = *(int *)ap->a_data;
-		lwkt_reltoken(&tty_token);
+		lockmgr(&sc->sm_lock, LK_EXCLUSIVE);
+		sc->level = *(int *)ap->a_data;
+		lockmgr(&sc->sm_lock, LK_RELEASE);
 		return 0;
 
 	case MOUSE_GETSTATUS:	/* get accumulated mouse events */
-		crit_enter();
-		*(mousestatus_t *)ap->a_data = mouse_status;
-		mouse_status.flags = 0;
-		mouse_status.obutton = mouse_status.button;
-		mouse_status.dx = 0;
-		mouse_status.dy = 0;
-		mouse_status.dz = 0;
-		crit_exit();
-		lwkt_reltoken(&tty_token);
+		lockmgr(&sc->sm_lock, LK_EXCLUSIVE);
+		*(mousestatus_t *)ap->a_data = sc->syncstatus;
+		sc->syncstatus.flags = 0;
+		sc->syncstatus.obutton = sc->syncstatus.button;
+		sc->syncstatus.dx = 0;
+		sc->syncstatus.dy = 0;
+		sc->syncstatus.dz = 0;
+		lockmgr(&sc->sm_lock, LK_RELEASE);
 		return 0;
 
 #if 0 /* notyet */
 	case MOUSE_GETVARS:	/* get internal mouse variables */
 	case MOUSE_SETVARS:	/* set internal mouse variables */
-		lwkt_reltoken(&tty_token);
 		return ENODEV;
 #endif
 
 	case MOUSE_READSTATE:	/* read status from the device */
 	case MOUSE_READDATA:	/* read data from the device */
-		lwkt_reltoken(&tty_token);
 		return ENODEV;
 	}
 
-	error = (*linesw[tp->t_line].l_ioctl)(tp, ap->a_cmd, ap->a_data, ap->a_fflag, ap->a_cred);
-	if (error != ENOIOCTL) {
-		lwkt_reltoken(&tty_token);
-		return error;
-	}
-	error = ttioctl(tp, ap->a_cmd, ap->a_data, ap->a_fflag);
-	if (error != ENOIOCTL) {
-		lwkt_reltoken(&tty_token);
-		return error;
-	}
-	lwkt_reltoken(&tty_token);
 	return ENOTTY;
+}
+
+static struct filterops smfiltops =
+        { FILTEROP_MPSAFE | FILTEROP_ISFD, NULL, smfilter_detach, smfilter };
+
+static int
+smkqfilter(struct dev_kqfilter_args *ap)
+{
+	struct sysmouse_state *sc = &mouse_state;
+	struct knote *kn = ap->a_kn;
+	struct klist *klist;
+
+	ap->a_result = 0;
+
+	switch (kn->kn_filter) {
+	case EVFILT_READ:
+		kn->kn_fop = &smfiltops;
+		kn->kn_hook = (caddr_t)sc;
+		break;
+	default:
+		ap->a_result = EOPNOTSUPP;
+		return (0);
+	}
+
+	klist = &sc->rkq.ki_note;
+	knote_insert(klist, kn);
+
+	return (0);
+}
+
+static void
+smfilter_detach(struct knote *kn)
+{
+	struct sysmouse_state *sc = &mouse_state;
+	struct klist *klist;
+
+	klist = &sc->rkq.ki_note;
+	knote_remove(klist, kn);
+}
+
+static int
+smfilter(struct knote *kn, long hint)
+{
+	struct sysmouse_state *sc = &mouse_state;
+	int ready = 0;
+
+	lockmgr(&sc->sm_lock, LK_EXCLUSIVE);
+	if (sc->fifo->fill > 0) {
+		ready = 1;
+		kn->kn_data = 0;
+	}
+	lockmgr(&sc->sm_lock, LK_RELEASE);
+
+	return ready;
+}
+
+static void
+smqueue(struct sysmouse_state *sc, mouse_info_t *info)
+{
+	struct event_fifo *f = sc->fifo;
+
+	if (f->fill >= FIFO_SIZE) {
+		f->fill = FIFO_SIZE;
+		f->buf[f->start] = *info;
+		f->start = (f->start + 1) % FIFO_SIZE;
+		f->dropped++;
+	} else {
+		f->buf[(f->start + f->fill) % FIFO_SIZE] = *info;
+		f->fill++;
+	}
+
+}
+
+static void
+smget(struct sysmouse_state *sc, mouse_info_t *info)
+{
+	struct event_fifo *f = sc->fifo;
+
+	if (f->fill > 0)
+		*info = f->buf[f->start];
+}
+
+static void
+smpop(struct sysmouse_state *sc)
+{
+	struct event_fifo *f = sc->fifo;
+
+	if (f->fill > 0) {
+		f->fill--;
+		f->start = (f->start + 1) % FIFO_SIZE;
+	}
 }
 
 static void
 sm_attach_mouse(void *unused)
 {
+	struct sysmouse_state *sc = &mouse_state;
 	cdev_t dev;
 
+	lockinit(&mouse_state.sm_lock, "sysmouse", 0, LK_CANRECURSE);
+	sc->fifo = NULL;
+
 	dev = make_dev(&sm_ops, 0, UID_ROOT, GID_WHEEL, 0600, "sysmouse");
-	/* sysmouse doesn't have scr_stat */
 }
 
 SYSINIT(sysmouse, SI_SUB_DRIVERS, SI_ORDER_ANY, sm_attach_mouse, NULL);
 
-int
-sysmouse_event(mouse_info_t *info)
+static int
+sysmouse_updatestatus(mousestatus_t *status, mouse_info_t *info)
+{
+	int x, y, z;
+
+	status->obutton = status->button;
+
+	switch (info->operation) {
+	case MOUSE_ACTION:
+		status->button = info->u.data.buttons;
+		/* FALL THROUGH */
+	case MOUSE_MOTION_EVENT:
+		x = info->u.data.x;
+		y = info->u.data.y;
+		z = info->u.data.z;
+		break;
+	case MOUSE_BUTTON_EVENT:
+		x = y = z = 0;
+		if (info->u.event.value > 0)
+			status->button |= info->u.event.id;
+		else
+			status->button &= ~info->u.event.id;
+		break;
+	default:
+		return 0;
+	}
+
+	status->dx += x;
+	status->dy += y;
+	status->dz += z;
+	status->flags |= ((x || y || z) ? MOUSE_POSCHANGED : 0)
+			 | (status->obutton ^ status->button);
+
+	return 1;
+}
+
+/* Requires buf to hold at least 8 bytes, returns number of bytes written */
+static int
+sysmouse_evtopkt(struct sysmouse_state *sc, mouse_info_t *info, u_char *buf)
 {
 	/* MOUSE_BUTTON?DOWN -> MOUSE_MSC_BUTTON?UP */
 	static int butmap[8] = {
@@ -298,72 +455,78 @@ sysmouse_event(mouse_info_t *info)
 	    MOUSE_MSC_BUTTON1UP,
 	    0,
 	};
-	u_char buf[8];
 	int x, y, z;
-	int i;
 
-	lwkt_gettoken(&tty_token);
-	switch (info->operation) {
-	case MOUSE_ACTION:
-        	mouse_status.button = info->u.data.buttons;
-		/* FALL THROUGH */
-	case MOUSE_MOTION_EVENT:
-		x = info->u.data.x;
-		y = info->u.data.y;
-		z = info->u.data.z;
-		break;
-	case MOUSE_BUTTON_EVENT:
-		x = y = z = 0;
-		if (info->u.event.value > 0)
-			mouse_status.button |= info->u.event.id;
-		else
-			mouse_status.button &= ~info->u.event.id;
-		break;
-	default:
-		lwkt_reltoken(&tty_token);
+	sc->readstatus.dx = 0;
+	sc->readstatus.dy = 0;
+	sc->readstatus.dz = 0;
+	sc->readstatus.flags = 0;
+	if (sysmouse_updatestatus(&sc->readstatus, info) == 0)
 		return 0;
-	}
 
-	mouse_status.dx += x;
-	mouse_status.dy += y;
-	mouse_status.dz += z;
-	mouse_status.flags |= ((x || y || z) ? MOUSE_POSCHANGED : 0)
-			      | (mouse_status.obutton ^ mouse_status.button);
-	if (mouse_status.flags == 0) {
-		lwkt_reltoken(&tty_token);
+	/* We aren't using the sc->readstatus.dx/dy/dz values */
+
+	if (sc->readstatus.flags == 0)
 		return 0;
-	}
 
-	if ((sysmouse_tty == NULL) || !(sysmouse_tty->t_state & TS_ISOPEN)) {
-		lwkt_reltoken(&tty_token);
-		return mouse_status.flags;
-	}
+	x = (info->operation == MOUSE_BUTTON_EVENT ? 0 : info->u.data.x);
+	y = (info->operation == MOUSE_BUTTON_EVENT ? 0 : info->u.data.y);
+	z = (info->operation == MOUSE_BUTTON_EVENT ? 0 : info->u.data.z);
 
 	/* the first five bytes are compatible with MouseSystems' */
 	buf[0] = MOUSE_MSC_SYNC
-		 | butmap[mouse_status.button & MOUSE_STDBUTTONS];
+		 | butmap[sc->readstatus.button & MOUSE_STDBUTTONS];
 	x = imax(imin(x, 255), -256);
 	buf[1] = x >> 1;
 	buf[3] = x - buf[1];
 	y = -imax(imin(y, 255), -256);
 	buf[2] = y >> 1;
 	buf[4] = y - buf[2];
-	for (i = 0; i < MOUSE_MSC_PACKETSIZE; ++i)
-		(*linesw[sysmouse_tty->t_line].l_rint)(buf[i], sysmouse_tty);
-	if (mouse_level >= 1) {
+	if (sc->level >= 1) {
 		/* extended part */
-        	z = imax(imin(z, 127), -128);
+		z = imax(imin(z, 127), -128);
         	buf[5] = (z >> 1) & 0x7f;
         	buf[6] = (z - (z >> 1)) & 0x7f;
-        	/* buttons 4-10 */
-        	buf[7] = (~mouse_status.button >> 3) & 0x7f;
-        	for (i = MOUSE_MSC_PACKETSIZE; i < MOUSE_SYS_PACKETSIZE; ++i)
-			(*linesw[sysmouse_tty->t_line].l_rint)(buf[i],
-							       sysmouse_tty);
+		/* buttons 4-10 */
+		buf[7] = (~sc->readstatus.button >> 3) & 0x7f;
 	}
 
-	lwkt_reltoken(&tty_token);
-	return mouse_status.flags;
+	if (sc->level >= 1)
+		return 8;
+
+	return 5;
+}
+
+int
+sysmouse_event(mouse_info_t *info)
+{
+	struct sysmouse_state *sc = &mouse_state;
+	int ret;
+
+	lockmgr(&sc->sm_lock, LK_EXCLUSIVE);
+	ret = sysmouse_updatestatus(&sc->syncstatus, info);
+	if (ret != 0)
+		ret = sc->syncstatus.flags;
+	if (!sc->opened) {
+		lockmgr(&sc->sm_lock, LK_RELEASE);
+		return ret;
+	}
+
+	switch (info->operation) {
+	case MOUSE_ACTION:
+	case MOUSE_MOTION_EVENT:
+	case MOUSE_BUTTON_EVENT:
+		smqueue(sc, info);
+		lockmgr(&sc->sm_lock, LK_RELEASE);
+		wakeup(sc);
+		KNOTE(&sc->rkq.ki_note, 0);
+		break;
+	default:
+		lockmgr(&sc->sm_lock, LK_RELEASE);
+		break;
+	}
+
+	return ret;
 }
 
 #endif /* !SC_NO_SYSMOUSE */
