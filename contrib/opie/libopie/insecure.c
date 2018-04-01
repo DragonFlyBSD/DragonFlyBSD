DragonFlyBSD/DragonFlyBSD

commit 562a180a58780b719830629307e6283c30c85229

Fix a number of gcc80 -Wsizeof-pointer-memaccess warnings.
The file is reused in various utilities.

However, the mid- to long-term plan is to remove legacy utmp code and
only go with standard utmpx.

usr.bin/who/utmpentry.c
@@ -310,9 +310,9 @@ getentry(struct utmpentry *e, struct utmp *up)
 	 * reason we use the size of the _source_ as the length
 	 * argument.
 	 */
-	(void)strncpy(e->name, up->ut_name, sizeof(up->ut_name));
-	(void)strncpy(e->line, up->ut_line, sizeof(up->ut_line));
-	(void)strncpy(e->host, up->ut_host, sizeof(up->ut_host));
+	(void)strncpy(e->name, up->ut_name, sizeof(e->name));
+	(void)strncpy(e->line, up->ut_line, sizeof(e->line));
+	(void)strncpy(e->host, up->ut_host, sizeof(e->host));
 
 	e->tv.tv_sec = up->ut_time;
 	e->tv.tv_usec = 0;
@@ -343,9 +343,9 @@ getentryx(struct utmpentry *e, struct utmpx *up)
 	 * reason we use the size of the _source_ as the length
 	 * argument.
 	 */
-	(void)strncpy(e->name, up->ut_name, sizeof(up->ut_name));
-	(void)strncpy(e->line, up->ut_line, sizeof(up->ut_line));
-	(void)strncpy(e->host, up->ut_host, sizeof(up->ut_host));
+	(void)strncpy(e->name, up->ut_name, sizeof(e->name));
+	(void)strncpy(e->line, up->ut_line, sizeof(e->line));
+	(void)strncpy(e->host, up->ut_host, sizeof(e->host));
 
 	e->tv = up->ut_tv;
 	e->pid = up->ut_pid;

commit 6f2950e09572b9cf2567a43570b1b42e90117d8c

Fix -Wsizeof-pointer-memaccess better.
My previous commits:

56267d362d5769c8df07bf26d5e322610e0d24b4
562a180a58780b719830629307e6283c30c85229

caused out of bounds write by one byte. This commit fixes the
warning and restores the code what was there before.

contrib/opie/libopie/insecure.c
@@ -135,7 +135,8 @@ int opieinsecure FUNCTION_NOARGS
 	char host[sizeof(utmp.ut_host) + 1];
 	insecure = 1;
 
-	strncpy(host, utmp.ut_host, sizeof(host));
+	strncpy(host, utmp.ut_host, sizeof(host) - 1);
+	host[sizeof(host) - 1] = '\0';
 
 	if ((s = strchr(host, ':')) != NULL) {
 	  int n = s - host;

libexec/comsat/comsat.c
@@ -212,7 +212,8 @@ notify(struct utmp *utp, char *file, off_t offset, int folder)
 	}
 	tcgetattr(fileno(tp), &tio);
 	cr = ((tio.c_oflag & (OPOST|ONLCR)) == (OPOST|ONLCR)) ?  "\n" : "\n\r";
-	strncpy(name, utp->ut_name, sizeof(name));
+	strncpy(name, utp->ut_name, sizeof(name) - 1);
+	name[sizeof(name) - 1] = '\0';
 	switch (stb.st_mode & (S_IXUSR | S_IXGRP)) {
 	case S_IXUSR:
 	case (S_IXUSR | S_IXGRP):

usr.bin/who/utmpentry.c
@@ -310,9 +310,9 @@ getentry(struct utmpentry *e, struct utmp *up)
 	 * reason we use the size of the _source_ as the length
 	 * argument.
 	 */
-	(void)strncpy(e->name, up->ut_name, sizeof(e->name));
-	(void)strncpy(e->line, up->ut_line, sizeof(e->line));
-	(void)strncpy(e->host, up->ut_host, sizeof(e->host));
+	(void)strncpy(e->name, up->ut_name, sizeof(e->name) - 1);
+	(void)strncpy(e->line, up->ut_line, sizeof(e->line) - 1);
+	(void)strncpy(e->host, up->ut_host, sizeof(e->host) - 1);
 
 	e->tv.tv_sec = up->ut_time;
 	e->tv.tv_usec = 0;
@@ -343,9 +343,9 @@ getentryx(struct utmpentry *e, struct utmpx *up)
 	 * reason we use the size of the _source_ as the length
 	 * argument.
 	 */
-	(void)strncpy(e->name, up->ut_name, sizeof(e->name));
-	(void)strncpy(e->line, up->ut_line, sizeof(e->line));
-	(void)strncpy(e->host, up->ut_host, sizeof(e->host));
+	(void)strncpy(e->name, up->ut_name, sizeof(e->name) - 1);
+	(void)strncpy(e->line, up->ut_line, sizeof(e->line) - 1);
+	(void)strncpy(e->host, up->ut_host, sizeof(e->host) - 1);
 
 	e->tv = up->ut_tv;
 	e->pid = up->ut_pid;