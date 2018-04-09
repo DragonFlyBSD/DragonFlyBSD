29f13cb6490754babc4058fa2e432c23471f53a9
Bill Yuan <bycn82@dragonflybsd.org>
2018-04-09 13:02:55 +0000

    ipfw3nat: nat func to replace the src or dst
    The `src` will be replaced by `alias` when a packet is leaving the system.
    Hence, the packet is from `src` to `dst` before been translated. And after
    been translated, the packet is from `alias` to `dst`.
    
    The state for outgoing packet will be stored in the nat_context of current
    CPU. But due to the nature of the NAT, the returning packet may be handled
    by another CPU. Hence, a state for the returning packet will be prepared and
    store into the nat_context of the right CPU.

--- sys/net/ipfw3_nat/ip_fw3_nat.c
+++ sys/net/ipfw3_nat/ip_fw3_nat.c
@@ -73,6 +73,19 @@
 #include <net/ipfw3/ip_fw.h>
 #include "ip_fw3_nat.h"
 
+/*
+ * Lockless Kernel NAT
+ *
+ * The `src` will be replaced by `alias` when a packet is leaving the system.
+ * Hence, the packet is from `src` to `dst` before been translated. And after
+ * been translated, the packet is from `alias` to `dst`.
+ *
+ * The state for outgoing packet will be stored in the nat_context of current
+ * CPU. But due to the nature of the NAT, the returning packet may be handled
+ * by another CPU. Hence, a state for the returning packet will be prepared and
+ * store into the nat_context of the right CPU.
+ */
+
 struct ip_fw3_nat_context	*ip_fw3_nat_ctx[MAXCPU];
 static struct callout 		ip_fw3_nat_cleanup_callout;
 extern struct ipfw_context 	*ipfw_ctx[MAXCPU];
@@ -85,6 +98,21 @@ SYSCTL_INT(_net_inet_ip_fw3_nat, OID_AUTO, cleanup_interval, CTLFLAG_RW,
 
 RB_PROTOTYPE(state_tree, nat_state, entries, nat_state_cmp);
 RB_GENERATE(state_tree, nat_state, entries, nat_state_cmp);
+
+static __inline uint16_t
+fix_cksum(uint16_t cksum, uint16_t old_info, uint16_t new_info, uint8_t is_udp)
+{
+	uint32_t tmp;
+
+	if (is_udp && !cksum)
+		return (0x0000);
+	tmp = cksum + old_info - new_info;
+	tmp = (tmp >> 16) + (tmp & 65535);
+	tmp = tmp & 65535;
+	if (is_udp && !tmp)
+		return (0xFFFF);
+	return tmp;
+}
 
 void
 check_nat(int *cmd_ctl, int *cmd_val, struct ip_fw_args **args,
@@ -118,10 +146,220 @@ check_nat(int *cmd_ctl, int *cmd_val, struct ip_fw_args **args,
 }
 
 int
-ip_fw3_nat(struct ip_fw_args *args, struct cfg_nat *t, struct mbuf *m)
+ip_fw3_nat(struct ip_fw_args *args, struct cfg_nat *nat, struct mbuf *m)
 {
-	/* TODO */
+	struct nat_state *s, *k;
+	struct ip *ip = mtod(m, struct ip *);
+	struct in_addr *addr;
+	struct in_addr *old_addr = NULL;
+	uint16_t *old_port = NULL;
+	uint16_t *port = NULL, *csum = NULL, dlen = 0;
+	uint8_t udp = 0;
+	boolean_t pseudo = FALSE;
+	struct state_tree *tree_in = NULL, *tree_out = NULL;
+	struct nat_state *s1 = NULL, *s2, *dup;
+
+	struct in_addr oaddr;
+	uint16_t oport;
+
+	old_addr = &oaddr;
+	old_port = &oport;
+
+	k = &nat->tmp;
+	if (args->oif == NULL) {
+		/* for outgoing packets */
+		addr = &ip->ip_dst;
+		k->saddr = args->f_id.src_ip;
+		k->daddr = ntohl(args->f_id.dst_ip);
+		k->proto = args->f_id.proto;
+		switch (ip->ip_p) {
+		case IPPROTO_TCP:
+			k->sport = args->f_id.src_port;
+			k->dport = ntohs(args->f_id.dst_port);
+			tree_in = &nat->tree_tcp_in;
+			port = &L3HDR(struct tcphdr, ip)->th_dport;
+			csum = &L3HDR(struct tcphdr, ip)->th_sum;
+			break;
+		case IPPROTO_UDP:
+			k->sport = args->f_id.src_port;
+			k->dport = ntohs(args->f_id.dst_port);
+			tree_in = &nat->tree_udp_in;
+			port = &L3HDR(struct udphdr, ip)->uh_dport;
+			csum = &L3HDR(struct udphdr, ip)->uh_sum;
+			udp = 1;
+			break;
+		case IPPROTO_ICMP:
+			k->sport = L3HDR(struct icmp, ip)->icmp_id;;
+			k->dport = L3HDR(struct icmp, ip)->icmp_id;;
+			tree_in = &nat->tree_icmp_in;
+			port = &L3HDR(struct icmp, ip)->icmp_id;
+			csum = &L3HDR(struct icmp, ip)->icmp_cksum;
+			break;
+		default:
+			panic("ipfw3: unsupported proto %u", ip->ip_p);
+		}
+		s = RB_FIND(state_tree, tree_in, k);
+		if (s == NULL) {
+			goto oops;
+		}
+	} else {
+		/* for incoming packets */
+		addr = &ip->ip_src;
+		k->saddr = args->f_id.src_ip;
+		k->daddr = args->f_id.dst_ip;
+		k->proto = args->f_id.proto;
+		switch (ip->ip_p) {
+		case IPPROTO_TCP:
+			k->sport = args->f_id.src_port;
+			k->dport = args->f_id.dst_port;
+			m->m_pkthdr.csum_flags = CSUM_TCP;
+			tree_in = &nat->tree_tcp_in;
+			tree_out = &nat->tree_tcp_out;
+			port = &L3HDR(struct tcphdr, ip)->th_sport;
+			csum = &L3HDR(struct tcphdr, ip)->th_sum;
+			break;
+		case IPPROTO_UDP:
+			k->sport = args->f_id.src_port;
+			k->dport = args->f_id.dst_port;
+			m->m_pkthdr.csum_flags = CSUM_UDP;
+			tree_in = &nat->tree_udp_in;
+			tree_out = &nat->tree_udp_out;
+			port = &L3HDR(struct udphdr, ip)->uh_sport;
+			csum = &L3HDR(struct udphdr, ip)->uh_sum;
+			udp = 1;
+			break;
+		case IPPROTO_ICMP:
+			k->sport = 0;
+			k->dport = 0;
+			tree_in = &nat->tree_icmp_in;
+			tree_out = &nat->tree_icmp_out;
+			port = &L3HDR(struct icmp, ip)->icmp_id;
+			csum = &L3HDR(struct icmp, ip)->icmp_cksum;
+			break;
+		default:
+			panic("ipfw3: unsupported proto %u", ip->ip_p);
+		}
+		s = RB_FIND(state_tree, tree_out, k);
+		if (s == NULL) {
+			switch  (ip->ip_p) {
+			case IPPROTO_TCP:
+				m->m_pkthdr.csum_flags = CSUM_TCP;
+				s1 = kmalloc(LEN_NAT_STATE, M_IP_FW3_NAT,
+						M_INTWAIT | M_NULLOK | M_ZERO);
+				s1->saddr = args->f_id.src_ip;
+				s1->daddr = args->f_id.dst_ip;
+				s1->proto = args->f_id.proto;
+
+				s1->sport = args->f_id.src_port;
+				s1->dport = args->f_id.dst_port;
+
+				nat_state_get_alias(s1, nat, tree_out);
+				/* TODO */
+				dup = RB_INSERT(state_tree, tree_out, s1);
+				s2 = kmalloc(LEN_NAT_STATE, M_IP_FW3_NAT,
+						M_INTWAIT | M_NULLOK | M_ZERO);
+				s2->saddr = args->f_id.dst_ip;
+				s2->daddr = nat->ip.s_addr;
+				s2->proto = args->f_id.proto;
+
+				s2->sport = s1->dport;
+				s2->dport = s1->alias_port;
+				s2->alias_addr = htonl(args->f_id.src_ip);
+				s2->alias_port = htons(args->f_id.src_port);
+				dup = RB_INSERT(state_tree, tree_in, s2);
+				break;
+			case IPPROTO_UDP:
+				m->m_pkthdr.csum_flags = CSUM_UDP;
+				s1 = kmalloc(LEN_NAT_STATE, M_IP_FW3_NAT,
+						M_INTWAIT | M_NULLOK | M_ZERO);
+				s1->saddr = args->f_id.src_ip;
+				s1->daddr = args->f_id.dst_ip;
+				s1->proto = args->f_id.proto;
+
+				s1->sport = args->f_id.src_port;
+				s1->dport = args->f_id.dst_port;
+
+				nat_state_get_alias(s1, nat, tree_out);
+				dup = RB_INSERT(state_tree, tree_out, s1);
+				s2 = kmalloc(LEN_NAT_STATE, M_IP_FW3_NAT,
+						M_INTWAIT | M_NULLOK | M_ZERO);
+				s2->saddr = args->f_id.dst_ip;
+				s2->daddr = nat->ip.s_addr;
+				s2->proto = args->f_id.proto;
+
+				s2->sport = s1->dport;
+				s2->dport = s1->alias_port;
+
+				s2->alias_addr = htonl(args->f_id.src_ip);
+				s2->alias_port = htons(args->f_id.src_port);
+				dup = RB_INSERT(state_tree, tree_in, s2);
+				break;
+			case IPPROTO_ICMP:
+				s1 = kmalloc(LEN_NAT_STATE, M_IP_FW3_NAT,
+						M_INTWAIT | M_NULLOK | M_ZERO);
+				s1->saddr = args->f_id.src_ip;
+				s1->daddr = args->f_id.dst_ip;
+				s1->proto = args->f_id.proto;
+
+				s1->sport = *port;
+				s1->dport = *port;
+
+				s1->alias_addr = nat->ip.s_addr;
+				s1->alias_port = htons(s1->saddr % 1024);
+
+				dup = RB_INSERT(state_tree, tree_out, s1);
+
+				s2 = kmalloc(LEN_NAT_STATE, M_IP_FW3_NAT,
+						M_INTWAIT | M_NULLOK | M_ZERO);
+				s2->saddr = args->f_id.dst_ip;
+				s2->daddr = nat->ip.s_addr;
+				s2->proto = args->f_id.proto;
+
+				s2->sport = s1->alias_port;
+				s2->dport = s1->alias_port;
+
+				s2->alias_addr = htonl(args->f_id.src_ip);
+				s2->alias_port = *port;
+
+				dup = RB_INSERT(state_tree, tree_in, s2);
+				break;
+			default :
+				goto oops;
+			}
+			s = s1;
+		}
+	}
+	*old_addr = *addr;
+	*old_port = *port;
+	if (m->m_pkthdr.csum_flags & (CSUM_UDP | CSUM_TCP | CSUM_TSO)) {
+		if ((m->m_pkthdr.csum_flags & CSUM_TSO) == 0) {
+			dlen = ip->ip_len - (ip->ip_hl << 2);
+		}
+		pseudo = TRUE;
+	}
+	if (!pseudo) {
+		const uint16_t *oaddr, *naddr;
+		oaddr = (const uint16_t *)&old_addr->s_addr;
+		naddr = (const uint16_t *)&s->alias_addr;
+		ip->ip_sum = fix_cksum(ip->ip_sum, oaddr[0], naddr[0], 0);
+		ip->ip_sum = fix_cksum(ip->ip_sum, oaddr[1], naddr[1], 0);
+		if (ip->ip_p != IPPROTO_ICMP) {
+			*csum = fix_cksum(*csum, oaddr[0], naddr[0], udp);
+			*csum = fix_cksum(*csum, oaddr[1], naddr[1], udp);
+		}
+	}
+	addr->s_addr = s->alias_addr;
+	if (!pseudo) {
+		*csum = fix_cksum(*csum, *port, s->alias_port, udp);
+	}
+	*port = s->alias_port;
+
+	if (pseudo) {
+		*csum = in_pseudo(ip->ip_src.s_addr, ip->ip_dst.s_addr, htons(dlen + ip->ip_p));
+	}
 	return IP_FW_NAT;
+oops:
+	return IP_FW_DENY;
 }
 
 int
