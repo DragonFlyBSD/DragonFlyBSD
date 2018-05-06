80dc92f3d8972cb7b8318deab0cd1b5d563e0c35
Sascha Wildner <saw@online.de>
2018-05-06 19:05:15 +0200

    ppp(8): Don't go via <sys/tty.h> for bringing in <termios.h>.
    Include it directly.

--- usr.sbin/ppp/physical.c
+++ usr.sbin/ppp/physical.c
@@ -36,8 +36,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/tty.h>	/* TIOCOUTQ */
 #include <sys/uio.h>
+#include <termios.h>
 #include <time.h>
 #include <unistd.h>
 #include <utmp.h>
