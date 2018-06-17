cf49343ddd4a2da7ffb632e5c4015f707573a790
Eitan Adler <lists@eitanadler.com>
2018-06-17 18:44:07 +0000

    liby: build with WARNS=6; style
    - add header rather than include local details
    - add const
    - remove extraneous void cast
    
    - See discussion on FreeBSD r335270 for motivation for copyright
    details.

--- lib/liby/yyerror.h
+++ lib/liby/yyerror.h
@@ -1,8 +1,7 @@
 /*-
- * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
- * Copyright (c) 1990, 1993
- *	The Regents of the University of California.  All rights reserved.
+ * Copyright (c) 2018 Eitan Adler
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -12,14 +11,11 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -28,19 +24,13 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * @(#)yyerror.c	8.1 (Berkeley) 6/4/93
- *
- * $DragonFly: src/lib/liby/yyerror.c,v 1.3 2008/09/30 16:57:06 swildner Exp $
+ * $FreeBSD$
  */
 
-#include <stdio.h>
+#ifndef YYERROR_H
+#define YYERROR_H
 
-#include "yyerror.h"
+int yyerror(const char *msg);
+int yyparse(void);
 
-int
-yyerror(const char *msg)
-{
-
-	fprintf(stderr, "%s\n", msg);
-	return(0);
-}
+#endif /* YYERROR_H */
