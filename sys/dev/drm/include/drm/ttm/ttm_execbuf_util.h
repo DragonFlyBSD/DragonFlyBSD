3c3b358edcbaed5481ecc31c61d05bedd7ddee6f
François Tigeot <ftigeot@wolfpond.org>
2019-03-11 23:00:40 +0100

    drm/ttm: convert to the reservation api
    Additional-bugfix-by: dillon

--- sys/dev/drm/include/drm/ttm/ttm_execbuf_util.h
+++ sys/dev/drm/include/drm/ttm/ttm_execbuf_util.h
@@ -27,13 +27,12 @@
 /*
  * Authors: Thomas Hellstrom <thellstrom-at-vmware-dot-com>
  */
-/* $FreeBSD: head/sys/dev/drm2/ttm/ttm_execbuf_util.h 247835 2013-03-05 09:49:34Z kib $ */
 
 #ifndef _TTM_EXECBUF_UTIL_H_
 #define _TTM_EXECBUF_UTIL_H_
 
 #include <drm/ttm/ttm_bo_api.h>
-#include <linux/reservation.h>
+#include <linux/list.h>
 
 /**
  * struct ttm_validate_buffer
