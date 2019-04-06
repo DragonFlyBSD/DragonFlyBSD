6ea3cdb41c339457c9f826fb8b68773cd7025aef
zrj <rimvydas.jasinskas@gmail.com>
2019-04-06 18:30:41 +0300

    Kernel: Minor whitespace cleanup in kern_kinfo.c.

--- sys/kern/kern_kinfo.c
+++ sys/kern/kern_kinfo.c
@@ -1,14 +1,14 @@
 /*-
  * Copyright (c) 2007 The DragonFly Project.  All rights reserved.
- * 
+ *
  * This code is derived from software contributed to The DragonFly Project
  * by Simon 'corecode' Schubert <corecode@fs.ei.tum.de>
  * by Thomas E. Spanjaard <tgen@netphreax.net>
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
@@ -18,7 +18,7 @@
  * 3. Neither the name of The DragonFly Project nor the names of its
  *    contributors may be used to endorse or promote products derived
  *    from this software without specific, prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -272,7 +272,7 @@ fill_kinfo_proc_kthread(struct thread *td, struct kinfo_proc *kp)
 	kp->kp_lwp.kl_wchan = (uintptr_t)td->td_wchan;
 	if (td->td_flags & TDF_RUNQ)
 		kp->kp_lwp.kl_stat = LSRUN;
-	else 
+	else
 		kp->kp_lwp.kl_stat = LSSLEEP;
 	if (td->td_wmesg) {
 		strncpy(kp->kp_lwp.kl_wmesg, td->td_wmesg, WMESGLEN);
