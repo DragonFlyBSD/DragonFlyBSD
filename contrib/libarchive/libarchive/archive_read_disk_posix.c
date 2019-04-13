a38db9ddcce4262f46468f4220da9d16e1d53b18
zrj <rimvydas.jasinskas@gmail.com>
2019-04-13 16:11:39 +0300

    libarchive: Revert local changes.
    Fixed in new version.

--- contrib/libarchive/libarchive/archive_read_disk_posix.c
+++ contrib/libarchive/libarchive/archive_read_disk_posix.c
@@ -1504,7 +1504,7 @@ setup_current_filesystem(struct archive_read_disk *a)
 	struct tree *t = a->tree;
 	struct statfs sfs;
 #if defined(HAVE_GETVFSBYNAME) && defined(VFCF_SYNTHETIC)
-	struct vfsconf vfc;
+	struct xvfsconf vfc;
 #endif
 	int r, xr = 0;
 #if !defined(HAVE_STRUCT_STATFS_F_NAMEMAX)
