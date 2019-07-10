332f271b6ade3730fa64f5bde8f95e449128f8f7
Sascha Wildner <saw@online.de>
2019-07-10 21:25:14 +0200

    ACPICA: Remove unneeded __va_*() definitions, include <sys/stdarg.h>.

--- sys/contrib/dev/acpica/source/include/platform/acgcc.h
+++ sys/contrib/dev/acpica/source/include/platform/acgcc.h
@@ -164,7 +164,7 @@ typedef __builtin_va_list       va_list;
 #define va_arg(v, l)            __builtin_va_arg(v, l)
 #define va_copy(d, s)           __builtin_va_copy(d, s)
 #else
-#include <stdarg.h>
+#include <sys/stdarg.h>
 #endif
 #endif
 
