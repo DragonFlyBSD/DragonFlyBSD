2770d3b4871e854523e8d324af48166d2d363561
zrj <rimvydas.jasinskas@gmail.com>
2019-08-31 14:58:37 +0300

    <sys/fnv_hash.h>: Remove unused fnv_64*() inline functions.
     Unused in base (neither OpenBSD or NetBSD has this header).  Once needed
     64 variants can be added from www.isthe.com/chongo/src/fnv/hash_64.c
     For now keep fnv_32_str() inline, but disable it.  It could be used in
     several kernel places instead of fnv_32_buf().  The smbfs_hash() in
     sys/vfs/smbfs/smbfs_node.c could be adjusted to use <sys/fnv_hash.h>.
    
     Reduces kernel.stripped size by 72 bytes.

--- sys/sys/fnv_hash.h
+++ sys/sys/fnv_hash.h
@@ -16,13 +16,9 @@
 #endif
 
 typedef u_int32_t Fnv32_t;
-typedef u_int64_t Fnv64_t;
 
 #define FNV1_32_INIT ((Fnv32_t) 33554467UL)
-#define FNV1_64_INIT ((Fnv64_t) 0xcbf29ce484222325ULL)
-
 #define FNV_32_PRIME ((Fnv32_t) 0x01000193UL)
-#define FNV_64_PRIME ((Fnv64_t) 0x100000001b3ULL)
 
 static __inline Fnv32_t
 fnv_32_buf(const void *buf, size_t len, Fnv32_t hval)
@@ -36,6 +32,8 @@ fnv_32_buf(const void *buf, size_t len, Fnv32_t hval)
 	return hval;
 }
 
+/* currently unused */
+#if 0
 static __inline Fnv32_t
 fnv_32_str(const char *str, Fnv32_t hval)
 {
@@ -48,30 +46,6 @@ fnv_32_str(const char *str, Fnv32_t hval)
 	}
 	return hval;
 }
-
-static __inline Fnv64_t
-fnv_64_buf(const void *buf, size_t len, Fnv64_t hval)
-{
-	const u_int8_t *s = (const u_int8_t *)buf;
-
-	while (len-- != 0) {
-		hval *= FNV_64_PRIME;
-		hval ^= *s++;
-	}
-	return hval;
-}
-
-static __inline Fnv64_t
-fnv_64_str(const char *str, Fnv64_t hval)
-{
-	const u_int8_t *s = (const u_int8_t *)str;
-	u_register_t c;		 /* 64 bit on x86_64 */
-
-	while ((c = *s++) != 0) {
-		hval *= FNV_64_PRIME;
-		hval ^= c;
-	}
-	return hval;
-}
-
 #endif
+
+#endif	/* !_SYS_FNV_HASH_H_ */
