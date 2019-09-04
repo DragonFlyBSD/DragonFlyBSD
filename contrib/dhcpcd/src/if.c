231319383219ed492df932cd96414fe5ca28f8dc
Roy Marples <roy@marples.name>
2019-09-04 20:43:28 +0100

    dhcpcd: Remove left over patches.

--- contrib/dhcpcd/src/if.c
+++ contrib/dhcpcd/src/if.c
@@ -475,7 +475,6 @@ if_discover(struct dhcpcd_ctx *ctx, struct ifaddrs **ifaddrs,
 				/* FALLTHROUGH */
 #endif
 #ifdef IFT_L2VLAN
-				/* FALLTHROUGH */
 			case IFT_L2VLAN: /* FALLTHROUGH */
 #endif
 #ifdef IFT_L3IPVLAN
