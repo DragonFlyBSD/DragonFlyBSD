f445029874928ec5dbddbce0e63ff289cdf58eb3
Sascha Wildner <saw@online.de>
2019-09-10 20:20:44 +0200

    date(1): Switch to utmpx. Drop utmp support.

--- bin/date/date.c
+++ bin/date/date.c
@@ -43,9 +43,7 @@
 #include <unistd.h>
 #include <locale.h>
 #include <libutil.h>
-#ifdef SUPPORT_UTMPX
 #include <utmpx.h>
-#endif
 
 #include "vary.h"
 
@@ -255,22 +253,12 @@ setthetime(const char *fmt, const char *p, int jflag)
 		errx(1, "nonexistent time");
 
 	if (!jflag) {
-#ifdef SUPPORT_UTMP
-		logwtmp("|", "date", "");
-#endif
-#ifdef SUPPORT_UTMPX
 		logwtmpx("|", "date", "", 0, OLD_TIME);
-#endif
 		tv.tv_sec = tval;
 		tv.tv_usec = 0;
 		if (settimeofday(&tv, NULL))
 			err(1, "settimeofday (timeval)");
-#ifdef SUPPORT_UTMP
-		logwtmp("{", "date", "");
-#endif
-#ifdef SUPPORT_UTMPX
 		logwtmpx("{", "date", "", 0, NEW_TIME);
-#endif
 
 		if ((p = getlogin()) == NULL)
 			p = "???";
