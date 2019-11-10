DragonFlyBSD/DragonFlyBSD

commit f8b849d1082177cbfa11f941feeeced54815dc56

calendar(1): Use login caps for 'calendar -a'
* Use 'fork()' and 'setusercontext(LOGIN_SETALL)' for every user.  With
  this change, the complex function 'cal()' that parses user-owned
  calendars is now executed with appropriate user privileges.

  Previously it was run with privileges dropped only temporary for
  execution of 'cal()', and fully dropped only before invoking 'sendmail'.

* In 'calendar -a' mode, the 'getenv("HOME")' in 'cal_fopen()' function
  always returns the root's home directory instead of the current user's
  home (after fork() and setusercontext()), which makes 'chdir(home)'
  fail.

  Fix this issue by moving the 'chdir(home)' to the 'main()' function in
  'calendar.c'.  In the meantime, back to home directory in
  'cal_fopen()' function after opening the calendar file.

Partially based on FreeBSD's commit:
https://github.com/freebsd/freebsd/commit/7ff75f4a5a3d9ada5bd68384fdded357fa4c1105

usr.bin/calendar/calendar.c
@@ -32,9 +32,12 @@
  * $FreeBSD: head/usr.bin/calendar/calendar.c 326025 2017-11-20 19:49:47Z pfg $
  */
 
+#include <sys/types.h>
+
 #include <err.h>
 #include <errno.h>
 #include <locale.h>
+#include <login_cap.h>
 #include <pwd.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -162,28 +165,35 @@ main(int argc, char *argv[])
 	}
 
 	if (doall) {
-		if (setgroups(0, NULL) == -1) {
-			err(EXIT_FAILURE, "setgroups");
-		}
 		while ((pw = getpwent()) != NULL) {
-			if (setegid(pw->pw_gid) == -1) {
-				warn("%s: setegid", pw->pw_name);
+			pid_t pid;
+
+			if (chdir(pw->pw_dir) == -1)
 				continue;
-			}
-			if (initgroups(pw->pw_name, pw->pw_gid) == -1) {
-				warn("%s: initgroups", pw->pw_name);
-				continue;
-			}
-			if (seteuid(pw->pw_uid) == -1) {
-				warn("%s: seteuid", pw->pw_name);
-				continue;
-			}
-			if (chdir(pw->pw_dir) != -1)
+
+			pid = fork();
+			if (pid < 0)
+				err(1, "fork");
+			if (pid == 0) {
+				login_cap_t *lc;
+
+				lc = login_getpwclass(pw);
+				if (setusercontext(lc, pw, pw->pw_uid,
+						   LOGIN_SETALL) != 0)
+					errx(1, "setusercontext");
+
 				cal();
-			if (seteuid(0) == -1)
-				warn("%s: seteuid back to 0", pw->pw_name);
+				exit(0);
+			}
 		}
 	} else {
+		char *home = getenv("HOME");
+
+		if (home == NULL || *home == '\0')
+			errx(1, "Cannot get home directory");
+		if (chdir(home) != 0)
+			errx(1, "Cannot enter home directory: \"%s\"", home);
+
 		cal();
 	}
 

usr.bin/calendar/io.c
@@ -75,7 +75,6 @@ static char *extradata[MAXCOUNT];
 
 static FILE	*cal_fopen(const char *file);
 static bool	 cal_parse(FILE *in, FILE *out);
-static void	 changeuser(void);
 static void	 closecal(FILE *fp);
 static FILE	*opencalin(void);
 static FILE	*opencalout(void);
@@ -104,31 +103,32 @@ trimlr(char **buf)
 static FILE *
 cal_fopen(const char *file)
 {
-	FILE *fp;
-	char *home = getenv("HOME");
+	FILE *fp = NULL;
+	char *cwd = NULL;
+	char cwdpath[MAXPATHLEN];
 	unsigned int i;
 
-	if (home == NULL || *home == '\0') {
-		warnx("Cannot get home directory");
-		return (NULL);
-	}
-
-	if (chdir(home) != 0) {
-		warnx("Cannot enter home directory");
-		return (NULL);
-	}
+	/* Already in home directory before calling cal() in main() */
+	if (getcwd(cwdpath, sizeof(cwdpath)) != NULL)
+		cwd = cwdpath;
+	else
+		warnx("Cannot get current working directory");
 
 	for (i = 0; i < nitems(calendarHomes); i++) {
 		if (chdir(calendarHomes[i]) != 0)
 			continue;
 
 		if ((fp = fopen(file, "r")) != NULL)
-			return (fp);
+			break;
 	}
 
-	warnx("can't open calendar file \"%s\"", file);
+	if (cwd && chdir(cwdpath) != 0)
+		warnx("Cannot back to original directory: \"%s\"", cwdpath);
 
-	return (NULL);
+	if (fp == NULL)
+		warnx("Cannot open calendar file \"%s\"", file);
+
+	return (fp);
 }
 
 static int
@@ -467,8 +467,6 @@ closecal(FILE *fp)
 			close(pdes[0]);
 		}
 		close(pdes[1]);
-		/* become the user properly */
-		changeuser();
 		execl(_PATH_SENDMAIL, "sendmail", "-i", "-t", "-F",
 		    "\"Reminder Service\"", (char *)NULL);
 		warn(_PATH_SENDMAIL);
@@ -494,32 +492,4 @@ done:
 	unlink(path);
 	while (wait(&status) >= 0)
 		;
-}
-
-static void
-changeuser(void)
-{
-	uid_t uid;
-	gid_t gid;
-
-	uid = geteuid();
-	gid = getegid();
-	assert(uid == pw->pw_uid);
-	assert(gid == pw->pw_gid);
-
-	if (seteuid(0) == -1) {
-		err(EXIT_FAILURE, "%s: changing user: cannot reassert uid 0",
-		    pw->pw_name);
-	}
-	if (setgid(gid) == -1) {
-		err(EXIT_FAILURE, "%s: cannot assume gid %d",
-		    pw->pw_name, (int)gid);
-	}
-	if (initgroups(pw->pw_name, gid) == -1) {
-		err(EXIT_FAILURE, "%s: cannot initgroups", pw->pw_name);
-	}
-	if (setuid(uid) == -1) {
-		err(EXIT_FAILURE, "%s: cannot assume uid %d",
-		    pw->pw_name, (int)uid);
-	}
 }

usr.bin/calendar/Makefile
@@ -5,8 +5,8 @@ PROG=	calendar
 SRCS=	calendar.c locale.c events.c dates.c parsedata.c io.c day.c \
 	ostern.c paskha.c pom.c sunpos.c
 
-DPADD=	${LIBM}
-LDADD=	-lm
+DPADD=	${LIBM} ${LIBUTIL}
+LDADD=	-lm -lutil
 
 .if !defined(NO_SHARE)
 INTER=		de_AT.UTF-8 \

commit c22cba45a51e10e12de190c2d12f2628b40a9ace

calendar(1): Search calendar file in current directory first
This fixes the regression introduced in commit
f8b849d1082177cbfa11f941feeeced54815dc56.

usr.bin/calendar/calendar.c
@@ -187,13 +187,6 @@ main(int argc, char *argv[])
 			}
 		}
 	} else {
-		char *home = getenv("HOME");
-
-		if (home == NULL || *home == '\0')
-			errx(1, "Cannot get home directory");
-		if (chdir(home) != 0)
-			errx(1, "Cannot enter home directory: \"%s\"", home);
-
 		cal();
 	}
 

usr.bin/calendar/io.c
@@ -117,10 +117,18 @@ cal_fopen(const char *file)
 {
 	FILE *fp = NULL;
 	char *cwd = NULL;
+	char *home;
 	char cwdpath[MAXPATHLEN];
 	unsigned int i;
 
-	/* Already in home directory before calling cal() in main() */
+	if (!doall) {
+		home = getenv("HOME");
+		if (home == NULL || *home == '\0')
+			errx(1, "Cannot get home directory");
+		if (chdir(home) != 0)
+			errx(1, "Cannot enter home directory: \"%s\"", home);
+	}
+
 	if (getcwd(cwdpath, sizeof(cwdpath)) != NULL)
 		cwd = cwdpath;
 	else
@@ -416,7 +424,7 @@ static FILE *
 opencalin(void)
 {
 	struct stat sbuf;
-	FILE *fpin;
+	FILE *fpin = NULL;
 
 	/* open up calendar file */
 	if ((fpin = fopen(calendarFile, "r")) == NULL) {
@@ -431,6 +439,12 @@ opencalin(void)
 			fpin = cal_fopen(calendarFile);
 		}
 	}
+
+	if (fpin == NULL) {
+		errx(1, "No calendar file: \"%s\" or \"~/%s/%s\"",
+				calendarFile, calendarHomes[0], calendarFile);
+	}
+
 	return (fpin);
 }
 