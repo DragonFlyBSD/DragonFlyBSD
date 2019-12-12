0ce3268cc54af241d22cd75ffaf86549ae51876e
François Tigeot <ftigeot@wolfpond.org>
2019-12-12 12:08:07 +0100

    drm/i915: Revert unintended changes in i915_params.c
    They were introduced in 1487f78699db6b645ec307a207e4611f50a7cc53

--- sys/dev/drm/i915/i915_params.c
+++ sys/dev/drm/i915/i915_params.c
@@ -45,7 +45,7 @@ struct i915_params i915 __read_mostly = {
 	.fastboot = 0,
 	.prefault_disable = 0,
 	.load_detect_test = 0,
-	.reset = true,
+	.reset = 1,
 	.invert_brightness = 0,
 	.disable_display = 0,
 	.enable_cmd_parser = 1,
@@ -64,21 +64,25 @@ struct i915_params i915 __read_mostly = {
 };
 
 module_param_named(modeset, i915.modeset, int, 0400);
+TUNABLE_INT("drm.i915.modeset", &i915.modeset);
 MODULE_PARM_DESC(modeset,
 	"Use kernel modesetting [KMS] (0=disable, "
 	"1=on, -1=force vga console preference [default])");
 
 module_param_named_unsafe(panel_ignore_lid, i915.panel_ignore_lid, int, 0600);
+TUNABLE_INT("drm.i915.panel_ignore_lid", &i915.panel_ignore_lid);
 MODULE_PARM_DESC(panel_ignore_lid,
 	"Override lid status (0=autodetect, 1=autodetect disabled [default], "
 	"-1=force lid closed, -2=force lid open)");
 
 module_param_named_unsafe(semaphores, i915.semaphores, int, 0400);
+TUNABLE_INT("drm.i915.semaphores", &i915.semaphores);
 MODULE_PARM_DESC(semaphores,
 	"Use semaphores for inter-ring sync "
 	"(default: -1 (use per-chip defaults))");
 
 module_param_named_unsafe(enable_rc6, i915.enable_rc6, int, 0400);
+TUNABLE_INT("drm.i915.enable_rc6", &i915.enable_rc6);
 MODULE_PARM_DESC(enable_rc6,
 	"Enable power-saving render C-state 6. "
 	"Different stages can be selected via bitmask values "
@@ -92,26 +96,31 @@ MODULE_PARM_DESC(enable_dc,
 	"(-1=auto [default]; 0=disable; 1=up to DC5; 2=up to DC6)");
 
 module_param_named_unsafe(enable_fbc, i915.enable_fbc, int, 0600);
+TUNABLE_INT("drm.i915.enable_fbc", &i915.enable_fbc);
 MODULE_PARM_DESC(enable_fbc,
 	"Enable frame buffer compression for power savings "
 	"(default: -1 (use per-chip default))");
 
 module_param_named_unsafe(lvds_channel_mode, i915.lvds_channel_mode, int, 0400);
+TUNABLE_INT("drm.i915.lvds_channel_mode", &i915.lvds_channel_mode);
 MODULE_PARM_DESC(lvds_channel_mode,
 	 "Specify LVDS channel mode "
 	 "(0=probe BIOS [default], 1=single-channel, 2=dual-channel)");
 
 module_param_named_unsafe(lvds_use_ssc, i915.panel_use_ssc, int, 0600);
+TUNABLE_INT("drm.i915.panel_use_ssc", &i915.panel_use_ssc);
 MODULE_PARM_DESC(lvds_use_ssc,
 	"Use Spread Spectrum Clock with panels [LVDS/eDP] "
 	"(default: auto from VBT)");
 
 module_param_named_unsafe(vbt_sdvo_panel_type, i915.vbt_sdvo_panel_type, int, 0400);
+TUNABLE_INT("drm.i915.vbt_sdvo_panel_type", &i915.vbt_sdvo_panel_type);
 MODULE_PARM_DESC(vbt_sdvo_panel_type,
 	"Override/Ignore selection of SDVO panel mode in the VBT "
 	"(-2=ignore, -1=auto [default], index in VBT BIOS table)");
 
 module_param_named_unsafe(reset, i915.reset, bool, 0600);
+TUNABLE_INT("drm.i915.reset", &i915.reset);
 MODULE_PARM_DESC(reset, "Attempt GPU resets (default: true)");
 
 module_param_named_unsafe(enable_hangcheck, i915.enable_hangcheck, bool, 0644);
@@ -121,16 +130,19 @@ MODULE_PARM_DESC(enable_hangcheck,
 	"(default: true)");
 
 module_param_named_unsafe(enable_ppgtt, i915.enable_ppgtt, int, 0400);
+TUNABLE_INT("drm.i915.enable_ppgtt", &i915.enable_ppgtt);
 MODULE_PARM_DESC(enable_ppgtt,
 	"Override PPGTT usage. "
 	"(-1=auto [default], 0=disabled, 1=aliasing, 2=full, 3=full with extended address space)");
 
 module_param_named_unsafe(enable_execlists, i915.enable_execlists, int, 0400);
+TUNABLE_INT("drm.i915.enable_execlists", &i915.enable_execlists);
 MODULE_PARM_DESC(enable_execlists,
 	"Override execlists usage. "
 	"(-1=auto [default], 0=disabled, 1=enabled)");
 
 module_param_named_unsafe(enable_psr, i915.enable_psr, int, 0600);
+TUNABLE_INT("drm.i915.enable_psr", &i915.enable_psr);
 MODULE_PARM_DESC(enable_psr, "Enable PSR "
 		 "(0=disabled, 1=enabled - link mode chosen per-platform, 2=force link-standby mode, 3=force link-off mode) "
 		 "Default: -1 (use per-chip default)");
@@ -162,6 +174,7 @@ MODULE_PARM_DESC(load_detect_test,
 	"For developers only.");
 
 module_param_named_unsafe(invert_brightness, i915.invert_brightness, int, 0600);
+TUNABLE_INT("drm.i915.panel_invert_brightness", &i915.invert_brightness);
 MODULE_PARM_DESC(invert_brightness,
 	"Invert backlight brightness "
 	"(-1 force normal, 0 machine defaults, 1 force inversion), please "
@@ -177,6 +190,7 @@ MODULE_PARM_DESC(enable_cmd_parser,
 		 "Enable command parsing (1=enabled [default], 0=disabled)");
 
 module_param_named_unsafe(use_mmio_flip, i915.use_mmio_flip, int, 0600);
+TUNABLE_INT("drm.i915.use_mmio_flip", &i915.use_mmio_flip);
 MODULE_PARM_DESC(use_mmio_flip,
 		 "use MMIO flips (-1=never, 0=driver discretion [default], 1=always)");
 
