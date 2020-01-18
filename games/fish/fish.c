DragonFlyBSD/DragonFlyBSD

commit ef388479f147d546783f2649dc60c1453178cfde

[fish] don't allow users to request cards they have made books for
It is not technically legal to request a card you don't have in your
hand, even if you have the book for it. This really only matters if
you're playing with more than one deck, which fish(6) does not support,
but since this is a critical bug, fix it.

games/fish/fish.c
@@ -169,10 +169,15 @@ usermove(void)
 			continue;
 		}
 		n = p - cards;
-		if (userhand[n]) {
+		if (userhand[n] <= 3) {
 			userasked[n] = 1;
 			return(n);
 		}
+		if (userhand[n] == 4) {
+			(void)printf("You already have all of those.\n");
+			continue;
+		}
+
 		if (nrandom(3) == 1)
 			printf("You don't have any of those!\n");
 		else

commit 864b1bf00d7b22db663382e0b04c5dd823f0630e

[fish] require the user have the card they ask for
This bug was introduced in ef388479 by me.

Reported by:	Matthew Kern <mkern@alconconstruction.com>

games/fish/fish.c
@@ -175,7 +175,7 @@ usermove(void)
 			continue;
 		}
 		n = p - cards;
-		if (userhand[n] <= 3) {
+		if (userhand[n] <= 3 && userhand[n]) {
 			userasked[n] = 1;
 			return(n);
 		}