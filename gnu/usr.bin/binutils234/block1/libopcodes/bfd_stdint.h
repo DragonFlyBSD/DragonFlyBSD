c03f3e4c2902bc190c6e27497fcbd19337441aae
zrj <rimvydas.jasinskas@gmail.com>
2020-02-03 11:25:41 +0200

    binutils234: Add configs and pregenerated headers.

---
+++ gnu/usr.bin/binutils234/block1/libopcodes/bfd_stdint.h
@@ -0,0 +1,9 @@
+/* generated for  gcc 8.3 [DragonFly] Release/2019-02-22 */
+
+#ifndef GCC_GENERATED_STDINT_H
+#define GCC_GENERATED_STDINT_H 1
+
+#include <sys/types.h>
+#include <stdint.h>
+
+#endif /* GCC_GENERATED_STDINT_H */
