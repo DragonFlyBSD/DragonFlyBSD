12b1d9fbe4c4be22308f69c006399cd4eee3a258
zrj <zrj@dragonflybsd.org>
2020-02-15 18:22:13 +0200

    Retire the binutils-2.25.

--- contrib/binutils-2.25/bfd/bfdver.h
+++
@@ -1,4 +0,0 @@
-#define BFD_VERSION_DATE 20150721
-#define BFD_VERSION 225010000
-#define BFD_VERSION_STRING  "(GNU Binutils) " "2.25.1"
-#define REPORT_BUGS_TO "<http://www.sourceware.org/bugzilla/>"
