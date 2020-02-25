a7c16d7a0ef2fbc7976ed1efcc727a0c8e1b01b3
Matthew Dillon <dillon@apollo.backplane.com>
2020-02-24 21:29:36 -0800

    kernel - Simple cache line optimizations
    * Reorder struct vm_page, struct vnode, and struct vm_object a bit
      to improve cache-line locality.
    
    * Use atomic_fcmpset_*() instead of atomic_cmpset_*() in several
      places to reduce the inter-cpu cache coherency load a bit.

--- sys/kern/kern_synch.c
+++ sys/kern/kern_synch.c
@@ -355,8 +355,8 @@ updatepcpu(struct lwp *lp, int cpticks, int ttlticks)
 			  LOOKUP_PRIME) % slpque_tablesize)
 #define TCHASHSHIFT(x)	((x) >> 4)
 
-static uint32_t	slpque_tablesize;
-static cpumask_t *slpque_cpumasks;
+__read_mostly static uint32_t	slpque_tablesize;
+__read_mostly static cpumask_t *slpque_cpumasks;
 
 SYSCTL_UINT(_kern, OID_AUTO, slpque_tablesize, CTLFLAG_RD, &slpque_tablesize,
     0, "");
