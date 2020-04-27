ab6448f252482f7129efe54045c7f9a360799f77
Tomohiro Kusumi <kusumi.tomohiro@gmail.com>
2020-04-28 01:40:53 +0900

    sbin/fsck_msdosfs: Fix a bug with dirty file system handling
    r356313 broke handling of dirty file system because we have restricted
    the correction of "odd" byte sequences to checkfat(), and as a result
    the dirty bit is never cleared.  The old fsck_msdosfs code would write
    FAT twice to fix the dirty bit, which is also not ideal.
    
    Fix this by introducing a new rountine, cleardirty() which will perform
    the set of clean bit only, and use it in checkfilesys() if we thought
    the file system was dirty.
    
    https://svnweb.freebsd.org/base?view=revision&revision=356313
    
    from freebsd/freebsd@ffa396196c4c0fcf8a6bf4d1dd88ffb9ce531ccc

--- sbin/fsck_msdosfs/fat.c
+++ sbin/fsck_msdosfs/fat.c
@@ -553,6 +553,53 @@ valid_cl(struct fat_descriptor *fat, cl_t cl)
 	return (cl >= CLUST_FIRST && cl < boot->NumClusters);
 }
 
+int
+cleardirty(struct fat_descriptor *fat)
+{
+	int fd, ret = FSERROR;
+	struct bootblock *boot;
+	u_char *buffer;
+	size_t len;
+	off_t off;
+
+	boot = boot_of_(fat);
+	fd = fd_of_(fat);
+
+	if (boot->ClustMask != CLUST16_MASK && boot->ClustMask != CLUST32_MASK)
+		return 0;
+
+	off = boot->bpbResSectors;
+	off *= boot->bpbBytesPerSec;
+
+	buffer = malloc(len = boot->bpbBytesPerSec);
+	if (buffer == NULL) {
+		perr("No memory for FAT sectors (%zu)", len);
+		return 1;
+	}
+
+	if ((size_t)pread(fd, buffer, len, off) != len) {
+		perr("Unable to read FAT");
+		goto err;
+	}
+
+	if (boot->ClustMask == CLUST16_MASK) {
+		buffer[3] |= 0x80;
+	} else {
+		buffer[7] |= 0x08;
+	}
+
+	if ((size_t)pwrite(fd, buffer, len, off) != len) {
+		perr("Unable to write FAT");
+		goto err;
+	}
+
+	ret = FSOK;
+
+err:
+	free(buffer);
+	return ret;
+}
+
 /*
  * Read a FAT from disk. Returns 1 if successful, 0 otherwise.
  */
