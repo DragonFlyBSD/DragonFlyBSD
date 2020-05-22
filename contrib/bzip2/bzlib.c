d8d77baac2714febf36f4ac37afccbbbdcdc8857
Sascha Wildner <saw@online.de>
2020-05-22 23:34:50 +0200

    libstand: Apply our bzlib.c patch to master.
    All it does is to add #ifdefs to compile decompression support only.
    
    While here, remove some unneeded headers from SRCS.

--- contrib/bzip2/bzlib.c
+++ contrib/bzip2/bzlib.c
@@ -30,6 +30,7 @@
 
 #include "bzlib_private.h"
 
+#ifndef BZ_NO_COMPRESS
 
 /*---------------------------------------------------*/
 /*--- Compression stuff                           ---*/
@@ -85,6 +86,7 @@ void BZ2_bz__AssertH__fail ( int errcode )
 }
 #endif
 
+#endif /* BZ_NO_COMPRESS */
 
 /*---------------------------------------------------*/
 static
@@ -111,6 +113,7 @@ void default_bzfree ( void* opaque, void* addr )
    if (addr != NULL) free ( addr );
 }
 
+#ifndef BZ_NO_COMPRESS
 
 /*---------------------------------------------------*/
 static
@@ -483,6 +486,7 @@ int BZ_API(BZ2_bzCompressEnd)  ( bz_stream *strm )
    return BZ_OK;
 }
 
+#endif /* BZ_NO_COMPRESS */
 
 /*---------------------------------------------------*/
 /*--- Decompression stuff                         ---*/
@@ -877,6 +881,7 @@ int BZ_API(BZ2_bzDecompressEnd)  ( bz_stream *strm )
    return BZ_OK;
 }
 
+#ifndef BZ_NO_COMPRESS
 
 #ifndef BZ_NO_STDIO
 /*---------------------------------------------------*/
@@ -1566,6 +1571,7 @@ const char * BZ_API(BZ2_bzerror) (BZFILE *b, int *errnum)
 }
 #endif
 
+#endif /* BZ_NO_COMPRESS */
 
 /*-------------------------------------------------------------*/
 /*--- end                                           bzlib.c ---*/
