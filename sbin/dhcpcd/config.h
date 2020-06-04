3da30e6b7a21bf2dc6c41d928ab3bc72a96aef00
Roy Marples <roy@marples.name>
2020-06-04 13:27:21 +0000

    Build with dhcpcd-9.1.1

--- sbin/dhcpcd/config.h
+++ sbin/dhcpcd/config.h
@@ -7,6 +7,9 @@
 #define	DBDIR			"/var/db/dhcpcd"
 #define	RUNDIR			"/var/run/dhcpcd"
 #endif
+#ifndef PRIVSEP_USER
+#define PRIVSEP_USER		 "_dhcp"
+#endif
 #define	HAVE_IFAM_ADDRFLAGS
 #define	HAVE_IFADDRS_ADDRFLAGS
 #define	HAVE_OPEN_MEMSTREAM
@@ -18,7 +21,7 @@
 #define	RBTEST
 #include			"compat/rbtree.h"
 #define	HAVE_REALLOCARRAY
-#define	HAVE_KQUEUE
+#define	HAVE_PPOLL
 #ifdef	USE_PRIVATECRYPTO
 #define	HAVE_MD5_H
 #define	SHA2_H			<openssl/sha.h>
