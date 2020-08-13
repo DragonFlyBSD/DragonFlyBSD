0c0d8fe827b371713b0b1183a3891857821950a3
Sascha Wildner <saw@online.de>
2020-08-13 20:13:47 +0200

    Stop mixing the userland specific <bsd.sys.mk> into the module build.
    I don't know why it was there initially because we inherited it this way.
    
    The only changes on the compile line are eliminating a -std=gcc99
    (our userland -std setting) which was overridden by the kernel's
    own -std=c99 anyway, and a second copy of our kernel warning flags.
    By extension, it also makes the build log shorter by some percent.

--- share/mk/bsd.kmod.mk
+++ share/mk/bsd.kmod.mk
@@ -1,5 +1,4 @@
 # $FreeBSD: src/share/mk/bsd.kmod.mk,v 1.75.2.4 2001/08/01 17:14:26 obrien Exp $
-# $DragonFly: src/share/mk/bsd.kmod.mk,v 1.3 2003/08/20 19:38:45 dillon Exp $
 
 # Search for kernel source tree in standard places.
 .for _dir in ${.CURDIR}/.. ${.CURDIR}/../.. ${.CURDIR}/../../.. ${.CURDIR}/../../../.. ${.CURDIR}/../../../../.. /sys /usr/src/sys
@@ -13,5 +12,3 @@ SYSDIR=	${_dir}
 
 .include "${SYSDIR}/conf/kmod.mk"
 .include "${SYSDIR}/conf/kern.paths.mk"
-
-.include <bsd.sys.mk>
