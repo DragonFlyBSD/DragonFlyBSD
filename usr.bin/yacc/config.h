d4abd2f4139e349f6331f02c2a3167ea05d37703
Sascha Wildner <saw@online.de>
2020-09-19 11:32:24 +0200

    byacc: Update README.DRAGONFLY and config.h.

--- usr.bin/yacc/config.h
+++ usr.bin/yacc/config.h
@@ -22,16 +22,52 @@
 /* Define if you have the <fcntl.h> header file. */
 #define HAVE_FCNTL_H 1
 
+/* Define if you have the `getopt' function. */
+#define HAVE_GETOPT 1
+
+/* Define if you have the <getopt.h> header file. */
+#define HAVE_GETOPT_H 1
+
+/* Define to 1 if getopt variables are declared in header */
+#define HAVE_GETOPT_HEADER 1
+
+/* Define if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
 /* Define if you have the `dbmalloc' library (-ldbmalloc). */
 /* #undef HAVE_LIBDBMALLOC */
 
 /* Define if you have the `dmalloc' library (-ldmalloc). */
 /* #undef HAVE_LIBDMALLOC */
 
+/* Define if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
 /* Define to 1 if mkstemp() is available and working. */
 #define HAVE_MKSTEMP 1
 
-/* Define to 1 if vsnprintf() is available and working. */
+/* Define if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define if you have the `vsnprintf' function. */
 #define HAVE_VSNPRINTF 1
 
 /* Define to maximum table size (default: 32500) */
@@ -40,8 +76,14 @@
 /* Define to 1 if filesystem supports mixed-case filenames. */
 #define MIXEDCASE_FILENAMES 1
 
+/* Define to 1 if we must include getopt.h */
+/* #undef NEED_GETOPT_H */
+
 /* Define to 1 if you want to perform memory-leak testing. */
 /* #undef NO_LEAKS */
+
+/* Define if you have the ANSI C header files. */
+#define STDC_HEADERS 1
 
 /* Define to the system name. */
 #define SYSTEM_NAME "dragonfly5.9"
@@ -60,3 +102,6 @@
 
 /* Define to 1 if you want to perform memory-leak testing. */
 /* #undef YY_NO_LEAKS */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef mode_t */
