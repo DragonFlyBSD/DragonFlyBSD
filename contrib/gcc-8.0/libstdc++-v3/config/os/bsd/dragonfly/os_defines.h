2ecd6c66235a4bd9079e32b0be890f15d4fd0cb0
Sascha Wildner <saw@online.de>
2020-10-03 19:56:19 +0200

    gcc: Allow our C++ headers to survive -Wsystem-headers (2).
    The previous commit (894c19b2d84fd28593ce799678d7d9f7d33936ef) wasn't
    actually working because __LONG_LONG_SUPPORTED wasn't yet known.
    
    Include <sys/cdefs.h> for it.
    
    Reported-by: tuxillo

--- contrib/gcc-8.0/libstdc++-v3/config/os/bsd/dragonfly/os_defines.h
+++ contrib/gcc-8.0/libstdc++-v3/config/os/bsd/dragonfly/os_defines.h
@@ -26,6 +26,8 @@
 #ifndef _GLIBCXX_OS_DEFINES
 #define _GLIBCXX_OS_DEFINES 1
 
+#include <sys/cdefs.h>	/* for __LONG_LONG_SUPPORTED */
+
 // System-specific #define, typedefs, corrections, etc, go here.  This
 // file will come before all others.
 
@@ -37,10 +39,9 @@
 #define _GLIBCXX_USE_C99_DYNAMIC (!(__ISO_C_VISIBLE >= 1999))
 #define _GLIBCXX_USE_C99_LONG_LONG_CHECK 1
 #ifdef __LONG_LONG_SUPPORTED
-#define	__CXX_LONG_LONG_SUPPORTED	1
+#define _GLIBCXX_USE_C99_LONG_LONG_DYNAMIC _GLIBCXX_USE_C99_DYNAMIC
 #else
-#define	__CXX_LONG_LONG_SUPPORTED	0
+#define _GLIBCXX_USE_C99_LONG_LONG_DYNAMIC 1
 #endif
-#define _GLIBCXX_USE_C99_LONG_LONG_DYNAMIC (_GLIBCXX_USE_C99_DYNAMIC || !__CXX_LONG_LONG_SUPPORTED)
 
 #endif
