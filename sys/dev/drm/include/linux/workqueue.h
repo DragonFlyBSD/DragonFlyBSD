c17dd299cec907bb472a1f824300cad3290019b3
François Tigeot <ftigeot@wolfpond.org>
2020-11-11 20:49:32 +0100

    drm/linux: Rewrite the workqueue implementation
    Most workqueues now use one runner thread per logical CPU, except for
    WQ_UNBOUND ones, which only have a single kernel thread.
    
    This commit contains critical fixes from Matthew Dillon for multithread
    interaction issues in queue_work() and the cancel functions.

--- sys/dev/drm/include/linux/workqueue.h
+++ sys/dev/drm/include/linux/workqueue.h
@@ -36,18 +36,30 @@
 #include <linux/atomic.h>
 #include <linux/cpumask.h>
 
-#include <sys/taskqueue.h>
+#define WQ_HIGHPRI	1
+#define WQ_UNBOUND	2
 
-struct workqueue_struct {
-	struct taskqueue	*taskqueue;
-	struct lock		flags_lock;
-	bool			is_draining;
-};
+struct workqueue_worker;
 
 struct work_struct {
-	struct	task 		work_task;
-	struct	taskqueue	*taskqueue;
-	void			(*func)(struct work_struct *);
+	STAILQ_ENTRY(work_struct) ws_entries;
+	void	(*func)(struct work_struct *);
+	struct workqueue_worker *worker;
+	bool	on_queue;
+	bool	running;
+	bool	canceled;
+};
+
+struct workqueue_worker {
+	STAILQ_HEAD(ws_list, work_struct) ws_list_head;
+	struct thread *worker_thread;
+	struct lock worker_lock;
+};
+
+struct workqueue_struct {
+	bool	is_draining;
+	int	num_workers;
+	struct	workqueue_worker (*workers)[];
 };
 
 struct delayed_work {
@@ -62,21 +74,13 @@ to_delayed_work(struct work_struct *work)
 	return container_of(work, struct delayed_work, work);
 }
 
-
-static inline void
-_work_fn(void *context, int pending)
-{
-	struct work_struct *work;
-
-	work = context;
-	work->func(work);
-}
-
-#define	INIT_WORK(work, _func) 	 					\
-do {									\
-	(work)->func = (_func);						\
-	(work)->taskqueue = NULL;					\
-	TASK_INIT(&(work)->work_task, 0, _work_fn, (work));		\
+#define INIT_WORK(work, _func) 		 	\
+do {						\
+	(work)->ws_entries.stqe_next = NULL;	\
+	(work)->func = (_func);			\
+	(work)->on_queue = false;		\
+	(work)->running = false;		\
+	(work)->canceled = false;		\
 } while (0)
 
 #define INIT_WORK_ONSTACK(work, _func)	INIT_WORK(work, _func)
@@ -87,137 +91,7 @@ do {									\
 	callout_init_mp(&(_work)->timer);				\
 } while (0)
 
-#define	INIT_DEFERRABLE_WORK	INIT_DELAYED_WORK
-
-#define	schedule_work(work)						\
-do {									\
-	taskqueue_enqueue_optq(taskqueue_thread[mycpuid], &(work)->taskqueue, &(work)->work_task);	\
-} while (0)
-
-#define	flush_scheduled_work()	flush_taskqueue(taskqueue_thread[mycpuid])
-
-static inline int queue_work(struct workqueue_struct *q, struct work_struct *work)
-{
-	/* Return opposite val to align with Linux logic */
-	return !taskqueue_enqueue_optq((q)->taskqueue, &(work)->taskqueue, &(work)->work_task);
-}
-
-static inline void
-_delayed_work_fn(void *arg)
-{
-	struct delayed_work *work;
-
-	work = arg;
-	taskqueue_enqueue_optq(work->work.taskqueue, &work->work.taskqueue, &work->work.work_task);
-}
-
-static inline int
-queue_delayed_work(struct workqueue_struct *wq, struct delayed_work *work,
-    unsigned long delay)
-{
-	int pending;
-
-	pending = work->work.work_task.ta_pending;
-	work->work.taskqueue = wq->taskqueue;
-	if (delay != 0) {
-		callout_reset(&work->timer, delay, _delayed_work_fn, work);
-	} else {
-		_delayed_work_fn((void *)work);
-	}
-
-	return (!pending);
-}
-
-static inline bool schedule_delayed_work(struct delayed_work *dwork,
-                                         unsigned long delay)
-{
-        struct workqueue_struct wq;
-
-        wq.taskqueue = taskqueue_thread[mycpuid];
-        return queue_delayed_work(&wq, dwork, delay);
-}
-
-struct workqueue_struct * _create_workqueue_common(char *name, int cpus);
-
-#define	create_singlethread_workqueue(name)				\
-	_create_workqueue_common(name, 1)
-
-#define	create_workqueue(name)						\
-	_create_workqueue_common(name, MAXCPU)
-
-#define alloc_ordered_workqueue(name, flags)				\
-	_create_workqueue_common(name, 1)
-
-#define alloc_workqueue(name, flags, max_active)			\
-	_create_workqueue_common(name, max_active)
-
-void destroy_workqueue(struct workqueue_struct *wq);
-
-#define	flush_workqueue(wq)	flush_taskqueue((wq)->taskqueue)
-
-static inline void
-_flush_fn(void *context, int pending)
-{
-}
-
-static inline void
-flush_taskqueue(struct taskqueue *tq)
-{
-	struct task flushtask;
-
-	TASK_INIT(&flushtask, 0, _flush_fn, NULL);
-	taskqueue_enqueue(tq, &flushtask);
-	taskqueue_drain(tq, &flushtask);
-}
-
-static inline int
-cancel_work_sync(struct work_struct *work)
-{
-	if (taskqueue_cancel_simple(&work->work_task))
-		taskqueue_drain_simple(&work->work_task);
-	return 0;
-}
-
-/*
- * This may leave work running on another CPU as it does on Linux.
- */
-static inline int
-cancel_delayed_work(struct delayed_work *work)
-{
-	callout_stop(&work->timer);
-	return (taskqueue_cancel_simple(&work->work.work_task) == 0);
-}
-
-static inline int
-cancel_delayed_work_sync(struct delayed_work *work)
-{
-	callout_cancel(&work->timer);
-	if (taskqueue_cancel_simple(&work->work.work_task))
-		taskqueue_drain_simple(&work->work.work_task);
-	return 0;
-}
-
-static inline bool
-mod_delayed_work(struct workqueue_struct *wq, struct delayed_work *dwork,
-		                      unsigned long delay)
-{
-	cancel_delayed_work(dwork);
-	queue_delayed_work(wq, dwork, delay);
-	return false;
-}
-
-static inline bool
-flush_work(struct work_struct *work)
-{
-	taskqueue_drain_simple(&work->work_task);
-
-	return true;
-}
-
-static inline void
-destroy_work_on_stack(struct work_struct *work)
-{
-}
+#define INIT_DELAYED_WORK_ONSTACK(work, _func)	INIT_DELAYED_WORK(work, _func)
 
 /* System-wide workqueues */
 extern struct workqueue_struct *system_wq;
@@ -226,21 +100,68 @@ extern struct workqueue_struct *system_long_wq;
 extern struct workqueue_struct *system_unbound_wq;
 extern struct workqueue_struct *system_power_efficient_wq;
 
-static inline unsigned int
-work_busy(struct work_struct *work)
-{
-	/* Just pretend nothing is busy, this function is unreliable anyway */
-	return 0;
-}
+#define alloc_ordered_workqueue(name, flags) \
+	_create_workqueue_common(name, (flags) | WQ_UNBOUND)
 
-bool flush_delayed_work(struct delayed_work *dwork);
+#define alloc_workqueue(name, flags, max_active) \
+	_create_workqueue_common(name, flags)
 
-void drain_workqueue(struct workqueue_struct *wq);
+#define create_singlethread_workqueue(name) \
+	_create_workqueue_common(name, WQ_UNBOUND)
+
+struct workqueue_struct *_create_workqueue_common(const char *name, int flags);
+
+int queue_work(struct workqueue_struct *wq, struct work_struct *work);
+int queue_delayed_work(struct workqueue_struct *wq, struct delayed_work *work,
+    unsigned long delay);
 
 static inline bool
-work_pending(struct work_struct * work)
+schedule_work(struct work_struct *work)
 {
-	return work->work_task.ta_pending;
+	return queue_work(system_wq, work);
 }
+
+static inline bool schedule_delayed_work(struct delayed_work *dwork,
+                                         unsigned long delay)
+{
+        return queue_delayed_work(system_wq, dwork, delay);
+}
+
+bool cancel_work_sync(struct work_struct *work);
+bool cancel_delayed_work(struct delayed_work *dwork);
+bool cancel_delayed_work_sync(struct delayed_work *dwork);
+
+/* XXX: Return value not used in drm code */
+static inline bool
+mod_delayed_work(struct workqueue_struct *wq,
+		 struct delayed_work *dwork, unsigned long delay)
+{
+	cancel_delayed_work(dwork);
+	queue_delayed_work(wq, dwork, delay);
+	return false;
+}
+
+void drain_workqueue(struct workqueue_struct *wq);
+void flush_workqueue(struct workqueue_struct *wq);
+
+bool flush_work(struct work_struct *work);
+bool flush_delayed_work(struct delayed_work *dwork);
+
+static inline void
+flush_scheduled_work(void)
+{
+	flush_workqueue(system_wq);
+}
+
+unsigned int work_busy(struct work_struct *work);
+bool work_pending(struct work_struct *work);
+
+bool delayed_work_pending(struct delayed_work *dw);
+
+void destroy_workqueue(struct workqueue_struct *wq);
+
+void destroy_work_on_stack(struct work_struct *work);
+
+void destroy_delayed_work_on_stack(struct delayed_work *work);
 
 #endif	/* _LINUX_WORKQUEUE_H_ */
--- sys/dev/drm/linux_workqueue.c
+++ sys/dev/drm/linux_workqueue.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015-2020 François Tigeot <ftigeot@wolfpond.org>
+ * Copyright (c) 2020 Matthew Dillon <dillon@backplane.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,18 +28,155 @@
 #include <drm/drmP.h>
 #include <linux/workqueue.h>
 
+#include <sys/kthread.h>
+
+/*
+   Running behaviour, from kernel.org docs:
+   - While there are work items on the workqueue the worker executes the functions
+   associated with the work items one after the other.
+   - When there is no work item left on the workqueue the worker becomes idle.
+
+   There are two worker-pools,
+   one for normal work items
+   and the other for high priority ones, for each possible CPU
+   and some extra worker-pools to serve work items queued on unbound workqueues
+   - the number of these backing pools is dynamic.
+ */
+
+/* XXX: Linux functions often enable/disable irqs on the CPU they run on
+ * this should be investigated */
+
 struct workqueue_struct *system_wq;
 struct workqueue_struct *system_highpri_wq;
 struct workqueue_struct *system_long_wq;
 struct workqueue_struct *system_unbound_wq;
 struct workqueue_struct *system_power_efficient_wq;
 
-static int init_workqueues(void *arg)
+/*
+ * Linux now uses these worker pools:
+ * - (per cpu) regular
+ * - (per cpu) regular high priority
+ * - ordered
+ * - ordered high priority
+ * - unbound
+ * - unbound high priority
+ */
+
+static inline void
+process_all_work(struct workqueue_worker *worker)
+{
+	struct work_struct *work;
+	bool didcan;
+
+	while (STAILQ_FIRST(&worker->ws_list_head)) {
+		work = STAILQ_FIRST(&worker->ws_list_head);
+		STAILQ_REMOVE_HEAD(&worker->ws_list_head, ws_entries);
+		work->on_queue = false;
+
+		/* A work shouldn't be executed concurrently on a single cpu */
+		if (work->running)
+			continue;
+
+		/* Do not run canceled works */
+		if (work->canceled) {
+			/* XXX: should we allow canceled works to be reenabled ? */
+			work->canceled = false;
+			continue;
+		}
+
+		work->running = true;
+		lockmgr(&worker->worker_lock, LK_RELEASE);
+		work->func(work);
+		lwkt_yield();
+		lockmgr(&worker->worker_lock, LK_EXCLUSIVE);
+		if (work->on_queue == false)
+			work->worker = NULL;
+		didcan = work->canceled;
+		cpu_sfence();
+		work->running = false;
+		if (didcan == true)
+			wakeup(work);
+	}
+}
+
+static void
+wq_worker_thread(void *arg)
+{
+	struct workqueue_worker *worker = arg;
+
+	lockmgr(&worker->worker_lock, LK_EXCLUSIVE);
+	while (1) {
+		process_all_work(worker);
+		lksleep(worker, &worker->worker_lock, 0, "wqidle", 0);
+	}
+	lockmgr(&worker->worker_lock, LK_RELEASE);
+}
+
+/*
+ * Return false if work was already on a queue
+ * Return true and queue it if this was not the case
+ */
+int
+queue_work(struct workqueue_struct *wq, struct work_struct *work)
+{
+	struct workqueue_worker *worker;
+	int ret = false;
+
+	/* XXX: should we block instead ? */
+	if (wq->is_draining)
+		return false;
+
+	if (wq->num_workers > 1)
+		worker = &(*wq->workers)[mycpuid];
+	else
+		worker = &(*wq->workers)[0];
+
+	lockmgr(&worker->worker_lock, LK_EXCLUSIVE);
+	work->canceled = false;
+	if (work->on_queue == false || work->running == false) {
+		if (work->on_queue == false) {
+			STAILQ_INSERT_TAIL(&worker->ws_list_head, work,
+					   ws_entries);
+			work->on_queue = true;
+			work->worker = worker;
+			wakeup_one(worker);
+		}
+		ret = true;
+	}
+	lockmgr(&worker->worker_lock, LK_RELEASE);
+
+	return ret;
+}
+
+static inline void
+_delayed_work_fn(void *arg)
+{
+	struct delayed_work *dw = arg;
+
+	queue_work(system_wq, &dw->work);
+}
+
+int
+queue_delayed_work(struct workqueue_struct *wq, struct delayed_work *work,
+    unsigned long delay)
+{
+	int pending = work->work.on_queue; // XXX: running too ?
+	if (delay != 0) {
+		callout_reset(&work->timer, delay, _delayed_work_fn, work);
+	} else {
+		_delayed_work_fn((void *)work);
+	}
+
+	return (!pending);
+}
+
+static int
+init_workqueues(void *arg)
 {
 	system_wq = alloc_workqueue("system_wq", 0, 1);
-	system_highpri_wq = alloc_workqueue("system_highpri_wq", 0, 1);
+	system_highpri_wq = alloc_workqueue("system_highpri_wq", WQ_HIGHPRI, 1);
 	system_long_wq = alloc_workqueue("system_long_wq", 0, 1);
-	system_unbound_wq = alloc_workqueue("system_unbound_wq", 0, 1);
+	system_unbound_wq = alloc_workqueue("system_unbound_wq", WQ_UNBOUND, 1);
 	system_power_efficient_wq = alloc_workqueue("system_power_efficient_wq", 0, 1);
 
 	return 0;
@@ -56,24 +194,62 @@ static int destroy_workqueues(void *arg)
 }
 
 struct workqueue_struct *
-_create_workqueue_common(char *name, int cpus)
+_create_workqueue_common(const char *name, int flags)
 {
 	struct workqueue_struct *wq;
+	int priority, error;
 
-	wq = kmalloc(sizeof(*wq), M_DRM, M_WAITOK);
-	wq->taskqueue = taskqueue_create((name), M_WAITOK,
-	    taskqueue_thread_enqueue,  &wq->taskqueue);
-	taskqueue_start_threads(&wq->taskqueue, cpus,
-				TDPRI_KERN_DAEMON, -1, "%s", name);
+	wq = kmalloc(sizeof(*wq), M_DRM, M_WAITOK | M_ZERO);
 
-	return (wq);
+	if (flags & WQ_HIGHPRI)
+		priority = TDPRI_INT_SUPPORT;
+	else
+		priority = TDPRI_KERN_DAEMON;
+
+	if (flags & WQ_UNBOUND) {
+		wq->num_workers = 1;
+	} else {
+		wq->num_workers = ncpus;
+	}
+	wq->workers = kmalloc(sizeof(struct workqueue_worker) * wq->num_workers,
+			M_DRM, M_WAITOK | M_ZERO);
+
+	for (int i = 0;i < wq->num_workers; i++) {
+		struct workqueue_worker *worker = &(*wq->workers)[i];
+
+		lockinit(&worker->worker_lock, "lwq", 0, 0);
+		STAILQ_INIT(&worker->ws_list_head);
+		if (wq->num_workers > 1) {
+			error = lwkt_create(wq_worker_thread, worker,
+				    &worker->worker_thread, NULL, TDF_NOSTART, i, "%s/%d", name, i);
+		} else {
+			error = lwkt_create(wq_worker_thread, worker,
+				    &worker->worker_thread, NULL, TDF_NOSTART, -1, name);
+		}
+		if (error) {
+			kprintf("%s: lwkt_create(%s/%d): error %d",
+			    __func__, name, i, error);
+			/* XXX: destroy kernel threads and free workers[] if applicable */
+			kfree(wq);
+			return NULL;
+		}
+		lwkt_setpri_initial(worker->worker_thread, priority);
+		lwkt_schedule(worker->worker_thread);
+	}
+
+	return wq;
 }
 
 void
 destroy_workqueue(struct workqueue_struct *wq)
 {
-	taskqueue_free(wq->taskqueue);
+	drain_workqueue(wq);
+//	wq->is_draining = true;
+#if 0	/* XXX TODO */
+	kill_all_threads;
+	kfree(wq->wq_threads);
 	kfree(wq);
+#endif
 }
 
 SYSINIT(linux_workqueue_init, SI_SUB_DRIVERS, SI_ORDER_MIDDLE, init_workqueues, NULL);
@@ -86,16 +262,180 @@ flush_delayed_work(struct delayed_work *dwork)
 	return flush_work(&dwork->work);
 }
 
+/* Wait until the wq becomes empty */
 void
 drain_workqueue(struct workqueue_struct *wq)
 {
-	lockmgr(&wq->flags_lock, LK_EXCLUSIVE);
+	struct workqueue_worker *worker;
+
 	wq->is_draining = true;
-	lockmgr(&wq->flags_lock, LK_RELEASE);
 
-	flush_taskqueue(wq->taskqueue);
+	for (int i=0;i < wq->num_workers; i++) {
+		worker = &(*wq->workers)[i];
 
-	lockmgr(&wq->flags_lock, LK_EXCLUSIVE);
-	wq->is_draining = false;
-	lockmgr(&wq->flags_lock, LK_RELEASE);
+		lockmgr(&worker->worker_lock, LK_EXCLUSIVE);
+		while (!STAILQ_EMPTY(&worker->ws_list_head)) {
+		/* XXX: introduces latency */
+			tsleep(&drain_workqueue, 0, "wkdrain", 1);
+		}
+		lockmgr(&worker->worker_lock, LK_RELEASE);
+	}
+
+	/* XXX: No more work will be queued. is that right ? */
+//	wq->is_draining = false;
+}
+
+bool
+work_pending(struct work_struct *work)
+{
+	/* XXX: is on_queue the only constraint ? */
+	return work->on_queue;
+}
+
+unsigned int
+work_busy(struct work_struct *work)
+{
+	return (work->on_queue || work->running);
+}
+
+static inline void
+__flush_work_func(struct work_struct *work)
+{
+	wakeup_one(work);
+}
+
+/* XXX introduces latency ? */
+void
+flush_workqueue(struct workqueue_struct *wq)
+{
+	struct work_struct __flush_work;
+
+	INIT_WORK(&__flush_work, __flush_work_func);
+
+	queue_work(wq, &__flush_work);
+	while (__flush_work.on_queue || __flush_work.running) {
+		tsleep(&__flush_work, 0, "flshwq", 0);
+	}
+}
+
+/*
+ * Wait until a work is done (has been executed)
+ * Return true if this function had to wait, and false otherwise
+ */
+bool
+flush_work(struct work_struct *work)
+{
+	int ret = false;
+
+	/* XXX: probably unreliable */
+	while (work->on_queue || work->running) {
+		ret = true;
+		/* XXX: use something more intelligent than tsleep() */
+		tsleep(&flush_work, 0, "flshwrk", 1);
+	}
+
+	return ret;
+}
+
+static inline bool
+_cancel_work(struct work_struct *work, bool sync_wait)
+{
+	struct workqueue_worker *worker;
+	bool ret;
+
+	ret = false;
+
+	for (;;) {
+		if (work->on_queue) {
+			worker = work->worker;
+			if (worker == NULL)
+				continue;
+			lockmgr(&worker->worker_lock, LK_EXCLUSIVE);
+			if (worker != work->worker || work->on_queue == false) {
+				lockmgr(&worker->worker_lock, LK_RELEASE);
+				continue;
+			}
+			STAILQ_REMOVE(&worker->ws_list_head, work,
+				      work_struct, ws_entries);
+			work->on_queue = false;
+			ret = true;
+			lockmgr(&worker->worker_lock, LK_RELEASE);
+		}
+		if (work->running == false)
+			break;
+
+		worker = work->worker;
+		if (worker == NULL)
+			continue;
+		lockmgr(&worker->worker_lock, LK_EXCLUSIVE);
+		if (worker != work->worker || work->running == false) {
+			lockmgr(&worker->worker_lock, LK_RELEASE);
+			continue;
+		}
+		work->canceled = true;
+		ret = true;
+		if (sync_wait == false) {
+			lockmgr(&worker->worker_lock, LK_RELEASE);
+			break;
+		}
+		/* XXX this races */
+		lksleep(work, &worker->worker_lock, 0, "wqcan", 1);
+		lockmgr(&worker->worker_lock, LK_RELEASE);
+		/* retest */
+	}
+
+	return ret;
+}
+
+/*
+ * If work was queued, remove it from the queue and return true.
+ * If work was not queued, return false.
+ * In any case, wait for work to complete or be removed from the workqueue,
+ * callers may free associated data structures after this call.
+ */
+bool
+cancel_work_sync(struct work_struct *work)
+{
+	return _cancel_work(work, true);
+}
+
+/* Return false if work wasn't pending
+ * Return true if work was pending and canceled */
+bool
+cancel_delayed_work(struct delayed_work *dwork)
+{
+	struct work_struct *work = &dwork->work;
+
+	work->canceled = true;
+	callout_cancel(&dwork->timer);
+
+	return _cancel_work(work, false);
+}
+
+bool
+cancel_delayed_work_sync(struct delayed_work *dwork)
+{
+	struct work_struct *work = &dwork->work;
+
+	work->canceled = true;
+	callout_cancel(&dwork->timer);
+
+	return _cancel_work(work, true);
+}
+
+bool
+delayed_work_pending(struct delayed_work *dw)
+{
+	/* XXX: possibly wrong if the timer hasn't yet fired */
+	return work_pending(&dw->work);
+}
+
+void
+destroy_work_on_stack(struct work_struct *work)
+{
+}
+
+void
+destroy_delayed_work_on_stack(struct delayed_work *work)
+{
 }
