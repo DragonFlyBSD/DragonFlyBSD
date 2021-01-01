ec21d9fbfac028108ef0d30283d74b804e16da33
Aaron LI <aly@aaronly.me>
2021-01-01 22:08:34 +0800

    Import stdbuf(1) together with libstdbuf(3) from FreeBSD
    The stdbuf(1) is a utility to change the initial buffering of stdin,
    stdout and stderr streams for a given command.  It achieves this
    functionality by preloading the libstdbuf(3) library and configuring the
    needed environment variables.  Under the hood, libstdbuf(3) library
    calls setvbuf(3) to do the actual work.
    
    Changes from FreeBSD:
    * Removed 32bit i386 code from stdbuf(1)
    * Fix typos and update HISTORY section in libstdbuf.3 man page
    * Update the example to actually work on DragonFly

--- share/mk/bsd.libnames.mk
+++ share/mk/bsd.libnames.mk
@@ -76,6 +76,7 @@ LIBSMB?=	${DESTDIR}${LIBDIR}/libsmb.a
 LIBSSH?=	${DESTDIR}${LIBDIR}/priv/libprivate_ssh.a
 LIBSSL?=	${DESTDIR}${LIBDIR}/priv/libprivate_ssl.a
 LIBSTAND?=	${DESTDIR}${LIBDIR}/libstand.a
+LIBSTDBUF?=	${DESTDIR}${LIBDIR}/libstdbuf.a
 LIBTCPLAY?=	${DESTDIR}${LIBDIR}/libtcplay.a
 LIBUSBHID?=	${DESTDIR}${LIBDIR}/libusbhid.a
 LIBUSB?=	${DESTDIR}${LIBDIR}/libusb.a
