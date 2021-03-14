9b1c08e5ca6e018d53e7ff69bd0c5b09c7d3ed0b
zrj <rimvydas.jasinskas@gmail.com>
2021-03-14 09:31:41 +0000

    <sys/endian.h>: Prefer double underscore versions.

--- sys/sys/endian.h
+++ sys/sys/endian.h
@@ -59,16 +59,16 @@
  * endian to host byte order functions as detailed in byteorder(9).
  */
 #if _BYTE_ORDER == _LITTLE_ENDIAN
-#define	htobe16(x)	bswap16((x))
-#define	htobe32(x)	bswap32((x))
-#define	htobe64(x)	bswap64((x))
+#define	htobe16(x)	__bswap16((x))
+#define	htobe32(x)	__bswap32((x))
+#define	htobe64(x)	__bswap64((x))
 #define	htole16(x)	((__uint16_t)(x))
 #define	htole32(x)	((__uint32_t)(x))
 #define	htole64(x)	((__uint64_t)(x))
 
-#define	be16toh(x)	bswap16((x))
-#define	be32toh(x)	bswap32((x))
-#define	be64toh(x)	bswap64((x))
+#define	be16toh(x)	__bswap16((x))
+#define	be32toh(x)	__bswap32((x))
+#define	be64toh(x)	__bswap64((x))
 #define	le16toh(x)	((__uint16_t)(x))
 #define	le32toh(x)	((__uint32_t)(x))
 #define	le64toh(x)	((__uint64_t)(x))
@@ -76,16 +76,16 @@
 #define	htobe16(x)	((__uint16_t)(x))
 #define	htobe32(x)	((__uint32_t)(x))
 #define	htobe64(x)	((__uint64_t)(x))
-#define	htole16(x)	bswap16((x))
-#define	htole32(x)	bswap32((x))
-#define	htole64(x)	bswap64((x))
+#define	htole16(x)	__bswap16((x))
+#define	htole32(x)	__bswap32((x))
+#define	htole64(x)	__bswap64((x))
 
 #define	be16toh(x)	((__uint16_t)(x))
 #define	be32toh(x)	((__uint32_t)(x))
 #define	be64toh(x)	((__uint64_t)(x))
-#define	le16toh(x)	bswap16((x))
-#define	le32toh(x)	bswap32((x))
-#define	le64toh(x)	bswap64((x))
+#define	le16toh(x)	__bswap16((x))
+#define	le32toh(x)	__bswap32((x))
+#define	le64toh(x)	__bswap64((x))
 #endif /* _BYTE_ORDER == _LITTLE_ENDIAN */
 
 /* Alignment-agnostic encode/decode bytestream to/from little/big endian. */
