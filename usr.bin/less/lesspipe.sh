707cb6f4f0858a4cacd53dab8d73d5d553d0c52b
Sascha Wildner <saw@online.de>
2021-04-04 21:02:32 +0200

    less: Add {z,bz,xz,lz,zstd}less wrappers for viewing compressed files.
    Taken-from: FreeBSD

--- usr.bin/less/lesspipe.sh
+++ usr.bin/less/lesspipe.sh
@@ -2,9 +2,11 @@
 # ex:ts=8
 
 # $FreeBSD: src/usr.bin/less/lesspipe.sh,v 1.4 2007/05/24 18:28:08 le Exp $
-# $DragonFly: src/usr.bin/less/lesspipe.sh,v 1.3 2007/06/03 03:59:53 pavalos Exp $
 
 case "$1" in
+	*.zip)
+		exec unzip -c "$1" 2>/dev/null
+		;;
 	*.Z)
 		exec uncompress -c "$1"	2>/dev/null
 		;;
@@ -13,5 +15,14 @@ case "$1" in
 		;;
 	*.bz2)
 		exec bzip2 -d -c "$1"	2>/dev/null
+		;;
+	*.xz)
+		exec xz -d -c "$1"	2>/dev/null
+		;;
+	*.lzma)
+		exec lzma -d -c "$1"	2>/dev/null
+		;;
+	*.zst)
+		exec zstd -d -q -c "$1"	2>/dev/null
 		;;
 esac
