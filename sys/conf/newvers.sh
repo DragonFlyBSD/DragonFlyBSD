7be5ee01fc39e0f2c4273d2f1c4f5bc3d8ccfeda
Justin C. Sherrill <justin@shiningsilence.com>
2022-01-02 22:13:40 -0500

    6.3 version changes.

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -39,7 +39,7 @@ fi
 
 # Set the branch
 #
-BRANCH="DEVELOPMENT_6_1"
+BRANCH="DEVELOPMENT_6_3"
 
 TYPE="DragonFly"
 
--- sys/sys/param.h
+++ sys/sys/param.h
@@ -236,9 +236,11 @@
  * 600105 - add fdatasync()
  * 600106 - msdosfs support in makefs(8)
  * 600107 - remove sys/gnu/vfs/ext2fs
- */
+ * 600200 - 6.2 release
+ * 600300 - 6.3 development
+*/
 #undef __DragonFly_version
-#define __DragonFly_version 600107	/* propagated to newvers */
+#define __DragonFly_version 600300	/* propagated to newvers */
 
 #include <sys/_null.h>
 
