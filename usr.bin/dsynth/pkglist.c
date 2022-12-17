5d0718a8775196ba673595dea1fc0ccbc02b800b
Matthew Dillon <dillon@apollo.backplane.com>
2022-12-17 11:28:54 -0800

    dsynth - Ignore ports installed on the host during pkgscan
    * Set LOCALBASE=<some_empty_dir> in the make commands issued by the
      package scan so the host's installed packages are not probed.
    
    Reported-by: daftaupe

--- usr.bin/dsynth/pkglist.c
+++ usr.bin/dsynth/pkglist.c
@@ -967,6 +967,7 @@ childGetPackageInfo(bulk_t *bulk)
 	size_t len;
 	char *portpath;
 	char *flavarg;
+	char *localbase;
 	const char *cav[MAXCAC];
 	pid_t pid;
 	int cac;
@@ -992,6 +993,19 @@ childGetPackageInfo(bulk_t *bulk)
 	cav[cac++] = portpath;
 	if (flavarg)
 		cav[cac++] = flavarg;
+
+	/*
+	 * Prevent postgresql, mysql, and other package Makefile tests
+	 * from accessing the host system's /usr/local by setting LOCALBASE
+	 * to an empty directory.
+	 */
+	asprintf(&localbase, "LOCALBASE=%s/empty", BuildBase);
+	cav[cac++] = localbase;
+
+	/*
+	 * Variables we need to retrieve (order is specific to the switch
+	 * below)
+	 */
 	cav[cac++] = "-VPKGVERSION";
 	cav[cac++] = "-VPKGFILE:T";
 	cav[cac++] = "-VALLFILES";
@@ -1013,6 +1027,7 @@ childGetPackageInfo(bulk_t *bulk)
 	fp = dexec_open(portpath + strlen(DPortsPath) + 1, cav, cac,
 			&pid, NULL, 1, 1);
 	freestrp(&flavarg);
+	freestrp(&localbase);
 
 	pkg = allocpkg();
 	if (flavor)
@@ -1223,6 +1238,7 @@ static void
 childOptimizeEnv(bulk_t *bulk)
 {
 	char *portpath;
+	char *localbase;
 	char *ptr;
 	FILE *fp;
 	int line;
@@ -1233,14 +1249,23 @@ childOptimizeEnv(bulk_t *bulk)
 
 	asprintf(&portpath, "%s/%s/%s", DPortsPath, bulk->s1, bulk->s2);
 
+	/*
+	 * Prevent postgresql, mysql, and other package Makefile tests
+	 * from accessing the host system's /usr/local by setting LOCALBASE
+	 * to an empty directory.
+	 */
+	asprintf(&localbase, "LOCALBASE=%s/empty", BuildBase);
+
 	cac = 0;
 	cav[cac++] = MAKE_BINARY;
 	cav[cac++] = "-C";
 	cav[cac++] = portpath;
+	cav[cac++] = localbase;
 	cav[cac++] = "-V_PERL5_FROM_BIN";
 
 	fp = dexec_open(portpath + strlen(DPortsPath) + 1, cav, cac,
 			&pid, NULL, 1, 1);
+	free(localbase);
 	free(portpath);
 
 	line = 1;
