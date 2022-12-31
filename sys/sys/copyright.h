0307318e55ce98612d64dfb54afa766d9806b1b9
Sascha Wildner <saw@online.de>
2022-12-31 19:23:00 +0100

    Bump copyrights. Happy new year!!

--- sys/sys/copyright.h
+++ sys/sys/copyright.h
@@ -35,7 +35,7 @@
 
 /* DragonFly */
 #define COPYRIGHT_DragonFly \
-	"Copyright (c) 2003-2022 The DragonFly Project.\n"
+	"Copyright (c) 2003-2023 The DragonFly Project.\n"
 
 /* Berkeley */
 #define COPYRIGHT_UCB \
