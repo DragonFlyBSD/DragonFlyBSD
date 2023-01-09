3f53c53686fc9e236fb9949c8d2a665ce27832be
Tomohiro Kusumi <tkusumi@netbsd.org>
2023-01-09 00:12:47 -0800

    sys/vfs/hammer2: Avoid void* pointer arithmetic
    Warned on NetBSD.
    error: pointer of type 'void *' used in arithmetic [-Werror=pointer-arith]
    
    This commit only fixes HAMMER2 code.
    There are obviously bunch of others in DragonFly tree.

--- sys/vfs/hammer2/hammer2_ondisk.c
+++ sys/vfs/hammer2/hammer2_ondisk.c
@@ -516,7 +516,7 @@ hammer2_read_volume_header(struct vnode *devvp, const char *path,
 
 		/* verify volume header CRC's */
 		crc0 = vd->icrc_sects[HAMMER2_VOL_ICRC_SECT0];
-		crc1 = hammer2_icrc32(bp->b_data + HAMMER2_VOLUME_ICRC0_OFF,
+		crc1 = hammer2_icrc32((char *)bp->b_data + HAMMER2_VOLUME_ICRC0_OFF,
 				      HAMMER2_VOLUME_ICRC0_SIZE);
 		if (crc0 != crc1) {
 			hprintf("%s #%d: volume header crc mismatch sect0 %08x/%08x\n",
@@ -526,7 +526,7 @@ hammer2_read_volume_header(struct vnode *devvp, const char *path,
 			continue;
 		}
 		crc0 = vd->icrc_sects[HAMMER2_VOL_ICRC_SECT1];
-		crc1 = hammer2_icrc32(bp->b_data + HAMMER2_VOLUME_ICRC1_OFF,
+		crc1 = hammer2_icrc32((char *)bp->b_data + HAMMER2_VOLUME_ICRC1_OFF,
 				      HAMMER2_VOLUME_ICRC1_SIZE);
 		if (crc0 != crc1) {
 			hprintf("%s #%d: volume header crc mismatch sect1 %08x/%08x\n",
@@ -536,7 +536,7 @@ hammer2_read_volume_header(struct vnode *devvp, const char *path,
 			continue;
 		}
 		crc0 = vd->icrc_volheader;
-		crc1 = hammer2_icrc32(bp->b_data + HAMMER2_VOLUME_ICRCVH_OFF,
+		crc1 = hammer2_icrc32((char *)bp->b_data + HAMMER2_VOLUME_ICRCVH_OFF,
 				      HAMMER2_VOLUME_ICRCVH_SIZE);
 		if (crc0 != crc1) {
 			hprintf("%s #%d: volume header crc mismatch vh %08x/%08x\n",
