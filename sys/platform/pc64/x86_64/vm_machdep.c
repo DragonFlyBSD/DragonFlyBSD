dba7cc785cea9d1507bd9fed277dd6f702f0382f
Sascha Wildner <saw@online.de>
2023-03-30 17:10:22 +0200

    kernel/vm: Add missing opt_reset.h include to get at BROKEN_KEYBOARD_RESET.

--- sys/platform/pc64/x86_64/vm_machdep.c
+++ sys/platform/pc64/x86_64/vm_machdep.c
@@ -42,6 +42,8 @@
  * $FreeBSD: src/sys/i386/i386/vm_machdep.c,v 1.132.2.9 2003/01/25 19:02:23 dillon Exp $
  */
 
+#include "opt_reset.h"
+
 #include <sys/param.h>
 #include <sys/systm.h>
 #include <sys/malloc.h>
