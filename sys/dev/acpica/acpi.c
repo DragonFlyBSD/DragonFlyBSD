DragonFlyBSD/DragonFlyBSD

commit ad52dd3206eb084703b50aa38ac18b72d5da8a4c

kernel/acpi: Sync acpi_resource with FreeBSD.
* Use AcpiWalkResources() for parsing _CRS result.

* Add support for 64-bit address range resources.

* Add workaround for BIOSes that specify edge-sensitive but active low
  settings for ACPI-enumerated serial ports (such as the one found on
  an Intel D2500CCE mobo).

* Minor fixes/cleanup.

sys/dev/acpica/acpivar.h
@@ -369,14 +369,14 @@ ACPI_STATUS	acpi_eval_osc(device_t dev, ACPI_HANDLE handle,
 struct acpi_parse_resource_set {
     void	(*set_init)(device_t dev, void *arg, void **context);
     void	(*set_done)(device_t dev, void *context);
-    void	(*set_ioport)(device_t dev, void *context, uint32_t base,
-		    uint32_t length);
-    void	(*set_iorange)(device_t dev, void *context, uint32_t low,
-		    uint32_t high, uint32_t length, uint32_t align);
-    void	(*set_memory)(device_t dev, void *context, uint32_t base,
-		    uint32_t length);
-    void	(*set_memoryrange)(device_t dev, void *context, uint32_t low,
-		    uint32_t high, uint32_t length, uint32_t align);
+    void	(*set_ioport)(device_t dev, void *context, uint64_t base,
+		    uint64_t length);
+    void	(*set_iorange)(device_t dev, void *context, uint64_t low,
+		    uint64_t high, uint64_t length, uint64_t align);
+    void	(*set_memory)(device_t dev, void *context, uint64_t base,
+		    uint64_t length);
+    void	(*set_memoryrange)(device_t dev, void *context, uint64_t low,
+		    uint64_t high, uint64_t length, uint64_t align);
     void	(*set_irq)(device_t dev, void *context, uint8_t *irq,
 		    int count, int trig, int pol);
     void	(*set_ext_irq)(device_t dev, void *context, uint32_t *irq,

commit 8506772f4f44fcae9c78e61800e54be0399905f8

acpi: Narrow workaround for broken interrupt settings
Commit ad52dd3206 ("Sync acpi_resource with FreeBSD") introduced a
workaround for some broken BIOSes that specified active-lo instead of
active-hi polarity for ISA IRQs for UARTs.  The workaround assumed that
edge-sensitive ISA IRQs on x86 should always be active-hi.  However,
some recent AMD systems actually use active-lo edge-sensitive ISA IRQs
(and not just for UARTs, but also for the keyboard and PS/2 mouse
devices) and the override causes interrupts to be dropped resulting in
boot time hangs, non-working keyboards, etc.

Add a hw.acpi.override_isa_irq_polarity tunable (readable as a sysctl
post-boot) to control this quirk.  It can be set to 1 to force enable
the override and 0 to disable it.  The log of FreeBSD-PR (see below)
mentions an Intel motherboard as the sample case, so default the
tunable to 1 on systems with an Intel CPU and 0 otherwise.

Special thanks to Matthias Lanter <freebsd@lanter-it.ch> for tracking
down boot time issues on recent AMD systems to mismatched interrupt
polarity.

Taken-from: FreeBSD [1]

FreeBSD-PR: https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=270707

[1]: https://github.com/freebsd/freebsd-src/commit/0a34d050ae8ea14feddd3d2a62fd2f612613b2c5

share/man/man4/acpi.4
@@ -25,7 +25,7 @@
 .\"
 .\" $FreeBSD: src/share/man/man4/acpi.4,v 1.61.8.1 2009/04/15 03:14:26 kensmith Exp $
 .\"
-.Dd May 29, 2020
+.Dd July 25, 2024
 .Dt ACPI 4
 .Os
 .Sh NAME
@@ -270,6 +270,12 @@ is a valid list of two interfaces
 .Qq Li FreeBSD
 and
 .Qq Li Linux .
+.It Va hw.acpi.override_isa_irq_polarity (x86)
+Forces active-lo polarity for edge-triggered ISA interrupts.
+Some older systems incorrectly specify active-lo polarity for ISA
+interrupts and this override fixes those systems.
+This override is enabled by default on systems with Intel CPUs,
+but can be enabled or disabled by setting the tunable explicitly.
 .It Va hw.acpi.reset_video
 Enables calling the VESA reset BIOS vector on the resume path.
 This can fix some graphics cards that have problems such as LCD white-out

sys/dev/acpica/acpi.c
@@ -65,6 +65,7 @@
 #include <bus/pci/pci_cfgreg.h>
 #include <bus/pci/pcivar.h>
 #include <bus/pci/pci_private.h>
+#include <machine/cputypes.h>
 
 #include <vm/vm_param.h>
 
@@ -310,6 +311,10 @@ TUNABLE_INT("debug.acpi.allow_method_calls", &acpi_allow_mcall);
 static int acpi_susp_bounce;
 SYSCTL_INT(_debug_acpi, OID_AUTO, suspend_bounce, CTLFLAG_RW,
     &acpi_susp_bounce, 0, "Don't actually suspend, just test devices.");
+
+#if defined(__x86_64__)
+int acpi_override_isa_irq_polarity;
+#endif
 
 /*
  * ACPI can only be loaded as a module by the loader; activating it after
@@ -631,6 +636,22 @@ acpi_attach(device_t dev)
     SYSCTL_ADD_INT(&sc->acpi_sysctl_ctx, SYSCTL_CHILDREN(sc->acpi_sysctl_tree),
 	OID_AUTO, "handle_reboot", CTLFLAG_RW,
 	&sc->acpi_handle_reboot, 0, "Use ACPI Reset Register to reboot");
+
+#if defined(__x86_64__)
+    /*
+     * Enable workaround for incorrect ISA IRQ polarity by default on
+     * systems with Intel CPUs.
+     */
+    if (cpu_vendor_id == CPU_VENDOR_INTEL)
+	acpi_override_isa_irq_polarity = 1;
+
+    TUNABLE_INT_FETCH("hw.acpi.override_isa_irq_polarity",
+	&acpi_override_isa_irq_polarity);
+    SYSCTL_ADD_INT(&sc->acpi_sysctl_ctx, SYSCTL_CHILDREN(sc->acpi_sysctl_tree),
+	OID_AUTO, "override_isa_irq_polarity", CTLFLAG_RD,
+	&acpi_override_isa_irq_polarity, 0,
+	"Force active-hi polarity for edge-triggered ISA IRQs");
+#endif
 
     /*
      * Default to 1 second before sleeping to give some machines time to

sys/dev/acpica/acpi_resource.c
@@ -137,14 +137,11 @@ acpi_config_intr(device_t dev, ACPI_RESOURCE *res)
     }
 
 #if defined(__x86_64__)
-    /*
-     * XXX: Certain BIOSes have buggy AML that specify an IRQ that is
-     * edge-sensitive and active-lo.  However, edge-sensitive IRQs
-     * should be active-hi.  Force IRQs with an ISA IRQ value to be
-     * active-hi instead.
-     */
-    if (irq < 16 && trig == ACPI_EDGE_SENSITIVE && pol == ACPI_ACTIVE_LOW)
+    if (irq < 16 && trig == ACPI_EDGE_SENSITIVE && pol == ACPI_ACTIVE_LOW &&
+	acpi_override_isa_irq_polarity) {
+	device_printf(dev, "forcing active-hi polarity for IRQ %u\n", irq);
 	pol = ACPI_ACTIVE_HIGH;
+    }
 #endif
 
     if (trig == ACPI_EDGE_SENSITIVE)

sys/dev/acpica/acpivar.h
@@ -239,6 +239,18 @@ extern int	acpi_quirks;
 #define ACPI_Q_MADT_IRQ0	(1 << 2)
 #define ACPI_Q_BATT_RATE_ABS	(1 << 3)
 
+#if defined(__x86_64__)
+/*
+ * Certain Intel BIOSes have buggy AML that specify an IRQ that is
+ * edge-sensitive and active-lo.  Normally, edge-sensitive IRQs should
+ * be active-hi.  If this value is non-zero, edge-sensitive ISA IRQs
+ * are forced to be active-hi instead.  At least some AMD systems use
+ * active-lo edge-sensitive ISA IRQs, so this setting is only enabled
+ * by default on systems with Intel CPUs.
+ */
+extern int	acpi_override_isa_irq_polarity;
+#endif
+
 /*
  * Note that the low ivar values are reserved to provide
  * interface compatibility with ISA drivers which can also