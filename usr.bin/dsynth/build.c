53c96f5130f5e530674e5d7ac2f32f3cd16e8352
Aaron LI <aly@aaronly.me>
2025-03-14 10:41:30 +0800

    dsynth(1): Use PROC_REAP_KILL to reap all

--- usr.bin/dsynth/build.c
+++ usr.bin/dsynth/build.c
@@ -2865,8 +2865,22 @@ skip:
 static void
 phaseReapAll(void)
 {
-	struct reaper_status rs;
 	int status;
+
+#if __DragonFly_version >= 600510
+	struct reaper_kill rk;
+
+	memset(&rk, 0, sizeof(rk));
+	rk.signal = SIGKILL;
+
+	while (procctl(P_PID, getpid(), PROC_REAP_KILL, &rk) == 0) {
+		if (rk.killed == 0)
+			break;
+		while (wait3(&status, 0, NULL) > 0)
+			;
+	}
+#else
+	struct reaper_status rs;
 
 	while (procctl(P_PID, getpid(), PROC_REAP_STATUS, &rs) == 0) {
 		if ((rs.flags & PROC_REAP_ACQUIRE) == 0)
@@ -2878,6 +2892,8 @@ phaseReapAll(void)
 				;
 		}
 	}
+#endif
+
 	while (wait3(&status, 0, NULL) > 0)
 		;
 }
