b3438121d85e7393286d815a2a433bd18fa09abe
Michael Neumann <mneumann@ntecs.de>
2025-08-09 13:54:27 +0200

    v4l - add missing types to V4L header

--- sys/contrib/v4l/videodev2.h
+++ sys/contrib/v4l/videodev2.h
@@ -62,7 +62,13 @@
 
 #ifdef __DragonFly__
 #define __user
+#include <sys/types.h>
+typedef uint8_t		__u8;
+typedef uint16_t	__u16;
+typedef uint32_t	__u32;
 typedef uint64_t	__u64;
+typedef int16_t		__s16;
+typedef int32_t		__s32;
 typedef int64_t		__s64;
 typedef uint32_t	__le32;
 #else
