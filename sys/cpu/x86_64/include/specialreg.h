cf7392bd79db78fd1f6b92a31a6cc8455e632144
Aaron LI <aly@aaronly.me>
2025-08-23 09:47:26 +0800

    cpu/x86_64: Move PAT mode defines to its own <pat.h> header
    The main purpose is to make NVMM be more self-contained and not require
    <machine/specialreg.h>, and thus no collision with various MSRs and
    other macros.
    
    This change also help reduce the pollution from <machine/specialreg.h>
    header in general.
    
    Referred-to: NetBSD

--- sys/cpu/x86_64/include/specialreg.h
+++ sys/cpu/x86_64/include/specialreg.h
@@ -684,18 +684,6 @@
 #define	MSR_HWP_STATUS		0x777
 
 /*
- * PAT modes.
- */
-#define	PAT_UNCACHEABLE		0x00
-#define	PAT_WRITE_COMBINING	0x01
-#define	PAT_WRITE_THROUGH	0x04
-#define	PAT_WRITE_PROTECTED	0x05
-#define	PAT_WRITE_BACK		0x06
-#define	PAT_UNCACHED		0x07
-#define	PAT_VALUE(i, m)		((long)(m) << (8 * (i)))
-#define	PAT_MASK(i)		PAT_VALUE(i, 0xff)
-
-/*
  * Constants related to MTRRs
  */
 #define	MTRR_UNCACHEABLE	0x00
