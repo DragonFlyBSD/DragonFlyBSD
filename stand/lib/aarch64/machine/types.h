a2a0a9e263d60a3a379caa4f623c95ea393c500a
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-22 00:45:49 +0100

    stand: add aarch64 machine types

---
+++ stand/lib/aarch64/machine/types.h
@@ -0,0 +1,18 @@
+#ifndef _MACHINE_TYPES_H_
+#define _MACHINE_TYPES_H_
+
+#include <machine/stdint.h>
+
+typedef __int64_t __off_t;
+typedef __int32_t __pid_t;
+typedef __int64_t __register_t;
+typedef __uint64_t __rlim_t;
+typedef __int64_t __suseconds_t;
+typedef __int64_t __clock_t;
+typedef __int32_t __clockid_t;
+typedef __uintlp_t __size_t;
+typedef __intlp_t __ssize_t;
+typedef __int64_t __time_t;
+typedef __int32_t __timer_t;
+
+#endif
