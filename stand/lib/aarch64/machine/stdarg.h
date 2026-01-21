c9814f1c2bbff382d3cc5a59aa160c4dc1f8bbfd
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-22 00:44:55 +0100

    stand: add aarch64 machine headers

---
+++ stand/lib/aarch64/machine/stdarg.h
@@ -0,0 +1,6 @@
+#ifndef _MACHINE_STDARG_H_
+#define _MACHINE_STDARG_H_
+
+typedef __builtin_va_list __va_list;
+
+#endif
---
+++ stand/lib/aarch64/machine/stdint.h
@@ -0,0 +1,15 @@
+#ifndef _MACHINE_STDINT_H_
+#define _MACHINE_STDINT_H_
+
+typedef signed char __int8_t;
+typedef unsigned char __uint8_t;
+typedef short __int16_t;
+typedef unsigned short __uint16_t;
+typedef int __int32_t;
+typedef unsigned int __uint32_t;
+typedef long long __int64_t;
+typedef unsigned long long __uint64_t;
+typedef long __intlp_t;
+typedef unsigned long __uintlp_t;
+
+#endif
