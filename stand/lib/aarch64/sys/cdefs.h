c21feefa2b96c4ed26f75531ed1416a2678e1a18
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-22 00:59:27 +0100

    stand: add __unused macro

--- stand/lib/aarch64/sys/cdefs.h
+++ stand/lib/aarch64/sys/cdefs.h
@@ -5,6 +5,7 @@
 
 #define __dead2 __attribute__((__noreturn__))
 #define __printflike(fmtarg, firstvararg) __attribute__((__format__(__printf__, fmtarg, firstvararg)))
+#define __unused __attribute__((__unused__))
 
 #ifdef __cplusplus
 #define __BEGIN_DECLS extern "C" {
