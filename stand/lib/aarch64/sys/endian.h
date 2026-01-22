c1cdcae0750a817eb834f174616843b55664074c
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-22 01:09:51 +0100

    stand: add aarch64 endian header

---
+++ stand/lib/aarch64/sys/endian.h
@@ -0,0 +1,12 @@
+#ifndef _SYS_ENDIAN_H_
+#define _SYS_ENDIAN_H_
+
+#define _LITTLE_ENDIAN 1234
+#define _BIG_ENDIAN 4321
+
+#define _BYTE_ORDER _LITTLE_ENDIAN
+
+#define _QUAD_HIGHWORD 1
+#define _QUAD_LOWWORD 0
+
+#endif
