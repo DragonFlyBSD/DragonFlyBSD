55eff3d0ab95ccbed3293acee0d75dfdd6fc26fc
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-22 01:50:48 +0100

    stand: add uint8/16/32/64_t to sys/types.h for aarch64

--- stand/lib/aarch64/sys/types.h
+++ stand/lib/aarch64/sys/types.h
@@ -10,12 +10,16 @@
 
 typedef __int8_t int8_t;
 typedef __uint8_t u_int8_t;
+typedef __uint8_t uint8_t;
 typedef __int16_t int16_t;
 typedef __uint16_t u_int16_t;
+typedef __uint16_t uint16_t;
 typedef __int32_t int32_t;
 typedef __uint32_t u_int32_t;
+typedef __uint32_t uint32_t;
 typedef __int64_t int64_t;
 typedef __uint64_t u_int64_t;
+typedef __uint64_t uint64_t;
 
 typedef unsigned char u_char;
 typedef unsigned short u_short;
