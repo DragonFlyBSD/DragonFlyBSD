49ac2fafa585c1edfd5fc86d02c808c00c526aa2
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-22 01:50:16 +0100

    stand: fix elf shim include guards for aarch64

--- stand/lib/aarch64/sys/elf64.h
+++ stand/lib/aarch64/sys/elf64.h
@@ -2,8 +2,8 @@
  * Shim for sys/elf64.h - include types before the real header.
  */
 
-#ifndef _SYS_ELF64_H_
-#define _SYS_ELF64_H_
+#ifndef _AARCH64_SHIM_SYS_ELF64_H_
+#define _AARCH64_SHIM_SYS_ELF64_H_
 
 #include <sys/types.h>
 #include <stdint.h>
@@ -14,4 +14,4 @@
 /* Now include the real elf64.h from the kernel tree */
 #include "../../../../sys/sys/elf64.h"
 
-#endif /* !_SYS_ELF64_H_ */
+#endif /* !_AARCH64_SHIM_SYS_ELF64_H_ */
--- stand/lib/aarch64/sys/elf_common.h
+++ stand/lib/aarch64/sys/elf_common.h
@@ -2,8 +2,8 @@
  * Shim for sys/elf_common.h - include types before the real header.
  */
 
-#ifndef _SYS_ELF_COMMON_H_
-#define _SYS_ELF_COMMON_H_
+#ifndef _AARCH64_SHIM_SYS_ELF_COMMON_H_
+#define _AARCH64_SHIM_SYS_ELF_COMMON_H_
 
 #include <sys/types.h>
 #include <stdint.h>
@@ -11,4 +11,4 @@
 /* Now include the real elf_common.h from the kernel tree */
 #include "../../../../sys/sys/elf_common.h"
 
-#endif /* !_SYS_ELF_COMMON_H_ */
+#endif /* !_AARCH64_SHIM_SYS_ELF_COMMON_H_ */
