9d6a7fd327373fe8f7de19d502aa5c4d9857667b
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-22 01:00:21 +0100

    stand: add aarch64 errno

---
+++ stand/lib/aarch64/sys/errno.h
@@ -0,0 +1,26 @@
+#ifndef _SYS_ERRNO_H_
+#define _SYS_ERRNO_H_
+
+#define EPERM 1
+#define ENOENT 2
+#define EIO 5
+#define ENXIO 6
+#define ENOEXEC 8
+#define EBADF 9
+#define ENOMEM 12
+#define ENODEV 19
+#define ENOTDIR 20
+#define EISDIR 21
+#define EINVAL 22
+#define EMFILE 24
+#define EFBIG 27
+#define EROFS 30
+#define EOPNOTSUPP 95
+#define ETIMEDOUT 60
+#define ESTALE 70
+#define EBADRPC 72
+#define EFTYPE 79
+
+#define ELAST 100
+
+#endif
