dbdcf86960d3ee8cf2d28fc072b997e3f8077342
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-22 01:52:54 +0100

    stand: add __va_start/__va_end to machine/stdarg.h for aarch64

--- stand/lib/aarch64/machine/stdarg.h
+++ stand/lib/aarch64/machine/stdarg.h
@@ -3,4 +3,8 @@
 
 typedef __builtin_va_list __va_list;
 
+#define __va_start(ap, last) __builtin_va_start(ap, last)
+#define __va_end(ap) __builtin_va_end(ap)
+#define __va_arg(ap, type) __builtin_va_arg(ap, type)
+
 #endif
