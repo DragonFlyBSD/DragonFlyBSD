00c8d9340aae6ad099155e4dd251cabee0394c37
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-22 02:01:52 +0100

    stand: add IP address class macros to netinet/in.h shim for aarch64

--- stand/lib/aarch64/netinet/in.h
+++ stand/lib/aarch64/netinet/in.h
@@ -29,4 +29,24 @@ struct sockaddr_in {
 #define	IPPROTO_TCP		6
 #define	IPPROTO_UDP		17
 
+/* IP address class macros */
+#define	IN_CLASSA(i)		(((uint32_t)(i) & 0x80000000) == 0)
+#define	IN_CLASSA_NET		0xff000000
+#define	IN_CLASSA_NSHIFT	24
+#define	IN_CLASSA_HOST		0x00ffffff
+#define	IN_CLASSA_MAX		128
+
+#define	IN_CLASSB(i)		(((uint32_t)(i) & 0xc0000000) == 0x80000000)
+#define	IN_CLASSB_NET		0xffff0000
+#define	IN_CLASSB_NSHIFT	16
+#define	IN_CLASSB_HOST		0x0000ffff
+#define	IN_CLASSB_MAX		65536
+
+#define	IN_CLASSC(i)		(((uint32_t)(i) & 0xe0000000) == 0xc0000000)
+#define	IN_CLASSC_NET		0xffffff00
+#define	IN_CLASSC_NSHIFT	8
+#define	IN_CLASSC_HOST		0x000000ff
+
+#define	IN_CLASSD(i)		(((uint32_t)(i) & 0xf0000000) == 0xe0000000)
+
 #endif
