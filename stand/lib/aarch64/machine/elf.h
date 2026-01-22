a5e21639dc16dea387c154c6a9025fd80e555bee
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-22 02:08:30 +0100

    stand: add ELF generic macros and aarch64 machine definitions to elf.h shim
    Include sys/elf_generic.h which provides the __elfN() macro needed for
    bootstrap.h to declare elf64_loadfile and elf64_obj_loadfile functions.
    
    Also add machine-specific ELF definitions for aarch64:
    - ELF_ARCH, ELF_TARG_CLASS, ELF_TARG_DATA, ELF_TARG_MACH
    - Basic relocation types (R_AARCH64_NONE, etc.)
    
    This fixes the 'elf64_loadfile undeclared' error when building the
    aarch64 EFI loader.

--- stand/lib/aarch64/machine/elf.h
+++ stand/lib/aarch64/machine/elf.h
@@ -1,7 +1,34 @@
 #ifndef _MACHINE_ELF_H_
 #define _MACHINE_ELF_H_
 
+/*
+ * ELF definitions for the AArch64 architecture.
+ */
+
+#ifndef __ELF_WORD_SIZE
+#define	__ELF_WORD_SIZE	64	/* Used by <sys/elf_generic.h> */
+#endif
+
 #include <sys/elf32.h>
 #include <sys/elf64.h>
+#include <sys/elf_generic.h>
 
-#endif
+#define	ELF_ARCH	EM_AARCH64
+
+#define	ELF_MACHINE_OK(x) ((x) == EM_AARCH64)
+
+/* Define "machine" characteristics */
+#define	ELF_TARG_CLASS	ELFCLASS64
+#define	ELF_TARG_DATA	ELFDATA2LSB
+#define	ELF_TARG_MACH	EM_AARCH64
+#define	ELF_TARG_VER	1
+
+/*
+ * AArch64 relocation types
+ */
+#define	R_AARCH64_NONE		0
+#define	R_AARCH64_ABS64		257
+#define	R_AARCH64_ABS32		258
+#define	R_AARCH64_RELATIVE	1027
+
+#endif /* !_MACHINE_ELF_H_ */
