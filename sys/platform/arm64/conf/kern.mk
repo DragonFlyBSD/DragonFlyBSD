63c8a24ddb5ed3edf993b8b33821e2e208a5af66
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-25 13:21:05 +0100

    arm64: use aarch64-none-elf toolchain

--- sys/platform/arm64/conf/kern.mk
+++ sys/platform/arm64/conf/kern.mk
@@ -1,2 +1,6 @@
 # Placeholder for arm64 platform-specific kernel flags
 INLINE_LIMIT?=	8000
+CC=		aarch64-none-elf-gcc
+LD=		aarch64-none-elf-ld
+OBJCOPY=	aarch64-none-elf-objcopy
+AS=		aarch64-none-elf-as
