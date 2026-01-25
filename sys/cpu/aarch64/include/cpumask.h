06a1dc93929861161775141ad654bf921f76594a
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-25 14:06:36 +0100

    arm64: add cpumask type stub

--- sys/cpu/aarch64/include/cpumask.h
+++ sys/cpu/aarch64/include/cpumask.h
@@ -1,20 +1,26 @@
 /*
- * Placeholder for arm64 cpumask definition.
+ * Placeholder cpumask definitions for arm64.
  */
 
 #ifndef _CPU_CPUMASK_H_
-#define _CPU_CPUMASK_H_
+#define	_CPU_CPUMASK_H_
 
-typedef unsigned long __cpumask_t;
+#include <sys/types.h>
 
-#define __CPU_ZERO(set)         do { *(set) = 0; } while (0)
-#define __CPU_SET(cpu, set)     do { *(set) |= (1UL << (cpu)); } while (0)
-#define __CPU_CLR(cpu, set)     do { *(set) &= ~(1UL << (cpu)); } while (0)
-#define __CPU_ISSET(cpu, set)   ((*(set) & (1UL << (cpu))) != 0)
-#define __CPU_COUNT(set)        __builtin_popcountl(*(set))
-#define __CPU_AND(dst, set1, set2) do { *(dst) = (*(set1) & *(set2)); } while (0)
-#define __CPU_OR(dst, set1, set2)  do { *(dst) = (*(set1) | *(set2)); } while (0)
-#define __CPU_XOR(dst, set1, set2) do { *(dst) = (*(set1) ^ *(set2)); } while (0)
-#define __CPU_EQUAL(set1, set2)    (*(set1) == *(set2))
+typedef struct {
+	__uint64_t	ary[4];
+} __cpumask_t;
+
+typedef __cpumask_t cpumask_t;
+
+#define CPUMASK_ELEMENTS	4
+
+#define CPUMASK_INITIALIZER_ALLONES	{ .ary = { (__uint64_t)-1, \
+					  (__uint64_t)-1, \
+					  (__uint64_t)-1, \
+					  (__uint64_t)-1 } }
+#define CPUMASK_INITIALIZER_ONLYONE	{ .ary = { 1, 0, 0, 0 } }
+
+#define CPUMASK_SIMPLE(cpu)	((__uint64_t)1 << (cpu))
 
 #endif /* !_CPU_CPUMASK_H_ */
