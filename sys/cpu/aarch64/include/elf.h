e705554f5d38ed85724c3ed18b5fadc3706be817
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-25 14:02:09 +0100

    arm64: flesh out elf and lwbuf stubs

--- sys/cpu/aarch64/include/elf.h
+++ sys/cpu/aarch64/include/elf.h
@@ -19,6 +19,55 @@
 #include <sys/elf64.h>
 #include <sys/elf_generic.h>
 
+#ifndef __ElfType
+#define __ElfType(t) Elf64_##t
+#endif
+
+typedef struct {
+	int	a_type;
+	union {
+		int	a_val;
+	} a_un;
+} Elf32_Auxinfo;
+
+typedef struct {
+	long	a_type;
+	union {
+		long	a_val;
+		void	*a_ptr;
+		void	(*a_fcn)(void);
+	} a_un;
+} Elf64_Auxinfo;
+
+__ElfType(Auxinfo);
+
+#define	AT_NULL		0
+#define	AT_IGNORE	1
+#define	AT_EXECFD	2
+#define	AT_PHDR		3
+#define	AT_PHENT	4
+#define	AT_PHNUM	5
+#define	AT_PAGESZ	6
+#define	AT_BASE		7
+#define	AT_FLAGS	8
+#define	AT_ENTRY	9
+#define	AT_BRK		10
+#define	AT_DEBUG	11
+#define	AT_NOTELF	10
+#define	AT_UID		11
+#define	AT_EUID		12
+#define	AT_GID		13
+#define	AT_EGID		14
+#define	AT_EXECPATH	15
+#define	AT_CANARY	16
+#define	AT_CANARYLEN	17
+#define	AT_OSRELDATE	18
+#define	AT_NCPUS	19
+#define	AT_PAGESIZES	20
+#define	AT_PAGESIZESLEN	21
+#define	AT_STACKPROT	23
+#define	AT_COUNT	24
+
 #define ELF_ARCH	EM_AARCH64
 
 #if __ELF_WORD_SIZE == 32
@@ -36,5 +85,10 @@
  * aarch64 load base for PIE binaries (placeholder).
  */
 #define ET_DYN_LOAD_ADDR	0x01021000
+
+#ifdef _KERNEL
+#define ELF_RTLD_ADDR(vmspace) \
+	(round_page((vm_offset_t)(vmspace)->vm_daddr + maxdsiz))
+#endif
 
 #endif /* !_CPU_ELF_H_ */
--- sys/cpu/aarch64/include/lwbuf.h
+++ sys/cpu/aarch64/include/lwbuf.h
@@ -5,8 +5,11 @@
 #ifndef _CPU_LWBUF_H_
 #define _CPU_LWBUF_H_
 
-#ifndef _CPU_TYPES_H_
-#include <machine/types.h>
+#ifndef _SYS_TYPES_H_
+#include <sys/types.h>
+#endif
+#ifndef _SYS_PARAM_H_
+#include <sys/param.h>
 #endif
 
 struct vm_page;
@@ -17,5 +20,38 @@ struct lwbuf {
 	vm_page_t	m;
 	vm_offset_t	kva;
 };
+
+static __inline vm_page_t
+lwbuf_page(struct lwbuf *lwb)
+{
+	return (lwb->m);
+}
+
+static __inline vm_offset_t
+lwbuf_kva(struct lwbuf *lwb)
+{
+	return (lwb->kva);
+}
+
+static __inline struct lwbuf *
+lwbuf_alloc(vm_page_t m, struct lwbuf *lwb_cache)
+{
+	struct lwbuf *lwb = lwb_cache;
+
+	lwb->m = m;
+	lwb->kva = 0;
+
+	return (lwb);
+}
+
+static __inline void
+lwbuf_free(struct lwbuf *lwb)
+{
+	lwb->m = NULL;
+}
+
+#define lwbuf_set_global(lwb)
+
+#define LWBUF_IS_OPTIMAL
 
 #endif /* !_CPU_LWBUF_H_ */
