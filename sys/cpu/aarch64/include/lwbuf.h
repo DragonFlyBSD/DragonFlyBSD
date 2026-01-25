4f5bf73fee9caaf2fbbb60f50ad5f6483e5e9506
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-25 13:49:14 +0100

    arm64: add lwbuf struct stub

--- sys/cpu/aarch64/include/lwbuf.h
+++ sys/cpu/aarch64/include/lwbuf.h
@@ -5,4 +5,17 @@
 #ifndef _CPU_LWBUF_H_
 #define _CPU_LWBUF_H_
 
+#ifndef _CPU_TYPES_H_
+#include <machine/types.h>
+#endif
+
+struct vm_page;
+typedef struct vm_page *vm_page_t;
+typedef unsigned long vm_offset_t;
+
+struct lwbuf {
+	vm_page_t	m;
+	vm_offset_t	kva;
+};
+
 #endif /* !_CPU_LWBUF_H_ */
