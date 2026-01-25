8827e0de8b7e1a617855a56100a0c97c833f0d7c
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-25 13:25:02 +0100

    arm64: use absolute aarch64-none-elf paths

--- sys/platform/arm64/conf/kern.mk
+++ sys/platform/arm64/conf/kern.mk
@@ -1,6 +1,6 @@
 # Placeholder for arm64 platform-specific kernel flags
 INLINE_LIMIT?=	8000
-CC=		aarch64-none-elf-gcc
-LD=		aarch64-none-elf-ld
-OBJCOPY=	aarch64-none-elf-objcopy
-AS=		aarch64-none-elf-as
+CC=		/usr/local/bin/aarch64-none-elf-gcc
+LD=		/usr/local/bin/aarch64-none-elf-ld
+OBJCOPY=	/usr/local/bin/aarch64-none-elf-objcopy
+AS=		/usr/local/bin/aarch64-none-elf-as
