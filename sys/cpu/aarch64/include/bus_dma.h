b1612ed6738a329fc28fc7f450e8379500943083
Antonio Huete Jimenez <tuxillo@quantumachine.net>
2026-01-25 21:30:04 +0100

    arm64: add bus space limits

--- sys/cpu/aarch64/include/bus_dma.h
+++ sys/cpu/aarch64/include/bus_dma.h
@@ -13,4 +13,13 @@ typedef u_int64_t bus_size_t;
 typedef u_int64_t bus_space_tag_t;
 typedef u_int64_t bus_space_handle_t;
 
+#define BUS_SPACE_MAXSIZE_24BIT 0xFFFFFFUL
+#define BUS_SPACE_MAXSIZE_32BIT	0xFFFFFFFFUL
+#define BUS_SPACE_MAXSIZE	(64 * 1024)
+#define BUS_SPACE_MAXADDR_24BIT	0xFFFFFFUL
+#define BUS_SPACE_MAXADDR_32BIT	0xFFFFFFFFUL
+#define BUS_SPACE_MAXADDR	0xFFFFFFFFFFFFFFFFUL
+
+#define BUS_SPACE_UNRESTRICTED	(~0)
+
 #endif /* !_CPU_BUS_DMA_H_ */
