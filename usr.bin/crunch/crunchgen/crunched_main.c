DragonFlyBSD/DragonFlyBSD

commit 73dce941a522281d4f4fcbf07d038bc8b4be4031

crunchgen(1): Fix compilation warnings (prepare for future update)
Fix the following compilation warnings when we enable WARNS for
initrd/{rescue,rescue.libcrypto} build in the future:

```
rescue.c: In function 'cmpstringp':
rescue.c:88:20: error: cast discards 'const' qualifier from pointer target type [-Werror=cast-qual]
  const char *s1 = *(const char **)p1;
                    ^
rescue.c:89:20: error: cast discards 'const' qualifier from pointer target type [-Werror=cast-qual]
  const char *s2 = *(const char **)p2;
                    ^
rescue.c: In function 'crunched_usage':
rescue.c:96:1: error: old-style function definition [-Werror=old-style-definition]
 crunched_usage()
 ^~~~~~~~~~~~~~
cc1: all warnings being treated as errors
```

Discussed-with: swildner

usr.bin/crunch/crunchgen/crunched_main.c
@@ -83,14 +83,13 @@ crunched_main(int argc, char **argv, char **envp)
 static int
 cmpstringp(const void *p1, const void *p2)
 {
-	const char *s1 = *(const char **)p1;
-	const char *s2 = *(const char **)p2;
-	return strcmp(s1, s2);
+	return strcmp((const char *)p1, (const char *)p2);
 }
 
 
+__dead2
 static void
-crunched_usage()
+crunched_usage(void)
 {
 	int nprog = 0, columns = 0;
 	int i, len;

commit 2e3a87ce41bfdf759c1936e31cd8cb7be0a9c1cc

crunchgen(1): Fix program sorting bug in cmpstringp()
I mistakenly introduced the bug in the previous commit
73dce941a522281d4f4fcbf07d038bc8b4be4031 where I tried to fix the
compilation warnings.

usr.bin/crunch/crunchgen/crunched_main.c
@@ -83,7 +83,9 @@ crunched_main(int argc, char **argv, char **envp)
 static int
 cmpstringp(const void *p1, const void *p2)
 {
-	return strcmp((const char *)p1, (const char *)p2);
+	const char *s1 = *(char * const *)p1;
+	const char *s2 = *(char * const *)p2;
+	return strcmp(s1, s2);
 }
 
 