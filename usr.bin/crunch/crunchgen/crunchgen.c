8410ea0817ce17438e0a10fc4ba6c9697281048a
Aaron LI <aly@aaronly.me>
2026-09-02 21:19:30 +0800

    crunchgen(1): Remove the unneeded '-dc' linker flag
    crunchide(1) does not hide symbols by making them local for many years.
    We've also been using '-fno-common' compiler flag for a long time. So
    the '-dc' linker flag is obsolete. Just remove it.
    
    Obtained-from: FreeBSD (https://reviews.freebsd.org/D34215)

--- usr.bin/crunch/crunchgen/crunchgen.c
+++ usr.bin/crunch/crunchgen/crunchgen.c
@@ -1188,8 +1188,7 @@ prog_makefile_rules(FILE *outmk, prog_t *p)
 		fprintf(outmk, " ${%s_LIBS}", p->ident);
 	fprintf(outmk, "\n");
 
-	fprintf(outmk, "\t${CC} -nostdlib -Wl,-dc -r "
-	    "-o ${.TARGET} ${.ALLSRC}\n");
+	fprintf(outmk, "\t${CC} -nostdlib -r -o ${.TARGET} ${.ALLSRC}\n");
 	fprintf(outmk, "\tcrunchide -k _crunched_%s_stub ", p->ident);
 	for (lst = p->keeplist; lst != NULL; lst = lst->next)
 		fprintf(outmk, "-k %s ", lst->str);
